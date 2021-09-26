8eb9fd306f904f4ac0b59d22e8a3c4c819ccbb43
Robin Andeer <robin@hedvig.com>
2021-09-26 22:16:07 +0200

    Setup Next.js

---
+++ src/pages/index.tsx
@@ -0,0 +1,15 @@
+import type { NextPage } from 'next'
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <main>
+        <h1>
+          This is a demo
+        </h1>
+      </main>
+    </div>
+  )
+}
+
+export default Home
