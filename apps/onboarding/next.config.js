1b2df54f1120c29c795d477adbee61b2e8ba44e6
Robin Andeer <robin@hedvig.com>
2021-12-19 15:07:54 +0100

    Redirect old forever URL

--- apps/onboarding/next.config.js
+++ apps/onboarding/next.config.js
@@ -4,4 +4,13 @@ const { i18n } = require('./next-i18next.config')
 module.exports = {
   reactStrictMode: true,
   i18n,
+  async redirects() {
+    return [
+      {
+        source: '/forever/:code',
+        destination: '/forever?code=:code',
+        permanent: true,
+      },
+    ]
+  },
 }
