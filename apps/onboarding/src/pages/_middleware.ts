0b1bfc9b9f0014ff63567f8ddec73c76c8ee5659
Robin Andeer <robin@hedvig.com>
2021-12-19 16:28:59 +0100

    Handle geo redirects
    Update locale redirects.

--- apps/onboarding/src/pages/_middleware.ts
+++ apps/onboarding/src/pages/_middleware.ts
@@ -1,7 +1,31 @@
 import { NextRequest, NextResponse } from 'next/server';
 
 const PUBLIC_FILE = /\.(.*)$/
-const DEFAULT_LOCALE = 'se-en'
+
+const DEFAULT_MARKET = 'se'
+const DEFAULT_LANGUAGE = 'en'
+const DEFAULT_LOCALE = `${DEFAULT_MARKET}-${DEFAULT_LANGUAGE}`
+// https://en.wikipedia.org/wiki/ISO_3166-1_alpha-2
+const SUPPORTED_MARKETS = ['se', 'dk', 'no']
+
+const guessLocale = (request: NextRequest) => {
+  const requestCountry = request.geo?.country?.toLowerCase()
+
+  if (requestCountry && SUPPORTED_MARKETS.includes(requestCountry)) {
+    const requestLanguage = request.headers.get('accept-language')?.split(',')?.[0]
+
+    switch (requestCountry) {
+      case 'se':
+        return requestLanguage?.includes('sv') ? 'se' : 'se-en'
+      case 'dk':
+        return requestLanguage?.includes('da') ? 'dk' : 'dk-en'
+      case 'no':
+        return requestLanguage?.includes('nb') || requestLanguage?.includes('nn') ? 'no' : 'no-en'
+    }
+  }
+
+  return null
+}
 
 export function middleware(request: NextRequest) {
   const shouldHandleLocale =
@@ -9,7 +33,10 @@ export function middleware(request: NextRequest) {
     !request.nextUrl.pathname.includes('/api/') &&
     request.nextUrl.locale === 'default';
 
-  return shouldHandleLocale
-    ? NextResponse.redirect(`/${DEFAULT_LOCALE}${request.nextUrl.pathname}`)
-    : undefined;
+  if (shouldHandleLocale) {
+    const locale = guessLocale(request) || DEFAULT_LOCALE
+    return NextResponse.redirect(`/${locale}${request.nextUrl.href}`)
+  }
+
+  return undefined
 }
