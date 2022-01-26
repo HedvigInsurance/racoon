854fec79546a011309df6fccc3632548c6f317bd
Robin Andeer <robin@hedvig.com>
2022-01-26 16:50:28 +0100

    Update embark tests

--- apps/onboarding/cypress/integration/se-needer-apartment-flow.spec.ts
+++ apps/onboarding/cypress/integration/se-needer-apartment-flow.spec.ts
@@ -5,44 +5,55 @@ it('should get a price quote for a Swedish apartment', () => {
 
   cy.visit(`/se-en/new-member/home-accident-needer`)
 
-  // eslint-disable-next-line testing-library/await-async-utils
-  cy.wait(1000)
-  cy.get('button').contains('Accept All Cookies').click()
-
+  // insurance-type
+  cy.contains('button', 'Accept All Cookies', { timeout: 10000 }).click()
   cy.contains('button', 'Home Insurance').click()
 
+  // home-type
   cy.contains('button', 'Apartment', { timeout: 1000 }).click()
 
+  // home-type-apartment
   cy.contains('button', 'I own it', { timeout: 1000 }).click()
 
-  cy.contains('p', 'square meters', { timeout: 1000 })
-    .siblings('input[type=number]')
-    .type('55{enter}')
+  // living-space-apartment
+  cy.contains('How big is the apartment?', { timeout: 5000 }).should('be.visible')
+  cy.focused().type(`${faker.datatype.number({ min: 30, max: 120 })}{enter}`)
 
-  cy.contains('p', /^people*/)
-    .siblings('input[type=number]')
-    .type('2{enter}')
+  // household-size-apartment
+  cy.contains('How many people should be covered by the insurance?', { timeout: 5000 }).should(
+    'be.visible',
+  )
+  cy.focused().type(`${faker.datatype.number({ min: 1, max: 6 })}{enter}`)
 
-  cy.get('input[inputmode=numeric]', { timeout: 1000 }).type('021118-8941{enter}')
+  // personal-number-apartment
+  cy.contains('All we need now is your personal identity number.', { timeout: 5000 }).should(
+    'be.visible',
+  )
+  cy.focused().type('021118-8941{enter}')
 
-  cy.contains('First name', { timeout: 5000 })
-    .parent()
-    .siblings('input[type=text]')
-    .type(faker.name.firstName())
+  // name
+  cy.contains("What's your name?", { timeout: 5000 }).should('be.visible')
+  cy.focused().type(faker.name.firstName())
+
   cy.contains('Surname')
     .parent()
     .siblings('input[type=text]')
     .type(`${faker.name.lastName()}{enter}`)
 
+  // new-address-apartment
   const address = faker.address.streetAddress()
-  cy.contains('Address', { timeout: 1000 }).parent().siblings('input[type=text]').type(address)
+  cy.contains('What address do you want an offer for?', { timeout: 5000 }).should('be.visible')
+  cy.focused().type(address)
   cy.contains('Postal code')
     .parent()
     .siblings('input[type=text]')
     .type(`${faker.address.zipCode('### ##')}{enter}`)
 
-  cy.get('input[type=email]', { timeout: 1000 }).type(`${faker.internet.email()}{enter}`)
+  // email-apartment
+  cy.contains('OK, we are almost there.', { timeout: 5000 }).should('be.visible')
+  cy.focused().type(`${faker.internet.email()}{enter}`)
 
+  // offer page
   cy.contains('Your price quote', { timeout: 10000 }).should('be.visible')
   cy.contains(address).should('be.visible')
 })
--- apps/onboarding/cypress/integration/se-needer-house-flow.ts
+++ apps/onboarding/cypress/integration/se-needer-house-flow.ts
@@ -5,45 +5,48 @@ it('should get a price quote for a Swedish house', () => {
 
   cy.visit(`/se-en/new-member/home-accident-needer`)
 
-  // eslint-disable-next-line testing-library/await-async-utils
-  cy.wait(1000)
-  cy.get('button').contains('Accept All Cookies').click()
-
+  // insurance-type
+  cy.contains('button', 'Accept All Cookies', { timeout: 10000 }).click()
   cy.contains('button', 'Home & Accident Insurance').click()
 
+  // home-type
   cy.contains('button', 'House', { timeout: 1000 }).click()
 
+  // home-type-villa
   cy.contains('button', 'I own it', { timeout: 1000 }).click()
 
-  cy.get('input[inputmode=numeric]', { timeout: 1000 }).type('971023-2068{enter}')
+  // personal-number-villa
+  cy.contains('All we need now is your personal identity number.', { timeout: 5000 }).should(
+    'be.visible',
+  )
+  cy.focused().type('971023-2068{enter}')
 
-  cy.contains('First name', { timeout: 5000 })
-    .parent()
-    .siblings('input[type=text]')
-    .type(faker.name.firstName())
+  // manual-name-villa
+  cy.contains("What's your name?", { timeout: 5000 }).should('be.visible')
+  cy.focused().type(faker.name.firstName())
   cy.contains('Surname')
     .parent()
     .siblings('input[type=text]')
     .type(`${faker.name.lastName()}{enter}`)
 
+  // manual-address-villa
   const address = faker.address.streetAddress()
-  cy.contains('Address', { timeout: 1000 }).parent().siblings('input[type=text]').type(address)
+  cy.contains('What address do you want an offer for?', { timeout: 5000 }).should('be.visible')
+  cy.focused().type(address)
   cy.contains('Postal code')
     .parent()
     .siblings('input[type=text]')
     .type(`${faker.address.zipCode('### ##')}{enter}`)
 
   // manual-info-villa
-  cy.contains('p', 'square meters', { timeout: 1000 })
-    .first()
-    .siblings('input[type=number]')
-    .type(faker.datatype.number({ min: 30, max: 120 }).toString())
-
-  cy.contains('p', 'square meters', { timeout: 1000 })
-    .last()
+  cy.contains("We can't fetch any information about your house.", { timeout: 5000 }).should(
+    'be.visible',
+  )
+  cy.focused().type(faker.datatype.number({ min: 30, max: 120 }).toString())
+  cy.contains('span', 'Ancillary area')
+    .parent()
     .siblings('input[type=number]')
     .type(faker.datatype.number({ min: 0, max: 40 }).toString())
-
   cy.contains('span', 'Year of construction')
     .parent()
     .siblings('input[type=number]')
@@ -56,18 +59,18 @@ it('should get a price quote for a Swedish house', () => {
   cy.contains('button', 'Four floors or less', { timeout: 1000 }).click()
 
   // household-size-villa
-  cy.contains('p', /^people*/)
-    .siblings('input[type=number]')
-    .type(`${faker.datatype.number({ min: 0, max: 6 })}{enter}`)
+  cy.contains('How many people should be covered', { timeout: 5000 }).should('be.visible')
+  cy.focused().type(`${faker.datatype.number({ min: 0, max: 6 })}{enter}`)
 
   // subletting
   cy.contains('button', "No, I don't sublet", { timeout: 1000 }).click()
 
   // extra-buildings
-  cy.contains('button', 'extra-buildings', { timeout: 1000 }).click()
+  cy.contains('button', 'Continue', { timeout: 1000 }).click()
 
   // email-villa
-  cy.get('input[type=email]', { timeout: 1000 }).type(`${faker.internet.email()}{enter}`)
+  cy.contains('OK, we are almost there.', { timeout: 5000 }).should('be.visible')
+  cy.focused().type(`${faker.internet.email()}{enter}`)
 
   // offer page
   cy.contains('Your price quote', { timeout: 10000 }).should('be.visible')
