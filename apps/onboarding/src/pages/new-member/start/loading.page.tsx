2db422eac961735ca8d8b90e3158fc84de6639db
Gustav Eén <gustaveen@gmail.com>
2022-02-21 15:27:40 +0100

    Add simple page layout component

--- apps/onboarding/src/pages/new-member/start/loading.page.tsx
+++ apps/onboarding/src/pages/new-member/start/loading.page.tsx
@@ -1,8 +1,11 @@
 import type { NextPage } from 'next'
+import { PageLayout } from './components/page-layout'
 
 const LoadingPage: NextPage = () => {
   return (
+    <PageLayout>
       <div>Loading...</div>
+    </PageLayout>
   )
 }
 
