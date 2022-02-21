2f1a6d85e5a69585c06de6495013abf87791e52a
Ellen Styrélius <ellen@hedvig.com>
2022-02-21 17:23:24 +0100

    Add Heading component and story to ui package

--- packages/ui/src/index.tsx
+++ packages/ui/src/index.tsx
@@ -3,6 +3,7 @@ export { Button, LinkButton } from './components/button'
 export { Separate } from './components/separate'
 export { Space } from './components/space'
 export { InputField } from './components/input-field'
+export { Heading } from './components/heading'
 
 export type { Level } from './lib/media-query'
 export { mq } from './lib/media-query'
