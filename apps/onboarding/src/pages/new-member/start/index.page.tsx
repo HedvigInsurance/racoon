e7407c326a5aa234bd7fe42d3227f37c80beb08e
Robin Andeer <robin.andeer@gmail.com>
2022-02-21 14:05:34 +0100

    Create new page: /new-member/start

---
+++ apps/onboarding/src/pages/new-member/start/index.page.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from 'next'
+
+const NewMemberStartPage: NextPage = () => {
+  return (
+    <main>
+      <h1>New member start</h1>
+    </main>
+  )
+}
+
+export default NewMemberStartPage
