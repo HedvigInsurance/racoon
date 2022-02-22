1a458f6410743f2501f612b65741e66d506bf0ea
Amanda Flodström <amanda@hedvig.com>
2022-02-22 15:55:32 +0100

    Add link to embark for manual flow

--- apps/onboarding/src/pages/new-member/start/index.page.tsx
+++ apps/onboarding/src/pages/new-member/start/index.page.tsx
@@ -1,36 +1,42 @@
-import { Heading } from 'ui'
+import { Heading, LinkButton, Space } from 'ui'
+
 import type { NextPage } from 'next'
 import { PageLayout } from './components/page-layout'
 import { SsnInputForm } from './components/ssn-input-form'
 import styled from '@emotion/styled'
 
-type FormValue = {
-  ssn: string
-}
-
 const Wrapper = styled.div({
   padding: '1rem',
   width: '100%',
-  maxWidth: '50rem',
+  maxWidth: '30rem',
 })
 
 const Text = styled.p(({ theme }) => ({
   lineHeight: '1.5rem',
   fontSize: '1rem',
   color: theme.colors.gray700,
+  margin: 0,
 }))
 
 const NewMemberStartPage: NextPage = () => {
   return (
     <PageLayout>
       <Wrapper>
-        <Heading variant="s" headingLevel="h1" colorVariant="dark">
-          Skaffa en snabbare, enklare och smidigare försäkring
-        </Heading>
-        <Text>
-          Vi använder personnumret för att hämta uppgifter om ditt hem för att ge dig ett pris.
-        </Text>
-        <SsnInputForm />
+        <Space y={1}>
+          <Heading variant="s" headingLevel="h1" colorVariant="dark">
+            Skaffa en snabbare, enklare och smidigare försäkring
+          </Heading>
+          <Text>
+            Vi använder personnumret för att hämta uppgifter om ditt hem för att ge dig ett pris.
+          </Text>
+          <SsnInputForm />
+          <LinkButton
+            href="https://www.hedvig.com/se-en/new-member/new/insurance-type"
+            $variant="text"
+          >
+            {'Eller fyll i manuellt >'}
+          </LinkButton>
+        </Space>
       </Wrapper>
     </PageLayout>
   )
