db38e7bf8bf54ef4ebf29026c3c775695ff32d1e
Gustav Eén <gustaveen@gmail.com>
2022-02-23 11:16:22 +0100

    Add reusable price component and add intro (#26)

---
+++ apps/onboarding/src/pages/new-member/cart/components/monthly-price.tsx
@@ -0,0 +1,24 @@
+import { LinkButton } from 'ui'
+import React from 'react'
+import styled from '@emotion/styled'
+import { useFormattedPrice } from '@/pages/new-member/cart/hooks/use-formatted-price'
+import { useTranslation } from 'next-i18next'
+
+export type PriceProps = {
+  price: { amount: number; currency: string }
+}
+
+const Amount = styled.p(({ theme }) => ({
+  fontSize: '1rem',
+  lineHeight: '1.5rem',
+  color: theme.colors.gray900,
+  margin: 0,
+}))
+
+
+export const MonthlyPrice = ({ price }: PriceProps) => {
+  const { t } = useTranslation()
+  const formattedPrice = useFormattedPrice(price)
+
+  return <Amount>{`${formattedPrice}${t('PRICE_SUFFIX_INTERVAL')}`}</Amount>
+}
