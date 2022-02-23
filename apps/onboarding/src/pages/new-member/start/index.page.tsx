a062766086c275e8aad91eb1a79018bd738ba2b9
Ellen Styrélius <ellen@hedvig.com>
2022-02-23 13:24:36 +0100

    Start page (#30)
    * Change Button font, align center in page-layout component, create ssn-input-form component and begin adding components to start-page
    
    * Fix link button and add focus styles to buttons
    
    * Add link to embark for manual flow
    
    * Add Switch component with tick icon and put into ssn input form
    
    * Fix Switch component so that it actually works :)
    
    * Make submit button disabled if the user doesn't want a quote for their current address
    
    * Remove duplicate 'fontFamily' key that weirdly appeared in Button component
    
    * Add top (and bottom) padding to start page Wrapper
    
    * Remove centering layout styles from PageLayout and add those styles to a PageWrapper for the start page instead
    
    * Change styling of unchecked checkbox
    
    * Fix link to manual onboarding flow
    
    * Replace display 'flex' with 'grid' for Switch Wrapper, in order to confidently use 'gap' property :)
    
    Co-authored-by: Amanda Flodström <amanda@hedvig.com>

--- apps/onboarding/src/pages/new-member/start/index.page.tsx
+++ apps/onboarding/src/pages/new-member/start/index.page.tsx
@@ -1,10 +1,51 @@
+import { Heading, LinkButton, Space, mq } from 'ui'
+
 import type { NextPage } from 'next'
+import { PageLayout } from './components/page-layout'
+import { SsnInputForm } from './components/ssn-input-form'
+import styled from '@emotion/styled'
+
+const PageWrapper = styled.div({
+  width: '100vw',
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+})
+const InnerWrapper = styled.div({
+  padding: '6rem 1rem',
+  width: '100%',
+  maxWidth: '30rem',
+  [mq.lg]: {
+    paddingTop: '8rem',
+  },
+})
+const Text = styled.p(({ theme }) => ({
+  lineHeight: '1.5rem',
+  fontSize: '1rem',
+  color: theme.colors.gray700,
+  margin: 0,
+}))
 
 const NewMemberStartPage: NextPage = () => {
   return (
-    <main>
-      <h1>New member start</h1>
-    </main>
+    <PageLayout>
+      <PageWrapper>
+        <InnerWrapper>
+          <Space y={1}>
+            <Heading variant="s" headingLevel="h1" colorVariant="dark">
+              Skaffa en snabbare, enklare och smidigare försäkring
+            </Heading>
+            <Text>
+              Vi använder personnumret för att hämta uppgifter om ditt hem för att ge dig ett pris.
+            </Text>
+            <SsnInputForm />
+            <LinkButton href="https://www.hedvig.com/se-en/new-member/new" $variant="text">
+              {'Eller fyll i manuellt >'}
+            </LinkButton>
+          </Space>
+        </InnerWrapper>
+      </PageWrapper>
+    </PageLayout>
   )
 }
 
