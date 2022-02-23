bd6e6580a283eb2a06519fc4b613b9ec514d036a
amandaflod <59652803+amandaflod@users.noreply.github.com>
2022-02-23 17:36:21 +0100

    Add start page hero component and add it to page (#37)

--- apps/onboarding/src/pages/new-member/start/index.page.tsx
+++ apps/onboarding/src/pages/new-member/start/index.page.tsx
@@ -1,6 +1,7 @@
 import { Heading, LinkButton, Space, mq } from 'ui'
 
 import { Footer } from './components/footer'
+import { Hero } from './components/hero'
 import type { NextPage } from 'next'
 import { PageLayout } from './components/page-layout'
 import { SsnInputForm } from './components/ssn-input-form'
@@ -13,7 +14,7 @@ const PageWrapper = styled.div({
   alignItems: 'center',
 })
 const InnerWrapper = styled.div({
-  padding: '6rem 1rem',
+  padding: '1rem',
   width: '100%',
   maxWidth: '30rem',
   [mq.lg]: {
@@ -33,7 +34,8 @@ const Spacer = styled.div({
 
 const NewMemberStartPage: NextPage = () => {
   return (
-    <PageLayout>
+    <PageLayout headerVariant="light">
+      <Hero />
       <PageWrapper>
         <InnerWrapper>
           <Space y={1}>
