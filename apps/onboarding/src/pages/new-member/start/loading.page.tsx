91f3bb5a67680558fae9b01b1b0ae191bc7a7ad4
Robin Andeer <robin.andeer@gmail.com>
2022-02-25 10:30:16 +0100

    Update loading text
    Make animation shorter.

--- apps/onboarding/src/pages/new-member/start/loading.page.tsx
+++ apps/onboarding/src/pages/new-member/start/loading.page.tsx
@@ -15,7 +15,6 @@ const Wrapper = styled.div({
 
 const LoadingContent = styled.div({
   position: 'relative',
-  maxWidth: '20ch',
   margin: '0 auto',
   overflow: 'hidden',
   boxSizing: 'border-box',
@@ -46,25 +45,24 @@ const fadeInUp = keyframes({
 const Text = styled.p(({ theme }) => ({
   margin: 0,
   fontSize: '1.5rem',
-  lineHeight: 1.33,
+  lineHeight: 1.5,
   fontFamily: theme.fonts.body,
 
   opacity: 0,
-  animation: `${fadeInUp} 3.5s cubic-bezier(0.39, 0.575, 0.565, 1) infinite`,
+  animation: `${fadeInUp} 5.5s cubic-bezier(0.39, 0.575, 0.565, 1) infinite`,
 }))
 
 const TEXTS = [
-  'Building your home insurance',
-  'Covers fire, burglary,',
-  'theft, damage,',
-  'assault, travels,',
-  'sickness on vacation,',
-  'appliances,',
-  'among other things.',
-  'All-risk included.',
-  'No paperwork,',
-  'no phone lines,',
-  'no hassle.',
+  'Building your insurance',
+  'Comprehensive coverage',
+  'All-risk included',
+  'Cancel at any time',
+  'Five-star rated service',
+  'No paperwork',
+  'or phone lines',
+  'No interrogations',
+  'or shaming',
+  'No problems',
 ]
 
 const LoadingPage: NextPage = () => {
