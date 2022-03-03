d608719b8c5ab16a47af7b20eeb1799a7fbcc2b0
Gustav Eén <gustaveen@gmail.com>
2022-03-03 15:24:39 +0100

    Add tracking for user properties (#72)
    * Add tracking for user properties
    
    * Match pageData from current onboarding

--- apps/onboarding/src/services/gtm.ts
+++ apps/onboarding/src/services/gtm.ts
@@ -1,8 +1,52 @@
+import { useCurrentLocale } from '@/lib/l10n'
+import { useEffect } from 'react'
+
 export const GTM_ID = process.env.NEXT_PUBLIC_GOOGLE_TAG_MANAGER_ID
 
 export const pageview = (url: string) => {
-  window.dataLayer?.push({
-    event: 'pageview',
-    page: url,
+  pushToGTMDataLayer({
+    event: 'virtual_page_view',
+      pageData: {
+        page: url,
+        title: document.title,
+      },
   })
 }
+
+type GTMUserProperties = {
+  market: string
+  environment: 'development' | 'production' | 'test'
+}
+
+type GTMPageData = {
+  page: string
+  title: string
+}
+
+type DataLayerObject = {
+  event?: string
+  userProperties?: GTMUserProperties
+  pageData?: GTMPageData
+}
+
+/**
+ * Track user properties
+ */
+export const useGTMUserProperties = () => {
+  const environment = process.env.NODE_ENV
+  const market = useCurrentLocale().marketLabel.toLowerCase()
+  
+  useEffect(() => {
+    pushToGTMDataLayer({
+      userProperties: {
+        environment,
+        market,
+      },
+    })
+  }, [environment, market])
+}
+
+export const pushToGTMDataLayer = (obj: DataLayerObject) => {
+  window.dataLayer?.push(obj)
+}
+
