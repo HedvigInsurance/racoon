6bb5eb14ee3efbc13ea62af80f8641de47608faa
Gustav Eén <gustaveen@gmail.com>
2022-03-23 15:43:24 +0100

    Update block imports

--- apps/market-web/src/components/DynamicComponent.tsx
+++ apps/market-web/src/components/DynamicComponent.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import SbEditable from 'storyblok-react'
 import { AccordionBlock } from '@/blocks/AccordionBlock/AccordionBlock'
+import { BannerBlock } from '@/blocks/BannerBlock/BannerBlock'
+import { HeadlineBlock } from '@/blocks/HeadlineBlock/HeadlineBlock'
+import { PlainTextBlock } from '@/blocks/PlainTextBlock'
+import { SpacerBlock } from '@/blocks/SpacerBlock'
 import type { StoryblokBaseBlock, StoryblokComponentName } from '@/lib/types'
-import { HeadlineBlock } from '../blocks/HeadlineBlock/HeadlineBlock'
-import { PlainTextBlock } from '../blocks/PlainTextBlock'
-import { SpacerBlock } from '../blocks/SpacerBlock'
 
 type Props = {
   block: StoryblokBaseBlock
@@ -12,7 +13,7 @@ type Props = {
 
 export const BlockComponents: Record<StoryblokComponentName, React.FC<any>> = {
   accordion_block: AccordionBlock,
-  banner_block: HeadlineBlock,
+  banner_block: BannerBlock,
   headline_block: HeadlineBlock,
   spacer_block: SpacerBlock,
   plain_text_block: PlainTextBlock,
