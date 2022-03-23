18164aa70c0ac7f23102a46609b139defd535b5e
=MB <=marian.bucuresteanu.hs@gmail.com>
2022-03-23 10:30:18 +0200

    BodyText, Nav components

---
+++ apps/onboarding/src/components/Nav/Header.tsx
@@ -0,0 +1,36 @@
+import { HedvigLogo, mq } from 'ui'
+
+import { LanguageSwitcher } from '../language-switcher'
+import React from 'react'
+import styled from '@emotion/styled'
+
+const HeaderContainer = styled.div((props) => ({
+  padding: '22.5px 0',
+  paddingLeft: '2rem',
+  paddingRight: '1.75rem',
+  height: '4.5rem',
+  position: 'sticky',
+  display: 'flex',
+  alignItems: 'center',
+  backgroundColor: props.theme.colors.white,
+  top: 0,
+  zIndex: 100,
+  [mq.sm]: {
+    height: '5rem',
+  },
+}))
+
+const HeaderMenu = styled.div({
+  marginLeft: 'auto',
+})
+
+export const Header = () => {
+  return (
+    <HeaderContainer>
+      <HedvigLogo />
+      <HeaderMenu>
+        <LanguageSwitcher />
+      </HeaderMenu>
+    </HeaderContainer>
+  )
+}
