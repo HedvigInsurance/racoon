2229bb9f9ea6538c72f058c6ba86709ecdefb461
Simon Aunér <simonauner@gmail.com>
2022-04-01 06:41:30 +0200

    Add action when clicking BurgerButton story

--- packages/ui/src/components/BurgerButton/BurgerButton.stories.tsx
+++ packages/ui/src/components/BurgerButton/BurgerButton.stories.tsx
@@ -1,10 +1,10 @@
-import styled from '@emotion/styled'
 import { ComponentMeta, ComponentStory } from '@storybook/react'
 import { BurgerButton, BurgerButtonProps } from './BurgerButton'
 
 export default {
   title: 'UI / Button / BurgerButton',
   component: BurgerButton,
+  argsTypes: { onClick: { action: 'clicked' } },
 } as ComponentMeta<typeof BurgerButton>
 
 const Template: ComponentStory<typeof BurgerButton> = (props: BurgerButtonProps) => {
@@ -12,8 +12,4 @@ const Template: ComponentStory<typeof BurgerButton> = (props: BurgerButtonProps)
 }
 
 export const Default = Template.bind({})
-Default.args = {
-  onClick: () => {
-    console.log('click')
-  },
-}
+Default.args = {}
