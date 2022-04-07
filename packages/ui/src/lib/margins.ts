873fef340d20dc6c2ab2f9beb7d01fa196dc3b4b
Simon Aunér <simonauner@gmail.com>
2022-04-07 08:46:29 +0200

    Rename margin props to mb etc

--- packages/ui/src/lib/margins.ts
+++ packages/ui/src/lib/margins.ts
@@ -1,17 +1,26 @@
 export type Margins = {
-  marginLeft?: string
-  marginRight?: string
-  marginTop?: string
-  marginBottom?: string
-  margin?: string
+  // margin-left and margin-right
+  mx?: string
+  // margin-top and margin-bottom
+  my?: string
+  // margin-left
+  ms?: string
+  // margin-right
+  me?: string
+  // margin-top
+  mt?: string
+  // margin-bottom
+  mb?: string
+  // all margins
+  m?: string
 }
 
-export const getMargins = (margins: Margins & any) => {
+export const getMargins = (margins: Margins) => {
   return {
-    margin: margins.margin,
-    marginTop: margins.marginTop,
-    marginBottom: margins.marginBottom,
-    marginLeft: margins.marginLeft,
-    marginRight: margins.marginRight,
+    margin: margins.m,
+    marginTop: margins.mt || margins.my,
+    marginBottom: margins.mb || margins.my,
+    marginLeft: margins.ms || margins.mx,
+    marginRight: margins.me || margins.mx,
   }
 }
