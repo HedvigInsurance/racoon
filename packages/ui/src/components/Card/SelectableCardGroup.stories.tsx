9fa9ad8d23826794cea12adb20b6b25d9e58e97e
Simon Aunér <simonauner@gmail.com>
2022-04-20 18:09:40 +0200

    Add SelectableCard

---
+++ packages/ui/src/components/Card/SelectableCardGroup.stories.tsx
@@ -0,0 +1,35 @@
+import { ComponentMeta, ComponentStory } from '@storybook/react'
+import { Space } from '../space'
+import { CardContent } from './Card'
+import { SelectableCard } from './SelectableCard'
+import { SelectableCardGroup } from './SelectableCardGroup'
+
+export default {
+  title: 'UI / Card / Selectable Card ',
+  component: SelectableCardGroup,
+  args: {},
+  argsTypes: { onChange: { action: 'onChange' } },
+} as ComponentMeta<typeof SelectableCardGroup>
+
+const RadioTemplate: ComponentStory<typeof SelectableCardGroup> = (args) => {
+  return (
+    <SelectableCardGroup {...args}>
+      <Space y={1}>
+        <SelectableCard size="md" id="first">
+          <CardContent>Home insurance</CardContent>
+        </SelectableCard>
+        <SelectableCard size="md" id="second">
+          <CardContent>
+            Home insurance &amp;
+            <br /> Accident insurance
+          </CardContent>
+        </SelectableCard>
+      </Space>
+    </SelectableCardGroup>
+  )
+}
+
+export const AsRadioButton = RadioTemplate.bind({})
+AsRadioButton.args = {
+  name: 'some_name',
+}
---
+++ packages/ui/src/components/Card/SelectableCardGroup.tsx
@@ -0,0 +1,74 @@
+import * as RadioGroup from '@radix-ui/react-radio-group'
+import React, { useEffect, useState } from 'react'
+import { QuotePriceCard } from '../QuotePriceCard/QuotePriceCard'
+import { SelectableCard } from './SelectableCard'
+
+type QuotePriceCardGroupProps = {
+  onChange: (selectedId: string) => void
+  name: string
+  children: React.ReactNode
+}
+
+const recursiveMap = (children: React.ReactNode, fn: (child: React.ReactNode) => void): any => {
+  return React.Children.map(children, (child) => {
+    if (!React.isValidElement(child)) {
+      return child
+    }
+
+    if (child.props.children) {
+      child = React.cloneElement(child, {
+        children: recursiveMap(child.props.children, fn),
+      })
+    }
+
+    return fn(child)
+  })
+}
+
+const cloneChildren = (
+  children: React.ReactNode,
+  selectedId: string,
+  callback: (id: string) => void,
+) => {
+  return recursiveMap(children, (child) => {
+    // Checking isValidElement is the safe way and avoids a typescript
+    // error too.
+    if (
+      React.isValidElement(child) &&
+      (child.type === SelectableCard || child.type === QuotePriceCard)
+    ) {
+      if (!child.props.id) {
+        throw new Error('Id must be defined on child components for SelectableCardGroup')
+      }
+
+      return React.cloneElement(child, {
+        checked: child.props.id === selectedId,
+        onChange: () => callback(child.props.id),
+        as: 'radio',
+      })
+    }
+    return child
+  })
+}
+
+export const SelectableCardGroup = ({ children, onChange, name }: QuotePriceCardGroupProps) => {
+  const [selectedCardId, setSelectedCardId] = useState('')
+  const [clonedChildren, setClonedChildren] = useState(
+    cloneChildren(children, selectedCardId, setSelectedCardId),
+  )
+
+  const handleOnValueChange = (newValue: any) => {
+    setSelectedCardId(newValue)
+    onChange(newValue)
+  }
+
+  useEffect(() => {
+    setClonedChildren(cloneChildren(children, selectedCardId, setSelectedCardId))
+  }, [selectedCardId, onChange, setClonedChildren, children])
+
+  return (
+    <RadioGroup.Root name={name} onValueChange={handleOnValueChange}>
+      {clonedChildren}
+    </RadioGroup.Root>
+  )
+}
