1c123e63b682c45aa1956d9bade3834158fc68c6
Gustav Eén <gustaveen@gmail.com>
2022-04-29 11:41:01 +0200

    Feat / Global styles CSS reset (#174)
    * Update globalStyles with up to date CSS resets
    
    * Make sure hero image is full height

--- packages/ui/src/lib/globalStyles.ts
+++ packages/ui/src/lib/globalStyles.ts
@@ -3,54 +3,69 @@ import { colorsV3, fonts, getCdnFontFaces } from '@hedviginsurance/brand'
 
 export const globalStyles = css`
   ${getCdnFontFaces()}
+  /***
+    The following CSS reset is heavily inspired by:
+    https://github.com/elad2412/the-new-css-reset
+  ***/
 
-  * {
+
+  /*
+    Remove all the styles of the "User-Agent-Stylesheet", except for the 'display' property
+  */
+  *:where(:not(iframe, canvas, img, svg, video):not(svg *)) {
+    all: unset;
+    display: revert;
+  }
+
+  /* Preferred box-sizing model */
+  *,
+  *::before,
+  *::after {
+    box-sizing: border-box;
+  }
+
+  /* Set default font rules and color on body */
+  body {
+    color: ${colorsV3.gray900};
+    background-color: ${colorsV3.gray100};
+    font-family: ${fonts.HEDVIG_LETTERS_STANDARD}, sans-serif;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
   }
 
-  html {
-    box-sizing: border-box;
-  }
-
-  *,
-  *:before,
-  *:after {
-    box-sizing: inherit;
-  }
-
-  body {
-    margin: 0;
-    padding: 0;
-    color: ${colorsV3.gray900};
-    background-color: ${colorsV3.gray100};
-    font-family: ${fonts.HEDVIG_LETTERS_STANDARD}, sans-serif;
-  }
-
-  h1,
-  h2,
-  h3,
-  h4,
-  h5,
-  h6 {
-    font-weight: 400;
-  }
-
+  /* Responsive images */
   img {
     max-width: 100%;
     height: auto;
   }
 
-  a {
-    color: inherit;
-    &:hover,
-    &:focus {
-      color: inherit;
-    }
+  /* Sane display value for media elements */
+  img,
+  iframe,
+  picture,
+  video,
+  canvas,
+  svg {
+    display: block;
   }
 
-  input,
+  /* Remove default list styles */
+  ol,
+  ul {
+    list-style: none;
+    margin: 0;
+    padding: 0;
+  }
+
+  /* Reapply the pointer cursor for anchor tags */
+  a,
   button {
-    font-size: inherit;
+    cursor: revert;
+  }
+
+  /* Fix the feature of 'hidden' attribute.
+   "display:revert;" revert to element instead of attribute */
+  :where([hidden]) {
+    display: none;
   }
 `
