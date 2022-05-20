e76d713e2350b6168fbc315adc2d576569b2518e
Guilherme Popolin <guilherme@hedvig.com>
2022-05-20 10:54:41 +0200

    refactor(LandingPage.types): removed next dependency

--- apps/onboarding/src/components/LandingPage/LandingPage.types.ts
+++ apps/onboarding/src/components/LandingPage/LandingPage.types.ts
@@ -1,10 +1,13 @@
-import { ImageProps } from 'next/image'
-
 export type Insurance = {
   id: string
   name: string
   description: string
-  img: ImageProps
+  img: {
+    src: string
+    alt?: string
+    blurDataURL?: string
+    objectPosition?: string
+  }
   fieldName: string
   isPreselected?: boolean
   isAdditionalCoverage?: boolean
