22668e639d60c5aebe436b79ff5ec85dc400c488
Robin Andeer <robin.andeer@gmail.com>
2022-05-23 17:12:14 +0200

    Feature / Update landing page footer, always sticky (#218)

---
+++ apps/onboarding/src/components/LandingPage/FixedFooter.tsx
@@ -0,0 +1,21 @@
+import styled from '@emotion/styled'
+import { PropsWithChildren } from 'react'
+
+const FixedContainer = styled.div((props) => ({
+  position: 'fixed',
+  bottom: 0,
+  left: 0,
+  right: 0,
+  backgroundColor: props.theme.colors.white,
+  boxShadow: '0px -4px 10px rgba(0, 0, 0, 0.1)',
+
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+  height: '5rem',
+  padding: '0 1rem',
+}))
+
+export const FixedFooter = ({ children }: PropsWithChildren<{}>) => {
+  return <FixedContainer>{children}</FixedContainer>
+}
