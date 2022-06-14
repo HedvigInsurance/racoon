7533655607f67f694eb882c4bec0780c024bf8a2
Robin Andeer <robin.andeer@gmail.com>
2022-06-14 17:59:55 +0200

    Setup separate "store" app

--- apps/onboarding/src/pages/index.tsx
+++ apps/onboarding/src/pages/index.tsx
@@ -1,8 +1,33 @@
-import type { NextPage } from 'next'
-import { FrontPage } from '@/components/FrontPage/FrontPage'
+import styled from '@emotion/styled'
+import Link from 'next/link'
+import { PageLink } from '@/lib/PageLink'
 
-const NextFrontPage: NextPage = () => {
-  return <FrontPage />
+const Wrapper = styled.main({
+  height: '100vh',
+  display: 'flex',
+  flexDirection: 'column',
+  justifyContent: 'center',
+  alignItems: 'center',
+})
+
+const Heading = styled.h1({
+  fontWeight: 'bold',
+})
+
+const HomePage = () => {
+  return (
+    <Wrapper>
+      <Heading>Racoon</Heading>
+
+      <Link href={PageLink.forever({ code: '8batky' })}>
+        <a>Forever page</a>
+      </Link>
+
+      <Link href={PageLink.landing()}>
+        <a>Landing page</a>
+      </Link>
+    </Wrapper>
+  )
 }
 
-export default NextFrontPage
+export default HomePage
