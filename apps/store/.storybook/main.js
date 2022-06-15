99ff99074de9ccf65163e2c3da946fe3243859bb
Gustav Eén <gustaveen@gmail.com>
2022-06-15 22:21:26 +0200

    Add storybook-addon-next

--- apps/store/.storybook/main.js
+++ apps/store/.storybook/main.js
@@ -1,6 +1,6 @@
 module.exports = {
   stories: ['../src/**/*.stories.@(ts|tsx)'],
-  addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
+  addons: ['@storybook/addon-links', '@storybook/addon-essentials', 'storybook-addon-next'],
   framework: '@storybook/react',
   core: {
     builder: '@storybook/builder-webpack5',
