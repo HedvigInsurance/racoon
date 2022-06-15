9d1e99650eb940aad3a24c60180dd5b49571af6a
Robin Andeer <robin.andeer@gmail.com>
2022-06-15 15:28:04 +0200

    Setup testing Jest + Testing Library

---
+++ apps/store/jest.setup.js
@@ -0,0 +1,7 @@
+// Used for @testing-library/react
+// Learn more: https://github.com/testing-library/jest-dom
+import '@testing-library/jest-dom/extend-expect'
+// Polyfills 'fetch' used in react components
+import 'whatwg-fetch'
+
+jest.mock('next/router', () => require('next-router-mock'))
---
+++ apps/store/src/test/utils.tsx
@@ -0,0 +1,45 @@
+import { RenderOptions, render } from '@testing-library/react'
+import { RouterContext } from 'next/dist/shared/lib/router-context'
+import { NextRouter } from 'next/router'
+import { ReactElement, ReactNode } from 'react'
+
+const createRouterMock = (router?: Partial<NextRouter>): NextRouter => ({
+  basePath: '',
+  pathname: '/',
+  route: '/',
+  asPath: '/',
+  query: {},
+  push: () => Promise.resolve(true),
+  replace: () => Promise.resolve(true),
+  reload: () => {},
+  back: () => {},
+  prefetch: () => Promise.resolve(),
+  beforePopState: () => {},
+  events: {
+    on: () => {},
+    off: () => {},
+    emit: () => {},
+  },
+  isFallback: false,
+  isLocaleDomain: false,
+  isPreview: false,
+  isReady: false,
+  ...router,
+})
+
+const AllTheProviders =
+  ({ router }: { router?: Partial<NextRouter> }) =>
+  // eslint-disable-next-line react/display-name
+  ({ children }: { children: ReactNode }) => {
+    return (
+      <RouterContext.Provider value={createRouterMock(router)}>{children}</RouterContext.Provider>
+    )
+  }
+
+const customRender = (
+  ui: ReactElement,
+  options?: Omit<RenderOptions, 'wrapper'> & { router?: Partial<NextRouter> },
+) => render(ui, { wrapper: AllTheProviders({ router: options?.router }), ...options })
+
+export * from '@testing-library/react'
+export { customRender as render }
