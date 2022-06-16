70cb41caab994baefde5df08628a9b25dace8f38
Robin Andeer <robin.andeer@gmail.com>
2022-06-16 17:34:40 +0200

    Add summary labels to completed form groups
    Update useTranslateTextLabel to handle translations with placeholders.
    Add global storybook script.

--- apps/store/src/components/PriceCalculator/PriceCalculator.tsx
+++ apps/store/src/components/PriceCalculator/PriceCalculator.tsx
@@ -7,23 +7,31 @@ type OnSubmitParams = {
   data: FormData
 }
 
-type PriceCalculatorProps = {
+export type PriceCalculatorProps = {
   form: PriceForm
   onSubmit: (params: OnSubmitParams) => void
 }
 
+const FORM_DATA = {}
+
 export const PriceCalculator = ({ onSubmit, form }: PriceCalculatorProps) => {
-  const translateTextLabel = useTranslateTextLabel()
+  const translateTextLabel = useTranslateTextLabel({ data: FORM_DATA })
   const activeGroup = form.groups.find((group) => group.state !== 'VALID')
 
   return (
     <div>
-      {form.groups.map(({ id, title, inputs }) => (
+      {form.groups.map(({ id, title, inputs, state, summary }) => (
         <div key={id}>
           <h2>{translateTextLabel(title)}</h2>
-          {activeGroup?.id === id && (
+          {activeGroup?.id === id ? (
             <FormGroup onSubmit={(data) => onSubmit({ id, data })} inputs={inputs} />
-          )}
+          ) : state === 'VALID' ? (
+            <div>
+              {summary.labels.map((label) => (
+                <p key={label.key}>{label.key}</p>
+              ))}
+            </div>
+          ) : null}
         </div>
       ))}
 
