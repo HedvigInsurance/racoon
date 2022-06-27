5905eecca719f16a69098ba8dfd0d0d11071b809
amandaflod <59652803+amandaflod@users.noreply.github.com>
2022-06-27 14:08:27 +0200

    Fix html lang based on locale (#253)

--- apps/onboarding/src/pages/_document.tsx
+++ apps/onboarding/src/pages/_document.tsx
@@ -1,11 +1,22 @@
 import { fonts } from '@hedviginsurance/brand'
 import Document, { Head, Html, Main, NextScript } from 'next/document'
+import { locales, LocaleLabel } from '@/lib/l10n/locales'
 import { gtmDevScript, gtmProdScript } from '@/services/analytics/gtm'
 
 export default class MyDocument extends Document {
+  lang(): string {
+    const { locale } = this.props
+
+    if (locale && Object.keys(locales).includes(locale)) {
+      return locales[locale as LocaleLabel].htmlLang
+    }
+
+    return 'en'
+  }
+
   render() {
     return (
-      <Html>
+      <Html lang={this.lang()}>
         <Head>
           {Object.values(fonts).map((fontName) => (
             <link
