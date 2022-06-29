82e609c05495ff832962bac109c84373c26ce74d
Guilherme Popolin <guilherme@hedvig.com>
2022-06-29 15:20:21 +0200

    refact(store): setup msw only for node environment

--- apps/store/src/mocks/index.ts
+++ apps/store/src/mocks/index.ts
@@ -1,9 +1,3 @@
-if (typeof window === 'undefined') {
-  const { server } = require('./server')
-  server.listen()
-} else {
-  const { worker } = require('./browser')
-  worker.start({ onUnhandledRequest: 'bypass' })
-}
+import { server } from './server'
 
-export {}
+server.listen({ onUnhandledRequest: 'bypass' })
