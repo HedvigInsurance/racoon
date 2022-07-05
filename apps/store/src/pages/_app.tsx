e220e0e6ad63a63fd01cf7783737a4f165514936
Robin Andeer <robin.andeer@gmail.com>
2022-07-05 16:14:46 +0200

    Feature / Remove custom Emotion cache setup (#267)

--- apps/store/src/pages/_app.tsx
+++ apps/store/src/pages/_app.tsx
@@ -1,6 +1,4 @@
 import { ApolloProvider } from '@apollo/client'
-import createCache from '@emotion/cache'
-import { CacheProvider } from '@emotion/react'
 import type { AppPropsWithLayout } from 'next/app'
 import Head from 'next/head'
 import { ThemeProvider } from 'ui'
@@ -10,8 +8,6 @@ import * as Datadog from '@/services/datadog'
 import { CartContext, useCartContextStore } from '@/services/mockCartService'
 
 Datadog.initRum()
-
-const cache = createCache({ key: 'next' })
 
 const MyApp = ({ Component, pageProps }: AppPropsWithLayout) => {
   const apolloClient = useApollo(pageProps)
@@ -26,14 +22,12 @@ const MyApp = ({ Component, pageProps }: AppPropsWithLayout) => {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
       <ApolloProvider client={apolloClient}>
-        <CacheProvider value={cache}>
-          <ThemeProvider>
-            <GlobalStyles />
-            <CartContext.Provider value={cartStore}>
-              {getLayout(<Component {...pageProps} />)}
-            </CartContext.Provider>
-          </ThemeProvider>
-        </CacheProvider>
+        <ThemeProvider>
+          <GlobalStyles />
+          <CartContext.Provider value={cartStore}>
+            {getLayout(<Component {...pageProps} />)}
+          </CartContext.Provider>
+        </ThemeProvider>
       </ApolloProvider>
     </>
   )
