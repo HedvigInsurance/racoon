0fc41b08a4455410ecf2454e06e283700f19099d
Robin Andeer <robin.andeer@gmail.com>
2022-07-14 13:18:41 +0200

    Feature / Add PriceCard component (#282)
    Add align prop to SpaceFlex component.

--- apps/store/src/components/SpaceFlex/SpaceFlex.tsx
+++ apps/store/src/components/SpaceFlex/SpaceFlex.tsx
@@ -5,16 +5,23 @@ type SpaceFlexProps = {
   children?: ReactNode
   space?: number
   direction?: 'horizontal' | 'vertical'
+  align?: 'start' | 'center' | 'end'
 }
 
-const SpaceFlexWrapper = styled.div(({ space, direction }: SpaceFlexProps) => ({
+const SpaceFlexWrapper = styled.div(({ space, direction, align }: SpaceFlexProps) => ({
   display: 'flex',
   gap: `${space}rem`,
   flexDirection: direction === 'horizontal' ? 'row' : 'column',
+  alignItems: align,
 }))
 
-export const SpaceFlex = ({ children, space = 1, direction = 'horizontal' }: SpaceFlexProps) => (
-  <SpaceFlexWrapper space={space} direction={direction}>
+export const SpaceFlex = ({
+  children,
+  space = 1,
+  direction = 'horizontal',
+  align = 'start',
+}: SpaceFlexProps) => (
+  <SpaceFlexWrapper space={space} direction={direction} align={align}>
     {children}
   </SpaceFlexWrapper>
 )
