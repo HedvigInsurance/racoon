2a350fd405fc6b50df971ae829de87f1e92ffdd6
Robin Andeer <robin.andeer@gmail.com>
2022-07-25 14:41:15 +0200

    Add mocks for cart API
    Refactor other mocks to use a shared database.
    Update to use shared line items.

--- apps/store/src/mocks/ShopSessionMock/ShopSessionMock.helpers.ts
+++ apps/store/src/mocks/ShopSessionMock/ShopSessionMock.helpers.ts
@@ -1,16 +1,9 @@
-import { factory, primaryKey } from '@mswjs/data'
-import { uuidv4 } from '../helpers'
-
-export const db = factory({
-  shopSession: {
-    id: primaryKey(() => uuidv4()),
-  },
-})
+import { db } from '../db'
 
 export const shopSessionFind = (sessionId: string) => {
   return db.shopSession.findFirst({ where: { id: { equals: sessionId } } })
 }
 
 export const shopSessionCreate = () => {
-  return db.shopSession.create()
+  return db.shopSession.create({ cart: db.cart.create() })
 }
