312c43d898af49844aaeaf9f11c60987aabc28c9
Robin Andeer <robin.andeer@gmail.com>
2022-07-28 17:17:23 +0200

    GRW-1282 / Feature / Add SiteFooter component (#326)
    - Add Storybook story for site footer.
    - Extract InputSelect as global component.
    - Implement SiteFooter in LayoutWithMenu.

---
+++ apps/store/src/components/SiteFooter/SiteFooter.stories.tsx
@@ -0,0 +1,27 @@
+import { ComponentMeta, Story } from '@storybook/react'
+import { Locale } from '@/lib/l10n/types'
+import { PageLink } from '@/lib/PageLink'
+import { SiteFooter, SiteFooterProps } from './SiteFooter'
+
+export default {
+  title: 'Site Footer',
+  component: SiteFooter,
+  argTypes: {
+    onChangeLocale: { action: 'onChangeLocale' },
+  },
+} as ComponentMeta<typeof SiteFooter>
+
+const Template: Story<SiteFooterProps> = (props) => {
+  return <SiteFooter {...props} />
+}
+
+export const Default = Template.bind({})
+Default.parameters = {
+  layout: 'fullscreen',
+  nextRouter: {
+    path: '/products/[product]',
+    asPath: PageLink.product({ locale: Locale.SvSe, id: 'home' }),
+    locale: Locale.SvSe,
+  },
+}
+Default.args = {}
---
+++ apps/store/src/components/SiteFooter/SiteFooter.tsx
@@ -0,0 +1,78 @@
+import styled from '@emotion/styled'
+import { ChangeEvent, useRef } from 'react'
+import { Space } from 'ui'
+import { InputSelect } from '@/components/InputSelect/InputSelect'
+import { Locale } from '@/lib/l10n/types'
+import { useCurrentLocale } from '@/lib/l10n/useCurrentLocale'
+import { SpaceFlex } from '../SpaceFlex/SpaceFlex'
+import { Field, MARKET_MAP, TEMP_TRANSLATIONS } from './SiteFooter.constants'
+import { findLocale } from './SiteFooter.helpers'
+
+export type SiteFooterProps = {
+  onChangeLocale: (locale: Locale | undefined) => void
+}
+
+export const SiteFooter = ({ onChangeLocale }: SiteFooterProps) => {
+  const formRef = useRef<HTMLFormElement>(null)
+  const { marketLabel: currentMarket, htmlLang: currentLanguage } = useCurrentLocale()
+
+  const marketList = Object.keys(MARKET_MAP).map((market) => ({
+    name: TEMP_TRANSLATIONS[`MARKET_LABEL_${market}`],
+    value: market,
+  }))
+
+  const languageList = MARKET_MAP[currentMarket].map((language) => ({
+    name: TEMP_TRANSLATIONS[`LANGUAGE_LABEL_${language}`],
+    value: language,
+  }))
+
+  const handleSelectChange = () => {
+    formRef.current?.dispatchEvent(new Event('submit', { cancelable: true, bubbles: true }))
+  }
+
+  const handleSubmit = (event: ChangeEvent<HTMLFormElement>) => {
+    event.preventDefault()
+    onChangeLocale(findLocale(event.target[Field.Market].value, event.target[Field.Language].value))
+  }
+
+  return (
+    <Wrapper y={2}>
+      <form ref={formRef} onSubmit={handleSubmit}>
+        <SpaceFlex>
+          <Flex>
+            <InputSelect
+              name={Field.Market}
+              onChange={handleSelectChange}
+              value={currentMarket}
+              options={marketList}
+            />
+          </Flex>
+
+          <Flex>
+            <InputSelect
+              name={Field.Language}
+              onChange={handleSelectChange}
+              value={currentLanguage}
+              options={languageList}
+            />
+          </Flex>
+        </SpaceFlex>
+      </form>
+
+      <TextMuted>Hedvig © 2022, All rights reserved</TextMuted>
+    </Wrapper>
+  )
+}
+
+const Wrapper = styled(Space)(({ theme }) => ({
+  width: '100%',
+  backgroundColor: theme.colors.gray200,
+  padding: `${theme.space[6]} ${theme.space[4]}`,
+}))
+
+const Flex = styled.div({ flex: 1 })
+
+const TextMuted = styled.p(({ theme }) => ({
+  color: theme.colors.gray600,
+  fontSize: theme.fontSizes[1],
+}))
