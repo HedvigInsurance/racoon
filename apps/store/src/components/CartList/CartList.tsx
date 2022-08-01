0c2917c86e52200828f41f2d652d98fabb07e597
Dan Popoutanu <63097139+dendoesit@users.noreply.github.com>
2022-08-01 22:16:51 +0300

    cart page (#327)

--- apps/store/src/components/CartList/CartList.tsx
+++ apps/store/src/components/CartList/CartList.tsx
@@ -1,82 +1,32 @@
 import styled from '@emotion/styled'
-import Link from 'next/link'
-import { useContext } from 'react'
-import { Button, Space, CrossIcon } from 'ui'
-import { Text } from '@/components/Text/Text'
-import { PageLink } from '@/lib/PageLink'
-import { CartContext } from '@/services/mockCartService'
-import { ProductNames } from '@/services/mockProductService'
+import { CartCard } from '@/components/CartCard/CartCard'
+import { CartPageProps, ProductData } from '../CartPage/CartPageProps.types'
 
-const BundleWrapper = styled.div({
-  marginLeft: '0.5rem',
-})
-
-type CartListProps = {
-  filterByProductName?: ProductNames
-  showBundles?: boolean
-  showLinks?: boolean
-}
+const CartUl = styled.ul(({ theme }) => ({
+  paddingLeft: '0',
+  listStyleType: 'none',
+  marginTop: theme.space[5],
+}))
 
 /**
  * This component shows all items currently in the cart.
  *
  * It can optionally filter by a CmsProduct name to only show relevant products.
  */
-export const CartList = ({ filterByProductName, showBundles, showLinks }: CartListProps) => {
-  const cartContext = useContext(CartContext)
 
-  if (!cartContext) {
-    throw new Error('ProductPage cannot be rendered outside CartContext')
-  }
-
-  const { cart, removeItem } = cartContext
-
-  const handleClickRemove = (id: string) => {
-    removeItem(id)
-  }
-
-  const items = filterByProductName
-    ? cart.items.filter((item) => item.product.name === filterByProductName)
-    : cart.items
-
-  if (items.length === 0) return null
+type Props = CartPageProps
+export const CartList = ({ products }: Props) => {
+  // const handleClickRemove = (id: string) => {
+  //send BE request to remove item from cart
+  // }
 
   return (
-    <ul>
-      {items.map((item) => (
-        <li key={item.id}>
-          <Space x={0.5}>
-            <span>
-              {showLinks ? (
-                <Link href={PageLink.product({ slug: item.product.slug })}>
-                  {item.product.displayName}
-                </Link>
-              ) : (
-                <span>{item.product.displayName}</span>
-              )}
-              , price: {item.price}
-            </span>
-            <Button
-              onClick={() => handleClickRemove(item.id)}
-              size="sm"
-              variant="text"
-              icon={<CrossIcon />}
-            ></Button>
-          </Space>
-          {item.product.insurances.length > 1 && showBundles && (
-            <BundleWrapper>
-              <Text size="s">
-                Included in this bundle:
-                <ul>
-                  {item.product.insurances.map((insurance) => (
-                    <li key={insurance.name}>{insurance.displayName}</li>
-                  ))}
-                </ul>
-              </Text>
-            </BundleWrapper>
-          )}
+    <CartUl>
+      {products.map((item: ProductData, id: number) => (
+        <li key={id}>
+          <CartCard title={item.name} price={item.cost} currency={item.currency} />
         </li>
       ))}
-    </ul>
+    </CartUl>
   )
 }
--- apps/store/src/components/CartPage/CartPage.tsx
+++ apps/store/src/components/CartPage/CartPage.tsx
@@ -1,39 +1,27 @@
 import Link from 'next/link'
-import { useContext } from 'react'
-import { Heading, Space } from 'ui'
+import { Button, Heading, Space } from 'ui'
 import { CartList } from '@/components/CartList/CartList'
+import { PriceBreakdown } from '@/components/PriceBreakdown.tsx/PriceBreakdown'
 import { PageLink } from '@/lib/PageLink'
-import { CartContext } from '@/services/mockCartService'
 
 export const CartPage = () => {
-  const cartContext = useContext(CartContext)
-
-  if (!cartContext) {
-    throw new Error('ProductPage cannot be rendered outside CartContext')
-  }
-
-  const { cart } = cartContext
+  const products = [
+    { name: 'Home Insurance', cost: 250, currency: 'SEK' },
+    { name: 'Apartment Insurance', cost: 100, currency: 'SEK' },
+  ]
+  const cost = { total: 350, subTotal: 250 }
 
   return (
     <>
       <Heading as="h1" variant="standard.40">
         Cart
       </Heading>
-      <Space y={2}>
-        <CartList showBundles showLinks />
-
-        <p>
-          Items in cart: {cart.items.length}, total price: {cart.price}
-        </p>
-
-        <footer>
-          <Link href={PageLink.cartReview()}>Check Out</Link>
-        </footer>
-
-        <div>
-          <Link href={PageLink.store()}>Go to shop</Link>
-        </div>
-      </Space>
+      <Space y={2} />
+      <CartList products={products} />
+      <PriceBreakdown currency="SEK" products={products} cost={cost} />
+      <Button fullWidth={true}>
+        <Link href={PageLink.cartReview()}>Check Out</Link>
+      </Button>
     </>
   )
 }
