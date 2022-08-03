f3dad7c44f354216f423b5c9743639a8ba29f299
Robin Andeer <robin.andeer@gmail.com>
2022-08-03 09:54:26 +0200

    Add visual scroll to button component for ProductPage (#344)
    Add framer-motion as dependency to store app.
    Add basic pill button component.
    Add example usage in Storybook.

---
+++ apps/store/src/components/ProductPage/ScrollPast/ScrollPast.tsx
@@ -0,0 +1,45 @@
+import styled from '@emotion/styled'
+import { motion, useScroll } from 'framer-motion'
+import { useEffect, useState } from 'react'
+
+export type ScrollPastProps = {
+  targetRef: React.RefObject<HTMLElement>
+  children: React.ReactNode
+}
+
+export const ScrollPast = ({ targetRef, children }: ScrollPastProps) => {
+  const { scrollY } = useScroll()
+
+  const [hasScrolledPassed, setHasScrolledPassed] = useState(false)
+  useEffect(() => {
+    return scrollY.onChange((latest) => {
+      if (targetRef.current) {
+        const elementEnd = targetRef.current.offsetTop + targetRef.current.clientHeight
+        setHasScrolledPassed(latest > elementEnd)
+      }
+    })
+  }, [scrollY, targetRef])
+
+  return (
+    <StyledWrapper
+      variants={{
+        hidden: { opacity: 0, transitionEnd: { display: 'none' } },
+        visible: { opacity: 1, display: 'flex' },
+      }}
+      initial="hidden"
+      animate={hasScrolledPassed ? 'visible' : 'hidden'}
+    >
+      {children}
+    </StyledWrapper>
+  )
+}
+
+const StyledWrapper = styled(motion.div)(({ theme }) => ({
+  position: 'fixed',
+  bottom: 0,
+  left: 0,
+  right: 0,
+  paddingBottom: theme.space[4],
+  display: 'flex',
+  justifyContent: 'center',
+}))
