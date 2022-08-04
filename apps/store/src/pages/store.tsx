889d6570c12428add7572b69e9503ab27072e36b
Guilherme Popolin <guilherme@hedvig.com>
2022-08-04 14:57:56 +0200

    chore(store): removed undesired log

--- apps/store/src/pages/store.tsx
+++ apps/store/src/pages/store.tsx
@@ -12,7 +12,6 @@ const NextStorePage: NextPageWithLayout<StoryblokPageProps> = ({ story: initialS
 
 export const getServerSideProps: GetServerSideProps<StoryblokPageProps> = async (context) => {
   const story = await getStoreStory(context.preview)
-  console.log(story)
 
   return {
     props: {
