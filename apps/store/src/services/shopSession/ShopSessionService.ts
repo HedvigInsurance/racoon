ae352a43857567c891953dae69e1d3f20270ee0d
Robin Andeer <robin.andeer@gmail.com>
2022-08-09 15:43:22 +0200

    Replace getOrCreateShopSession mutation
    Add shopSession query + shopSessionCreate mutation.
    Update useCart API to use new operations.
    Update ShopSession service to use new operations.

--- apps/store/src/services/shopSession/ShopSessionService.ts
+++ apps/store/src/services/shopSession/ShopSessionService.ts
@@ -1,8 +1,11 @@
-import { ApolloClient, ApolloQueryResult } from '@apollo/client'
+import { ApolloClient } from '@apollo/client'
 import {
-  ShopSessionFindOrCreateDocument,
-  ShopSessionFindOrCreateQuery,
-  ShopSessionFindOrCreateQueryVariables,
+  ShopSessionCreateMutationVariables,
+  ShopSessionCreateDocument,
+  ShopSessionDocument,
+  ShopSession,
+  ShopSessionCreateMutation,
+  ShopSessionQueryVariables,
 } from '@/services/apollo/generated'
 import { SimplePersister } from '@/services/persister/Persister.types'
 
@@ -12,32 +15,44 @@ export class ShopSessionService {
     private readonly apolloClient: ApolloClient<unknown>,
   ) {}
 
-  public get shopSessionId() {
-    return this.persister.fetch()
+  public async getOrCreate(params: ShopSessionCreateMutationVariables) {
+    const existingShopSession = await this.get()
+
+    if (existingShopSession) return existingShopSession
+
+    return await this.create(params)
   }
 
-  public async fetch(createParams: FetchParams) {
+  public async get() {
     const shopSessionId = this.persister.fetch()
+    if (!shopSessionId) return null
 
-    const {
-      data: { shopSessionFindOrCreate: shopSession },
-    } = await this.shopSessionFindOrCreate({
-      shopSessionId,
-      ...createParams,
+    const { data: shopSession } = await this.apolloClient.query<
+      ShopSession,
+      ShopSessionQueryVariables
+    >({
+      query: ShopSessionDocument,
+      variables: { shopSessionId },
     })
 
-    this.persister.save(shopSession.id)
     return shopSession
   }
 
-  private async shopSessionFindOrCreate(
-    variables: ShopSessionFindOrCreateQueryVariables,
-  ): Promise<ApolloQueryResult<ShopSessionFindOrCreateQuery>> {
-    return await this.apolloClient.query({
-      query: ShopSessionFindOrCreateDocument,
+  private async create(variables: ShopSessionCreateMutationVariables) {
+    const result = await this.apolloClient.mutate<
+      ShopSessionCreateMutation,
+      ShopSessionCreateMutationVariables
+    >({
+      mutation: ShopSessionCreateDocument,
       variables,
     })
+
+    const shopSession = result.data?.shopSessionCreate
+
+    if (!shopSession) throw new Error('Shop session not found')
+
+    this.persister.save(shopSession.id)
+
+    return shopSession
   }
 }
-
-type FetchParams = Omit<ShopSessionFindOrCreateQueryVariables, 'shopSessionId'>
