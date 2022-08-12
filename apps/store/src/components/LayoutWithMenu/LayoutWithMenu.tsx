be3d9b552a8c00e64564f44220282e5b58c07300
Alexander Lebedev <alexander.lebedev@gmail.com>
2022-08-12 20:20:03 +0300

    (WIP) ShopSessionContext POC

--- apps/store/src/components/LayoutWithMenu/LayoutWithMenu.tsx
+++ apps/store/src/components/LayoutWithMenu/LayoutWithMenu.tsx
@@ -1,7 +1,14 @@
+import { useApolloClient } from '@apollo/client'
 import styled from '@emotion/styled'
 import { useRouter } from 'next/router'
-import { ReactNode } from 'react'
+import { ReactNode, createContext, useMemo, useRef } from 'react'
 import { SiteFooter, SiteFooterProps } from '@/components/SiteFooter/SiteFooter'
+import { useCurrentLocale } from '@/lib/l10n/useCurrentLocale'
+import {
+  useShopSessionCreateMutation,
+  useShopSessionQuery as useShopSessionApolloQuery,
+} from '@/services/apollo/generated'
+import { setupShopSessionServiceClientSide } from '@/services/shopSession/ShopSession.helpers'
 import { TopMenu } from '../TopMenu/TopMenu'
 
 const Wrapper = styled.main({
@@ -22,13 +29,15 @@ export const LayoutWithMenu = ({ children }: LayoutWithMenuProps) => {
   }
 
   return (
-    <Wrapper>
-      <FixedHeader>
-        <TopMenu />
-      </FixedHeader>
-      {children}
-      <SiteFooter onChangeLocale={handleChangeLocale} />
-    </Wrapper>
+    <ShopSessionContextProvider>
+      <Wrapper>
+        <FixedHeader>
+          <TopMenu />
+        </FixedHeader>
+        {children}
+        <SiteFooter onChangeLocale={handleChangeLocale} />
+      </Wrapper>
+    </ShopSessionContextProvider>
   )
 }
 
@@ -38,3 +47,66 @@ const FixedHeader = styled.header({
   left: 0,
   right: 0,
 })
+
+function ShopSessionContextProvider({ children }: { children: ReactNode }) {
+  const { value } = useShopSessionContextProvider()
+  return <ShopSessionContext.Provider value={value}>{children}</ShopSessionContext.Provider>
+}
+
+// FIXME: Move to custom App component, extract to own module
+export const ShopSessionContext = createContext(null as any)
+
+function useShopSessionContextProvider() {
+  const resultRef = useRef({ called: false } as any)
+
+  const { countryCode } = useCurrentLocale()
+  const shopSessionService = useShopSessionService()
+
+  const [createShopSession] = useShopSessionCreateMutation({
+    variables: { countryCode },
+    onCompleted({ shopSessionCreate }) {
+      console.log('created', shopSessionCreate, resultRef.current.id)
+      // DEBUG: Comment for local development
+      // shopSessionService.save(shopSessionCreate)
+    },
+  })
+
+  const shopSessionId = shopSessionService.shopSessionId()
+
+  const queryResult = useShopSessionApolloQuery({
+    variables: shopSessionId ? { shopSessionId } : undefined,
+    skip: !shopSessionId,
+    onCompleted: ({ shopSession }: any) => {
+      if (shopSession.countryCode !== countryCode) {
+        createShopSession()
+      }
+    },
+  })
+
+  const contextValue = useMemo(
+    () => ({
+      // Lazy so that we don't create session until it's first needed
+      querySession() {
+        if (!resultRef.current.result) {
+          resultRef.current.result = queryResult
+          // FIXME: Re-create session on countryCode mismatch
+          if (!shopSessionService.shopSessionId()) {
+            setTimeout(() => createShopSession(), 0)
+          }
+        }
+        return resultRef.current.result
+      },
+    }),
+    [queryResult, createShopSession, shopSessionService],
+  )
+
+  Object.assign(resultRef.current, {
+    value: contextValue,
+  })
+  return resultRef.current
+}
+
+const useShopSessionService = () => {
+  const apolloClient = useApolloClient()
+  return setupShopSessionServiceClientSide(apolloClient)
+}
--- apps/store/src/services/shopSession/useShopSession.ts
+++ apps/store/src/services/shopSession/useShopSession.ts
@@ -1,61 +1,7 @@
-import { useApolloClient } from '@apollo/client'
-import { useMemo, useState } from 'react'
-import { useCurrentLocale } from '@/lib/l10n/useCurrentLocale'
-import {
-  ShopSessionQuery,
-  useShopSessionCreateMutation,
-  useShopSessionQuery as useShopSessionApolloQuery,
-} from '@/services/apollo/generated'
-import { setupShopSessionServiceClientSide } from '@/services/shopSession/ShopSession.helpers'
+import { useContext } from 'react'
+import { ShopSessionContext } from '@/components/LayoutWithMenu/LayoutWithMenu'
 
 export const useShopSession = () => {
-  const { countryCode } = useCurrentLocale()
-  const shopSessionService = useShopSessionService()
-
-  const [createShopSession] = useShopSessionCreateMutation({
-    variables: { countryCode },
-    onCompleted({ shopSessionCreate }) {
-      setShopSessionId(shopSessionCreate.id)
-      shopSessionService.save(shopSessionCreate)
-    },
-  })
-
-  const [shopSessionId, setShopSessionId] = useState(() => {
-    const shopSessionId = shopSessionService.shopSessionId()
-    if (!shopSessionId) createShopSession()
-    return shopSessionId
-  })
-
-  const result = useShopSessionQuery({
-    shopSessionId,
-    onCompleted: ({ shopSession }) => {
-      if (shopSession.countryCode !== countryCode) createShopSession()
-    },
-  })
-
-  return useMemo(() => {
-    const shopSessionCountryCode = result.data?.shopSession?.countryCode
-    return {
-      ...result,
-      data: shopSessionCountryCode !== countryCode ? undefined : result.data?.shopSession,
-    }
-  }, [result, countryCode])
-}
-
-const useShopSessionQuery = ({ shopSessionId, onCompleted }: UseShopSessionParams) => {
-  return useShopSessionApolloQuery({
-    variables: shopSessionId ? { shopSessionId } : undefined,
-    skip: !shopSessionId,
-    onCompleted,
-  })
-}
-
-type UseShopSessionParams = {
-  onCompleted: (data: ShopSessionQuery) => void
-  shopSessionId: string | null
-}
-
-const useShopSessionService = () => {
-  const apolloClient = useApolloClient()
-  return setupShopSessionServiceClientSide(apolloClient)
+  const { querySession } = useContext(ShopSessionContext)
+  return querySession()
 }
