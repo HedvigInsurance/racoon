a2d396a9f79ba07b86b0053a51027ce5101e8d88
Robin Andeer <robin.andeer@gmail.com>
2022-08-12 15:11:46 +0200

    GRW-1350 / Feature / Add ContentBlock for text (#387)
    Add Storyblok editor props to AccordionItemBlock.

---
+++ apps/store/src/blocks/ContentBlock.tsx
@@ -0,0 +1,28 @@
+import styled from '@emotion/styled'
+import { getStoryblokApi, Richtext, storyblokEditable } from '@storyblok/react'
+import { useMemo } from 'react'
+import { Space } from 'ui'
+import { SbBaseBlockProps } from '@/services/storyblok/storyblok'
+
+export type Props = SbBaseBlockProps<{
+  body: Richtext
+}>
+
+export const ContentBlock = ({ blok }: Props) => {
+  const contentHtml = useRichText(blok.body)
+  return (
+    <Wrapper {...storyblokEditable(blok)}>
+      <Space y={1} dangerouslySetInnerHTML={{ __html: contentHtml }} />
+    </Wrapper>
+  )
+}
+
+const Wrapper = styled.div(({ theme }) => ({
+  paddingLeft: theme.space[4],
+  paddingRight: theme.space[4],
+  textAlign: 'center',
+}))
+
+const useRichText = (body: Richtext) => {
+  return useMemo(() => getStoryblokApi().richTextResolver.render(body), [body])
+}
