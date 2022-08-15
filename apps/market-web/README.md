851fd1f3f38eb10252b5c3046415c8694d34b274
Gustav Eén <gustaveen@gmail.com>
2022-08-15 15:05:22 +0200

    Remove market-web from racoon

--- apps/market-web/README.md
+++
@@ -1,9 +0,0 @@
-## Getting Started
-
-First, run the development server:
-
-```bash
-yarn dev
-```
-
-Open [http://localhost:3001](http://localhost:3001) with your browser to see the result.
--- apps/market-web/src/helpers/textPosition.ts
+++
@@ -1,8 +0,0 @@
-export type TextPosition = 'left' | 'right' | 'center'
-export type CenterLeftTextPosition = 'center' | 'left'
-
-export const textFlexPositionMap = {
-  left: 'flex-start',
-  center: 'center',
-  right: 'flex-end',
-}
