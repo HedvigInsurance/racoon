ff40b22f7f570785a30e5d1ad4cfaa067b3bef48
Gustav Eén <gustaveen@gmail.com>
2022-08-16 10:16:03 +0200

    GRW-1357 / Remove market-web from racoon (#395)
    * Remove market-web from racoon
    
    * rerun CI

--- apps/market-web/src/components/icons/Plus.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import { Svg, SvgProps } from './Svg'
-
-export const Plus = (props: SvgProps) => (
-  <Svg viewBox="0 0 16 16" {...props}>
-    <path d="M8.5,0 L8.5,7.5 L16,7.5 L16,8.5 L8.5,8.5 L8.5,16 L7.5,16 L7.5,8.499 L0,8.5 L0,7.5 L7.5,7.499 L7.5,0 L8.5,0 Z"></path>
-  </Svg>
-)
