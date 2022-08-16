c47a9c57db1c8502971dadebda5b84b133fc35d7
Robin Andeer <robin.andeer@gmail.com>
2022-08-16 15:21:14 +0200

    GRW-1351 / Add Timeline component (#396)

---
+++ apps/store/src/components/Timeline/Timeline.tsx
@@ -0,0 +1,49 @@
+import styled from '@emotion/styled'
+import { CheckCircleIcon } from './CheckCircleIcon'
+
+const ICON_SIZE = '2.5rem'
+
+export const Root = styled.ul({
+  display: 'flex',
+  flexDirection: 'column',
+})
+
+export const Item = styled.li(({ theme }) => ({
+  position: 'relative',
+  display: 'flex',
+  alignItems: 'center',
+  gap: theme.space[2],
+}))
+
+export const Icon = () => {
+  return (
+    <IconWrapper>
+      <CheckCircleIcon size={ICON_SIZE} strokeWidth={1.5} />
+    </IconWrapper>
+  )
+}
+
+const IconWrapper = styled.div(({ theme }) => ({
+  paddingTop: theme.space[1],
+  paddingBottom: theme.space[1],
+}))
+
+export const Separator = styled.div({
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+  alignSelf: 'stretch',
+})
+
+export const Connector = styled.div(({ theme }) => ({
+  width: 2,
+  backgroundColor: theme.colors.gray900,
+  borderRadius: 1,
+  flexGrow: 1,
+}))
+
+export const Content = styled.div(({ theme }) => ({
+  flex: 1,
+  paddingTop: theme.space[3],
+  paddingBottom: theme.space[3],
+}))
