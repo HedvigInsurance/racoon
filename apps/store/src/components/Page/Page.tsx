1d457ef62a539ea4c0e2c20e14ad7c24e560800a
Gustav Eén <gustaveen@gmail.com>
2022-08-18 09:14:53 +0200

    GRW-1359 / Add global story (#407)
    * Refactor generic Page
    
    * Add globalStory to Page to be able fetch menus and other global content

---
+++ apps/store/src/components/Page/Page.tsx
@@ -0,0 +1,7 @@
+import { StoryblokComponent, useStoryblokState } from '@storyblok/react'
+import { StoryblokPageProps } from '@/services/storyblok/storyblok'
+
+export const Page = ({ story: initialStory }: StoryblokPageProps) => {
+  const story = useStoryblokState(initialStory)
+  return <StoryblokComponent blok={story.content} />
+}
