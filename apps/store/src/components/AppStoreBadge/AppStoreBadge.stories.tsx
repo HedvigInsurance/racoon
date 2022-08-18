730627231e43397599fbfa19d626a712bece9522
Robin Andeer <robin.andeer@gmail.com>
2022-08-18 10:29:29 +0200

    Add App Store badges

---
+++ apps/store/src/components/AppStoreBadge/AppStoreBadge.stories.tsx
@@ -0,0 +1,25 @@
+import { ComponentMeta, ComponentStory } from '@storybook/react'
+import { Locale } from '@/lib/l10n/types'
+import { AppStoreBadge } from './AppStoreBadge'
+
+export default {
+  title: 'AppStoreBadge',
+  component: AppStoreBadge,
+  args: {},
+} as ComponentMeta<typeof AppStoreBadge>
+
+const Template: ComponentStory<typeof AppStoreBadge> = (args) => {
+  return <AppStoreBadge {...args} />
+}
+
+export const Apple = Template.bind({})
+Apple.args = {
+  type: 'apple',
+  locale: Locale.SvSe,
+}
+
+export const Google = Template.bind({})
+Google.args = {
+  type: 'google',
+  locale: Locale.SvSe,
+}
