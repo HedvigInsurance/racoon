7764daab3afe8ce0e3e001871d3ebd3d0f5291da
Robin Andeer <robin.andeer@gmail.com>
2022-08-24 16:40:51 +0200

    Tweak Cart Page design (#430)
    Add Dialog base component.
    
    Refactor CartNotification.
    
    Update CartPage to match figma design.

--- apps/store/src/components/Dialog/Dialog.tsx
+++ apps/store/src/components/Dialog/Dialog.tsx
@@ -1,7 +1,6 @@
 import { keyframes } from '@emotion/react'
 import styled from '@emotion/styled'
 import * as DialogPrimitive from '@radix-ui/react-dialog'
-export { Header } from './Header'
 
 const overlayShow = keyframes({
   '0%': { opacity: 0 },
@@ -22,35 +21,36 @@ const StyledOverlay = styled(DialogPrimitive.Overlay)({
   },
 })
 
-const StyledContent = styled(DialogPrimitive.Content)(({ theme }) => ({
+export const Window = styled.div(({ theme }) => ({
   backgroundColor: theme.colors.white,
   boxShadow: 'hsl(206 22% 7% / 35%) 0px 10px 38px -10px, hsl(206 22% 7% / 20%) 0px 10px 20px -15px',
-  position: 'fixed',
-  top: 0,
-  left: 0,
-  right: 0,
-  width: '100vw',
   '@media (prefers-reduced-motion: no-preference)': {
     animation: `${contentShow} 150ms cubic-bezier(0.16, 1, 0.3, 1) forwards`,
   },
 }))
 
+const StyledContentWrapper = styled.div({
+  position: 'fixed',
+  inset: 0,
+})
+
 type ContentProps = {
   children: React.ReactNode
-  Title: React.ReactNode
   onClose?: () => void
+  className?: string
 }
 
-export const Content = ({ children, Title, onClose }: ContentProps) => {
+export const Content = ({ children, onClose, className }: ContentProps) => {
   const handleClose = () => onClose?.()
 
   return (
     <DialogPrimitive.Portal>
       <StyledOverlay />
-      <StyledContent onEscapeKeyDown={handleClose} onInteractOutside={handleClose}>
-        <DialogPrimitive.Title asChild>{Title}</DialogPrimitive.Title>
-        <DialogPrimitive.Description asChild>{children}</DialogPrimitive.Description>
-      </StyledContent>
+      <StyledContentWrapper className={className}>
+        <DialogPrimitive.Content onEscapeKeyDown={handleClose} onInteractOutside={handleClose}>
+          {children}
+        </DialogPrimitive.Content>
+      </StyledContentWrapper>
     </DialogPrimitive.Portal>
   )
 }
