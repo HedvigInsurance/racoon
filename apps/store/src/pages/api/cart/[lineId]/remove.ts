abc690d0be1bf4eff243cb61c0afe6642b19dc88
Robin Andeer <robin.andeer@gmail.com>
2022-08-25 11:46:01 +0200

    Implement pending UI for remove from cart
    Add useForm hook to override default form submit behviour.
    Setup basic state in CartCard for pending UI.
    Skip Dialog.Close since it requires button to forward ref.
    Update remove line item API route to handle errors.

--- apps/store/src/pages/api/cart/[lineId]/remove.ts
+++ apps/store/src/pages/api/cart/[lineId]/remove.ts
@@ -3,18 +3,35 @@ import { PageLink } from '@/lib/PageLink'
 import { initializeApollo } from '@/services/apollo/client'
 import { cartServiceInit } from '@/services/cart/CartService'
 import { getCurrentShopSessionServerSide } from '@/services/shopSession/ShopSession.helpers'
+import { ShopSession } from '@/services/shopSession/ShopSession.types'
 
 export const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   const { lineId } = req.query
+
   if (typeof lineId !== 'string') {
     return res.status(500).json({ message: 'Line ID is required' })
   }
 
   const apolloClient = initializeApollo()
-  const shopSession = await getCurrentShopSessionServerSide({ req, res, apolloClient })
+
+  let shopSession: ShopSession
+  try {
+    shopSession = await getCurrentShopSessionServerSide({ req, res, apolloClient })
+  } catch (error) {
+    console.error('Failed to setup ShopSession')
+    console.error(error)
+    return res.redirect(PageLink.cart())
+  }
+
   const cartService = cartServiceInit({ shopSession, apolloClient })
 
-  await cartService.lineRemove(lineId)
+  try {
+    await cartService.lineRemove(lineId)
+  } catch (error) {
+    console.warn('Failed to remove cart line item: %s', lineId)
+    console.warn(error)
+    return res.redirect(PageLink.cart())
+  }
 
   return res.redirect(302, PageLink.cart())
 }
