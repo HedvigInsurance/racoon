c492b90e580df390d97d18fbc1593df697a5ccd0
Alexander Lebedev <alexander.lebedev@gmail.com>
2022-08-31 14:31:55 +0200

    Safely handle empty string as header variant

--- packages/ui/src/components/HeadingNew/HeadingNew.tsx
+++ packages/ui/src/components/HeadingNew/HeadingNew.tsx
@@ -8,21 +8,23 @@ export type HeadingProps = Margins & {
   as: 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6'
   color?: 'dark' | 'light'
   children: React.ReactNode
-  variant?: HeadingVariant
+  variant?: HeadingVariant | ''
 }
 
 type HeadingBaseProps = Pick<HeadingProps, 'color' | 'variant'> & Margins
 
-const HeadingBase = styled.h2<HeadingBaseProps>(
-  ({ theme, color, variant = 'standard.32', ...props }) => ({
+const HeadingBase = styled.h2<HeadingBaseProps>(({ theme, color, variant, ...props }) => {
+  // GOTCHA: We may get empty string from Storyblok, this should be handled safely
+  variant = variant || 'standard.32'
+  return {
     color: color ? getColor(color) : 'currentColor',
     fontFamily: theme.fonts.heading,
     fontWeight: 400,
     lineHeight: 1.2,
     ...getMargins(props),
     ...getHeadingVariant(variant, theme),
-  }),
-)
+  }
+})
 
 export const Heading = ({ as, color, children, variant, ...rest }: HeadingProps) => {
   return (
