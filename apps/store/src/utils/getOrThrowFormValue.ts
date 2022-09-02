4537da4d21c00c32394351593d2c3d2cf8a97ed6
Robin Andeer <robin.andeer@gmail.com>
2022-09-02 10:12:38 +0200

    Implement API for updating contact details (#453)
    Refactor CheckoutPage to separate based on payments flow.
    
    Add new to handle form to update contact details.
    
    Update ShopSession query to include checkout information.
    
    Refactor getOrThrowFormValue to not use formidable dependency.

--- apps/store/src/utils/getOrThrowFormValue.ts
+++ apps/store/src/utils/getOrThrowFormValue.ts
@@ -1,7 +1,5 @@
-import type { Fields } from 'formidable'
-
-export const getOrThrowFormValue = (formData: Fields, field: string) => {
-  const value = formData[field]
+export const getOrThrowFormValue = (formData: FormData, field: string) => {
+  const value = formData.get(field)
   if (typeof value !== 'string') {
     throw new Error(`Missing form value for ${field}`)
   }
