f85528786cf4f21ad7d72223c06ae0e14afbe9fe
Karnell Schultz <50870173+KarnellSchultz@users.noreply.github.com>
2022-09-06 12:08:45 +0200

    fix: country selector page to use new countries

--- apps/store/src/components/CountrySelectorPage/CountrySelectorPage.tsx
+++ apps/store/src/components/CountrySelectorPage/CountrySelectorPage.tsx
@@ -1,9 +1,9 @@
 import styled from '@emotion/styled'
 import Link from 'next/link'
 import { LinkButton } from 'ui'
-import { Locale } from '@/lib/l10n/types'
+import { countries } from '@/lib/l10n/countries'
+import { TEMP_TRANSLATIONS } from '@/lib/l10n/locales'
 import { PageLink } from '@/lib/PageLink'
-import { ObjectKeys } from './CountrySelectorPage.helpers'
 
 type CountrySelectorPageProps = any
 
@@ -20,38 +20,22 @@ const CountryOptionContainer = styled.div({
   gap: 5,
 })
 
-const SupportedMarkets = {
-  Sweden: 'Sweden',
-  Norway: 'Norway',
-  Denmark: 'Denmark',
-} as const
-
-const MarketsMap = new Map([
-  [SupportedMarkets.Denmark, Locale.DaDk],
-  [SupportedMarkets.Norway, Locale.NbNo],
-  [SupportedMarkets.Sweden, Locale.SvSe],
-])
-
-type SupportedMarketsKeys = keyof typeof SupportedMarkets
-
-const getLocaleFromMarket = (market: SupportedMarketsKeys) => MarketsMap.get(market)
-
 export const CountrySelectorPage = (props: CountrySelectorPageProps) => {
   return (
     <Container {...props}>
       <h1>Select Country</h1>
       <CountryOptionContainer>
-        {ObjectKeys(SupportedMarkets).map((market) => {
-          return (
-            <Link
-              key={market}
-              href={PageLink.store({ locale: getLocaleFromMarket(market) })}
-              passHref
-            >
-              <LinkButton>{market}</LinkButton>
-            </Link>
-          )
-        })}
+        {Object.keys(countries).map((country) => (
+          <Link
+            key={country}
+            href={PageLink.store({
+              locale: countries[country as keyof typeof countries].defaultLocale,
+            })}
+            passHref
+          >
+            <LinkButton>{TEMP_TRANSLATIONS[`COUNTRY_LABEL_${country}`]}</LinkButton>
+          </Link>
+        ))}
       </CountryOptionContainer>
     </Container>
   )
