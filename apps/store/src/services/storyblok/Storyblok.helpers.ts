d726421f8bf021d8b0d05f72004420ead42fa737
Robin Andeer <robin.andeer@gmail.com>
2022-09-06 16:14:28 +0200

    Refactor TopMenuBlock to fix render issues (#483)
    Refactor TopMenuBlock to fix render issues
    
    Conditionally render NestedNavContainer vs. NavItem blocks.
    
    Remove React state in favor of CSS styles.
    
    Close top menu after route change is complete.
    
    Fix linking with Storyblok LinkField.

--- apps/store/src/services/storyblok/Storyblok.helpers.ts
+++ apps/store/src/services/storyblok/Storyblok.helpers.ts
@@ -1,4 +1,5 @@
 import { SbBlokData } from '@storyblok/react'
+import { LinkField } from './storyblok'
 
 export const filterByBlockType = <BlockData extends SbBlokData>(
   blocks: BlockData[] = [],
@@ -6,13 +7,26 @@ export const filterByBlockType = <BlockData extends SbBlokData>(
 ): BlockData[] => {
   const result: BlockData[] = []
   for (const block of blocks) {
-    if (block.component !== targetType) {
+    const blockOfType = checkBlockType(block, targetType)
+    if (blockOfType === null) {
       if (process.env.NODE_ENV !== 'production') {
-        throw new Error(`Found blok of type ${block.component}.  Only ${targetType} expected`)
+        throw new Error(`Found blok of type ${block.component}. Only ${targetType} expected`)
       }
     } else {
       result.push(block)
     }
   }
   return result
+}
+
+export const checkBlockType = <BlockData extends SbBlokData>(
+  block: SbBlokData,
+  targetType: string,
+): BlockData | null => {
+  if (block.component === targetType) return block as BlockData
+  else return null
+}
+
+export const getLinkFieldURL = (link: LinkField) => {
+  return link.cached_url === 'home' ? '/' : `/${link.cached_url}`
 }
