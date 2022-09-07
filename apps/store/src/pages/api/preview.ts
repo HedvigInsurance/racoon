d940e3f75594502ab04dc524c433a224c0a4db2b
Alexander Lebedev <alexander.lebedev@gmail.com>
2022-09-07 14:23:31 +0200

    Construct correct URLs for preview mode depending on country and locale

--- apps/store/src/pages/api/preview.ts
+++ apps/store/src/pages/api/preview.ts
@@ -1,19 +1,33 @@
 import { NextApiRequest, NextApiResponse } from 'next'
+import StoryblokClient from 'storyblok-js-client'
+import { countries } from '@/lib/l10n/countries'
+import { CountryLabel } from '@/lib/l10n/types'
 
 const preview = async (req: NextApiRequest, res: NextApiResponse) => {
-  const { slug = '' } = req.query
-  // get the storyblok params for the bridge to work
-  const params = req.url?.split('?') ?? []
-
   // Check the secret and next parameters
   // This secret should only be known to this API route and the CMS
   if (req.query.secret !== process.env.NEXT_PUBLIC_STORYBLOK_PREVIEW_SECRET) {
     return res.status(401).json({ message: 'Invalid token' })
   }
 
-  // Enable Preview Mode by setting the cookies
-  res.setPreviewData({})
+  const pageId = req.query._storyblok
+  const storyblokClient = new StoryblokClient({
+    accessToken: process.env.NEXT_PUBLIC_STORYBLOK_ACCESS_TOKEN,
+  })
+  const {
+    data: { story },
+  } = await storyblokClient.get(`cdn/stories/${pageId}`, { version: 'draft' })
 
+  const [countryLabel, ...slugFragments] = story.full_slug.split('/')
+
+  // Convert default => English if applicable
+  let locale = req.query._storyblok_lang
+  if (locale === 'default') {
+    const country = countries[countryLabel as CountryLabel]
+    locale = country.locales.find((locale) => locale.startsWith('en')) ?? country.defaultLocale
+  }
+
+  res.setPreviewData({})
   // Set cookie to None, so it can be read in the Storyblok iframe
   const cookies = res.getHeader('Set-Cookie') ?? []
   if (typeof cookies === 'object') {
@@ -22,9 +36,9 @@ const preview = async (req: NextApiRequest, res: NextApiResponse) => {
       cookies.map((cookie) => cookie.replace('SameSite=Lax', 'SameSite=None;Secure')),
     )
   }
-
-  // Redirect to the path from entry
-  res.redirect(`/${slug}?${params[1]}`)
+  const targetSlug = `/${locale}/${slugFragments.join('/')}`
+  console.debug(`Previewing ${targetSlug}`)
+  res.redirect(targetSlug)
 }
 
 export default preview
