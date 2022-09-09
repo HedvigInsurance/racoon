98203bb9fdc3d173e2ef20dfe8981db77072f462
Alexander Lebedev <alexander.lebedev@gmail.com>
2022-09-09 14:16:36 +0200

    Add why-did-you-render in development mode to debug render efficiency

--- apps/store/next.config.js
+++ apps/store/next.config.js
@@ -1,3 +1,4 @@
+const path = require('path')
 const { i18n } = require('./next-i18next.config')
 
 /** @type {import('next').NextConfig} */
@@ -11,6 +12,21 @@ const nextConfig = {
   },
   productionBrowserSourceMaps: true,
   i18n,
+  webpack(config, { dev, isServer }) {
+    if (dev && !isServer) {
+      const originalEntry = config.entry
+      config.entry = async () => {
+        const wdrPath = path.resolve(__dirname, './src/utils/whyDidYouRender.ts')
+        const entries = await originalEntry()
+        if (entries['main.js'] && !entries['main.js'].includes(wdrPath)) {
+          entries['main.js'].push(wdrPath)
+        }
+        return entries
+      }
+    }
+
+    return config
+  },
 }
 
 module.exports = nextConfig
