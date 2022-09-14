4c49162cfcec1fa8aa676384bdb0f229e4672a10
Gustav Eén <gustaveen@gmail.com>
2022-09-14 14:18:46 +0200

    Enable Storybook Composition

--- apps/store/.storybook/main.js
+++ apps/store/.storybook/main.js
@@ -2,6 +2,9 @@ module.exports = {
   stories: ['../src/**/*.stories.@(ts|tsx)'],
   addons: ['@storybook/addon-links', '@storybook/addon-essentials', 'storybook-addon-next'],
   framework: '@storybook/react',
+  features: {
+    buildStoriesJson: true,
+  },
   core: {
     builder: 'webpack5',
   },
--- packages/ui/.storybook/main.js
+++ packages/ui/.storybook/main.js
@@ -2,6 +2,15 @@ module.exports = {
   stories: ['../src/**/*.stories.@(ts|tsx)'],
   addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
   framework: '@storybook/react',
+  features: {
+    buildStoriesJson: true,
+  },
+  refs: {
+    store: {
+      title: 'Hedvig.com',
+      url: 'http://localhost:6008',
+    },
+  },
   core: {
     builder: 'webpack5',
   },
