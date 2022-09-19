f15dd5ca862cbfbcef6b6c1237e41a94a39833c0
Robin Andeer <robin.andeer@gmail.com>
2022-09-19 16:56:06 +0200

    Add form template for SE_ACCIDENT

--- apps/store/src/services/formTemplate/FormTemplateService.ts
+++ apps/store/src/services/formTemplate/FormTemplateService.ts
@@ -4,6 +4,8 @@ import NO_HOME_CONTENT_SCHEMA from './data/NO_HOME_CONTENT.json'
 import { NO_HOME_CONTENT_UI } from './data/NO_HOME_CONTENT_UI'
 import NO_TRAVEL from './data/NO_TRAVEL.json'
 import { NO_TRAVEL_UI } from './data/NO_TRAVEL_UI'
+import SE_ACCIDENT_SCHEMA from './data/SE_ACCIDENT.json'
+import { SE_ACCIDENT_UI } from './data/SE_ACCIDENT_UI'
 import SWEDISH_APARTMENT_SCHEMA from './data/SWEDISH_APARTMENT.json'
 import { SWEDISH_APARTMENT_UI } from './data/SWEDISH_APARTMENT_UI'
 import { FormTemplate, FormTemplateUISchema } from './FormTemplate.types'
@@ -11,12 +13,15 @@ import { FormTemplate, FormTemplateUISchema } from './FormTemplate.types'
 type Schema = JSONSchemaType<Record<string, unknown>>
 
 const SCHEMA: Record<string, Schema> = {
+  // TODO: investigate why we need explicit casting
   SWEDISH_APARTMENT: SWEDISH_APARTMENT_SCHEMA as unknown as Schema,
+  SE_ACCIDENT: SE_ACCIDENT_SCHEMA as unknown as Schema,
   NO_HOME_CONTENT: NO_HOME_CONTENT_SCHEMA as unknown as Schema,
   NO_TRAVEL: NO_TRAVEL as unknown as Schema,
 }
 const UI_SCHEMA: Record<string, FormTemplateUISchema> = {
   SWEDISH_APARTMENT: SWEDISH_APARTMENT_UI,
+  SE_ACCIDENT: SE_ACCIDENT_UI,
   NO_HOME_CONTENT: NO_HOME_CONTENT_UI,
   NO_TRAVEL: NO_TRAVEL_UI,
 }
