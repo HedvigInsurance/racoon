e43ab84be8f1d933272c9009e811dc35308ce467
Karnell Schultz <50870173+KarnellSchultz@users.noreply.github.com>
2022-09-22 10:39:00 +0200

    fix: add wrap prop to SpaceFlex comp

--- apps/store/src/components/SpaceFlex/SpaceFlex.tsx
+++ apps/store/src/components/SpaceFlex/SpaceFlex.tsx
@@ -6,18 +6,20 @@ type SpaceFlexProps = {
   space?: number
   direction?: 'horizontal' | 'vertical'
   align?: 'start' | 'center' | 'end'
+  wrap?: boolean
 }
 
-const SpaceFlexWrapper = styled.div(({ space, direction, align }: SpaceFlexProps) => ({
+const SpaceFlexWrapper = styled.div(({ space, direction, align, wrap }: SpaceFlexProps) => ({
   display: 'flex',
+  flexWrap: wrap ? 'wrap' : 'initial',
   gap: `${space}rem`,
   flexDirection: direction === 'horizontal' ? 'row' : 'column',
   alignItems: align,
 }))
 
 export const SpaceFlex = forwardRef<HTMLDivElement, SpaceFlexProps>(
-  ({ children, space = 1, direction = 'horizontal', align = 'start' }, ref) => (
-    <SpaceFlexWrapper ref={ref} space={space} direction={direction} align={align}>
+  ({ children, space = 1, direction = 'horizontal', align = 'start', wrap = false }, ref) => (
+    <SpaceFlexWrapper ref={ref} space={space} direction={direction} align={align} wrap={wrap}>
       {children}
     </SpaceFlexWrapper>
   ),
