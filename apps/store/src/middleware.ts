abf311cfa0c0eda64fc1771eedf36711e4c6a8b6
Karnell Schultz <50870173+KarnellSchultz@users.noreply.github.com>
2022-09-22 09:03:53 +0200

    fix routing url rewrite in middleware/ add logging

--- apps/store/src/middleware.ts
+++ apps/store/src/middleware.ts
@@ -1,6 +1,7 @@
 import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
 import { countries } from '@/lib/l10n/countries'
+import { routingLocale } from '@/lib/l10n/locales'
 
 export const config = {
   matcher: '/',
@@ -11,17 +12,19 @@ export function middleware(req: NextRequest) {
 
   switch (country) {
     case countries.NO.id:
-      req.nextUrl.pathname = countries.NO.defaultLocale
+      req.nextUrl.pathname = routingLocale(countries.NO.defaultLocale)
       break
     case countries.DK.id:
-      req.nextUrl.pathname = countries.DK.defaultLocale
+      req.nextUrl.pathname = routingLocale(countries.DK.defaultLocale)
       break
     case countries.SE.id:
-      req.nextUrl.pathname = countries.SE.defaultLocale
+      req.nextUrl.pathname = routingLocale(countries.SE.defaultLocale)
       break
     default:
       req.nextUrl.pathname = '/country-selector'
   }
+
+  console.info(`Routing visitor from ${country} to ${req.nextUrl}`)
 
   return NextResponse.rewrite(req.nextUrl)
 }
