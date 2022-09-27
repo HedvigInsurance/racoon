d23ee7edfbdb387017a1303c0523770a4a95e636
Robin Andeer <robin.andeer@gmail.com>
2022-09-27 10:44:22 +0200

    Add form template for SE accident (#593)
    <!--
    PR title: GRW-123 / Feature / Awesome new thing
    -->
    
    ## Describe your changes
    
    See title.
    
    ![Screen Shot 2022-09-26 at 16.37.07.png](https://graphite-user-uploaded-assets-prod.s3.amazonaws.com/OgXegTwxM9IeuXHZyw5h/4b36fbeb-8677-450b-937d-51d67a128541/Screen%20Shot%202022-09-26%20at%2016.37.07.png)
    
    <!--
    What changes are made?
    If there are many changes, a list might be a good format.
    If it makes sense, add screenshots and/or screen recordings here.
    -->
    
    ## Justify why they are needed
    
    ## Jira issue(s): []
    
    <!--
    If there is a Jira issue, add the key (e.g. GRW-123) between the brackets.
    A link to that issue will automatically be created.
    -->
    
    ## Checklist before requesting a review
    
    - [ ] I have performed a self-review of my code

---
+++ apps/store/src/services/PriceForm/data/SE_ACCIDENT.ts
@@ -0,0 +1,77 @@
+import { Template } from '../PriceForm.types'
+
+export const SE_ACCIDENT: Template = {
+  sections: [
+    {
+      id: 'your-info',
+      title: { key: 'Your info' },
+      submitLabel: { key: 'Next step' },
+      items: [
+        {
+          field: {
+            type: 'text',
+            name: 'ssn',
+            label: { key: 'Personal number' },
+            required: true,
+          },
+          layout: { columnSpan: 6 },
+        },
+      ],
+    },
+    {
+      id: 'your-home',
+      title: { key: 'Your home' },
+      submitLabel: { key: 'Next step' },
+      items: [
+        {
+          field: {
+            type: 'text',
+            name: 'street',
+            label: { key: 'Address' },
+            required: true,
+          },
+          layout: { columnSpan: 6 },
+        },
+        {
+          field: {
+            type: 'text',
+            name: 'zipCode',
+            label: { key: 'Postal code' },
+            minLength: 5,
+            maxLength: 5,
+            required: true,
+          },
+          layout: { columnSpan: 3 },
+        },
+        {
+          field: {
+            type: 'number',
+            name: 'livingSpace',
+            label: { key: 'Apartment size' },
+            required: true,
+            min: 0,
+          },
+          layout: { columnSpan: 3 },
+        },
+      ],
+    },
+    {
+      id: 'insured-people',
+      title: { key: 'Insured people' },
+      submitLabel: { key: 'Calculate price' },
+      items: [
+        {
+          field: {
+            type: 'number',
+            min: 0,
+            max: 5,
+            name: 'numberCoInsured',
+            label: { key: 'Number of co-insured' },
+            required: true,
+          },
+          layout: { columnSpan: 6 },
+        },
+      ],
+    },
+  ],
+}
