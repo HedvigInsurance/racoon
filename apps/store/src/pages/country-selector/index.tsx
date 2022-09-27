68f8250269a9c7aea46f64b5818d6617bf6d69be
Alexander Lebedev <alexander.lebedev@gmail.com>
2022-09-27 07:51:10 +0200

    Fix locale handling in country-selector page;  Remove layout, it's useless there

--- apps/store/src/pages/country-selector/index.tsx
+++ apps/store/src/pages/country-selector/index.tsx
@@ -1,14 +1,14 @@
 import type { GetServerSideProps, NextPageWithLayout } from 'next'
 import { CountrySelectorPage } from '@/components/CountrySelectorPage/CountrySelectorPage'
-import { LayoutWithMenu } from '@/components/LayoutWithMenu/LayoutWithMenu'
-import { Locale } from '@/lib/l10n/types'
+import { FALLBACK_LOCALE, routingLocale } from '@/lib/l10n/locales'
 import { getGlobalStory, StoryblokPageProps } from '@/services/storyblok/storyblok'
 
-export const getServerSideProps: GetServerSideProps = async ({ locale = Locale.EnSe }) => {
-  // Can be removed when we handle default locals
-  const getLocation = () => (locale === 'default' ? Locale.EnSe : locale)
+export const getServerSideProps: GetServerSideProps = async ({ locale }) => {
+  if (!locale || locale === 'default') {
+    locale = routingLocale(FALLBACK_LOCALE)
+  }
 
-  const [globalStory] = await Promise.all([getGlobalStory({ locale: getLocation() })])
+  const [globalStory] = await Promise.all([getGlobalStory({ locale })])
 
   return {
     props: {
@@ -20,7 +20,5 @@ export const getServerSideProps: GetServerSideProps = async ({ locale = Locale.E
 const NextCountrySelectorPage: NextPageWithLayout<StoryblokPageProps> = (props) => {
   return <CountrySelectorPage {...props} />
 }
-
-NextCountrySelectorPage.getLayout = (children) => <LayoutWithMenu>{children}</LayoutWithMenu>
 
 export default NextCountrySelectorPage
