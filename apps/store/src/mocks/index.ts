d9b895d28b3bd14a56ac7b07b586f7f74433576a
Robin Andeer <robin.andeer@gmail.com>
2022-09-28 09:42:40 +0200

    Refactor MSW setup to avoid require

--- apps/store/src/mocks/index.ts
+++ apps/store/src/mocks/index.ts
@@ -1,8 +1,13 @@
-if (typeof window === 'undefined') {
-  const { server } = require('./server')
-  server.listen({ onUnhandledRequest: 'bypass' })
-} else {
-  const { worker } = require('./browser')
-  worker.start({ onUnhandledRequest: 'bypass' })
+const initMocks = async () => {
+  if (typeof window === 'undefined') {
+    const { server } = await import('./server')
+    server.listen({ onUnhandledRequest: 'bypass' })
+  } else {
+    const { worker } = await import('./browser')
+    worker.start({ onUnhandledRequest: 'bypass' })
+  }
 }
+
+initMocks()
+
 export {}
