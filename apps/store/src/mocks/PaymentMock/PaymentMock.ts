4c57a0aeab6c8df3c081ffb7617342c1d95312b8
Guilherme Popolin <guilherme@hedvig.com>
2022-09-30 12:11:01 +0200

    refact: mocks

--- apps/store/src/mocks/PaymentMock/PaymentMock.ts
+++ apps/store/src/mocks/PaymentMock/PaymentMock.ts
@@ -1,4 +1,5 @@
 import { graphql } from 'msw'
+import { PaymentProvidersQuery, PaymentProvidersQueryVariables } from '@/services/apollo/generated'
 import { graphqlConstants } from '../helpers'
 import { getConstants } from './PaymentMock.constants'
 
@@ -7,61 +8,64 @@ const { PAYMENT_PROVIDERS } = getConstants()
 const api = graphql.link(graphqlConstants().GRAPHQL_ENDPOINT)
 
 export const mockPaymentHandlers = [
-  api.query(PAYMENT_PROVIDERS, (_, res, ctx) => {
-    return res(
-      ctx.data({
-        paymentProviders: [
-          {
-            __typename: 'PaymentProviderAdyen',
-            availablePaymentMethods: {
-              paymentMethods: [
-                {
-                  brands: ['visadankort', 'mc', 'visa'],
-                  details: [
-                    {
-                      key: 'encryptedCardNumber',
-                      type: 'cardToken',
-                    },
-                    {
-                      key: 'encryptedSecurityCode',
-                      type: 'cardToken',
-                    },
-                    {
-                      key: 'encryptedExpiryMonth',
-                      type: 'cardToken',
-                    },
-                    {
-                      key: 'encryptedExpiryYear',
-                      type: 'cardToken',
-                    },
-                    {
-                      key: 'holderName',
-                      optional: true,
-                      type: 'text',
-                    },
-                  ],
-                  name: 'Credit Card',
-                  type: 'scheme',
-                },
-                {
-                  configuration: {
-                    merchantId: '50',
-                    gatewayMerchantId: 'HEDVIG-DK',
+  api.query<PaymentProvidersQuery, PaymentProvidersQueryVariables>(
+    PAYMENT_PROVIDERS,
+    (_, res, ctx) => {
+      return res(
+        ctx.data({
+          paymentProviders: [
+            {
+              __typename: 'PaymentProviderAdyen',
+              availablePaymentMethods: {
+                paymentMethods: [
+                  {
+                    brands: ['visadankort', 'mc', 'visa'],
+                    details: [
+                      {
+                        key: 'encryptedCardNumber',
+                        type: 'cardToken',
+                      },
+                      {
+                        key: 'encryptedSecurityCode',
+                        type: 'cardToken',
+                      },
+                      {
+                        key: 'encryptedExpiryMonth',
+                        type: 'cardToken',
+                      },
+                      {
+                        key: 'encryptedExpiryYear',
+                        type: 'cardToken',
+                      },
+                      {
+                        key: 'holderName',
+                        optional: true,
+                        type: 'text',
+                      },
+                    ],
+                    name: 'Credit Card',
+                    type: 'scheme',
                   },
-                  details: [
-                    {
-                      key: 'paywithgoogle.token',
-                      type: 'payWithGoogleToken',
+                  {
+                    configuration: {
+                      merchantId: '50',
+                      gatewayMerchantId: 'HEDVIG-DK',
                     },
-                  ],
-                  name: 'Google Pay',
-                  type: 'paywithgoogle',
-                },
-              ],
+                    details: [
+                      {
+                        key: 'paywithgoogle.token',
+                        type: 'payWithGoogleToken',
+                      },
+                    ],
+                    name: 'Google Pay',
+                    type: 'paywithgoogle',
+                  },
+                ],
+              },
             },
-          },
-        ],
-      }),
-    )
-  }),
+          ],
+        }),
+      )
+    },
+  ),
 ]
