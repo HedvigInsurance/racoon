485f747664c0c8dd3258d863e420d964da679414
Robin Andeer <robin.andeer@gmail.com>
2022-10-04 12:40:22 +0200

    Integrate checkout signing API (#613)
    <!--
    PR title: GRW-123 / Feature / Awesome new thing
    -->
    
    ## Describe your changes
    
    Integrate Checkout signing API on checkout / contact details page.
    
    Move form logic inside checkout details pages.
    
    Keep track of started "checkout signing" in cookie (key = checkoutId).
    
    Flow: update contact details -> start sign -> poll for updates to checkout signing.
    
    Still to be done:
    
    - store the access token in a cookie after it's generated
    - update cookie settings to be secure
    
    <!--
    What changes are made?
    If there are many changes, a list might be a good format.
    If it makes sense, add screenshots and/or screen recordings here.
    -->
    
    ## Justify why they are needed
    
    Needed to support the "after sign" payment flow.
    
    We stored the checkout signing ID in a cookie in case the user reloads the page while a signing is ongoing.
    
    ## Jira issue(s): [GRW-1562]
    
    <!--
    If there is a Jira issue, add the key (e.g. GRW-123) between the brackets.
    A link to that issue will automatically be created.
    -->
    
    ## Checklist before requesting a review
    
    - [ ] I have performed a self-review of my code
    
    
    [GRW-1562]: https://hedvig.atlassian.net/browse/GRW-1562?atlOrigin=eyJpIjoiNWRkNTljNzYxNjVmNDY3MDlhMDU5Y2ZhYzA5YTRkZjUiLCJwIjoiZ2l0aHViLWNvbS1KU1cifQ

---
+++ apps/store/src/components/CheckoutContactDetailsPage/CheckoutContactDetailsPage.helpers.ts
@@ -0,0 +1,39 @@
+import { ApolloClient } from '@apollo/client'
+import { GetServerSidePropsContext } from 'next'
+import {
+  CheckoutSigningDocument,
+  CheckoutSigningQuery,
+  CheckoutSigningQueryVariables,
+} from '@/services/apollo/generated'
+
+type FetchParams = CheckoutSigningQueryVariables & {
+  apolloClient: ApolloClient<unknown>
+}
+
+const fetchCheckoutSigning = async ({ apolloClient, ...variables }: FetchParams) => {
+  const result = await apolloClient.query<CheckoutSigningQuery, CheckoutSigningQueryVariables>({
+    query: CheckoutSigningDocument,
+    variables,
+  })
+
+  return result.data.checkoutSigning
+}
+
+type FetchCurrentParams = Omit<FetchParams, 'checkoutSigningId'> & {
+  req: GetServerSidePropsContext['req']
+  checkoutId: string
+}
+
+export const fetchCurrentCheckoutSigning = async ({
+  req,
+  checkoutId,
+  ...params
+}: FetchCurrentParams) => {
+  const checkoutSigningId = req.cookies[checkoutId]
+
+  if (checkoutSigningId) {
+    return await fetchCheckoutSigning({ ...params, checkoutSigningId })
+  }
+
+  return null
+}
