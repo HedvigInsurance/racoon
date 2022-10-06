1d98cf45b7235bd70335aa2a83dcf29f33c0579f
Robin Andeer <robin.andeer@gmail.com>
2022-10-06 14:03:26 +0200

    Redirect to web onboarding to connect payment after sign

---
+++ apps/store/src/services/WebOnboarding/WebOnboarding.helpers.test.ts
@@ -0,0 +1,14 @@
+import { RoutingLocale } from '@/lib/l10n/types'
+import { convertRoutingLocale } from './WebOnboarding.helpers'
+
+describe('convertRoutingLocale', () => {
+  it.each<[RoutingLocale, string]>([
+    ['se', 'se'],
+    ['en-se', 'se-en'],
+    ['dk', 'dk'],
+    ['en-no', 'no-en'],
+    ['da-dk', 'dk'],
+  ])('asserts %p expecting %p', (locale, expected) => {
+    expect(convertRoutingLocale(locale)).toBe(expected)
+  })
+})
