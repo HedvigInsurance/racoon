5d63cbe54c077e7ad76f67b80819eca871cbd870
Robin Andeer <robin.andeer@gmail.com>
2022-10-07 11:18:41 +0200

    Redirect to web onboarding to connect payment after sign (#666)
    Redirect users to web-onboarding to connect payment (AFTER_SIGN).
    
    Add new env var `WEB_ONBOARDING_PAYMENT_URL_AFTER_SIGN`.
    
    Add `WebOnboarding` helpers to hide implementation details from the rest of the app.

---
+++ apps/store/src/services/WebOnboarding/WebOnboarding.helpers.ts
@@ -0,0 +1,37 @@
+import { RoutingLocale } from '@/lib/l10n/types'
+
+const PAYMENT_URL_TEMPLATE = process.env.WEB_ONBOARDING_PAYMENT_URL_AFTER_SIGN
+
+// We've historically been using different locale paths in Web Onboarding
+export const convertRoutingLocale = (locale: RoutingLocale) => {
+  switch (locale) {
+    case 'dk':
+    case 'no':
+    case 'se':
+      return locale
+
+    case 'en-dk':
+    case 'en-no':
+    case 'en-se':
+      return locale.split('-').reverse().join('-')
+
+    case 'da-dk':
+    case 'sv-se':
+    case 'nb-no':
+      return locale.slice(3)
+  }
+}
+
+type Params = {
+  locale: RoutingLocale
+  accessToken: string
+}
+
+export const getWebOnboardingPaymentURL = ({ locale, accessToken }: Params) => {
+  if (PAYMENT_URL_TEMPLATE) {
+    const baseURL = PAYMENT_URL_TEMPLATE.replace('{LOCALE}', convertRoutingLocale(locale))
+    return `${baseURL}?accessToken=${accessToken}`
+  }
+
+  return null
+}
