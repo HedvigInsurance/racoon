60f4fd53826016493e435a0164fe3ebe15f70ed7
Gustav Eén <gustaveen@gmail.com>
2022-10-13 14:04:40 +0200

    Switch from bgImage to Next Image in Hero block

--- apps/store/src/blocks/HeroBlock.tsx
+++ apps/store/src/blocks/HeroBlock.tsx
@@ -1,5 +1,6 @@
 import styled from '@emotion/styled'
 import { storyblokEditable } from '@storyblok/react'
+import Image from 'next/image'
 import { ButtonBlock, ButtonBlockProps } from '@/blocks/ButtonBlock'
 import { HeadingBlock, HeadingBlockProps } from '@/blocks/HeadingBlock'
 import { ExpectedBlockType, SbBaseBlockProps, StoryblokImage } from '@/services/storyblok/storyblok'
@@ -16,31 +17,53 @@ export const HeroBlock = ({ blok }: HeroBlockProps) => {
   const buttonBlocks = filterByBlockType(blok.buttons, ButtonBlock.blockName)
 
   return (
-    <HeroSection {...storyblokEditable(blok)} bgImage={blok.background.filename}>
-      <div>
-        {headingBlocks.map((nestedBlock) => (
-          <HeadingBlock blok={nestedBlock} key={nestedBlock._uid} />
-        ))}
-      </div>
-      <div>
-        {buttonBlocks.map((nestedBlock) => (
-          <ButtonBlock blok={nestedBlock} key={nestedBlock._uid} />
-        ))}
-      </div>
+    <HeroSection {...storyblokEditable(blok)}>
+      <HeroImageWrapper>
+        <Image
+          src={blok.background.filename}
+          alt={blok.background.alt}
+          layout="fill"
+          objectFit="cover"
+          objectPosition="center"
+        />
+      </HeroImageWrapper>
+      <HeroContent>
+        <div>
+          {headingBlocks.map((nestedBlock) => (
+            <HeadingBlock blok={nestedBlock} key={nestedBlock._uid} />
+          ))}
+        </div>
+        <div>
+          {buttonBlocks.map((nestedBlock) => (
+            <ButtonBlock blok={nestedBlock} key={nestedBlock._uid} />
+          ))}
+        </div>
+      </HeroContent>
     </HeroSection>
   )
 }
 HeroBlock.blockName = 'hero'
 
-const HeroSection = styled.section<{ bgImage: string }>(({ theme, bgImage }) => ({
+const HeroSection = styled.section(({ theme }) => ({
+  position: 'relative',
   display: 'flex',
   flexDirection: 'column',
   justifyContent: 'space-between',
   minHeight: '80vh',
   paddingTop: theme.space[9],
   paddingBottom: theme.space[9],
-  backgroundImage: `url(${bgImage})`,
-  backgroundSize: 'cover',
-  backgroundPosition: 'center',
-  textAlign: 'center',
 }))
+
+const HeroImageWrapper = styled.div({
+  zIndex: '-1',
+})
+
+const HeroContent = styled.div({
+  position: 'relative',
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+  justifyContent: 'space-between',
+  flexGrow: 1,
+  textAlign: 'center',
+})
