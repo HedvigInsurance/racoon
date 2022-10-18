743d309af1b5d826710f5891cd24a8c13d46b770
Robin Andeer <robin.andeer@gmail.com>
2022-10-18 16:48:07 +0200

    Add Insurely service integration

---
+++ apps/store/src/services/Insurely/Insurely.tsx
@@ -0,0 +1,99 @@
+// Documentation: https://docs.insurely.com/autofill-and-switcher
+
+import styled from '@emotion/styled'
+import Script from 'next/script'
+import { useEffect } from 'react'
+import { useCurrentLocale } from '@/lib/l10n/useCurrentLocale'
+
+const IFRAME_URL = 'https://dc.insurely.se/v2/'
+const BOOTSTRAP_SCRIPT_URL = 'https://dc.insurely.se/v2/assets/js/dc-bootstrap.js'
+
+enum EventName {
+  APP_LOADED = 'APP_LOADED',
+  APP_CLOSE = 'APP_CLOSE',
+  COLLECTION_ID = 'COLLECTION_ID',
+  RESULTS = 'RESULTS',
+}
+
+// Non-exhaustive list of Insurely iframe messages
+type InsurelyMessage =
+  | { name: EventName.APP_LOADED }
+  | { name: EventName.APP_CLOSE }
+  | { name: EventName.COLLECTION_ID; value: string }
+  | { name: EventName.RESULTS }
+
+type InsurelyIframeProps = BoostrapScriptProps & {
+  clientId: string
+  onLoaded?: () => void
+  onClose?: () => void
+  onStart?: (collectionId: string) => void
+  onCompleted?: () => void
+}
+
+export const InsurelyIframe = (props: InsurelyIframeProps) => {
+  const { clientId, onLoaded, onClose, onStart, onCompleted, ...boostrapProps } = props
+
+  useEffect(() => {
+    const handleMessage = ({ data }: MessageEvent<InsurelyMessage>) => {
+      switch (data.name) {
+        case EventName.APP_LOADED:
+          return onLoaded?.()
+
+        case EventName.APP_CLOSE:
+          return onClose?.()
+
+        case EventName.COLLECTION_ID:
+          return onStart?.(data.value)
+
+        case EventName.RESULTS:
+          return onCompleted?.()
+      }
+    }
+    window.addEventListener('message', handleMessage)
+    return () => window.removeEventListener('message', handleMessage)
+  }, [onLoaded, onClose, onStart, onCompleted])
+
+  return (
+    <>
+      <StyledIframe
+        title="Insurely"
+        src={`${IFRAME_URL}?clientId=${clientId}`}
+        sandbox="allow-scripts
+    allow-same-origin
+    allow-popups
+    allow-forms
+    allow-popups-to-escape-sandbox
+    allow-top-navigation"
+      />
+
+      <BoostrapScript {...boostrapProps} />
+    </>
+  )
+}
+
+type BoostrapScriptProps = Partial<{
+  personalNumber: string
+  company: string
+}>
+
+const BoostrapScript = ({ personalNumber, company }: BoostrapScriptProps) => {
+  const { language } = useCurrentLocale()
+
+  const handleLoad = () => {
+    window.setClientParams?.({
+      fontType: 'secondary',
+      hideResultsView: true,
+      language,
+      prefilledData: {
+        ...(personalNumber && { SWEDISH_PERSONAL_NUMBER: personalNumber }),
+        ...(company && { company }),
+      },
+    })
+  }
+
+  return <Script strategy="afterInteractive" src={BOOTSTRAP_SCRIPT_URL} onLoad={handleLoad} />
+}
+
+const StyledIframe = styled.iframe({
+  border: 0,
+})
