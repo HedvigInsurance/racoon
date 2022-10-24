290ce8c8988ac9211e5ec32b363d0b4d16644e52
Robin Andeer <robin.andeer@gmail.com>
2022-10-24 12:00:40 -0400

    Use cookies-next for Persister class

--- apps/store/src/services/persister/ServerCookiePersister.ts
+++ apps/store/src/services/persister/ServerCookiePersister.ts
@@ -1,4 +1,5 @@
-import Cookies from 'js-cookie'
+import { getCookie, removeCookies, setCookie } from 'cookies-next'
+import { OptionsType } from 'cookies-next/lib/types'
 import { GetServerSidePropsContext } from 'next'
 import { SimplePersister } from './Persister.types'
 
@@ -9,33 +10,23 @@ export class ServerCookiePersister implements SimplePersister {
     private readonly response: GetServerSidePropsContext['res'],
   ) {}
 
-  public save(value: string, cookieKey = this.cookieKey, options?: Cookies.CookieAttributes) {
-    const rawHeader = this.response.getHeader('Set-Cookie')
-    const existingHeaders = Array.isArray(rawHeader) ? rawHeader : []
-    const { expires } = options || {}
-    this.response.setHeader('Set-Cookie', [
-      ...existingHeaders,
-      `${cookieKey}=${value}; Max-Age=${this.calculateMaxAge(expires)} Path=/`,
-    ])
+  public save(value: string, cookieKey = this.cookieKey, options?: OptionsType) {
+    setCookie(cookieKey, value, {
+      ...this.defaultOptions(),
+      ...options,
+    })
   }
 
   public fetch(cookieKey = this.cookieKey) {
-    return this.request.cookies[cookieKey] ?? null
+    const cookieValue = getCookie(cookieKey, this.defaultOptions())
+    return typeof cookieValue === 'string' ? cookieValue : null
   }
 
   public reset(cookieKey = this.cookieKey) {
-    this.response.setHeader('Set-Cookie', [`${cookieKey}=; Max-Age=0`])
+    removeCookies(cookieKey, this.defaultOptions())
   }
 
-  /**
-   * Converts a Date into Unix Epoc since that's what Max-Age expects
-   * @param value Possible values for expires option. If number, treated as number of days to conform to js-cookie's implementation
-   * @returns Undefined or number: milliseconds until expiry
-   */
-  private calculateMaxAge(value: number | Date | undefined) {
-    if (typeof value === 'undefined') return value
-    if (typeof value === 'number') return value * 1000 * 60 * 60 * 24
-
-    return value.getTime()
+  private defaultOptions() {
+    return { path: '/', req: this.request, res: this.response }
   }
 }
