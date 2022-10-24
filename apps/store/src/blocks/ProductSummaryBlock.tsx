29e4e642da07e8d7e31a4a5f36d75ddce8f8252d
Gustav Eén <gustaveen@gmail.com>
2022-10-24 23:40:21 +0200

    Remove star score

--- apps/store/src/blocks/ProductSummaryBlock.tsx
+++ apps/store/src/blocks/ProductSummaryBlock.tsx
@@ -14,7 +14,6 @@ export type ProductSummaryBlockContext = {
 type Props = SbBaseBlockProps<{
   title?: string
   description: string
-  starScore: number
 }>
 
 export const ProductSummaryBlock = (props: Props) => {
@@ -23,11 +22,7 @@ export const ProductSummaryBlock = (props: Props) => {
 
   return (
     <Wrapper {...storyblokEditable}>
-      <ProductSummary
-        title={blockTitle}
-        starScore={props.blok.starScore}
-        gradient={PLACEHOLDER_GRADIENT}
-      >
+      <ProductSummary title={blockTitle} gradient={PLACEHOLDER_GRADIENT}>
         {props.blok.description}
       </ProductSummary>
     </Wrapper>
