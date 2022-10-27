e28abf1070463f897ca118b7e3b5742d8eedb98d
Gustav Eén <gustaveen@gmail.com>
2022-10-27 16:49:56 +0200

    Make Pillow clip-path responsive

--- apps/store/src/components/Pillow/Pillow.tsx
+++ apps/store/src/components/Pillow/Pillow.tsx
@@ -3,20 +3,29 @@ import styled from '@emotion/styled'
 type PillowProps = {
   fromColor: string
   toColor: string
-  size?: 'small' | 'medium' | 'large'
+  size?: 'small' | 'medium' | 'large' | 'xlarge'
 }
 
-export const Pillow = styled.div<PillowProps>(({ fromColor, toColor, size = 'medium' }) => ({
+export const PillowShape = styled.div<PillowProps>(({ fromColor, toColor, size = 'medium' }) => ({
   height: getSize(size),
   width: getSize(size),
   background: `linear-gradient(116.75deg, ${fromColor} 0%, ${toColor} 100%)`,
   backgroundClip: 'padding-box',
-  clipPath: `path(
-    'M44.458 55.22a173.915 173.915 0 0 1-32.919 0A11.924 11.924 0 0 1 .781 44.457a173.895 173.895 0 0 1 0-32.916A11.926 11.926 0 0 1 11.539.781a173.922 173.922 0 0 1 32.919 0 11.932 11.932 0 0 1 10.761 10.761 173.887 173.887 0 0 1 0 32.916 11.928 11.928 0 0 1-10.761 10.761Z'
-  )`,
+  clipPath: 'url(#pillowClipPath)',
 }))
 
-const getSize = (size: 'small' | 'medium' | 'large') => {
+export const Pillow = (props: PillowProps) => (
+  <>
+    <svg width="0" height="0">
+      <clipPath id="pillowClipPath" clipPathUnits="objectBoundingBox">
+        <path d="M0.794,0.986 a3,3,0,0,1,-0.588,0 a0.213,0.213,0,0,1,-0.192,-0.192 a3,3,0,0,1,0,-0.588 A0.213,0.213,0,0,1,0.206,0.014 a3,3,0,0,1,0.588,0 a0.213,0.213,0,0,1,0.192,0.192 a3,3,0,0,1,0,0.588 a0.213,0.213,0,0,1,-0.192,0.192"></path>
+      </clipPath>
+    </svg>
+    <PillowShape {...props} />
+  </>
+)
+
+const getSize = (size: 'small' | 'medium' | 'large' | 'xlarge') => {
   switch (size) {
     case 'small':
       return '3rem'
@@ -24,5 +33,7 @@ const getSize = (size: 'small' | 'medium' | 'large') => {
       return '3.5rem'
     case 'large':
       return '4rem'
+    case 'xlarge':
+      return '12.5rem'
   }
 }
