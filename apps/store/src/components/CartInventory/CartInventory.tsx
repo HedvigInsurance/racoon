9a237c3b59acc3a779ee8cc5bfa574b29ff55954
Robin Andeer <robin.andeer@gmail.com>
2022-10-28 10:05:53 -0400

    Add cart inventory components (#763)

---
+++ apps/store/src/components/CartInventory/CartInventory.tsx
@@ -0,0 +1,45 @@
+import styled from '@emotion/styled'
+import { Heading, Space } from 'ui'
+import { CartFragmentFragment, ProductOfferFragment } from '@/services/apollo/generated'
+import { useCurrencyFormatter } from '@/utils/useCurrencyFormatter'
+import { OfferInventoryItem } from './OfferInventoryItem'
+
+type Props = {
+  cart: CartFragmentFragment
+  onRemove?: (offer: ProductOfferFragment) => void
+}
+
+export const CartInventory = ({ cart, onRemove }: Props) => {
+  const currencyFormatter = useCurrencyFormatter(cart.cost.total.currencyCode)
+
+  return (
+    <Space y={1}>
+      <List>
+        {cart.entries.map((offer) => (
+          <li key={offer.id}>
+            <OfferInventoryItem offer={offer} onRemove={onRemove} />
+          </li>
+        ))}
+      </List>
+      <Footer>
+        <Heading as="h3" variant="standard.18">
+          Total
+        </Heading>
+        <Heading as="h3" variant="standard.18">
+          {currencyFormatter.format(cart.cost.total.amount)}/mån
+        </Heading>
+      </Footer>
+    </Space>
+  )
+}
+
+const List = styled.ul(({ theme }) => ({
+  display: 'flex',
+  flexDirection: 'column',
+  gap: theme.space[4],
+}))
+
+const Footer = styled.div({
+  display: 'flex',
+  justifyContent: 'space-between',
+})
---
+++ apps/store/src/components/CartInventory/InventoryItem.tsx
@@ -0,0 +1,48 @@
+import styled from '@emotion/styled'
+
+export const Root = styled.div(({ theme }) => ({
+  display: 'grid',
+  columnGap: theme.space[4],
+  rowGap: theme.space[1],
+  alignItems: 'center',
+  gridTemplateColumns: 'auto 1fr',
+  gridTemplateRows: 'auto auto',
+  gridTemplateAreas: `
+    'left  main'
+    'empty bottom'
+  `,
+}))
+
+export const Left = styled.div({
+  gridArea: 'left',
+})
+
+export const Main = styled.div(({ theme }) => ({
+  gridArea: 'main',
+
+  display: 'grid',
+  gridTemplateColumns: '1fr auto',
+  gridTemplateRows: 'auto auto',
+  columnGap: theme.space[4],
+  rowGap: theme.space[1],
+  gridTemplateAreas: `
+    'main-left   main-right'
+    'main-bottom main-bottom'
+  `,
+}))
+
+export const MainLeft = styled.div({
+  gridArea: 'main-left',
+})
+
+export const MainRight = styled.div({
+  gridArea: 'main-right',
+})
+
+export const MainBottom = styled.div({
+  gridArea: 'main-bottom',
+})
+
+export const Bottom = styled.div({
+  gridArea: 'bottom',
+})
