0c68b4396cebac50e55733b9268b203a16e4c3de
Gustav Eén <gustaveen@gmail.com>
2022-10-31 14:00:51 +0100

    Add color prop to Text

--- apps/store/src/components/Text/Text.tsx
+++ apps/store/src/components/Text/Text.tsx
@@ -1,19 +1,34 @@
+import isPropValid from '@emotion/is-prop-valid'
 import styled from '@emotion/styled'
 import { ReactNode } from 'react'
+import type { UIColor } from 'ui'
 
 type FontSize = 's' | 'm' | 'l' | 'xl'
 
 type TypographyProps = {
   size: FontSize
+  color?: UIColor
   children: ReactNode
 }
 
 const fontSizeMap = { s: 1, m: 2, l: 3, xl: 4 }
 
-const TextElement = styled.span<TypographyProps>(({ theme, size }) => ({
+const elementConfig = {
+  shouldForwardProp: (prop: string) => isPropValid(prop) && prop !== 'color',
+}
+
+const TextElement = styled(
+  'span',
+  elementConfig,
+)<TypographyProps>(({ theme, color, size }) => ({
+  color: color ? theme.colors[color] : 'inherit',
   fontSize: theme.fontSizes[fontSizeMap[size]],
 }))
 
-export const Text = ({ size = 'm', children }: TypographyProps) => {
-  return <TextElement size={size}>{children}</TextElement>
+export const Text = ({ color, size = 'm', children }: TypographyProps) => {
+  return (
+    <TextElement color={color} size={size}>
+      {children}
+    </TextElement>
+  )
 }
