fa8a0fba6da3db0e05e18a55ad50cd03e2f03a97
Gustav Eén <gustaveen@gmail.com>
2022-11-01 11:20:07 +0100

    GRW-1704 / Price Calculator updates part 1 (#790)
    * Add color prop to Text
    
    * Add text alignemnt and as prop to Text
    
    * Rearrange content in Price Calculator

--- apps/store/src/components/Text/Text.tsx
+++ apps/store/src/components/Text/Text.tsx
@@ -1,19 +1,37 @@
+import isPropValid from '@emotion/is-prop-valid'
 import styled from '@emotion/styled'
 import { ReactNode } from 'react'
+import type { UIColor } from 'ui'
 
 type FontSize = 's' | 'm' | 'l' | 'xl'
 
 type TypographyProps = {
+  as?: 'p' | 'span'
+  align?: 'center' | 'left' | 'right'
+  color?: UIColor
   size: FontSize
   children: ReactNode
 }
 
 const fontSizeMap = { s: 1, m: 2, l: 3, xl: 4 }
 
-const TextElement = styled.span<TypographyProps>(({ theme, size }) => ({
+const elementConfig = {
+  shouldForwardProp: (prop: string) => isPropValid(prop) && prop !== 'color',
+}
+
+const TextElement = styled(
+  'span',
+  elementConfig,
+)<TypographyProps>(({ theme, align, color, size }) => ({
+  color: color ? theme.colors[color] : 'inherit',
   fontSize: theme.fontSizes[fontSizeMap[size]],
+  ...(align && { textAlign: align }),
 }))
 
-export const Text = ({ size = 'm', children }: TypographyProps) => {
-  return <TextElement size={size}>{children}</TextElement>
+export const Text = ({ as = 'p', align, color, size = 'm', children }: TypographyProps) => {
+  return (
+    <TextElement as={as} align={align} color={color} size={size}>
+      {children}
+    </TextElement>
+  )
 }
