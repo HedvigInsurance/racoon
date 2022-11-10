e7dfc604c35ffba57c785df7d27ba042828f8a12
Alexander Lebedev <alexander.lebedev@gmail.com>
2022-11-10 10:25:23 +0100

    Comment out pluralization test

--- apps/store/src/pages/[[...slug]].tsx
+++ apps/store/src/pages/[[...slug]].tsx
@@ -1,9 +1,7 @@
 import { StoryblokComponent, useStoryblokState } from '@storyblok/react'
 import type { GetStaticPaths, GetStaticProps, NextPageWithLayout } from 'next'
-import { useTranslation } from 'next-i18next'
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
 import Head from 'next/head'
-import { useEffect } from 'react'
 import { HeadSeoInfo } from '@/components/HeadSeoInfo/HeadSeoInfo'
 import { LayoutWithMenu } from '@/components/LayoutWithMenu/LayoutWithMenu'
 import {
@@ -26,16 +24,18 @@ type RoutingPath = {
 
 const NextPage: NextPageWithLayout<StoryblokPageProps> = (props: StoryblokPageProps) => {
   const story = useStoryblokState(props.story)
-  const { t } = useTranslation()
 
-  useEffect(() => {
-    console.log('Pluralization test', [
-      t('TEST_PLURALIZE', { count: 0 }),
-      t('TEST_PLURALIZE', { count: 1 }),
-      t('TEST_PLURALIZE', { count: 2 }),
-      t('TEST_PLURALIZE', { count: 3 }),
-    ])
-  }, [t])
+  // TODO: Remove when we have at least one case of pluralization elsewhere
+  // const { t } = useTranslation()
+  //
+  // useEffect(() => {
+  //   console.log('Pluralization test', [
+  //     t('TEST_PLURALIZE', { count: 0 }),
+  //     t('TEST_PLURALIZE', { count: 1 }),
+  //     t('TEST_PLURALIZE', { count: 2 }),
+  //     t('TEST_PLURALIZE', { count: 3 }),
+  //   ])
+  // }, [t])
 
   return (
     <>
