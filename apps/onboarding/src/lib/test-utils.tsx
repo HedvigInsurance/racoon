9bec70475a31efb5057a0784cd115739fa07953b
Alexander Lebedev <alexander.lebedev@gmail.com>
2022-11-10 16:36:02 +0100

    Declare stricter type in test-utils, fixes typecheck

--- apps/onboarding/src/lib/test-utils.tsx
+++ apps/onboarding/src/lib/test-utils.tsx
@@ -5,29 +5,30 @@ import { NextRouter } from 'next/router'
 import { ReactElement, ReactNode } from 'react'
 import { createApolloClient } from '@/services/apollo'
 
-const createRouterMock = (router?: Partial<NextRouter>): NextRouter => ({
-  basePath: '',
-  pathname: '/',
-  route: '/',
-  asPath: '/',
-  query: {},
-  push: () => Promise.resolve(true),
-  replace: () => Promise.resolve(true),
-  reload: () => {},
-  back: () => {},
-  prefetch: () => Promise.resolve(),
-  beforePopState: () => {},
-  events: {
-    on: () => {},
-    off: () => {},
-    emit: () => {},
-  },
-  isFallback: false,
-  isLocaleDomain: false,
-  isPreview: false,
-  isReady: false,
-  ...router,
-})
+const createRouterMock = (router?: Partial<NextRouter>): NextRouter =>
+  ({
+    basePath: '',
+    pathname: '/',
+    route: '/',
+    asPath: '/',
+    query: {},
+    push: () => Promise.resolve(true),
+    replace: () => Promise.resolve(true),
+    reload: () => {},
+    back: () => {},
+    prefetch: () => Promise.resolve(),
+    beforePopState: () => {},
+    events: {
+      on: () => {},
+      off: () => {},
+      emit: () => {},
+    },
+    isFallback: false,
+    isLocaleDomain: false,
+    isPreview: false,
+    isReady: false,
+    ...router,
+  } as NextRouter)
 
 const apolloClient = createApolloClient()
 
