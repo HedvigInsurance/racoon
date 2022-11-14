c05308d14ae42b3fa2e79a8359839aaedd50eff6
Karnell Schultz <50870173+KarnellSchultz@users.noreply.github.com>
2022-11-14 13:14:05 +0100

    Grw-1759 - 🚗  🇸🇪  Registration field (#855)
    * create registration-number field
    
    * add registration field to the car template
    
    * remove unmasked value
    
    * remove hidden field / added html pattern
    
    * added hidden input back
    
    * rename to car registration field
    
    * remove old reg field
    
    * using the input fields name
    
    * remove console log

--- apps/store/src/components/PriceForm/AutomaticField.tsx
+++ apps/store/src/components/PriceForm/AutomaticField.tsx
@@ -3,6 +3,7 @@ import { InputSelect } from '@/components/InputSelect/InputSelect'
 import { HouseholdSizeField } from '@/components/PriceForm/HouseholdSize'
 import { InputField as InputFieldType } from '@/services/PriceForm/Field.types'
 import { JSONData } from '@/services/PriceForm/PriceForm.types'
+import { CarRegistrationNumberField } from './CarRegistrationField'
 import { ExtraBuildingsField } from './ExtraBuildingsField'
 import { InputRadio } from './InputRadio'
 import { SsnSeField } from './SsnSeField'
@@ -110,5 +111,7 @@ export const AutomaticField = ({ field, onSubmit, loading, autoFocus }: Props) =
 
     case 'ssn-se':
       return <SsnSeField field={field} />
+    case 'car-registration-number':
+      return <CarRegistrationNumberField field={field} />
   }
 }
