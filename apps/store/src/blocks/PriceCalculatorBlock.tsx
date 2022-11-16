855dacaac45429142922f3c9b2636e1508cffbc0
Robin Andeer <robin.andeer@gmail.com>
2022-11-16 12:46:21 -0500

    Remove price calculator title

--- apps/store/src/blocks/PriceCalculatorBlock.tsx
+++ apps/store/src/blocks/PriceCalculatorBlock.tsx
@@ -2,10 +2,10 @@ import { storyblokEditable } from '@storyblok/react'
 import { PriceCalculator } from '@/components/ProductPage/PriceCalculator/PriceCalculator'
 import { SbBaseBlockProps } from '@/services/storyblok/storyblok'
 
-type Props = SbBaseBlockProps<{ title: string }>
+type Props = SbBaseBlockProps<unknown>
 
 export const PriceCalculatorBlock = ({ blok }: Props) => {
-  return <PriceCalculator title={blok.title} {...storyblokEditable(blok)} />
+  return <PriceCalculator {...storyblokEditable(blok)} />
 }
 
 PriceCalculatorBlock.blockName = 'priceCalculator'
