821bf613e33f66815e7c3d627e1ed65ca9782783
Gustav Eén <gustaveen@gmail.com>
2022-11-23 16:01:48 +0100

    Render sections in tabs

--- apps/store/src/blocks/ProductPageBlock.tsx
+++ apps/store/src/blocks/ProductPageBlock.tsx
@@ -1,6 +1,7 @@
 import styled from '@emotion/styled'
 import { storyblokEditable, StoryblokComponent, SbBlokData } from '@storyblok/react'
 import { PurchaseForm } from '@/components/ProductPage/PurchaseForm/PurchaseForm'
+import * as Tabs from '@/components/ProductPage/Tabs'
 import { SbBaseBlockProps } from '@/services/storyblok/storyblok'
 
 type ProductPageBlockProps = SbBaseBlockProps<{
@@ -13,12 +14,27 @@ export const ProductPageBlock = ({ blok }: ProductPageBlockProps) => {
   return (
     <Main {...storyblokEditable(blok)}>
       <PurchaseForm />
-      {blok.overview?.map((nestedBlock) => (
-        <StoryblokComponent blok={nestedBlock} key={nestedBlock._uid} />
-      ))}
-      {blok.coverage?.map((nestedBlock) => (
-        <StoryblokComponent blok={nestedBlock} key={nestedBlock._uid} />
-      ))}
+
+      <Tabs.Tabs defaultValue="overview">
+        <Tabs.TabsList>
+          {/* TODO: Get tab labels from Storyblok */}
+          <Tabs.TabsTrigger value="overview">Overview</Tabs.TabsTrigger>
+          <Tabs.TabsTrigger value="coverage">Coverage</Tabs.TabsTrigger>
+        </Tabs.TabsList>
+
+        <Tabs.TabsContent value="overview">
+          {blok.overview?.map((nestedBlock) => (
+            <StoryblokComponent blok={nestedBlock} key={nestedBlock._uid} />
+          ))}
+        </Tabs.TabsContent>
+
+        <Tabs.TabsContent value="coverage">
+          {blok.coverage?.map((nestedBlock) => (
+            <StoryblokComponent blok={nestedBlock} key={nestedBlock._uid} />
+          ))}
+        </Tabs.TabsContent>
+      </Tabs.Tabs>
+
       {blok.body.map((nestedBlock) => (
         <StoryblokComponent blok={nestedBlock} key={nestedBlock._uid} />
       ))}
