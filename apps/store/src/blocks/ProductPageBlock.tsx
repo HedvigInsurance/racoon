09976f3970af7568f419e1568bd23cdb3ee19430
Gustav Eén <gustaveen@gmail.com>
2022-11-28 16:45:22 +0100

    Render different layout for mobile and desktop. (#924)
    * Render sections in tabs
    
    * Make PurchaseForm sticky besides Overview section in desktop
    
    * Get Tab labels from Storyblok
    
    * Use CSS media query to toggle layouts

--- apps/store/src/blocks/ProductPageBlock.tsx
+++ apps/store/src/blocks/ProductPageBlock.tsx
@@ -1,9 +1,13 @@
 import styled from '@emotion/styled'
 import { storyblokEditable, StoryblokComponent, SbBlokData } from '@storyblok/react'
+import { mq } from 'ui'
 import { PurchaseForm } from '@/components/ProductPage/PurchaseForm/PurchaseForm'
+import * as Tabs from '@/components/ProductPage/Tabs'
 import { SbBaseBlockProps } from '@/services/storyblok/storyblok'
 
 type ProductPageBlockProps = SbBaseBlockProps<{
+  overviewLabel: string
+  coverageLabel: string
   overview: SbBlokData[]
   coverage: SbBlokData[]
   body: SbBlokData[]
@@ -12,13 +16,48 @@ type ProductPageBlockProps = SbBaseBlockProps<{
 export const ProductPageBlock = ({ blok }: ProductPageBlockProps) => {
   return (
     <Main {...storyblokEditable(blok)}>
-      <PurchaseForm />
-      {blok.overview?.map((nestedBlock) => (
-        <StoryblokComponent blok={nestedBlock} key={nestedBlock._uid} />
-      ))}
-      {blok.coverage?.map((nestedBlock) => (
-        <StoryblokComponent blok={nestedBlock} key={nestedBlock._uid} />
-      ))}
+      <MobileLayout>
+        <PurchaseForm />
+        <Tabs.Tabs defaultValue="overview">
+          <Tabs.TabsList>
+            <Tabs.TabsTrigger value="overview">{blok.overviewLabel}</Tabs.TabsTrigger>
+            <Tabs.TabsTrigger value="coverage">{blok.coverageLabel}</Tabs.TabsTrigger>
+          </Tabs.TabsList>
+
+          <Tabs.TabsContent value="overview">
+            {blok.overview?.map((nestedBlock) => (
+              <StoryblokComponent blok={nestedBlock} key={nestedBlock._uid} />
+            ))}
+          </Tabs.TabsContent>
+
+          <Tabs.TabsContent value="coverage">
+            {blok.coverage?.map((nestedBlock) => (
+              <StoryblokComponent blok={nestedBlock} key={nestedBlock._uid} />
+            ))}
+          </Tabs.TabsContent>
+        </Tabs.Tabs>
+      </MobileLayout>
+
+      <DesktopLayout>
+        <>
+          <ProductUpper>
+            <div>
+              {blok.overview?.map((nestedBlock) => (
+                <StoryblokComponent blok={nestedBlock} key={nestedBlock._uid} />
+              ))}
+            </div>
+
+            <PurchaseFormWrapper>
+              <PurchaseForm />
+            </PurchaseFormWrapper>
+          </ProductUpper>
+
+          {blok.coverage?.map((nestedBlock) => (
+            <StoryblokComponent blok={nestedBlock} key={nestedBlock._uid} />
+          ))}
+        </>
+      </DesktopLayout>
+
       {blok.body.map((nestedBlock) => (
         <StoryblokComponent blok={nestedBlock} key={nestedBlock._uid} />
       ))}
@@ -30,4 +69,28 @@ ProductPageBlock.blockName = 'product'
 
 const Main = styled.main({
   width: '100%',
+})
+
+const ProductUpper = styled.div({
+  display: 'grid',
+  gridTemplateColumns: 'repeat(2, 1fr)',
+  alignItems: 'flex-start',
+})
+
+const PurchaseFormWrapper = styled.div({
+  position: 'sticky',
+  top: 0,
+})
+
+const MobileLayout = styled.div({
+  [mq.lg]: {
+    display: 'none',
+  },
+})
+
+const DesktopLayout = styled.div({
+  display: 'none',
+  [mq.lg]: {
+    display: 'block',
+  },
 })
