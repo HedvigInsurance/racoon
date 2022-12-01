4cd87aef7b30c6a8e7f01612cebb6ba237ded200
Gustav Eén <gustaveen@gmail.com>
2022-12-01 16:59:21 +0100

    Add autoplay setting to Video component and to Storyblok (#977)

--- apps/store/src/blocks/VideoBlock.tsx
+++ apps/store/src/blocks/VideoBlock.tsx
@@ -4,9 +4,16 @@ import { SbBaseBlockProps, StoryblokAsset } from '@/services/storyblok/storyblok
 type VideoBlockProps = SbBaseBlockProps<{
   video: StoryblokAsset
   poster?: StoryblokAsset
+  autoplay?: boolean
 }>
 
 export const VideoBlock = ({ blok }: VideoBlockProps) => {
-  return <Video sources={[{ url: blok.video.filename }]} poster={blok.poster?.filename} />
+  return (
+    <Video
+      sources={[{ url: blok.video.filename }]}
+      poster={blok.poster?.filename}
+      autoplay={blok.autoplay}
+    />
+  )
 }
 VideoBlock.blockName = 'videoBlock'
