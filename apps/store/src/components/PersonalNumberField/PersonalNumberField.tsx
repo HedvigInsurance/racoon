b7935362b7218d9cd66d6b28dfc83f317fe780bf
Karnell Schultz <50870173+KarnellSchultz@users.noreply.github.com>
2022-12-02 10:42:39 +0100

    use textfield for personalnumber field

--- apps/store/src/components/PersonalNumberField/PersonalNumberField.tsx
+++ apps/store/src/components/PersonalNumberField/PersonalNumberField.tsx
@@ -1,25 +1,17 @@
-import { useTheme } from '@emotion/react'
-import styled from '@emotion/styled'
-import { motion } from 'framer-motion'
 import Personnummer from 'personnummer'
 import { ChangeEventHandler, InputHTMLAttributes, useState } from 'react'
+import { TextField } from '../TextField/TextField'
 
 type Props = Omit<
   InputHTMLAttributes<HTMLInputElement>,
   'onAnimationStart' | 'onDragStart' | 'onDragEnd' | 'onDrag'
 >
 
-enum AnimationState {
-  Idle = 'IDLE',
-  Active = 'ACTIVE',
-}
-
 /**
  * Personal Number input field.
  * Only supports Swedish personal numbers.
  */
 export const PersonalNumberField = (props: Props) => {
-  const theme = useTheme()
   const { value: propValue, defaultValue, ...baseProps } = props
 
   const [value, setValue] = useState(() => {
@@ -28,7 +20,6 @@ export const PersonalNumberField = (props: Props) => {
     return ''
   })
 
-  const [isAnimating, setIsAnimating] = useState(false)
   const handleOnChange: ChangeEventHandler<HTMLInputElement> = (event) => {
     let value = event.target.value
 
@@ -36,19 +27,13 @@ export const PersonalNumberField = (props: Props) => {
       value = Personnummer.parse(value).format(true)
     }
     setValue(value)
-    setIsAnimating(true)
   }
-
-  const animationVariants = {
-    [AnimationState.Idle]: { backgroundColor: theme.colors.gray300 },
-    [AnimationState.Active]: { backgroundColor: 'rgba(197, 236, 127, 0.6)' },
-  } as const
 
   return (
     <>
       <input {...baseProps} type="text" value={value} readOnly hidden />
 
-      <Input
+      <TextField
         {...baseProps}
         type="text"
         name={undefined}
@@ -57,21 +42,7 @@ export const PersonalNumberField = (props: Props) => {
         maxLength={13}
         pattern="[0-9-+]"
         onChange={handleOnChange}
-        variants={animationVariants}
-        initial={AnimationState.Idle}
-        animate={isAnimating ? AnimationState.Active : AnimationState.Idle}
-        onAnimationComplete={() => setIsAnimating(false)}
       />
     </>
   )
 }
-
-const Input = styled(motion.input)(({ theme }) => ({
-  backgroundColor: theme.colors.gray300,
-  padding: theme.space[4],
-  borderRadius: theme.radius.sm,
-  width: '100%',
-
-  fontFamily: theme.fonts.body,
-  fontSize: theme.fontSizes[3],
-}))
