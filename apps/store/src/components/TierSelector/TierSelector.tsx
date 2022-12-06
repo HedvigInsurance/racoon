f8ab9d551e450a9d877b526ceb7712e8404e6549
Karnell Schultz <50870173+KarnellSchultz@users.noreply.github.com>
2022-12-06 10:39:31 +0100

    rename tierSelector

--- apps/store/src/components/TierSelector/TierSelector.tsx
+++ apps/store/src/components/TierSelector/TierSelector.tsx
@@ -1,24 +1,117 @@
+import styled from '@emotion/styled'
 import * as AccordionPrimitives from '@radix-ui/react-accordion'
-import { useTranslation } from 'next-i18next'
-import { Dispatch, SetStateAction } from 'react'
+import { Dispatch, PropsWithChildren, ReactElement, SetStateAction } from 'react'
+import { ChevronIcon } from 'ui'
 import { ProductOfferFragment } from '@/services/apollo/generated'
-import { useCurrencyFormatter } from '@/utils/useCurrencyFormatter'
-import { FormElement } from '../ProductPage/PurchaseForm/PurchaseForm.constants'
-import {
-  RecommendedItem,
-  SecondaryTextStyle,
-  TierItemContainer,
-  TitleContainer,
-  TitleItem,
-  Root,
-  Item,
-  HeaderWithTrigger,
-  Content,
-} from './TierSelectorStyles'
 
-type TierItemProps = {
+export const Root = AccordionPrimitives.Root
+export const Content = AccordionPrimitives.Content
+
+export const Item = styled(AccordionPrimitives.Item)(({ theme }) => ({
+  padding: theme.space[2],
+}))
+
+const Header = AccordionPrimitives.Header
+
+const Trigger = styled(AccordionPrimitives.Trigger)(({ theme }) => ({
+  height: theme.space[7],
+  width: '100%',
+  cursor: 'pointer',
+  display: 'flex',
+  alignItems: 'center',
+  textAlign: 'center',
+  justifyContent: 'space-between',
+  background: theme.colors.gray200,
+  boxShadow: '0px 1px 2px rgba(0, 0, 0, 0.2)',
+  borderRadius: theme.radius.xs,
+  borderBottom: `1px solid ${theme.colors.gray500}`,
+  padding: `0 ${theme.space[4]}`,
+
+  '[data-state=open] &': { borderRadius: `${theme.radius.xs}px ${theme.radius.xs}px 0 0` },
+}))
+
+const CenteredHeader = styled.div({
+  width: '100%',
+  display: 'flex',
+  alignContent: 'center',
+  justifyContent: 'space-around',
+})
+
+type HeaderWithTriggerProps = PropsWithChildren<unknown> & {
+  icon?: ReactElement
+}
+const TriggerIcon = styled(ChevronIcon)({
+  transition: 'transform 300ms',
+  '[data-state=open] &': { transform: 'rotate(180deg)' },
+})
+
+export const HeaderWithTrigger = ({
+  children,
+  icon = <TriggerIcon size="1rem" />,
+}: HeaderWithTriggerProps) => {
+  return (
+    <Header>
+      <Trigger>
+        <CenteredHeader>{children}</CenteredHeader>
+        {icon}
+      </Trigger>
+    </Header>
+  )
+}
+const RecommendedContainer = styled.div(({ theme }) => ({
+  paddingTop: theme.space[5],
+}))
+
+const RecommendedDot = styled.span(({ theme }) => ({
+  display: 'inline-block',
+  height: theme.radius.sm,
+  width: theme.radius.sm,
+  backgroundColor: theme.colors.black,
+  borderRadius: '50%',
+  marginRight: '.5rem',
+}))
+
+type RecommendedItemProps = {
+  children: React.ReactNode
+}
+export const RecommendedItem = ({ children }: RecommendedItemProps) => (
+  <RecommendedContainer>
+    <RecommendedDot />
+    {children}
+  </RecommendedContainer>
+)
+
+const TierItemContainer = styled.div<{ isSelected: boolean }>(({ theme, isSelected = false }) => ({
+  cursor: 'pointer',
+  display: 'flex',
+  flexDirection: 'column',
+  justifyContent: 'space-between',
+  padding: theme.space[4],
+  backgroundColor: isSelected ? '#E0F6BE' : theme.colors.gray200,
+
+  '&:last-of-type': {
+    borderRadius: `0 0 ${theme.radius.xs}px ${theme.radius.xs}px`,
+  },
+}))
+
+const TitleContainer = styled.div(({ theme }) => ({
+  fontSize: theme.fontSizes[3],
+  width: '100%',
+  display: 'flex',
+  justifyContent: 'space-between',
+}))
+
+const TitleItem = styled.div(({ theme }) => ({
+  paddingBottom: theme.space[1],
+}))
+
+export const SecondaryTextStyle = styled.div(({ theme }) => ({
+  color: theme.colors.gray700,
+}))
+
+export type TierItemProps = {
   title: string
-  price: string
+  price: number
   description: string
   isSelected?: boolean
   recommendedText?: string
@@ -26,91 +119,63 @@ type TierItemProps = {
   handleClick?: () => void
 } & AccordionPrimitives.AccordionItemProps
 
-const TierItem = ({
+export const TierItem = ({
   title,
   price,
   description,
   isSelected = false,
   recommendedText = '',
   handleClick,
-}: TierItemProps) => (
-  <TierItemContainer isSelected={isSelected} onClick={handleClick}>
-    <TitleContainer>
-      <TitleItem>{title}</TitleItem>
-      <TitleItem>
-        <SecondaryTextStyle>{price}</SecondaryTextStyle>
-      </TitleItem>
-    </TitleContainer>
-    <SecondaryTextStyle>{description}</SecondaryTextStyle>
-    {recommendedText ? <RecommendedItem>{recommendedText}</RecommendedItem> : null}
-  </TierItemContainer>
-)
+}: TierItemProps) => {
+  return (
+    <TierItemContainer isSelected={isSelected} onClick={handleClick}>
+      <TitleContainer>
+        <TitleItem>{title}</TitleItem>
+        <TitleItem>
+          <SecondaryTextStyle>{price} kr/mån</SecondaryTextStyle>
+        </TitleItem>
+      </TitleContainer>
+      <SecondaryTextStyle>{description}</SecondaryTextStyle>
+      {recommendedText ? <RecommendedItem>{recommendedText}</RecommendedItem> : null}
+    </TierItemContainer>
+  )
+}
 
 export type TierSelectorProps = {
   offers: Array<ProductOfferFragment>
   selectedOfferId: string
-  currencyCode: string
   onValueChange: Dispatch<SetStateAction<string>>
 }
-export const TierSelector = ({
-  offers,
-  selectedOfferId,
-  currencyCode,
-  onValueChange,
-}: TierSelectorProps) => {
-  const { t } = useTranslation()
-
-  const currencyFormatter = useCurrencyFormatter(currencyCode)
-  const selectedOffer = offers.find((offer) => offer.id === selectedOfferId)
-
+export const TierSelector = ({ offers, selectedOfferId, onValueChange }: TierSelectorProps) => {
   const handleClick = (id: string) => {
     onValueChange?.(id)
   }
 
-  if (offers.length === 1) {
-    return <input hidden readOnly name={FormElement.ProductOfferId} value={selectedOfferId} />
-  }
+  const selectedOffer = offers.find((offer) => offer.id === selectedOfferId)
 
   return (
     <Root type="multiple">
-      <input
-        type="text"
-        hidden
-        readOnly
-        value={selectedOfferId}
-        name={FormElement.ProductOfferId}
-      />
-      <Item value={selectedOfferId}>
+      <Item value="item-1">
         <HeaderWithTrigger>
-          {selectedOffer ? (
+          {selectedOfferId ? (
             <>
-              <div>{selectedOffer.variant.typeOfContract}</div>
-              <SecondaryTextStyle>
-                {t('MONTHLY_PRICE', {
-                  displayAmount: currencyFormatter.format(selectedOffer.price.amount),
-                })}
-              </SecondaryTextStyle>
+              <div>{selectedOffer?.variant.typeOfContract}</div>
+              <SecondaryTextStyle>{selectedOffer?.price.amount}/mån</SecondaryTextStyle>
             </>
           ) : (
-            <div>{t('TIER_SELECTOR_DEFAULT_LABEL')}</div>
+            <div>Välj skydd</div>
           )}
         </HeaderWithTrigger>
         <Content>
           {offers.map((offer) => {
-            const {
-              id,
-              price: { amount },
-            } = offer
-
+            const { id, price } = offer
             return (
               <TierItem
                 key={id}
                 value={id}
                 title={offer.variant.typeOfContract}
                 description="some description here"
-                price={t('MONTHLY_PRICE', {
-                  displayAmount: amount,
-                })}
+                price={price.amount}
                 isSelected={selectedOfferId === id}
                 handleClick={() => handleClick(id)}
               />
