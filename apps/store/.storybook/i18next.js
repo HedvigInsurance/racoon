eefc1f869e4e6088a5a50e0a0bdb72559798765d
Alexander Lebedev <alexander.lebedev@gmail.com>
2022-12-06 13:26:01 +0100

    Provide dictionary data to apps/store storybook

---
+++ apps/store/.storybook/i18next.js
@@ -0,0 +1,21 @@
+import i18n from 'i18next'
+import { initReactI18next } from 'react-i18next'
+import nextI18nextConfig from '../next-i18next.config'
+
+const requireTranslationsBackend = {
+  type: 'backend',
+  read(language, namespace, callback) {
+    import(`../public/locales/${language}/${namespace}.json`)
+      .then((resources) => {
+        callback(null, resources)
+      })
+      .catch((error) => {
+        callback(error, null)
+      })
+  },
+}
+
+i18n
+  .use(initReactI18next)
+  .use(requireTranslationsBackend)
+  .init({ ...nextI18nextConfig, lng: 'default', defaultNS: nextI18nextConfig.fallbackNS })
--- apps/store/.storybook/preview.js
+++ apps/store/.storybook/preview.js
@@ -1,6 +1,7 @@
 import { ThemeProvider } from 'ui'
 import { storybookFontStyles } from 'ui/src/lib/storybookFontStyles'
 import { Global } from '@emotion/react'
+import './i18next'
 
 export const parameters = {
   actions: { argTypesRegex: '^on[A-Z].*' },
