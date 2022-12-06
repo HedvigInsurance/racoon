385a62c474f601948f2f2b1ad78e8d096281d2b7
Robin Andeer <robin.andeer@gmail.com>
2022-12-06 15:00:37 +0100

    Add visual `PriceMatchBubble` component (#1017)
    ## Describe your changes
    
    Add visual `PriceMatchBubble`.
    
    ## Justify why they are needed
    
    ## Jira issue(s): [GRW-1897]
    
    ## Checklist before requesting a review
    
    - [ ] I have performed a self-review of my code
    
    
    [GRW-1897]: https://hedvig.atlassian.net/browse/GRW-1897?atlOrigin=eyJpIjoiNWRkNTljNzYxNjVmNDY3MDlhMDU5Y2ZhYzA5YTRkZjUiLCJwIjoiZ2l0aHViLWNvbS1KU1cifQ

---
+++ apps/store/src/components/ProductPage/PurchaseForm/PriceMatchBubble/PriceMatchBubble.tsx
@@ -0,0 +1,53 @@
+import styled from '@emotion/styled'
+import { Text } from '@/components/Text/Text'
+
+type Props = {
+  title: string
+  children: string
+}
+
+export const PriceMatchBubble = ({ title, children }: Props) => {
+  return (
+    <Root>
+      <Tip />
+      <Bubble>
+        <Text size="s" align="center">
+          {title}
+        </Text>
+        <Text size="s" color="gray700" align="center">
+          {children}
+        </Text>
+      </Bubble>
+    </Root>
+  )
+}
+
+const TIP_HEIGHT = '0.625rem'
+const HALF_TIP_WIDTH = '0.625rem'
+// @TODO: change to theme color
+const LIGHT_BLUE = '#E0F0F9'
+
+const Root = styled.div({
+  paddingBlockStart: TIP_HEIGHT,
+  position: 'relative',
+})
+
+const Bubble = styled.div(({ theme }) => ({
+  backgroundColor: LIGHT_BLUE,
+  paddingBlock: theme.space[3],
+  paddingInline: theme.space[4],
+  borderRadius: theme.radius.sm,
+}))
+
+const Tip = styled.div({
+  position: 'absolute',
+  top: 0,
+  left: '50%',
+  transform: `translateX(-${HALF_TIP_WIDTH})`,
+
+  width: 0,
+  height: 0,
+  borderLeft: `${HALF_TIP_WIDTH} solid transparent`,
+  borderRight: `${HALF_TIP_WIDTH} solid transparent`,
+  borderBottom: `${HALF_TIP_WIDTH} solid ${LIGHT_BLUE}`,
+})
