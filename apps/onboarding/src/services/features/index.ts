0b1d8e80ef60dc41142904a231564f873fdc33d1
Guilherme Popolin <guilherme@hedvig.com>
2022-12-07 15:19:46 +0100

    chore: removed FEATURE_HOUSE_INSURANCE and FEATURE_ACCIDENT_INSURANCE feature flags

--- apps/onboarding/src/services/features/index.ts
+++ apps/onboarding/src/services/features/index.ts
@@ -1,8 +1,6 @@
 import { MarketLabel } from '@/lib/types'
 
 export enum Feature {
-  ACCIDENT_INSURANCE = 'ACCIDENT_INSURANCE',
-  HOUSE_INSURANCE = 'HOUSE_INSURANCE',
   CROSS_SELL = 'CROSS_SELL',
 }
 
@@ -11,8 +9,6 @@ export const Features = {
     const normalize = (marketList = '') => marketList.includes(marketLabel)
 
     return {
-      ACCIDENT_INSURANCE: normalize(process.env.NEXT_PUBLIC_FEATURE_ACCIDENT_INSURANCE),
-      HOUSE_INSURANCE: normalize(process.env.NEXT_PUBLIC_FEATURE_HOUSE_INSURANCE),
       CROSS_SELL: normalize(process.env.NEXT_PUBLIC_FEATURE_CROSS_SELL),
     }
   },
