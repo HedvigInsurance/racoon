953a86a2a6f5b78c63f811123e55d6e80454ffc2
Karnell Schultz <50870173+KarnellSchultz@users.noreply.github.com>
2022-12-12 13:10:06 +0100

    remove duplicate components

--- apps/store/src/components/TierSelector/TierSelector.tsx
+++ apps/store/src/components/TierSelector/TierSelector.tsx
@@ -1,116 +1,20 @@
-import styled from '@emotion/styled'
 import * as AccordionPrimitives from '@radix-ui/react-accordion'
 import { useTranslation } from 'next-i18next'
-import { Dispatch, PropsWithChildren, ReactElement, SetStateAction } from 'react'
-import { ChevronIcon } from 'ui'
+import { Dispatch, SetStateAction } from 'react'
 import { ProductOfferFragment } from '@/services/apollo/generated'
 import { useCurrencyFormatter } from '@/utils/useCurrencyFormatter'
 import { FormElement } from '../ProductPage/PurchaseForm/PurchaseForm.constants'
-
-const Root = AccordionPrimitives.Root
-const Content = AccordionPrimitives.Content
-
-const Item = styled(AccordionPrimitives.Item)(({ theme }) => ({
-  padding: theme.space[2],
-}))
-
-const Header = AccordionPrimitives.Header
-
-const Trigger = styled(AccordionPrimitives.Trigger)(({ theme }) => ({
-  height: theme.space[7],
-  width: '100%',
-  cursor: 'pointer',
-  display: 'flex',
-  alignItems: 'center',
-  textAlign: 'center',
-  justifyContent: 'space-between',
-  background: theme.colors.gray200,
-  boxShadow: '0px 1px 2px rgba(0, 0, 0, 0.2)',
-  borderRadius: theme.radius.xs,
-  borderBottom: `1px solid ${theme.colors.gray500}`,
-  padding: `0 ${theme.space[4]}`,
-
-  '[data-state=open] &': { borderRadius: `${theme.radius.xs}px ${theme.radius.xs}px 0 0` },
-}))
-
-const CenteredHeader = styled.div({
-  width: '100%',
-  display: 'flex',
-  alignContent: 'center',
-  justifyContent: 'space-around',
-})
-
-type HeaderWithTriggerProps = PropsWithChildren<unknown> & {
-  icon?: ReactElement
-}
-const TriggerIcon = styled(ChevronIcon)({
-  transition: 'transform 300ms',
-  '[data-state=open] &': { transform: 'rotate(180deg)' },
-})
-
-const HeaderWithTrigger = ({
-  children,
-  icon = <TriggerIcon size="1rem" />,
-}: HeaderWithTriggerProps) => {
-  return (
-    <Header>
-      <Trigger>
-        <CenteredHeader>{children}</CenteredHeader>
-        {icon}
-      </Trigger>
-    </Header>
-  )
-}
-const RecommendedContainer = styled.div(({ theme }) => ({
-  paddingTop: theme.space[5],
-}))
-
-const RecommendedDot = styled.span(({ theme }) => ({
-  display: 'inline-block',
-  height: theme.radius.sm,
-  width: theme.radius.sm,
-  backgroundColor: theme.colors.black,
-  borderRadius: '50%',
-  marginRight: '.5rem',
-}))
-
-type RecommendedItemProps = {
-  children: React.ReactNode
-}
-const RecommendedItem = ({ children }: RecommendedItemProps) => (
-  <RecommendedContainer>
-    <RecommendedDot />
-    {children}
-  </RecommendedContainer>
-)
-
-const TierItemContainer = styled.div<{ isSelected: boolean }>(({ theme, isSelected = false }) => ({
-  cursor: 'pointer',
-  display: 'flex',
-  flexDirection: 'column',
-  justifyContent: 'space-between',
-  padding: theme.space[4],
-  backgroundColor: isSelected ? '#E0F6BE' : theme.colors.gray200,
-
-  '&:last-of-type': {
-    borderRadius: `0 0 ${theme.radius.xs}px ${theme.radius.xs}px`,
-  },
-}))
-
-const TitleContainer = styled.div(({ theme }) => ({
-  fontSize: theme.fontSizes[3],
-  width: '100%',
-  display: 'flex',
-  justifyContent: 'space-between',
-}))
-
-const TitleItem = styled.div(({ theme }) => ({
-  paddingBottom: theme.space[1],
-}))
-
-const SecondaryTextStyle = styled.div(({ theme }) => ({
-  color: theme.colors.gray700,
-}))
+import {
+  RecommendedItem,
+  SecondaryTextStyle,
+  TierItemContainer,
+  TitleContainer,
+  TitleItem,
+  Root,
+  Item,
+  HeaderWithTrigger,
+  Content,
+} from './TierSelectorStyles'
 
 type TierItemProps = {
   title: string
