08556da49f51a62a8b8673b6e88a2dc0eb7142e0
Gustav Eén <gustaveen@gmail.com>
2022-12-13 16:09:49 +0100

    Put back Card component in ui package

---
+++ packages/ui/src/components/Card/Card.stories.tsx
@@ -0,0 +1,35 @@
+import { ComponentMeta, ComponentStory } from '@storybook/react'
+import { Space } from 'ui/src/components/Space'
+import { Card, CardContent, CardMedia } from './Card'
+
+export default {
+  title: 'Card',
+  component: Card,
+  args: {},
+} as ComponentMeta<typeof Card>
+
+const Template: ComponentStory<typeof Card> = (args) => {
+  return (
+    <Space y={1}>
+      <Card {...args} size="sm">
+        <CardContent>I&apos;m a small card!</CardContent>
+      </Card>
+      <Card {...args} size="md">
+        <CardContent>I&apos;m a medium sized card!</CardContent>
+      </Card>
+      <Card {...args} size="lg">
+        <CardContent>I&apos;m a large card</CardContent>
+      </Card>
+      <Card {...args}>
+        <CardMedia
+          height={140}
+          image="https://source.unsplash.com/user/heytowner/?orientation=landscape"
+          alt="some image description"
+        />
+        <CardContent>I&apos;m a large card with an image</CardContent>
+      </Card>
+    </Space>
+  )
+}
+
+export const Default = Template.bind({})
