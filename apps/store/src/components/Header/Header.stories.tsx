f000b4c2dbb8b246868f809dc789383270399d7f
Robin Andeer <robin.andeer@gmail.com>
2022-12-15 16:11:36 +0100

    Update cart icon to match figma (#1100)
    ## Describe your changes
    
    Update to the new "icon".
    
    ![Screenshot 2022-12-15 at 16.03.33.png](https://graphite-user-uploaded-assets-prod.s3.amazonaws.com/OgXegTwxM9IeuXHZyw5h/22ee676e-278c-4ef2-b362-2ec568f9e2de/Screenshot%202022-12-15%20at%2016.03.33.png)
    
    ## Justify why they are needed
    
    I used an SVG since the border is 1.5px and we can't render that in CSS.
    
    ## Jira issue(s): []
    
    ## Checklist before requesting a review
    
    - [ ] I have performed a self-review of my code

--- apps/store/src/components/Header/Header.stories.tsx
+++ apps/store/src/components/Header/Header.stories.tsx
@@ -36,37 +36,11 @@ export const MockedShoppingCartMenuItem = ({ count = 0 }) => {
   return (
     <ShoppingCartMenuItemWrapper>
       <StyledLink href={PageLink.cart()} tabIndex={0} aria-label="shopping cart">
-        <ShoppingBagIcon />
+        <ShoppingBagIcon count={count} />
       </StyledLink>
-      <Counter value={count} />
     </ShoppingCartMenuItemWrapper>
   )
 }
-
-type CounterProps = { value: number }
-
-const Counter = ({ value }: CounterProps) => {
-  if (value <= 0) return null
-
-  return <StyledCounter>{value}</StyledCounter>
-}
-
-const StyledCounter = styled.span(({ theme }) => ({
-  pointerEvents: 'none',
-  position: 'absolute',
-  top: 1,
-  right: -6,
-  display: 'inline-flex',
-  alignItems: 'center',
-  justifyContent: 'center',
-  width: 14,
-  height: 14,
-  borderRadius: '50%',
-  backgroundColor: theme.colors.gray900,
-  color: theme.colors.gray200,
-  fontSize: 10,
-  fontWeight: 'bold',
-}))
 
 const MockedNavItems = () => {
   return (
