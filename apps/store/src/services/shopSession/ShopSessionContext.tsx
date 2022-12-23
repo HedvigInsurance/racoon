de7cef14d15db18bf896e23975d59f9504eb6053
Alexander Lebedev <alexander.lebedev@gmail.com>
2022-12-23 13:36:28 +0100

    Refactor ShopSessionContext: - use ref instead of memo to ensure we always return stable value - use lazy query client-side for less re-renders and fetch during

--- apps/store/src/services/shopSession/ShopSessionContext.tsx
+++ apps/store/src/services/shopSession/ShopSessionContext.tsx
@@ -1,6 +1,7 @@
 import { useApolloClient } from '@apollo/client'
-import { createContext, PropsWithChildren, useContext, useEffect, useMemo } from 'react'
-import { ShopSessionQueryResult, useShopSessionQuery } from '@/services/apollo/generated'
+import { createContext, PropsWithChildren, useContext, useEffect, useMemo, useRef } from 'react'
+import { ShopSessionQueryResult, useShopSessionLazyQuery } from '@/services/apollo/generated'
+import { ShopSession } from '@/services/shopSession/ShopSession.types'
 import { isBrowser } from '@/utils/env'
 import { useCurrentCountry } from '@/utils/l10n/useCurrentCountry'
 import { setupShopSessionServiceClientSide } from './ShopSession.helpers'
@@ -14,7 +15,9 @@ export const ShopSessionProvider = ({ children, shopSessionId: initialShopSessio
   return <ShopSessionContext.Provider value={contextValue}>{children}</ShopSessionContext.Provider>
 }
 
-export const useShopSession = () => {
+type ShopSessionResult = ShopSessionQueryResult & { shopSession?: ShopSession }
+
+export const useShopSession = (): ShopSessionResult => {
   const { countryCode } = useCurrentCountry()
 
   const queryResult = useContext(ShopSessionContext)
@@ -24,37 +27,45 @@ export const useShopSession = () => {
     )
   }
 
-  return useMemo(() => {
-    const shopSession = queryResult.data?.shopSession
-    return {
-      ...queryResult,
-      // Ignore session from different country.
-      // This probably means old session was fetched and new one is being created
-      shopSession: shopSession?.countryCode === countryCode ? shopSession : undefined,
-    }
-  }, [queryResult, countryCode])
+  // Make sure result object is stable
+  const result = useRef<ShopSessionResult>({} as ShopSessionResult)
+  Object.assign(result.current, queryResult)
+
+  // Ignore session from different country.
+  // This probably means old session was fetched and new one is being created
+  const shopSession = queryResult.data?.shopSession
+  result.current.shopSession = shopSession?.countryCode === countryCode ? shopSession : undefined
+
+  return result.current
 }
 
 const useShopSessionContextValue = (initialShopSessionId?: string) => {
   const { countryCode } = useCurrentCountry()
   const apolloClient = useApolloClient()
   // Only used client-side
-  const shopSessionService = useMemo(
+  const shopSessionServiceClientSide = useMemo(
     () => setupShopSessionServiceClientSide(apolloClient),
     [apolloClient],
   )
-  const shopSessionId = shopSessionService.shopSessionId() ?? initialShopSessionId
 
-  const queryResult = useShopSessionQuery({
-    variables: shopSessionId ? { shopSessionId } : undefined,
-    skip: !shopSessionId,
+  const [fetchSession, queryResult] = useShopSessionLazyQuery({
+    // Prevent network requests and ensure we trigger an error on cache miss, which should never happen
+    fetchPolicy: 'cache-only',
   })
 
+  // Fetch client-side, service ensures it only happens once
   useEffect(() => {
     if (isBrowser()) {
-      shopSessionService.getOrCreate({ countryCode })
+      shopSessionServiceClientSide.getOrCreate({ countryCode }).then((shopSession) => {
+        fetchSession({ variables: { shopSessionId: shopSession.id } })
+      })
     }
-  }, [countryCode, shopSessionService])
+  }, [shopSessionServiceClientSide, countryCode, fetchSession])
+
+  // Fetch once during SSR
+  if (!isBrowser() && initialShopSessionId && !queryResult.called) {
+    fetchSession({ variables: { shopSessionId: initialShopSessionId } })
+  }
 
   return queryResult
 }
