8e203f3dbb3a931ea0d01dc8640e59872350dfe8
Alexander Lebedev <alexander.lebedev@gmail.com>
2022-12-30 13:44:42 +0100

    Separate useHandleSubmitAddToCart and useCartEntryAdd

--- apps/store/src/components/ProductPage/PurchaseForm/useHandleSubmitAddToCart.ts
+++ apps/store/src/components/ProductPage/PurchaseForm/useHandleSubmitAddToCart.ts
@@ -1,6 +1,6 @@
 import { datadogLogs } from '@datadog/browser-logs'
-import { FormEventHandler } from 'react'
-import { useCartEntryAddMutation } from '@/services/apollo/generated'
+import { FormEventHandler, useCallback } from 'react'
+import { CartEntryAddMutation, useCartEntryAddMutation } from '@/services/apollo/generated'
 import { useTracking } from '@/services/Tracking/useTracking'
 import { getOrThrowFormValue } from '@/utils/getOrThrowFormValue'
 import { FormElement } from './PurchaseForm.constants'
@@ -13,38 +13,50 @@ type Params = {
 
 // Temporary implementation, we should set startDate on priceIntent before adding to cart
 export const useHandleSubmitAddToCart = ({ cartId, onSuccess }: Params) => {
-  const [addEntry, { loading }] = useCartEntryAddMutation()
-  const tracking = useTracking()
-
-  // @TODO: expose and handle errors
+  const [addEntry, { loading }] = useCartEntryAdd()
   const handleSubmit: FormEventHandler<HTMLFormElement> = async (event) => {
     event.preventDefault()
 
     const formData = new FormData(event.currentTarget)
     const productOfferId = getOrThrowFormValue(formData, FormElement.ProductOfferId)
 
+    // @TODO: expose and handle errors
     addEntry({
       variables: { cartId, offerId: productOfferId },
-      onCompleted(data) {
-        // TODO: Handle userError
-        if (!data.cartEntriesAdd.cart) return
-
-        const addedOffer = data.cartEntriesAdd.cart.entries.find(
-          (entry) => entry.id === productOfferId,
-        )
-        if (addedOffer) {
-          tracking.reportAddToCart(addedOffer)
-        } else {
-          datadogLogs.logger.warn('Added offer missing in cart, this should not happen', {
-            cartId,
-            productOfferId,
-          })
-        }
-
+      onCompleted() {
         onSuccess(productOfferId)
       },
     })
   }
 
   return [handleSubmit, loading] as const
+}
+
+type CartEntryAddOptions = Parameters<typeof useCartEntryAddMutation>[0]
+
+const useCartEntryAdd = (mutationOptions: CartEntryAddOptions = {}) => {
+  const tracking = useTracking()
+
+  const handleCompleted = useCallback(
+    (data: CartEntryAddMutation) => {
+      if (!data.cartEntriesAdd.cart) return
+
+      const { variables } = mutationOptions
+
+      const addedOffer = data.cartEntriesAdd.cart.entries.find(
+        (entry) => entry.id === variables?.offerId,
+      )
+      if (addedOffer) {
+        tracking.reportAddToCart(addedOffer)
+      } else {
+        datadogLogs.logger.warn('Added offer missing in cart, this should not happen', {
+          ...variables,
+        })
+      }
+
+      mutationOptions.onCompleted?.(data)
+    },
+    [mutationOptions, tracking],
+  )
+  return useCartEntryAddMutation({ ...mutationOptions, onCompleted: handleCompleted })
 }
