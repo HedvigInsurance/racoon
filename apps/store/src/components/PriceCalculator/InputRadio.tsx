a6fc6ec541e0c6a60c398c9b9bb8ede41ae32db7
Robin Andeer <robin.andeer@gmail.com>
2023-01-02 15:59:03 +0100

    Update design of radio input field (#1221)

--- apps/store/src/components/PriceCalculator/InputRadio.tsx
+++ apps/store/src/components/PriceCalculator/InputRadio.tsx
@@ -1,104 +1,87 @@
 import styled from '@emotion/styled'
 import * as RadioGroup from '@radix-ui/react-radio-group'
-import { InputBase, InputBaseProps } from 'ui'
+import { motion } from 'framer-motion'
+import { Space, Text } from 'ui'
 import { SpaceFlex } from '@/components/SpaceFlex/SpaceFlex'
-import { TickIcon } from './TickIcon'
+import { useHighlightAnimation } from '@/utils/useHighlightAnimation'
 
-const Item = styled(RadioGroup.Item)(({ theme }) => ({
-  padding: 0,
-  backgroundColor: theme.colors.white,
-  boxShadow: '0px 1px 2px rgba(0, 0, 0, 0.1)',
-  borderRadius: '0.5rem',
-  width: '100%',
+type RootProps = {
+  label: string
+  children: React.ReactNode
+  value?: string
+  onValueChange?: (value: string) => void
+  required?: boolean
+  defaultValue?: string
+  name?: string
+}
 
-  border: '2px solid transparent',
-  textAlign: 'left',
-  display: 'flex',
-  flexDirection: 'column',
-  alignItems: 'stretch',
+export const Root = ({ children, label, onValueChange, ...props }: RootProps) => {
+  const { highlight, animationProps } = useHighlightAnimation()
+
+  const handleValueChange = (value: string) => {
+    highlight()
+    onValueChange?.(value)
+  }
+
+  return (
+    <Card y={0.75} {...animationProps}>
+      <Text size="md">{label}</Text>
+      <RadioGroup.Root onValueChange={handleValueChange} {...props}>
+        <SpaceFlex space={1} align="center">
+          {children}
+        </SpaceFlex>
+      </RadioGroup.Root>
+    </Card>
+  )
+}
+
+const Card = styled(motion(Space))(({ theme }) => ({
+  padding: `${theme.space.sm} ${theme.space.md}`,
+  borderRadius: theme.radius.sm,
+  backgroundColor: theme.colors.gray300,
+}))
+
+type ItemProps = {
+  label: string
+  value: string
+  id?: string
+  autoFocus?: boolean
+}
+
+export const Item = ({ value, label, id, autoFocus }: ItemProps) => {
+  const identifier = id ?? `radio-${value}`
+
+  return (
+    <SpaceFlex space={0.5} align="center">
+      <StyledItem id={identifier} value={value} autoFocus={autoFocus}>
+        <StyledIndicator />
+      </StyledItem>
+      <label htmlFor={identifier}>{label}</label>
+    </SpaceFlex>
+  )
+}
+
+const StyledItem = styled(RadioGroup.Item)(({ theme }) => ({
+  width: '1.375rem',
+  height: '1.375rem',
+
+  border: `1px solid ${theme.colors.gray500}`,
+  borderRadius: '50%',
 
   '&[data-state=checked]': {
     borderColor: theme.colors.gray900,
   },
 
   '&:focus': {
-    borderColor: theme.colors.purple500,
+    // @TODO: pending design
+    boxShadow: '0 0 0 2px rgba(0, 0, 0, 0.4)',
   },
 }))
 
-const InnerWrapper = styled.div({
-  padding: '1rem',
-  display: 'flex',
-  gap: '1rem',
-})
-
-const IndicatorWrapper = styled.div(({ theme }) => ({
-  display: 'inline-flex',
-  justifyContent: 'center',
-  alignItems: 'center',
-  height: '1.375rem',
-  width: '1.375rem',
-  border: `1px solid ${theme.colors.gray900}`,
-  borderRadius: '0.75rem',
-  cursor: 'pointer',
-  flexShrink: 0,
-}))
-
-const Indicator = styled(RadioGroup.Indicator)(({ theme }) => ({
+const StyledIndicator = styled(RadioGroup.Indicator)(({ theme }) => ({
+  display: 'block',
   backgroundColor: theme.colors.gray900,
-  borderRadius: '0.75rem',
+  borderRadius: '50%',
   width: '100%',
   height: '100%',
 }))
-
-type InputRadioProps = InputBaseProps & {
-  name: string
-  options: ReadonlyArray<{ label: string; value: string }>
-  value?: string
-  defaultValue?: string
-  onValueChange?: (value: string) => void
-  required?: boolean
-  placeholder?: string
-  autoFocus?: boolean
-}
-
-export const InputRadio = ({
-  options,
-  name,
-  onValueChange,
-  value,
-  defaultValue,
-  required,
-  autoFocus,
-  ...rest
-}: InputRadioProps) => {
-  return (
-    <InputBase {...rest}>
-      {() => (
-        <RadioGroup.Root
-          defaultValue={defaultValue}
-          value={value}
-          onValueChange={onValueChange}
-          name={name}
-          required={required}
-        >
-          <SpaceFlex direction="horizontal" space={0.5}>
-            {options.map(({ label, value: optionValue }, index) => (
-              <Item key={optionValue} value={optionValue} autoFocus={autoFocus && index === 0}>
-                <InnerWrapper>
-                  <IndicatorWrapper>
-                    <Indicator>
-                      <TickIcon size={'1.25rem'} />
-                    </Indicator>
-                  </IndicatorWrapper>
-
-                  {label}
-                </InnerWrapper>
-              </Item>
-            ))}
-          </SpaceFlex>
-        </RadioGroup.Root>
-      )}
-    </InputBase>
-  )
-}
