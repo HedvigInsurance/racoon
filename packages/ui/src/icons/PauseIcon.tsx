e9c850b23680d6bd79b9b0ac5c6cde1515b2e9d9
Guilherme S. Popolin <guilherme@hedvig.com>
2023-01-02 13:15:28 +0100

    fix(Play/Pause icons): update to use the same viewport (#1217)

--- packages/ui/src/icons/PauseIcon.tsx
+++ packages/ui/src/icons/PauseIcon.tsx
@@ -2,13 +2,17 @@ import { IconRoot, IconRootProps } from './Root'
 
 export const PauseIcon = ({ size = '1.5rem', ...props }: IconRootProps) => (
   <IconRoot
-    viewBox="0 0 256 256"
+    viewBox="0 0 24 24"
     fill="none"
     xmlns="http://www.w3.org/2000/svg"
     size={size}
     {...props}
   >
-    <rect fill="none" height="256" width="256" />
-    <path d="M216,48V208a16,16,0,0,1-16,16H164a16,16,0,0,1-16-16V48a16,16,0,0,1,16-16h36A16,16,0,0,1,216,48ZM92,32H56A16,16,0,0,0,40,48V208a16,16,0,0,0,16,16H92a16,16,0,0,0,16-16V48A16,16,0,0,0,92,32Z" />
+    <path
+      fill-rule="evenodd"
+      clip-rule="evenodd"
+      d="M7.5 4.5C6.67157 4.5 6 5.17157 6 6V18C6 18.8284 6.67157 19.5 7.5 19.5H9C9.82843 19.5 10.5 18.8284 10.5 18V6C10.5 5.17157 9.82843 4.5 9 4.5H7.5ZM15 4.5C14.1716 4.5 13.5 5.17157 13.5 6V18C13.5 18.8284 14.1716 19.5 15 19.5H16.5C17.3284 19.5 18 18.8284 18 18V6C18 5.17157 17.3284 4.5 16.5 4.5H15Z"
+      fill="currentColor"
+    />
   </IconRoot>
 )
