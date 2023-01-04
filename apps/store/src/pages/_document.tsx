13965b15a9a98792495f48081ad2a3a94ff1d05b
Robin Andeer <robin.andeer@gmail.com>
2023-01-04 12:49:28 +0100

    Add twitter + theme meta tags

--- apps/store/src/pages/_document.tsx
+++ apps/store/src/pages/_document.tsx
@@ -1,4 +1,5 @@
 import Document, { Head, Html, Main, NextScript } from 'next/document'
+import { theme } from 'ui'
 import { GTMBodyScript } from '@/services/gtm'
 import { contentFontClassName } from '@/utils/fonts'
 import { getLocaleOrFallback } from '@/utils/l10n/localeUtils'
@@ -11,7 +12,11 @@ export default class MyDocument extends Document {
   render() {
     return (
       <Html lang={this.lang()} dir="ltr">
-        <Head />
+        <Head>
+          <meta name="twitter:site" content="@hedvigapp" />
+          <meta name="twitter:card" content="summary_large_image" />
+          <meta name="theme-color" content={theme.colors.light} />
+        </Head>
         {/* Fallback for pages that don't pass className down to DOM */}
         <body className={contentFontClassName}>
           <GTMBodyScript />
