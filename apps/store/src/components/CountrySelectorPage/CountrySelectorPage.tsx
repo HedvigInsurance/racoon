fc4d2a1c2330d5b31daf56f9bc8871d6518a8409
Karnell Schultz <50870173+KarnellSchultz@users.noreply.github.com>
2023-01-04 10:42:02 +0100

    refactor country-selector page

--- apps/store/src/components/CountrySelectorPage/CountrySelectorPage.tsx
+++ apps/store/src/components/CountrySelectorPage/CountrySelectorPage.tsx
@@ -9,19 +9,6 @@ import { LOCALE_COOKIE_MAX_AGE, LOCALE_COOKIE_KEY } from '@/utils/l10n/locales'
 import { toRoutingLocale } from '@/utils/l10n/localeUtils'
 import { IsoLocale } from '@/utils/l10n/types'
 
-const Container = styled.div({
-  height: 400,
-  display: 'grid',
-  alignContent: 'center',
-  textAlign: 'center',
-  margin: '4rem 1rem',
-})
-
-const CountryOptionsContainer = styled.div({
-  display: 'grid',
-  gap: 8,
-})
-
 export const CountrySelectorPage = (props: StoryblokPageProps) => {
   const { t } = useTranslation()
   const cookiePersister = new CookiePersister(LOCALE_COOKIE_KEY)
@@ -30,26 +17,38 @@ export const CountrySelectorPage = (props: StoryblokPageProps) => {
     cookiePersister.save(toRoutingLocale(locale), undefined, { maxAge: LOCALE_COOKIE_MAX_AGE })
 
   return (
-    <Container {...props}>
-      <Space y={3}>
-        <Heading as="h1">
-          {t('COUNTRY_NOT_DETECTED', { defaultValue: 'COUNTRY_NOT_DETECTED' })}
-        </Heading>
-        <CountryOptionsContainer>
-          {Object.entries(countries).map(([country, countryData]) => (
-            <Link
-              key={country}
-              href={`/${toRoutingLocale(countryData.defaultLocale)}`}
-              passHref
-              legacyBehavior
-            >
-              <Button onClick={() => onHandleClick(countryData.defaultLocale)}>
-                {t(`COUNTRY_LABEL_${country}`, { defaultValue: `MISSING ${country}` })}
-              </Button>
-            </Link>
-          ))}
-        </CountryOptionsContainer>
-      </Space>
-    </Container>
+    <Wrapper {...props}>
+      <Section>
+        <Space y={3}>
+          <Heading as="h1">
+            {t('COUNTRY_NOT_DETECTED', { defaultValue: 'COUNTRY_NOT_DETECTED' })}
+          </Heading>
+          <Space y={0.5}>
+            {Object.entries(countries).map(([country, countryData]) => (
+              <Link
+                key={country}
+                href={`/${toRoutingLocale(countryData.defaultLocale)}`}
+                passHref
+                legacyBehavior
+              >
+                <Button onClick={() => onHandleClick(countryData.defaultLocale)}>
+                  {t(`COUNTRY_LABEL_${country}`, { defaultValue: `MISSING ${country}` })}
+                </Button>
+              </Link>
+            ))}
+          </Space>
+        </Space>
+      </Section>
+    </Wrapper>
   )
 }
+
+const Wrapper = styled(Space)({
+  maxWidth: '40rem',
+  marginLeft: 'auto',
+  marginRight: 'auto',
+})
+
+const Section = styled(Space)(({ theme }) => ({
+  padding: `${theme.space.xxl} ${theme.space.md} `,
+}))
