cc94ab170b75d665200c41bd7b693d618e36e839
Karnell Schultz <50870173+KarnellSchultz@users.noreply.github.com>
2023-01-05 13:15:58 +0100

    cleanup

--- apps/store/src/components/CartInventory/CartEntryItem.tsx
+++ apps/store/src/components/CartInventory/CartEntryItem.tsx
@@ -1,6 +1,9 @@
 import styled from '@emotion/styled'
 import { useTranslation } from 'next-i18next'
-import { Button, Dialog, Space, Text } from 'ui'
+import Link, { LinkProps } from 'next/link'
+import { FormEvent } from 'react'
+import { Button, Dialog, mq, Space, Text } from 'ui'
+import * as FullscreenDialog from '@/components/FullscreenDialog/FullscreenDialog'
 import { Pillow } from '@/components/Pillow/Pillow'
 import { SpaceFlex } from '@/components/SpaceFlex/SpaceFlex'
 import { useFormatter } from '@/utils/useFormatter'
@@ -17,30 +20,80 @@ export const CartEntryItem = (props: Props) => {
   const formatter = useFormatter()
 
   return (
-    <Wrapper>
-      <Pillow size="small" {...pillow} />
-      <Space y={1}>
-        <div>
-          <Text size="md">{title}</Text>
-          <Text size="md" color="textSecondary">
-            {/* @TODO: display "automatically switches" if cancellation is requested" */}
-            {startDate
-              ? t('CART_ENTRY_DATE_LABEL', { date: formatter.fromNow(startDate) })
-              : 'Starts sometime...'}
-          </Text>
-        </div>
-        <SpaceFlex space={0.25}>
-          <DetailsSheetDialog {...cartEntry}>
+    <FullscreenDialog.Root>
+      <MobileWrapper>
+        <Pillow size="small" {...pillow} />
+        <Space y={1}>
+          <div>
+            <Text size="md">{title}</Text>
+            <Text size="md" color="textSecondary">
+              {/* @TODO: display "automatically switches" if cancellation is requested" */}
+              {startDate
+                ? t('CART_ENTRY_DATE_LABEL', { date: formatter.fromNow(startDate), ns: 'cart' })
+                : 'Starts sometime...'}
+            </Text>
+          </div>
+          <SpaceFlex space={0.25}>
+            <LinkButtonSecondary href="#">
+              {t('VIEW_ENTRY_DETAILS_BUTTON', { ns: 'cart' })}
+            </LinkButtonSecondary>
             <Dialog.Trigger asChild>
               <Button variant="secondary" size="small">
                 {t('VIEW_ENTRY_DETAILS_BUTTON')}
               </Button>
             </Dialog.Trigger>
-          </DetailsSheetDialog>
-          <RemoveEntryDialog cartId={cartId} {...cartEntry}>
+          </SpaceFlex>
+        </Space>
+        <Text size="md">{formatter.monthlyPrice(cost)}</Text>
+      </MobileWrapper>
+
+      <DesktopWrapper>
+        <DesktopPillowTitle>
+          <DesktopPillowFlexItem>
+            <Pillow size="small" {...pillow} />
+            <Text size="md">{title}</Text>
+          </DesktopPillowFlexItem>
+        </DesktopPillowTitle>
+        <Space y={1}>
+          <div>
+            <Text size="md" color="textSecondary">
+              Bostadsrätt Tavastgatan 39 Startar 02 dec. 2023
+            </Text>
+            <Text size="md" color="textSecondary">
+              {/* @TODO: display "automatically switches" if cancellation is requested" */}
+              {startDate
+                ? t('CART_ENTRY_DATE_LABEL', { date: formatter.fromNow(startDate), ns: 'cart' })
+                : 'Starts sometime...'}
+            </Text>
+          </div>
+          <SpaceFlex space={0.25}>
+            <LinkButtonSecondary href="#">
+              {t('VIEW_ENTRY_DETAILS_BUTTON', { ns: 'cart' })}
+            </LinkButtonSecondary>
             <Dialog.Trigger asChild>
-              <Button variant="ghost" size="small">
-                {t('REMOVE_ENTRY_BUTTON')}
+              <TextButton disabled={loading}>{t('REMOVE_ENTRY_BUTTON', { ns: 'cart' })}</TextButton>
+            </Dialog.Trigger>
+          </SpaceFlex>
+        </Space>
+        <Text align="right" size="md">
+          {formatter.monthlyPrice(cost)}
+        </Text>
+      </DesktopWrapper>
+
+      <FullscreenDialog.Modal
+        Footer={
+          <>
+            <Button
+              form={REMOVE_CART_ENTRY_FORM}
+              type="submit"
+              loading={loading}
+              disabled={loading}
+            >
+              {t('REMOVE_ENTRY_MODAL_CONFIRM_BUTTON', { ns: 'cart' })}
+            </Button>
+            <FullscreenDialog.Close asChild>
+              <Button type="button" variant="ghost">
+                {t('REMOVE_ENTRY_MODAL_CANCEL_BUTTON', { ns: 'cart' })}
               </Button>
             </Dialog.Trigger>
           </RemoveEntryDialog>
@@ -51,8 +104,54 @@ export const CartEntryItem = (props: Props) => {
   )
 }
 
-const Wrapper = styled.li(({ theme }) => ({
+const MobileWrapper = styled.li(({ theme }) => ({
   display: 'grid',
   gridTemplateColumns: '3rem minmax(0, 1fr) auto',
-  gap: theme.space[3],
+  gap: theme.space.sm,
+  [mq.lg]: {
+    display: 'none',
+    gridTemplateColumns: '3fr 2fr 2fr',
+    gap: theme.space.sm,
+  },
 }))
+
+const DesktopWrapper = styled.li(({ theme }) => ({
+  display: 'none',
+  [mq.lg]: {
+    display: 'grid',
+    gridTemplateColumns: '3fr 2fr 2fr',
+    gap: theme.space.sm,
+  },
+}))
+const DesktopPillowTitle = styled.div({
+  display: 'flex',
+  alignItems: 'flex-start',
+})
+const DesktopPillowFlexItem = styled.div(({ theme }) => ({
+  display: 'flex',
+  alignItems: 'center',
+  gap: theme.space.sm,
+}))
+
+const SecondaryButton = styled.button(({ theme }) => ({
+  fontSize: theme.fontSizes[1],
+  paddingLeft: theme.space.md,
+  paddingRight: theme.space.md,
+  paddingTop: theme.space.xs,
+  paddingBottom: theme.space.xs,
+  borderRadius: theme.radius.xs,
+  backgroundColor: theme.colors.gray200,
+  boxShadow: '0px 1px 2px rgba(0, 0, 0, 0.15)',
+
+  ':disabled': {
+    opacity: 0.6,
+  },
+}))
+
+const LinkButtonSecondary = styled(SecondaryButton)<LinkProps>()
+LinkButtonSecondary.defaultProps = { as: Link }
+
+const TextButton = styled(SecondaryButton)({
+  backgroundColor: 'transparent',
+  boxShadow: 'none',
+})
