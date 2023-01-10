ff55757c9b548354ea153a2f997c037180ac0499
Robin Andeer <robin.andeer@gmail.com>
2023-01-10 17:13:49 +0100

    Update cart notification design for mobile

--- apps/store/src/components/CartNotification/CartNotification.stories.tsx
+++ apps/store/src/components/CartNotification/CartNotification.stories.tsx
@@ -1,76 +1,36 @@
-import { Story } from '@storybook/react'
-import { Button, Space, Dialog } from 'ui'
-import { CurrencyCode } from '@/services/apollo/generated'
-import { useFormatter } from '@/utils/useFormatter'
-import { Header } from './Header'
-import { Main } from './Main'
-import { ProductItem } from './ProductItem'
+import { ComponentStory } from '@storybook/react'
+import { Button, Dialog } from 'ui'
+import { CartNotificationContent } from './CartToast'
 
 const config = {
   title: 'Cart Notification',
-  argTypes: {},
 }
 
-type Props = {
-  name: string
-  price: number
-  currencyCode: CurrencyCode
+export const Open: ComponentStory<typeof CartNotificationContent> = (props) => (
+  <Dialog.Root open>
+    <CartNotificationContent {...props} />
+  </Dialog.Root>
+)
+Open.argTypes = { onClose: { action: 'onClose' } }
+Open.args = {
+  name: 'Hemförsäkring',
+  price: '179 kr/mån',
 }
 
-const Template: Story<Props> = (props) => {
-  return (
-    <>
-      <Header>Insurance added to cart</Header>
-      <NotificationContent {...props} />
-    </>
-  )
-}
-
-export const Content = Template.bind({})
-Content.args = {
-  name: 'Hedvig Home',
-  price: 179,
-  currencyCode: CurrencyCode.Sek,
-}
-
-const TemplateWithDialog: Story<Props> = (props) => {
+export const WithDialog: ComponentStory<typeof CartNotificationContent> = (props) => {
   return (
     <Dialog.Root>
       <Dialog.Trigger asChild>
         <Button>Open notification</Button>
       </Dialog.Trigger>
-      <Dialog.Content>
-        <Main y={1.5}>
-          <Header>Insurance added to cart</Header>
-          <NotificationContent {...props} />
-        </Main>
-      </Dialog.Content>
+      <CartNotificationContent {...props} />
     </Dialog.Root>
   )
 }
-
-export const WithDialog = TemplateWithDialog.bind({})
+WithDialog.argTypes = { onClose: { action: 'onClose' } }
 WithDialog.args = {
-  name: 'Hedvig Home',
-  price: 179,
-  currencyCode: CurrencyCode.Sek,
+  name: 'Hemförsäkring',
+  price: '179 kr/mån',
 }
 
 export default config
-
-// TODO: use CartToast or nothing - does not make sense to have separate presentational component here in story
-const NotificationContent = ({ name, price, currencyCode }: Props) => {
-  const formatter = useFormatter()
-
-  return (
-    <Space y={1.5}>
-      <ProductItem name={name} price={formatter.money({ amount: price, currencyCode })} />
-
-      <Space y={0.5}>
-        <Button>Proceed to cart (1)</Button>
-
-        <Button variant="ghost">Continue shopping</Button>
-      </Space>
-    </Space>
-  )
-}
