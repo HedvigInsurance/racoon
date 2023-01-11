6bbdeed917d0fe1695d053401a7978f07e16bfc4
Alexander Lebedev <alexander.lebedev@gmail.com>
2023-01-11 11:08:28 +0100

    Fix datadog config to send correct host in client and server logs

--- apps/store/src/services/logger/config.ts
+++ apps/store/src/services/logger/config.ts
@@ -1,17 +1,19 @@
 export const COMMON_CONFIG = {
-  service: process.env.DD_SERVICE || process.env.NEXT_PUBLIC_DATADOG_SERVICE_NAME,
   site: 'datadoghq.eu',
   env: process.env.NEXT_PUBLIC_DATADOG_ENV || 'dev',
 }
 
 export const SERVER_CONFIG = {
   ...COMMON_CONFIG,
+  service: process.env.DD_SERVICE,
   apiKey: process.env.DATADOG_API_KEY,
-  host: process.env.VERCEL_URL,
+  host: process.env.VERCEL_URL || 'localhost',
 }
 
 export const CLIENT_CONFIG = {
   ...COMMON_CONFIG,
+  service: process.env.NEXT_PUBLIC_DATADOG_SERVICE_NAME,
+  host: process.env.NEXT_PUBLIC_VERCEL_URL || 'localhost',
   sampleRate: 100,
   version: process.env.NEXT_PUBLIC_DATADOG_VERSION || process.env.NEXT_PUBLIC_VERCEL_GIT_COMMIT_SHA,
 }
