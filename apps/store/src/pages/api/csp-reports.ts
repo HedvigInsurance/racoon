ff2637ae4c5f96831b8563f212d1c915f16e4f1b
Alexander Lebedev <alexander.lebedev@gmail.com>
2023-01-11 10:37:31 +0100

    Log CSP violations without pino, use single-line format for Datadog parsing

--- apps/store/src/pages/api/csp-reports.ts
+++ apps/store/src/pages/api/csp-reports.ts
@@ -1,14 +1,13 @@
 import { NextApiRequest, NextApiResponse } from 'next'
-import logger from '@/services/logger/server'
 
 const cspReports = (req: NextApiRequest, res: NextApiResponse<void>) => {
   try {
     const report = JSON.parse(req.body)['csp-report']
     if (report) {
-      logger.child({ csp: report }).info('csp violation')
+      console.log('csp_violation', JSON.stringify(report))
     }
   } catch (err) {
-    logger.error('Failed to parse CSP report', err)
+    console.error('Failed to parse CSP report', err)
   }
   return res.status(204).send()
 }
