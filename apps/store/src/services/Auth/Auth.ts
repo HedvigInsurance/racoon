4d5d018cfc8f3319cfc5ae99a05219ccad26a4e0
Robin Andeer <robin.andeer@gmail.com>
2023-01-13 13:18:31 +0100

    Implement login endpoint for Swedish BankID method (#1318)

--- apps/store/src/services/Auth/Auth.ts
+++ apps/store/src/services/Auth/Auth.ts
@@ -1,5 +1,6 @@
 import { getCookie, setCookie, deleteCookie } from 'cookies-next'
 import { OptionsType } from 'cookies-next/lib/types'
+import { PageLink } from '@/utils/PageLink'
 
 const COOKIE_KEY = '_hvsession'
 const ACCESS_TOKEN_SESSION_FIELD = 'token'
@@ -13,8 +14,8 @@ const OPTIONS: OptionsType = {
   }),
 }
 
-export const save = (accessToken: string) => {
-  setCookie(COOKIE_KEY, serialize(accessToken), OPTIONS)
+export const save = (accessToken: string, { req, res }: CookieParams = {}) => {
+  setCookie(COOKIE_KEY, serialize(accessToken), { req, res, ...OPTIONS })
 }
 
 export type CookieParams = Pick<OptionsType, 'req' | 'res'>
@@ -47,5 +48,19 @@ const deserialize = (value: string) => {
   } catch (error) {
     console.warn('Unable to deserialize session')
     return undefined
+  }
+}
+
+type LoginSEOptions = Partial<{ nextUrl: string }>
+
+export const loginSE = async (ssn: string, { nextUrl }: LoginSEOptions = {}) => {
+  const response = await fetch(PageLink.apiLoginSe(), {
+    method: 'POST',
+    body: JSON.stringify({ ssn, nextUrl }),
+    headers: { 'Content-Type': 'application/json' },
+  })
+
+  if (!response.ok) {
+    throw new Error('Unable to login')
   }
 }
--- apps/store/src/utils/auth.ts
+++ apps/store/src/utils/auth.ts
@@ -1,30 +1,114 @@
 import { CookieParams, getAuthHeader } from '@/services/Auth/Auth'
 
+enum MemberLoginMethod {
+  SE_BANKID = 'SE_BANKID',
+}
+
+const BASE_URL = process.env.NEXT_PUBLIC_AUTH_ENDPOINT
+const Endpoint = {
+  TOKEN: `${BASE_URL}/oauth/token`,
+  MEMBER_AUTH_CODES: `${BASE_URL}/member-authorization-codes`,
+  MEMBER_LOGIN: `${BASE_URL}/member-login`,
+  LOGIN_STATUS: (statusUrl: string) => `${BASE_URL}${statusUrl}`,
+} as const
+
+type TokenResponse = { access_token?: string }
+
 export const exchangeAuthorizationCode = async (authorizationCode: string): Promise<string> => {
-  const url = `${process.env.NEXT_PUBLIC_AUTH_ENDPOINT}/oauth/token`
-  const data = await fetchJson<{ access_token: string }>(url, {
+  const { access_token: accessToken } = await fetchJson<TokenResponse>(Endpoint.TOKEN, {
     method: 'POST',
     body: JSON.stringify({
       grant_type: 'authorization_code',
       authorization_code: authorizationCode,
     }),
   })
-  const accessToken = data.access_token
   if (!accessToken) {
     throw new Error('No access_token field in response')
   }
   return accessToken
 }
 
+type MemberAuthCodeResponse = { authorizationCode: string }
+
 export const createAuthorizationCode = async (params?: CookieParams) => {
-  const url = `${process.env.NEXT_PUBLIC_AUTH_ENDPOINT}/member-authorization-codes`
-  const data = await fetchJson<{ authorizationCode: string }>(url, {
+  const data = await fetchJson<MemberAuthCodeResponse>(Endpoint.MEMBER_AUTH_CODES, {
     method: 'POST',
-    headers: {
-      ...getAuthHeader(params),
-    },
+    headers: getAuthHeader(params),
   })
   return data.authorizationCode
+}
+
+type MemberLoginResponseSuccess = {
+  result: 'success'
+  id: string
+  method: MemberLoginMethod
+  statusUrl: string
+
+  seBankIdProperties: {
+    orderRef: string
+    autoStartToken: string
+  }
+}
+
+type MemberLoginResponseError = {
+  result: 'error'
+  reason: string
+}
+
+type MemberLoginResponse = MemberLoginResponseSuccess | MemberLoginResponseError
+
+const memberLoginCreateSE = async (personalNumber: string) => {
+  const data = await fetchJson<MemberLoginResponse>(Endpoint.MEMBER_LOGIN, {
+    method: 'POST',
+    body: JSON.stringify({
+      method: MemberLoginMethod.SE_BANKID,
+      personalNumber,
+    }),
+  })
+
+  if (data.result === 'error') {
+    throw new Error('Failed to login: ' + data.reason)
+  }
+
+  return data
+}
+
+type MemberLoginStatusResponse =
+  | {
+      status: 'PENDING' | 'FAILED'
+      statusText: string
+    }
+  | {
+      status: 'COMPLETED'
+      authorizationCode: string
+    }
+
+const memberLoginStatus = async (statusUrl: string) => {
+  return await fetchJson<MemberLoginStatusResponse>(statusUrl)
+}
+
+const memberLoginStatusPoll = async (statusUrl: string): Promise<string> => {
+  const result = await memberLoginStatus(statusUrl)
+
+  if (result.status === 'COMPLETED') {
+    return result.authorizationCode
+  }
+
+  if (result.status === 'FAILED') {
+    throw new Error('Login failed: ' + result.statusText)
+  }
+
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(memberLoginStatusPoll(statusUrl))
+    }, 1000)
+  })
+}
+
+export const memberLoginSE = async (ssn: string) => {
+  const { statusUrl } = await memberLoginCreateSE(ssn)
+  const authorizationCode = await memberLoginStatusPoll(Endpoint.LOGIN_STATUS(statusUrl))
+  return await exchangeAuthorizationCode(authorizationCode)
 }
 
 const fetchJson = async <T extends object>(
