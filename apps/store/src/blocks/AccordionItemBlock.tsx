945b818f9bde3d652d9d04dbdce1991e6e09e73c
Guilherme S. Popolin <guilherme@hedvig.com>
2023-01-18 13:26:26 +0100

    GRW-2095 - fix(AccordionBlock): desktop layout (#1360)
    * fix(AccordionBlock): deskptop layout
    
    * styles(Accordion): updated content text color
    
    * fix(AccordionBlock): covers scenario where title and discription are not provided

--- apps/store/src/blocks/AccordionItemBlock.tsx
+++ apps/store/src/blocks/AccordionItemBlock.tsx
@@ -1,5 +1,6 @@
 import { storyblokEditable, renderRichText, ISbRichtext } from '@storyblok/react'
 import { useId } from 'react'
+import { Text } from 'ui'
 import * as Accordion from '@/components/Accordion/Accordion'
 import { SbBaseBlockProps } from '@/services/storyblok/storyblok'
 
@@ -13,7 +14,9 @@ export const AccordionItemBlock = ({ blok }: AccordionItemBlockProps) => {
   const contentHtml = renderRichText(blok.body)
   return (
     <Accordion.Item value={blok._uid || defaultId} {...storyblokEditable(blok)}>
-      <Accordion.HeaderWithTrigger>{blok.title}</Accordion.HeaderWithTrigger>
+      <Accordion.HeaderWithTrigger>
+        <Text size={{ _: 'md', md: 'xl' }}>{blok.title}</Text>
+      </Accordion.HeaderWithTrigger>
       <Accordion.Content>
         <div dangerouslySetInnerHTML={{ __html: contentHtml }} />
       </Accordion.Content>
