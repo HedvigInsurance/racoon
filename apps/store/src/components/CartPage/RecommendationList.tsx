0514c30467138539806ed26bf655e8351590de9c
Robin Andeer <robin.andeer@gmail.com>
2023-01-18 16:20:24 +0100

    Integrate product recommendations on Cart page (#1358)
    ## Describe your changes
    
    Integrate list of product recommendations on Cart page.
    
    Add recommendations when fetching `ShopSession`.
    
    Refactor Cart page to not use dynamic theme.
    
    ![Screenshot 2023-01-18 at 13.20.15.png](https://graphite-user-uploaded-assets-prod.s3.amazonaws.com/OgXegTwxM9IeuXHZyw5h/fd1cddbe-ff2f-4ee1-9392-8ea4aea69acd/Screenshot%202023-01-18%20at%2013.20.15.png)
    
    ![Screenshot 2023-01-18 at 13.19.56.png](https://graphite-user-uploaded-assets-prod.s3.amazonaws.com/OgXegTwxM9IeuXHZyw5h/9980df20-ae71-43db-b6e0-307e59c27d33/Screenshot%202023-01-18%20at%2013.19.56.png)
    
    ## Justify why they are needed
    
    We should probably consider only fetching recommendations on the Cart page and not with every shop session request but don't want to optimize too early.
    
    The API is not 100% working so that's why I didn't include photos.
    
    ## Jira issue(s): []
    
    <!--
    If there is a Jira issue, add the key (e.g. GRW-123) between the brackets.
    A link to that issue will automatically be created.
    -->
    
    ## Checklist before requesting a review
    
    - [ ] I have performed a self-review of my code

---
+++ apps/store/src/components/CartPage/RecommendationList.tsx
@@ -0,0 +1,103 @@
+import styled from '@emotion/styled'
+import { useTranslation } from 'next-i18next'
+import Image from 'next/image'
+import Link from 'next/link'
+import { Heading, mq, Space, Text, theme } from 'ui'
+import { ProductRecommendationFragment } from '@/services/apollo/generated'
+
+type Props = {
+  recommendations: Array<ProductRecommendationFragment>
+}
+
+export const RecommendationList = ({ recommendations }: Props) => {
+  const { t } = useTranslation('cart')
+
+  return (
+    <Wrapper y={1.5}>
+      <MobileHeader>
+        <Heading as="h2" variant="standard.24">
+          {t('RECOMMENDATIONS_HEADING')}
+        </Heading>
+      </MobileHeader>
+      <DesktopHeader>
+        <Heading as="h2" variant="standard.32">
+          {t('RECOMMENDATIONS_HEADING')}
+        </Heading>
+      </DesktopHeader>
+
+      <List>
+        {recommendations.map((recommendation) => (
+          <Link key={recommendation.id} href={recommendation.pageLink}>
+            <Space y={0.5}>
+              <StyledImage
+                // TODO: Use the correct image
+                src="https://a.storyblok.com/f/165473/330x396/573a75c77d/home-low.png"
+                alt=""
+                width={330}
+                height={396}
+              />
+              <ListItemContent>
+                <Heading as="h3" variant="standard.18">
+                  {recommendation.displayNameShort}
+                </Heading>
+                <Text size="lg" color="textSecondary">
+                  {recommendation.displayNameFull}
+                </Text>
+              </ListItemContent>
+            </Space>
+          </Link>
+        ))}
+      </List>
+    </Wrapper>
+  )
+}
+
+const Wrapper = styled(Space)({
+  paddingInline: theme.space.xs,
+  paddingBottom: theme.space.xl,
+
+  maxWidth: '40rem',
+  marginInline: 'auto',
+
+  [mq.lg]: {
+    maxWidth: '100%',
+  },
+})
+
+const MobileHeader = styled.div({
+  paddingInline: theme.space.xs,
+
+  [mq.lg]: {
+    display: 'none',
+  },
+})
+
+const DesktopHeader = styled.div({
+  display: 'none',
+
+  [mq.lg]: {
+    display: 'flex',
+    justifyContent: 'center',
+  },
+})
+
+const List = styled.div({
+  display: 'grid',
+  gridGap: theme.space.md,
+
+  [mq.sm]: {
+    gridTemplateColumns: 'repeat(auto-fit, 19rem)',
+  },
+
+  [mq.lg]: {
+    justifyContent: 'center',
+  },
+})
+
+const ListItemContent = styled.div({
+  paddingInline: theme.space.xs,
+
+  [mq.lg]: { paddingInline: 0 },
+})
+
+const StyledImage = styled(Image)({ width: '100%' })
