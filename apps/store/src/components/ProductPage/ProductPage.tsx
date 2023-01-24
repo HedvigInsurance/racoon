fc14028a18c1bcc32fe9f15517479ba840e31927
Alexander Lebedev <alexander.lebedev@gmail.com>
2023-01-24 14:39:05 +0100

    Track select_item ecommerce event when user visits product page

--- apps/store/src/components/ProductPage/ProductPage.tsx
+++ apps/store/src/components/ProductPage/ProductPage.tsx
@@ -1,9 +1,19 @@
 import { StoryblokComponent } from '@storyblok/react'
+import { useEffect } from 'react'
 import { PriceIntentContextProvider } from '@/components/ProductPage/PriceIntentContext'
+import { useTracking } from '@/services/Tracking/useTracking'
 import { ProductPageProps } from './ProductPage.types'
 import { ProductPageContextProvider } from './ProductPageContext'
 
 export const ProductPage = ({ story, ...props }: ProductPageProps) => {
+  // TODO: Provide some tracking API to use with router.ready to check if current page
+  //  is the first during window lifetime -> then replace effect with event subscriptions
+  const { id, displayNameShort } = props.productData
+  const tracking = useTracking()
+  useEffect(() => {
+    tracking.reportViewProductPage({ id, displayNameShort })
+  }, [tracking, id, displayNameShort])
+
   return (
     <ProductPageContextProvider {...props} story={story}>
       <PriceIntentContextProvider>
