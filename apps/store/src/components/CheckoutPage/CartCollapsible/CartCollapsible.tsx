72f403f7341253e840c0fe5e7fc01c4119c3e98f
Robin Andeer <robin.andeer@gmail.com>
2023-01-27 16:43:12 +0100

    Implement 12 column grid on Checkout page

--- apps/store/src/components/CheckoutPage/CartCollapsible/CartCollapsible.tsx
+++ apps/store/src/components/CheckoutPage/CartCollapsible/CartCollapsible.tsx
@@ -1,7 +1,7 @@
 import styled from '@emotion/styled'
 import * as Collapsible from '@radix-ui/react-collapsible'
 import { ReactNode, useState } from 'react'
-import { MinusIcon, Text } from 'ui'
+import { MinusIcon, Text, theme } from 'ui'
 import { CartCost } from '@/components/CartInventory/CartInventory.types'
 import { PlusIcon } from '@/components/Perils/PlusIcon'
 import { useFormatter } from '@/utils/useFormatter'
@@ -36,25 +36,26 @@ export const CartCollapsible = ({ children, title, cost }: Props) => {
   )
 }
 
-const Trigger = styled(Collapsible.Trigger)(({ theme }) => ({
+const Trigger = styled(Collapsible.Trigger)({
   width: '100%',
+  height: '3.5rem',
   display: 'flex',
   justifyContent: 'space-between',
   alignItems: 'center',
-  gap: theme.space[1],
+  gap: theme.space.xxs,
 
   backgroundColor: theme.colors.light,
   ':focus-visible': {
     borderRadius: theme.radius.xs,
     boxShadow: `${theme.colors.light} 0 0 0 3px, ${theme.colors.textPrimary} 0 0 0 4px`,
   },
-}))
+})
 
-const SpaceBetween = styled.div(({ theme }) => ({
+const SpaceBetween = styled.div({
   flex: 1,
   width: '100%',
   display: 'flex',
   justifyContent: 'space-between',
   alignItems: 'center',
-  gap: theme.space[2],
-}))
+  gap: theme.space.sm,
+})
