8a6f6c4edb94a064b0f67f5eaf2f89cb793449e3
Guilherme Popolin <guilherme@hedvig.com>
2023-01-30 14:35:41 +0100

    feat(RichtTextContent): added support for links

--- apps/store/src/blocks/AccordionItemBlock.tsx
+++ apps/store/src/blocks/AccordionItemBlock.tsx
@@ -1,3 +1,4 @@
+import styled from '@emotion/styled'
 import { storyblokEditable, renderRichText, ISbRichtext } from '@storyblok/react'
 import { useId } from 'react'
 import { Text } from 'ui'
@@ -18,10 +19,14 @@ export const AccordionItemBlock = ({ blok }: AccordionItemBlockProps) => {
       <Accordion.HeaderWithTrigger>
         <Text size={{ _: 'md', md: 'xl' }}>{blok.title}</Text>
       </Accordion.HeaderWithTrigger>
-      <Accordion.Content asChild>
+      <Content asChild>
         <RichTextContent dangerouslySetInnerHTML={{ __html: contentHtml }} />
-      </Accordion.Content>
+      </Content>
     </Accordion.Item>
   )
 }
 AccordionItemBlock.blockName = 'accordionItem'
+
+const Content = styled(Accordion.Content)({
+  paddingTop: 0,
+})
