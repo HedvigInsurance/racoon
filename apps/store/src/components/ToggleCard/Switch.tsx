537762de870b0d4106dcdcbb8a61a122401e5418
Robin Andeer <robin.andeer@gmail.com>
2023-01-30 10:59:49 +0100

    Use theme values in ToggleCard (#1460)

--- apps/store/src/components/ToggleCard/Switch.tsx
+++ apps/store/src/components/ToggleCard/Switch.tsx
@@ -1,5 +1,6 @@
 import styled from '@emotion/styled'
 import * as RadixSwitch from '@radix-ui/react-switch'
+import { theme } from 'ui'
 
 export type SwitchProps = RadixSwitch.SwitchProps
 
@@ -11,7 +12,7 @@ export const Switch = (props: SwitchProps) => {
   )
 }
 
-const SwitchWrapper = styled(RadixSwitch.Root)(({ theme }) => ({
+const SwitchWrapper = styled(RadixSwitch.Root)({
   boxSizing: 'border-box',
   width: '1.75rem',
   borderRadius: 9999,
@@ -19,24 +20,24 @@ const SwitchWrapper = styled(RadixSwitch.Root)(({ theme }) => ({
   alignItems: 'center',
   justifyContent: 'flex-start',
 
-  backgroundColor: theme.colors.gray500,
+  backgroundColor: theme.colors.opaque3,
   borderWidth: 1,
   borderStyle: 'solid',
   borderColor: 'transparent',
 
-  ':focus': {
-    borderColor: theme.colors.gray900,
+  ':focus-visible': {
+    borderColor: theme.colors.gray1000,
   },
 
   '&[data-state=checked]': {
-    backgroundColor: '#23CD61',
+    backgroundColor: theme.colors.greenElement,
     justifyContent: 'flex-end',
   },
-}))
+})
 
-const SwitchHandle = styled(RadixSwitch.Thumb)(({ theme }) => ({
+const SwitchHandle = styled(RadixSwitch.Thumb)({
   width: '1rem',
   height: '1rem',
   borderRadius: '100%',
-  backgroundColor: theme.colors.white,
-}))
+  backgroundColor: theme.colors.textNegative,
+})
