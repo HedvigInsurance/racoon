2d549c50fd5bef8f05fb95024189a45e3a831f49
Robin Andeer <robin.andeer@gmail.com>
2023-02-01 10:24:06 +0100

    Add endpoint to set default campaign code (#1499)
    * Add support for setting a campaign code when resuming a shop session
    
    * Add endpoint to set default campaign code

---
+++ apps/store/src/pages/api/campaign/[code].ts
@@ -0,0 +1,74 @@
+import { NextApiRequest, NextApiResponse } from 'next'
+import { initializeApollo } from '@/services/apollo/client'
+import {
+  RedeemCampaignDocument,
+  RedeemCampaignMutation,
+  RedeemCampaignMutationVariables,
+} from '@/services/apollo/generated'
+import { setupShopSessionServiceServerSide } from '@/services/shopSession/ShopSession.helpers'
+import { ShopSession } from '@/services/shopSession/ShopSession.types'
+import { getCountryByLocale } from '@/utils/l10n/countryUtils'
+import { getUrlLocale } from '@/utils/l10n/localeUtils'
+import { ORIGIN_URL, PageLink } from '@/utils/PageLink'
+
+/**
+ * Get or create a ShopSession, redeem a campaign code, and navigate to the next page.
+ */
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+  const { query, url } = req
+  const fallbackRedirect: [number, string] = [307, PageLink.home()]
+
+  const nextURL = new URL(ORIGIN_URL)
+  const nextQueryParam = query['next']
+  if (typeof nextQueryParam !== 'string') {
+    console.error('Missing next query parameter: ', url)
+    return res.redirect(...fallbackRedirect)
+  }
+  nextURL.pathname = nextQueryParam
+
+  const locale = getUrlLocale(nextURL.toString())
+  fallbackRedirect[1] = PageLink.home({ locale })
+
+  const { countryCode } = getCountryByLocale(locale)
+
+  const apolloClient = initializeApollo({ req, res })
+  const shopSessionService = setupShopSessionServiceServerSide({ apolloClient, req, res })
+
+  let shopSession: ShopSession
+  try {
+    shopSession = await shopSessionService.getOrCreate({ countryCode })
+  } catch (error) {
+    console.error('Error creating shop session', error)
+    return res.redirect(...fallbackRedirect)
+  }
+
+  const campaignCode = query['code']
+  if (typeof campaignCode !== 'string') {
+    console.error('Missing campaign code query parameter: ', url)
+    return res.redirect(...fallbackRedirect)
+  }
+
+  try {
+    const response = await apolloClient.mutate<
+      RedeemCampaignMutation,
+      RedeemCampaignMutationVariables
+    >({
+      mutation: RedeemCampaignDocument,
+      variables: { cartId: shopSession.cart.id, code: campaignCode },
+    })
+    if (!response.data?.cartRedeemCampaign.cart) {
+      const message = `Invalid campaign code: ${campaignCode}`
+      console.warn(message, response.data?.cartRedeemCampaign.userError?.message)
+      return res.redirect(...fallbackRedirect)
+    }
+  } catch (error) {
+    console.error(`Unable to redeem campaign: ${campaignCode}`, error)
+    return res.redirect(...fallbackRedirect)
+  }
+
+  const destination = nextURL.toString()
+  console.log(`Re-directing to destination: ${destination}`)
+  return res.redirect(307, destination)
+}
+
+export default handler
