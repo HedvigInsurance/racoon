8c70bbc89f4ef33b722a79d3e22b1c396516e4f3
Alexander Lebedev <alexander.lebedev@gmail.com>
2023-02-06 11:54:10 +0100

    Move BankIdIcon to ui

--- apps/store/src/components/PriceCalculator/SsnSeSection.tsx
+++ apps/store/src/components/PriceCalculator/SsnSeSection.tsx
@@ -126,10 +126,7 @@ const AuthenticationRequiredSsnSection = ({
 
 const AuthPromptWrapper = styled.div({
   position: 'absolute',
-  top: 0,
-  bottom: 0,
-  left: 0,
-  right: 0,
+  inset: 0,
   display: 'flex',
   flexDirection: 'column',
   justifyContent: 'center',
