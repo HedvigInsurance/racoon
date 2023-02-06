4543292b8dbabceb5f0d7f13225cb19c783c6564
Robin Andeer <robin.andeer@gmail.com>
2023-02-06 11:26:34 +0100

    Implement insurable limits as accordion

--- apps/store/src/components/InsurableLimits/InsurableLimits.tsx
+++ apps/store/src/components/InsurableLimits/InsurableLimits.tsx
@@ -1,39 +1,102 @@
 import styled from '@emotion/styled'
+import * as Accordion from '@radix-ui/react-accordion'
 import React from 'react'
-import { mq, theme } from 'ui'
+import { mq, PlusIcon, Text, theme } from 'ui'
 
-type InsurableLimitsProps = {
-  children: React.ReactNode
-  className?: string
+type InsurableLimit = {
+  label: string
+  description: string
+  value: string
 }
 
-export const InsurableLimits = ({ children, className }: InsurableLimitsProps) => {
+type InsurableLimitsProps = {
+  items: Array<InsurableLimit>
+}
+
+export const InsurableLimits = ({ items }: InsurableLimitsProps) => {
   return (
-    <Wrapper className={className}>
-      {React.Children.map(children, (child, index) => (
-        <ScrollableItem key={index}>{child}</ScrollableItem>
-      ))}
-    </Wrapper>
+    <Accordion.Root type="single" defaultValue={items[0].label}>
+      <Layout>
+        {items.map((item) => (
+          <AccordionItem key={item.label} value={item.label}>
+            <div>
+              <Accordion.Header>
+                <AccordionTrigger>
+                  <SingleLineText size="xl">{item.label}</SingleLineText>
+                  <PlusIcon />
+                </AccordionTrigger>
+              </Accordion.Header>
+              <Accordion.Content>
+                <Text size="xl">{item.description}</Text>
+              </Accordion.Content>
+            </div>
+            <Text size="xxl">{item.value}</Text>
+          </AccordionItem>
+        ))}
+      </Layout>
+    </Accordion.Root>
   )
 }
 
-const Wrapper = styled.div({
+const Layout = styled.div({
   display: 'flex',
-  gap: theme.space.xs,
-  scrollSnapType: 'x mandatory',
-  overflowX: 'auto',
+  flexDirection: 'column',
+  gap: theme.space.md,
 
   [mq.lg]: {
-    gap: theme.space.md,
+    flexDirection: 'row',
   },
 })
 
-const ScrollableItem = styled.div({
-  scrollSnapAlign: 'center',
-  flexShrink: 0,
-  maxWidth: '96%',
+const AccordionItem = styled(Accordion.Item)({
+  backgroundColor: theme.colors.opaque1,
+  borderRadius: theme.radius.md,
+  padding: theme.space.lg,
 
-  [mq.sm]: {
-    maxWidth: '20rem',
+  display: 'flex',
+  flexDirection: 'column',
+  justifyContent: 'space-between',
+  minHeight: '12rem',
+  transition: 'background-color 0.2s ease-in-out, min-height 0.2s ease-in-out',
+
+  '&[data-state=open]': {
+    backgroundColor: theme.colors.blueFill2,
+    minHeight: '24rem',
+  },
+
+  [mq.lg]: {
+    height: '24rem',
+
+    '&[data-state=open]': {
+      flex: 1,
+    },
+
+    '&[data-state=closed]': {
+      width: '20%',
+    },
+  },
+})
+
+const SingleLineText = styled(Text)({
+  whiteSpace: 'nowrap',
+  overflow: 'hidden',
+  textOverflow: 'ellipsis',
+})
+
+const AccordionTrigger = styled(Accordion.Trigger)({
+  width: '100%',
+  display: 'flex',
+  justifyContent: 'space-between',
+  alignItems: 'center',
+  gap: theme.space.md,
+  cursor: 'pointer',
+
+  '&[data-state=open]': {
+    display: 'none',
+  },
+
+  ':focus-visible': {
+    borderRadius: theme.radius.xs,
+    boxShadow: `${theme.colors.textPrimary} 0 0 0 1px`,
   },
 })
