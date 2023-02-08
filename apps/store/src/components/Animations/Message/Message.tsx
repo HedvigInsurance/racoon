0ecdb0bb8d79c91325a20300bfec29a09ba4a0df
Gustav Eén <gustaveen@gmail.com>
2023-02-08 10:34:08 +0100

    Dynamically import Lottie animations

--- apps/store/src/components/Animations/Message/Message.tsx
+++ apps/store/src/components/Animations/Message/Message.tsx
@@ -1,5 +1,7 @@
-import { LottieAnimation } from '../Root'
+import dynamic from 'next/dynamic'
 import animationData from './message.json'
+
+const LottieAnimation = dynamic(() => import('../Root').then((mod) => mod.LottieAnimation))
 
 export const Message = () => {
   return <LottieAnimation autoplay loop src={animationData} />
--- apps/store/src/components/Animations/NumberPad/NumberPad.tsx
+++ apps/store/src/components/Animations/NumberPad/NumberPad.tsx
@@ -1,5 +1,7 @@
-import { LottieAnimation } from '../Root'
+import dynamic from 'next/dynamic'
 import animationData from './numberPad.json'
+
+const LottieAnimation = dynamic(() => import('../Root').then((mod) => mod.LottieAnimation))
 
 export const NumberPad = () => {
   return <LottieAnimation autoplay loop src={animationData} />
