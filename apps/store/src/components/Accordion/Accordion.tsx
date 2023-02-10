63061187c6350a41dde7d99b9a6c24988f489205
Guilherme S. Popolin <guilherme@hedvig.com>
2023-02-10 10:07:03 +0100

    feat(Accordion): apply hover styles (#1607)

--- apps/store/src/components/Accordion/Accordion.tsx
+++ apps/store/src/components/Accordion/Accordion.tsx
@@ -10,20 +10,6 @@ export const Root = styled(AccordionPrimitives.Root)({
   gap: theme.space.xxs,
 })
 
-export const Item = styled(AccordionPrimitives.Item)({
-  backgroundColor: theme.colors.opaque1,
-  borderRadius: theme.radius.sm,
-  paddingInline: theme.space.md,
-  paddingBlock: theme.space.sm,
-
-  [mq.lg]: {
-    paddingInline: theme.space.lg,
-    paddingBlock: theme.space.md,
-  },
-})
-
-const Header = AccordionPrimitives.Header
-
 const Trigger = styled(AccordionPrimitives.Trigger)({
   width: '100%',
   cursor: 'pointer',
@@ -36,6 +22,31 @@ const Trigger = styled(AccordionPrimitives.Trigger)({
   [mq.lg]: {
     fontSize: theme.fontSizes.lg,
   },
+
+  '@media (hover: hover)': {
+    ':hover': {
+      cursor: 'pointer',
+      backgroundColor: theme.colors.gray200,
+    },
+  },
+})
+
+export const Item = styled(AccordionPrimitives.Item)({
+  backgroundColor: theme.colors.opaque1,
+  borderRadius: theme.radius.sm,
+  paddingInline: theme.space.md,
+  paddingBlock: theme.space.sm,
+
+  [mq.lg]: {
+    paddingInline: theme.space.lg,
+    paddingBlock: theme.space.md,
+  },
+
+  '@media (hover: hover)': {
+    [`:has(${Trigger}:hover)`]: {
+      backgroundColor: theme.colors.gray200,
+    },
+  },
 })
 
 type HeaderWithTriggerProps = PropsWithChildren<unknown> & {
@@ -44,14 +55,14 @@ type HeaderWithTriggerProps = PropsWithChildren<unknown> & {
 
 export const HeaderWithTrigger = ({ children }: HeaderWithTriggerProps) => {
   return (
-    <Header>
+    <AccordionPrimitives.Header>
       <Trigger>
         {children}
 
         <OpenIcon size="1rem" />
         <CloseIcon size="1rem" />
       </Trigger>
-    </Header>
+    </AccordionPrimitives.Header>
   )
 }
 
@@ -92,7 +103,6 @@ export const Content = styled(AccordionPrimitives.Content)({
   '[data-state=open] &': {
     animation: `${slideDown} 400ms cubic-bezier(0.65,0.05,0.36,1)`,
   },
-
   '[data-state=closed] &': {
     animation: `${slideUp} 400ms cubic-bezier(0.65,0.05,0.36,1)`,
   },
