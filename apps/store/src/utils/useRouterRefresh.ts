b5ea080d463c75d8a7de20b74a34d22fcc680a73
Alexander Lebedev <alexander.lebedev@gmail.com>
2023-02-15 15:24:02 +0100

    Remove useRefreshData and useRouterRefresh (#1657)
    * Support ?openPriceCalculator=1 on product pages
    
    * Use full reload to reliably clear shopSession when changing SSN
    
    * Remove useRefreshData and useRouterRefresh

--- apps/store/src/utils/useRouterRefresh.ts
+++
@@ -1,7 +0,0 @@
-import { useRouter } from 'next/router'
-import { useCallback } from 'react'
-
-export default function useRouterRefresh() {
-  const { asPath, replace } = useRouter()
-  return useCallback(() => replace(asPath, undefined, { scroll: false }), [replace, asPath])
-}
