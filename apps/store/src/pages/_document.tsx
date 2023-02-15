6e765bfebb98510226f875caa172cbeec04b6e10
Amanda Flodström <amanda@hedvig.com>
2023-02-15 16:12:07 +0100

    Add favicon files to public folder and tags to document.tsx

--- apps/store/src/pages/_document.tsx
+++ apps/store/src/pages/_document.tsx
@@ -17,6 +17,12 @@ export default class MyDocument extends Document {
           <meta name="twitter:site" content="@hedvigapp" />
           <meta name="twitter:card" content="summary_large_image" />
           <meta name="theme-color" content={theme.colors.light} />
+          <link rel="apple-touch-icon" sizes="76x76" href="/apple-touch-icon.png" />
+          <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+          <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+          <link rel="manifest" href="/site.webmanifest" />
+          <link rel="mask-icon" href="/safari-pinned-tab.svg" color={theme.colors.gray1000} />
+          <meta name="msapplication-TileColor" content={theme.colors.gray1000} />
         </Head>
         {/* Fallback for pages that don't pass className down to DOM */}
         <body className={contentFontClassName}>
