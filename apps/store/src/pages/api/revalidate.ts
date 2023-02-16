5e821d5581daf7e2a670f85f083b4a448a07b96b
Alexander Lebedev <alexander.lebedev@gmail.com>
2023-02-16 17:10:53 +0100

    Update storyblokCacheVersion in edge config based on Storyblok webhooks

--- apps/store/src/pages/api/revalidate.ts
+++ apps/store/src/pages/api/revalidate.ts
@@ -1,6 +1,8 @@
 import path from 'path'
+import { get as getFromConfig } from '@vercel/edge-config'
 import type { NextApiRequest, NextApiResponse } from 'next'
 import StoryblokClient from 'storyblok-js-client'
+import { publishStoryblokCacheVersion } from '@/services/storyblok/Storyblok.helpers'
 
 type Payload = {
   action: 'published' | 'unpublished' | 'deleted'
@@ -14,22 +16,31 @@ const SLUG_TO_ROUTE_MAP: Record<string, string> = {
   home: '/',
 }
 
-const STORYBLOK_API = new StoryblokClient({
-  accessToken: process.env.NEXT_PUBLIC_STORYBLOK_ACCESS_TOKEN,
-})
-
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   if (req.query.secret !== process.env.REVALIDATE_SECRET) {
     return res.status(401).json({ message: 'Invalid token' })
   }
+  const storyblokClient = new StoryblokClient({
+    accessToken: process.env.NEXT_PUBLIC_STORYBLOK_ACCESS_TOKEN,
+  })
 
   const { story_id } = req.body as Payload
 
   try {
-    const { data } = await STORYBLOK_API.getStory(`${story_id}`, {})
-    const route = SLUG_TO_ROUTE_MAP[data.story.full_slug] ?? data.story.full_slug
-    const pathToRevalidate = path.join('/', route)
+    const { data } = await storyblokClient.getStory(`${story_id}`, {})
+    const { cv } = data
+    const storyblokCacheVersion = await getFromConfig('storyblokCacheVersion')
+    if (storyblokCacheVersion !== cv) {
+      console.log(`Publishing new Storyblok cache version: ${cv}`)
+      try {
+        await publishStoryblokCacheVersion(cv)
+      } catch (error: unknown) {
+        console.error('Failed to publish Storyblok cache version', error)
+      }
+    }
 
+    const route = SLUG_TO_ROUTE_MAP[data.story.full_slug] ?? data.story.full_slug
+    const pathToRevalidate = path.join('/', route).replace(/\/$/, '')
     console.log(`Revalidating ${pathToRevalidate}`)
     await res.revalidate(pathToRevalidate)
     return res.json({ revalidated: true })
