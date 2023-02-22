f08cdd9970184f8bf74c8d9fc62041e7b8f91fb5
Amanda Flodström <amanda@hedvig.com>
2023-02-22 09:16:53 +0100

    Update comment

--- apps/store/src/components/ProductPage/PurchaseForm/PurchaseForm.tsx
+++ apps/store/src/components/ProductPage/PurchaseForm/PurchaseForm.tsx
@@ -64,7 +64,7 @@ export const PurchaseForm = () => {
 
   const handleComplete = (success: boolean) => {
     setFormState(success ? 'IDLE' : 'ERROR')
-    // @ts-expect-error - We use instant behavior here to avoid the scoll effect in the background of the form
+    // @ts-expect-error - "instant" behavior is not documented/typed but we still want to use it to avoid scroll animations behind the form
     !isLarge && window.scrollTo({ top: 0, left: 0, behavior: 'instant' })
   }
 
