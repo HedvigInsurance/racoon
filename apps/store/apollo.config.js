177c92d82b420b017d3a776edd1163757949f214
Robin Andeer <robin.andeer@gmail.com>
2023-02-23 09:41:43 +0100

    Update apollo config to point to staging variant (#1760)

--- apps/store/apollo.config.js
+++ apps/store/apollo.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   client: {
-    service: 'Octopus-fxevz',
+    service: 'Octopus-fxevz@staging',
     includes: ['schema.graphql', 'src/graphql/*.graphql'],
     excludes: ['src/services/apollo/generated.ts'],
   },
