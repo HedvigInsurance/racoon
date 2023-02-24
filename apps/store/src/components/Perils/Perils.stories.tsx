5faf9e90be931e76c87d714035a7f766bf28e1ee
Karnell Schultz <50870173+KarnellSchultz@users.noreply.github.com>
2023-02-24 13:48:34 +0100

    add vh fallback

--- apps/store/src/components/Perils/Perils.stories.tsx
+++ apps/store/src/components/Perils/Perils.stories.tsx
@@ -77,7 +77,7 @@ const PageWrapper = styled.div({
   display: 'flex',
   flexDirection: 'column',
   justifyContent: 'space-between',
-  height: '95dvh',
+  height: ['95dvh', '95vh'],
 })
 
 const PerilsWrapper = styled.div({
