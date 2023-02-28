0ebd97edec7f399640b04d9c719c78934373231d
Robin Andeer <robin.andeer@gmail.com>
2023-02-28 14:13:40 +0100

    Add feature toggle for success animation

--- apps/store/src/services/Flags/Flags.ts
+++ apps/store/src/services/Flags/Flags.ts
@@ -7,6 +7,7 @@ export const Flags = {
 const config = {
   INSURELY: process.env.NEXT_PUBLIC_FEATURE_INSURELY === 'true',
   ENGLISH_LANGUAGE: process.env.NEXT_PUBLIC_FEATURE_ENGLISH_LANGUAGE === 'true',
+  SUCCESS_ANIMATION: process.env.NEXT_PUBLIC_FEATURE_SUCCESS_ANIMATION === 'true',
 } as const
 
 export type FeatureFlag = keyof typeof config
