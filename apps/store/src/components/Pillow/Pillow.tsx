45408be4b44e153ea9dfebbc3a3bace7a10af7b3
Robin Andeer <robin.andeer@gmail.com>
2023-03-01 09:08:27 +0100

    Update pillow placeholder image

--- apps/store/src/components/Pillow/Pillow.tsx
+++ apps/store/src/components/Pillow/Pillow.tsx
@@ -1,19 +1,29 @@
 import styled from '@emotion/styled'
 import Image from 'next/image'
 
-const PLACEHOLDER = 'https://a.storyblok.com/f/165473/512x512/7996914970/se-apartment-rental.png'
-
 type PillowProps = {
   size?: 'xsmall' | 'small' | 'medium' | 'large' | 'xlarge' | 'xxlarge'
   src?: string
   alt?: string | null
 }
 
-export const Pillow = ({ alt, src = PLACEHOLDER, ...props }: PillowProps) => (
-  <StyledImage {...props} src={src} alt={alt ?? ''} width={80} height={80} />
-)
+export const Pillow = ({ alt, src, ...props }: PillowProps) => {
+  if (!src) return <FallbackPillow size={props.size} />
+  return <StyledImage {...props} src={src} alt={alt ?? ''} width={80} height={80} />
+}
 
 const StyledImage = styled(Image)<PillowProps>(({ size = 'medium' }) => getSize(size))
+
+const FallbackPillow = ({ size }: Pick<PillowProps, 'size'>) => {
+  return (
+    <svg {...getSize(size)} viewBox="0 0 480 480" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path
+        d="M3.79613e-05 240C2.56137e-05 428.992 101.382 480 240.298 480C379.214 480 480 432.012 480 240C480 47.9875 396.228 -5.43757e-05 240.298 -4.198e-05C84.3668 -6.01019e-05 5.03089e-05 51.0075 3.79613e-05 240Z"
+        fill="#F5F5F5"
+      />
+    </svg>
+  )
+}
 
 const getSize = (size: PillowProps['size']) => {
   switch (size) {
