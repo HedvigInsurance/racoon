9c91a639a2340d5fb5cec67a5ef8173801d7a7ca
amandaflod <59652803+amandaflod@users.noreply.github.com>
2023-03-02 16:02:09 +0100

    fix / date picker move chevron (#1857)
    * Fix alignment inside the date input so that the chevron icon is further down
    
    * Break into multiple attr

--- apps/store/src/components/ProductPage/PurchaseForm/CancellationForm/DateInput.tsx
+++ apps/store/src/components/ProductPage/PurchaseForm/CancellationForm/DateInput.tsx
@@ -2,7 +2,7 @@ import styled from '@emotion/styled'
 import { motion } from 'framer-motion'
 import { useTranslation } from 'next-i18next'
 import { ChangeEventHandler, InputHTMLAttributes, useId, useState } from 'react'
-import { ChevronIcon, Space, theme, Text } from 'ui'
+import { ChevronIcon, theme, Text } from 'ui'
 import { convertToDate } from '@/utils/date'
 import { useFormatter } from '@/utils/useFormatter'
 import { useHighlightAnimation } from '@/utils/useHighlightAnimation'
@@ -32,44 +32,51 @@ export const DateInput = (props: Props) => {
   }
 
   const displayValue = dateValue ? (
-    <InputText size="xl">{formatter.fromNow(dateValue)}</InputText>
+    <Text size="xl">{formatter.fromNow(dateValue)}</Text>
   ) : (
-    <InputText size="xl" color="textTertiary">
+    <Text size="xl" color="textTertiary">
       {t('DATE_INPUT_EMPTY_LABEL')}
-    </InputText>
+    </Text>
   )
 
   return (
-    <Space y={0.25}>
-      <Wrapper {...animationProps} data-active={!!inputValue}>
-        <Label htmlFor={inputProps.id} data-disabled={inputProps.disabled}>
-          {label}
-        </Label>
-        {displayValue}
-        <StyledInput
-          type="date"
-          id={identifier}
-          value={inputValue}
-          onChange={handleChange}
-          {...inputProps}
-        />
-
-        <StyledChevronIcon size="1rem" />
-      </Wrapper>
-    </Space>
+    <Wrapper {...animationProps} data-active={!!inputValue}>
+      <Label htmlFor={inputProps.id} data-disabled={inputProps.disabled}>
+        {label}
+      </Label>
+      <InnerWrapper>
+        {displayValue} <StyledChevronIcon size="1rem" />
+      </InnerWrapper>
+      <StyledInput
+        type="date"
+        id={identifier}
+        value={inputValue}
+        onChange={handleChange}
+        {...inputProps}
+      />
+    </Wrapper>
   )
 }
 
 const Wrapper = styled(motion.div)({
   position: 'relative',
-  display: 'flex',
-  flexDirection: 'column',
-  justifyContent: 'center',
   borderRadius: theme.radius.sm,
   backgroundColor: theme.colors.gray100,
   height: '4.5rem',
   width: '100%',
   cursor: 'pointer',
+})
+
+const InnerWrapper = styled.div({
+  width: '100%',
+  display: 'flex',
+  flexDirection: 'row',
+  alignItems: 'center',
+  justifyContent: 'space-between',
+  paddingTop: theme.space.xl,
+  paddingRight: '1.25rem',
+  paddingBottom: theme.space.sm,
+  paddingLeft: theme.space.md,
 })
 
 const Label = styled.label({
@@ -121,21 +128,11 @@ const StyledInput = styled.input({
   },
 })
 
-const InputText = styled(Text)({
-  position: 'absolute',
-  left: theme.space.md,
-  top: theme.space.xl,
-})
-
 const StyledChevronIcon = styled(ChevronIcon)(() => ({
-  position: 'absolute',
-  top: '50%',
-  right: '1.125rem',
   pointerEvents: 'none',
-  transform: 'translateY(-50%)',
   transition: 'transform 200ms cubic-bezier(0.77,0,0.18,1)',
 
   [`${Wrapper}:focus-within &`]: {
-    transform: 'translateY(-50%) rotate(180deg)',
+    transform: 'rotate(180deg)',
   },
 }))
