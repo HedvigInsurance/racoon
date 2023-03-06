06009bbd0ab6193fed681e672f67eab80b888c6f
Robin Andeer <robin.andeer@gmail.com>
2023-03-06 16:35:28 +0100

    Make cart page static (#1864)
    <!--
    PR title: GRW-123 / Feature / Awesome new thing
    -->
    
    ## Describe your changes
    
    <!--
    What changes are made?
    If there are many changes, a list might be a good format.
    If it makes sense, add screenshots and/or screen recordings here.
    -->
    
    ## Justify why they are needed
    
    ## Jira issue(s): []
    
    <!--
    If there is a Jira issue, add the key (e.g. GRW-123) between the brackets.
    A link to that issue will automatically be created.
    -->
    
    ## Checklist before requesting a review
    
    - [ ] I have performed a self-review of my code

--- apps/store/src/components/CartPage/CartPageProps.types.ts
+++ apps/store/src/components/CartPage/CartPageProps.types.ts
@@ -1,9 +1,9 @@
 import { CartCost, CartCampaign, CartEntry } from '@/components/CartInventory/CartInventory.types'
 
 export type CartPageProps = {
-  cartId: string
-  campaignsEnabled: boolean
-  campaigns: Array<CartCampaign>
-  cost: CartCost
-  entries: Array<CartEntry>
+  cartId?: string
+  campaignsEnabled?: boolean
+  campaigns?: Array<CartCampaign>
+  cost?: CartCost
+  entries?: Array<CartEntry>
 }
