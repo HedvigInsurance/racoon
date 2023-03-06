ff30afa7c704c6b8a4623f5461122e86c937f46f
Robin Andeer <robin.andeer@gmail.com>
2023-03-06 16:09:52 +0100

    Update image decoding for pillows to avoid 'popping' (#1890)
    <!--
    PR title: GRW-123 / Feature / Awesome new thing
    -->
    
    ## Describe your changes
    
    Update decoding for pillow images.
    
    <!--
    What changes are made?
    If there are many changes, a list might be a good format.
    If it makes sense, add screenshots and/or screen recordings here.
    -->
    
    ## Justify why they are needed
    
    Avoid seeing them load after you open the menu.
    
    ## Jira issue(s): []
    
    <!--
    If there is a Jira issue, add the key (e.g. GRW-123) between the brackets.
    A link to that issue will automatically be created.
    -->
    
    ## Checklist before requesting a review
    
    - [ ] I have performed a self-review of my code

--- apps/store/src/components/Pillow/Pillow.tsx
+++ apps/store/src/components/Pillow/Pillow.tsx
@@ -9,7 +9,9 @@ type PillowProps = {
 
 export const Pillow = ({ alt, src, ...props }: PillowProps) => {
   if (!src) return <FallbackPillow size={props.size} />
-  return <StyledImage {...props} src={src} alt={alt ?? ''} width={208} height={208} />
+  return (
+    <StyledImage {...props} src={src} alt={alt ?? ''} width={208} height={208} decoding="sync" />
+  )
 }
 
 const StyledImage = styled(Image)<PillowProps>(({ size = 'medium' }) => getSize(size))
