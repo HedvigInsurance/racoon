514caa1e7a0fa43d3eb427ca526137579087b21f
Karnell Schultz <50870173+KarnellSchultz@users.noreply.github.com>
2023-03-08 10:54:50 +0100

    create cart offer list comp

---
+++ apps/store/src/components/CartInventory/CartOfferList.tsx
@@ -0,0 +1,26 @@
+import styled from '@emotion/styled'
+import { ReactNode } from 'react'
+import { Separate, theme } from 'ui'
+
+type Props = { children: ReactNode }
+
+export const CartOfferList = ({ children }: Props) => {
+  return (
+    <StyledCartOfferList as="ul" Separator={<HorizontalLineWithSpace />}>
+      {children}
+    </StyledCartOfferList>
+  )
+}
+
+const StyledCartOfferList = styled(Separate)({
+  padding: 0,
+  listStyleType: 'none',
+  width: '100%',
+})
+
+const HorizontalLineWithSpace = styled.hr({
+  backgroundColor: theme.colors.gray300,
+  height: 1,
+  marginTop: theme.space.lg,
+  marginBottom: theme.space.lg,
+})
