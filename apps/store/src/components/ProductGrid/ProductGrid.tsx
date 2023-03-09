ab5e9040594c70907c6c0a359b5e78d2ff2f7f81
Gustav Eén <gustaveen@gmail.com>
2023-03-09 08:18:29 +0100

    Set max-width on ProductGrid

--- apps/store/src/components/ProductGrid/ProductGrid.tsx
+++ apps/store/src/components/ProductGrid/ProductGrid.tsx
@@ -1,6 +1,7 @@
 import styled from '@emotion/styled'
 import { Fragment } from 'react'
 import { mq, Space, theme } from 'ui'
+import { MAX_WIDTH } from '@/components/GridLayout/GridLayout'
 
 export type ProductGridProps<Item> = {
   title?: string
@@ -26,7 +27,9 @@ export const ProductGrid = <ItemType extends { key: string }>({
 }
 
 const Wrapper = styled(Space)({
+  maxWidth: MAX_WIDTH,
   height: '100%',
+  marginInline: 'auto',
 })
 
 const Title = styled.p({
