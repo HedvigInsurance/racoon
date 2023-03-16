f9d705843794cb293f4052c594bd60109d8b63e9
Gustav Eén <gustaveen@gmail.com>
2023-03-16 14:05:22 +0100

    Add inline link styles to RichText (#1950)

--- apps/store/src/blocks/RichTextBlock/RichTextBlock.styles.tsx
+++ apps/store/src/blocks/RichTextBlock/RichTextBlock.styles.tsx
@@ -2,7 +2,7 @@ import { css } from '@emotion/react'
 import { getHeadingVariantStyles } from 'ui/src/components/Heading/Heading.helpers'
 import { theme, mq } from 'ui'
 
-export const listStyling = css({
+export const listStyles = css({
   ul: {
     marginLeft: theme.space.xs,
   },
@@ -84,77 +84,100 @@ export const listStyling = css({
   },
 })
 
-export const richTextStyles = css({
-  fontSize: theme.fontSizes.md,
-  '.preamble': {
-    fontSize: theme.fontSizes.xl,
-  },
-
-  p: {
-    marginBlock: theme.space.md,
-    color: theme.colors.textSecondary,
-  },
-
-  'h4 + p': {
-    marginTop: 0,
-  },
-
-  h2: {
-    marginTop: theme.space.xl,
-    ...getHeadingVariantStyles({ _: 'standard.24', md: 'standard.32', xl: 'standard.48' }),
-  },
-
-  h3: {
-    marginTop: theme.space.xl,
-    ...getHeadingVariantStyles({ _: 'standard.18', md: 'standard.24', xl: 'standard.32' }),
-  },
-
-  h4: {
-    marginTop: theme.space.xl,
-    ...getHeadingVariantStyles({ _: 'standard.18', md: 'standard.24', xl: 'standard.32' }),
-  },
-
-  hr: {
-    marginBlock: theme.space.xxxl,
-    height: 1,
-    backgroundColor: theme.colors.borderOpaque,
-  },
-
-  [mq.md]: {
-    fontSize: theme.fontSizes.xl,
-    '.preamble': {
-      fontSize: theme.fontSizes.xxl,
+export const linkStyles = css({
+  a: {
+    position: 'relative',
+    '&::after': {
+      content: '""',
+      position: 'absolute',
+      bottom: 1,
+      left: 0,
+      width: '100%',
+      height: 2,
+      backgroundColor: theme.colors.borderOpaque,
     },
 
-    h2: {
-      marginTop: theme.space.xxl,
-    },
-
-    h3: {
-      marginTop: theme.space.xxl,
-    },
-
-    h4: {
-      marginTop: theme.space.xxl,
-    },
-  },
-
-  [mq.xl]: {
-    fontSize: theme.fontSizes.xxl,
-    '.preamble': {
-      fontSize: theme.fontSizes.xxxl,
-    },
-
-    h2: {
-      marginTop: theme.space.xxxl,
-    },
-
-    h3: {
-      marginTop: theme.space.xxxl,
-    },
-
-    h4: {
-      marginTop: theme.space.xxxl,
+    '&:hover::after': {
+      backgroundColor: theme.colors.blue600,
     },
   },
 })
+
+export const richTextStyles = css(
+  {
+    fontSize: theme.fontSizes.md,
+    '.preamble': {
+      fontSize: theme.fontSizes.xl,
+    },
+
+    p: {
+      marginBlock: theme.space.md,
+      color: theme.colors.textSecondary,
+    },
+
+    'h4 + p': {
+      marginTop: 0,
+    },
+
+    h2: {
+      marginTop: theme.space.xl,
+      ...getHeadingVariantStyles({ _: 'standard.24', md: 'standard.32', xl: 'standard.48' }),
+    },
+
+    h3: {
+      marginTop: theme.space.xl,
+      ...getHeadingVariantStyles({ _: 'standard.18', md: 'standard.24', xl: 'standard.32' }),
+    },
+
+    h4: {
+      marginTop: theme.space.xl,
+      ...getHeadingVariantStyles({ _: 'standard.18', md: 'standard.24', xl: 'standard.32' }),
+    },
+
+    hr: {
+      marginBlock: theme.space.xxxl,
+      height: 1,
+      backgroundColor: theme.colors.borderOpaque,
+    },
+
+    [mq.md]: {
+      fontSize: theme.fontSizes.xl,
+      '.preamble': {
+        fontSize: theme.fontSizes.xxl,
+      },
+
+      h2: {
+        marginTop: theme.space.xxl,
+      },
+
+      h3: {
+        marginTop: theme.space.xxl,
+      },
+
+      h4: {
+        marginTop: theme.space.xxl,
+      },
+    },
+
+    [mq.xl]: {
+      fontSize: theme.fontSizes.xxl,
+      '.preamble': {
+        fontSize: theme.fontSizes.xxxl,
+      },
+
+      h2: {
+        marginTop: theme.space.xxxl,
+      },
+
+      h3: {
+        marginTop: theme.space.xxxl,
+      },
+
+      h4: {
+        marginTop: theme.space.xxxl,
+      },
+    },
+  },
+  linkStyles,
+  listStyles,
+)
