3a5d0c983c708d4324986de68c2fb465e6e2a47c
Guilherme Popolin <guilherme@hedvig.com>
2023-03-20 17:42:44 +0100

    feat: added MixedBreedPicker

--- apps/store/src/components/PriceCalculator/MixedBreedPicker/MixedBreedPicker.stories.tsx
+++ apps/store/src/components/PriceCalculator/MixedBreedPicker/MixedBreedPicker.stories.tsx
@@ -1,38 +1,34 @@
 import { ComponentMeta, StoryFn } from '@storybook/react'
-import { useState } from 'react'
 import { MixedBreedPicker } from './MixedBreedPicker'
 
 export default {
   title: 'Inputs / Mixed Breed Picker',
   component: MixedBreedPicker,
-  // argTypes: { onCompanyChange: { action: 'changed' } },
 } as ComponentMeta<typeof MixedBreedPicker>
 
-const INITIAL_STATE = [
-  { id: '1', displayName: 'Foxhound' },
-  { id: '2', displayName: 'Poodle' },
+const BREEDS = [
+  {
+    id: '1',
+    displayName: 'Foxhound',
+  },
+  {
+    id: '2',
+    displayName: 'Poodle',
+  },
+  {
+    id: '3',
+    displayName: 'Labrador',
+  },
+  {
+    id: '4',
+    displayName: 'Pug',
+  },
+  {
+    id: '5',
+    displayName: 'Bulldog',
+  },
 ]
 
-type Item = (typeof INITIAL_STATE)[number]
-
 export const Default: StoryFn<typeof MixedBreedPicker> = () => {
-  const [items, setItems] = useState(INITIAL_STATE)
-
-  const handleSubmitDelete = (item: Item) => {
-    setItems(items.filter((prevItem) => prevItem.id !== item.id))
-  }
-
-  const handleSubmitAdd = (item: Item) => {
-    setItems([...items, item])
-  }
-
-  return (
-    <MixedBreedPicker
-      items={items}
-      onSubmitAdd={handleSubmitAdd}
-      loadingAdd={false}
-      onSubmitDelete={handleSubmitDelete}
-      loadingDelete={false}
-    />
-  )
+  return <MixedBreedPicker breeds={BREEDS} />
 }
--- apps/store/src/components/PriceCalculator/MixedBreedPicker/MixedBreedPicker.tsx
+++ apps/store/src/components/PriceCalculator/MixedBreedPicker/MixedBreedPicker.tsx
@@ -1,32 +1,37 @@
 import styled from '@emotion/styled'
-import { useTranslation } from 'next-i18next'
-import { FormEvent } from 'react'
-import { CrossIconSmall, InfoIcon, Space, Text, theme } from 'ui'
+import { useMemo, useState } from 'react'
+import { useTranslation } from 'react-i18next'
+import { Space, Text, CrossIconSmall, InfoIcon, theme } from 'ui'
 import { Combobox } from '@/components/Combobox/Combobox'
 import { SpaceFlex } from '@/components/SpaceFlex/SpaceFlex'
-
-type Item = { id: string; displayName: string }
+import { Breed } from '@/services/PriceCalculator/Field.types'
 
 type Props = {
-  items: Array<Item>
-  onSubmitAdd: (item: Item) => void
-  loadingAdd: boolean
-  onSubmitDelete: (item: Item) => void
-  loadingDelete: boolean
+  breeds: Array<Breed>
+  onSelectedBreedsChange?: (selectedBreeds: Array<Breed>) => void
 }
 
-export const MixedBreedPicker = (props: Props) => {
-  const { items, loadingDelete, onSubmitDelete, onSubmitAdd } = props
+export const MixedBreedPicker = ({ breeds, onSelectedBreedsChange }: Props) => {
+  const [selectedBreeds, setSelectedBreeds] = useState<Array<Breed>>([])
   const { t } = useTranslation('purchase-form')
 
-  const handleSubmitDelete = (item: Item) => (event: FormEvent) => {
-    event.preventDefault()
-    onSubmitDelete(item)
+  const handleDelete = (id: Breed['id']) => () => {
+    const newSelectedBreeds = selectedBreeds.filter((breed) => breed.id !== id)
+    setSelectedBreeds(newSelectedBreeds)
+    onSelectedBreedsChange?.(newSelectedBreeds)
   }
 
-  const handleSelect = (item: Item | null) => {
-    if (item) onSubmitAdd(item)
+  const handleAdd = (breed: Breed | null) => {
+    if (breed) {
+      const newSelectedBreeds = [...selectedBreeds, breed]
+      setSelectedBreeds(newSelectedBreeds)
+      onSelectedBreedsChange?.(newSelectedBreeds)
+    }
   }
+
+  const availableBreeds = useMemo(() => {
+    return breeds.filter((breed) => !selectedBreeds.includes(breed))
+  }, [breeds, selectedBreeds])
 
   return (
     <Wrapper>
@@ -35,25 +40,26 @@ export const MixedBreedPicker = (props: Props) => {
           {t('FIELD_SUB_BREEDS_LABEL')}
         </Text>
 
-        <List>
-          {items.map((item) => (
-            <ListItem key={item.id}>
-              <Text size="xl">{item.displayName}</Text>
-              <form onSubmit={handleSubmitDelete(item)}>
-                <DeleteButton type="submit" disabled={loadingDelete}>
+        {selectedBreeds.length > 0 && (
+          <List>
+            {selectedBreeds.map((breed) => (
+              <ListItem key={breed.id}>
+                <Text size="xl">{breed.displayName}</Text>
+                <DeleteButton type="button" onClick={handleDelete(breed.id)}>
                   <CrossIconSmall />
                 </DeleteButton>
-              </form>
-            </ListItem>
-          ))}
-        </List>
+              </ListItem>
+            ))}
+          </List>
+        )}
 
         <StyledCombobox
-          placeholder="Search breed..."
-          items={BREED_LIST}
-          onSelectedItemChange={handleSelect}
+          items={availableBreeds}
           displayValue={(breed) => breed.displayName}
-          disabled={loadingDelete}
+          selectedItem={null}
+          onSelectedItemChange={handleAdd}
+          placeholder="Search breed"
+          mutliSelect={true}
         />
       </Space>
     </Wrapper>
@@ -72,13 +78,21 @@ const List = styled.ul({
   flexDirection: 'column',
   gap: theme.space.xs,
 })
+
 const ListItem = styled.li({
   display: 'flex',
   justifyContent: 'space-between',
   alignItems: 'center',
 })
 
-const DeleteButton = styled.button({ cursor: 'pointer' })
+const DeleteButton = styled.button({
+  cursor: 'pointer',
+})
+
+// Workaround: generics are not preserved for styled components: shorturl.at/hqyL2
+const StyledCombobox = styled(Combobox)({
+  backgroundColor: theme.colors.opaque2,
+}) as typeof Combobox<Breed>
 
 type MessageProps = { children: string }
 
@@ -102,32 +116,3 @@ const MessageIcon = styled(InfoIcon)({
   position: 'relative',
   top: 2,
 })
-
-// Workaround: generics are not preserved for styled components: shorturl.at/hqyL2
-const StyledCombobox = styled(Combobox)({
-  backgroundColor: theme.colors.opaque2,
-}) as typeof Combobox<Item>
-
-// TODO: get from API
-const BREED_LIST: Array<Item> = [
-  {
-    id: '1',
-    displayName: 'Foxhound',
-  },
-  {
-    id: '2',
-    displayName: 'Poodle',
-  },
-  {
-    id: '3',
-    displayName: 'Labrador',
-  },
-  {
-    id: '4',
-    displayName: 'Pug',
-  },
-  {
-    id: '5',
-    displayName: 'Bulldog',
-  },
-]
