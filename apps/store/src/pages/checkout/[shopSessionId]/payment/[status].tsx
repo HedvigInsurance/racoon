261798d2c79cc669b0a3466ac354ae023cf11016
Robin Andeer <robin.andeer@gmail.com>
2023-03-27 10:14:49 +0200

    Always redirect if payment status page gets error (#2040)
    <!--
    PR title: GRW-123 / Feature / Awesome new thing
    -->
    
    ## Describe your changes
    
    Redirect to confirmation page if any error occurs during payment status page.
    
    <!--
    What changes are made?
    If there are many changes, a list might be a good format.
    If it makes sense, add screenshots and/or screen recordings here.
    -->
    
    ## Justify why they are needed
    
    ## Jira issue(s): []
    
    <!--
    If there is a Jira issue, add the key (e.g. GRW-123) between the brackets.
    A link to that issue will automatically be created.
    -->
    
    ## Checklist before requesting a review
    
    - [ ] I have performed a self-review of my code

--- apps/store/src/pages/checkout/[shopSessionId]/payment/[status].tsx
+++ apps/store/src/pages/checkout/[shopSessionId]/payment/[status].tsx
@@ -1,5 +1,5 @@
+import { ApolloClient } from '@apollo/client'
 import type { GetServerSideProps, NextPage } from 'next'
-import Link from 'next/link'
 import { CheckoutStep } from '@/components/CheckoutHeader/Breadcrumbs'
 import {
   fetchCheckoutSteps,
@@ -7,21 +7,14 @@ import {
 } from '@/components/CheckoutHeader/CheckoutHeader.helpers'
 import { initializeApolloServerSide } from '@/services/apollo/client'
 import { setupShopSessionServiceServerSide } from '@/services/shopSession/ShopSession.helpers'
+import { ShopSession } from '@/services/shopSession/ShopSession.types'
 import { isRoutingLocale } from '@/utils/l10n/localeUtils'
 import { PageLink } from '@/utils/PageLink'
 
-type Props = { shopSessionId: string }
+type Props = any
 type Params = { shopSessionId: string; status: string }
 
-const PaymentRedirectPage: NextPage<Props> = ({ shopSessionId }) => {
-  // TODO: Localise
-  return (
-    <div>
-      Something went wrong. Please{' '}
-      <Link href={PageLink.checkoutPayment({ shopSessionId })}>try again</Link>.
-    </div>
-  )
-}
+const PaymentRedirectPage: NextPage = () => null
 
 export const getServerSideProps: GetServerSideProps<Props, Params> = async (context) => {
   const { req, res, locale, params } = context
@@ -30,19 +23,34 @@ export const getServerSideProps: GetServerSideProps<Props, Params> = async (cont
   const { status, shopSessionId } = params ?? {}
   if (!status || !shopSessionId) return { notFound: true }
 
-  const apolloClient = await initializeApolloServerSide({ req, res, locale })
-  const shopSessionService = setupShopSessionServiceServerSide({ apolloClient, req, res })
-  const shopSession = await shopSessionService.fetchById(shopSessionId)
+  const fallbackRedirect = {
+    redirect: { destination: PageLink.confirmation({ locale, shopSessionId }), permanent: false },
+  } as const
+
+  let shopSession: ShopSession, apolloClient: ApolloClient<unknown>
+  try {
+    apolloClient = await initializeApolloServerSide({ req, res, locale })
+    const shopSessionService = setupShopSessionServiceServerSide({ apolloClient, req, res })
+    shopSession = await shopSessionService.fetchById(shopSessionId)
+  } catch (error) {
+    console.error(`Unable to fetch ShopSession: ${shopSessionId}`, error)
+    return fallbackRedirect
+  }
 
   if (status === 'success') {
-    const checkoutSteps = await fetchCheckoutSteps({ apolloClient, shopSession })
-    const currentStepIndex = checkoutSteps.findIndex((item) => item === CheckoutStep.Checkout)
-    const nextStep = checkoutSteps[currentStepIndex + 1]
-    const link = getCheckoutStepLink({ step: nextStep, shopSession, locale })
-
-    return { redirect: { destination: link, permanent: false } }
+    try {
+      const checkoutSteps = await fetchCheckoutSteps({ apolloClient, shopSession })
+      const currentStepIndex = checkoutSteps.findIndex((item) => item === CheckoutStep.Checkout)
+      const nextStep = checkoutSteps[currentStepIndex + 1]
+      const link = getCheckoutStepLink({ step: nextStep, shopSession, locale })
+      return { redirect: { destination: link, permanent: false } }
+    } catch (error) {
+      console.error(`Unable to fetch checkout steps for ShopSession: ${shopSessionId}`, error)
+      return fallbackRedirect
+    }
   } else {
-    return { props: { shopSessionId } }
+    console.warn(`Payment failed for ShopSession: ${shopSessionId}`, { status })
+    return fallbackRedirect
   }
 }
 
