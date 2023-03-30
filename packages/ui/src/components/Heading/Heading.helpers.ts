e2382f063261b9029238251c7b3b3acccc502cf8
Gustav Eén <gustaveen@gmail.com>
2023-03-30 15:30:20 +0200

    Set correct line-height on headings

--- packages/ui/src/components/Heading/Heading.helpers.ts
+++ packages/ui/src/components/Heading/Heading.helpers.ts
@@ -9,69 +9,86 @@ export type PossibleHeadingVariant = `standard.${StandardHeadingSize}` | `serif.
 const headings: Record<PossibleHeadingVariant, CSSObject> = {
   'standard.18': {
     fontSize: theme.fontSizes[3],
+    lineHeight: 1.32,
   },
   'standard.20': {
     fontSize: theme.fontSizes[4],
+    lineHeight: 1.32,
   },
   'standard.24': {
     fontSize: theme.fontSizes[5],
+    lineHeight: 1.26,
   },
   'standard.32': {
     fontSize: theme.fontSizes[6],
     letterSpacing: '-0.01em',
+    lineHeight: 1.26,
   },
   'standard.40': {
     fontSize: theme.fontSizes[7],
     letterSpacing: '-0.01em',
+    lineHeight: 1.26,
   },
   'standard.48': {
     fontSize: theme.fontSizes[8],
     letterSpacing: '-0.01em',
+    lineHeight: 1.26,
   },
   'standard.56': {
     fontSize: theme.fontSizes[9],
     letterSpacing: '-0.02em',
+    lineHeight: 1.26,
   },
   'standard.72': {
     fontSize: theme.fontSizes[10],
     letterSpacing: '-0.02em',
+    lineHeight: 1.12,
   },
   'standard.96': {
     fontSize: theme.fontSizes[11],
     letterSpacing: '-0.02em',
+    lineHeight: 1.12,
   },
   'serif.20': {
     fontSize: theme.fontSizes[4],
     fontFamily: theme.fonts.small,
+    lineHeight: 1.32,
   },
   'serif.24': {
     fontSize: theme.fontSizes[5],
     fontFamily: theme.fonts.small,
+    lineHeight: 1.26,
   },
   'serif.32': {
     fontSize: theme.fontSizes[6],
     fontFamily: theme.fonts.big,
+    lineHeight: 1.26,
   },
   'serif.40': {
     fontSize: theme.fontSizes[7],
     fontFamily: theme.fonts.big,
+    lineHeight: 1.26,
   },
   'serif.48': {
     fontSize: theme.fontSizes[8],
     letterSpacing: '-0.01em',
     fontFamily: theme.fonts.big,
+    lineHeight: 1.26,
   },
   'serif.56': {
     fontSize: theme.fontSizes[9],
     fontFamily: theme.fonts.big,
+    lineHeight: 1.26,
   },
   'serif.72': {
     fontSize: theme.fontSizes[10],
     fontFamily: theme.fonts.big,
+    lineHeight: 1.12,
   },
   'serif.96': {
     fontSize: theme.fontSizes[11],
     fontFamily: theme.fonts.big,
+    lineHeight: 1.12,
   },
 }
 
