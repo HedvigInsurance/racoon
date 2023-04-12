55526b113865433badaf89c80cd81f937af9d875
Gustav Eén <gustaveen@gmail.com>
2023-04-12 11:25:02 +0200

    Add Pet categories

--- apps/store/src/services/Tracking/adtraction.ts
+++ apps/store/src/services/Tracking/adtraction.ts
@@ -1,5 +1,7 @@
 import { CartFragmentFragment } from '@/services/apollo/generated'
 
+// Adtraction Product Category Tracking https://adtraction.com/productcategory.htm?tp=1719019650
+// We don't need to send amount since provision isn't based on order amount for our program
 export const getAdtractionProductCategories = (cart: CartFragmentFragment) => {
   const categoryList = cart.entries.reduce((acc, product) => {
     if (product.variant.typeOfContract.includes('ACCIDENT')) return [...acc, 'accident_0']
@@ -8,6 +10,8 @@ export const getAdtractionProductCategories = (cart: CartFragmentFragment) => {
     if (product.variant.typeOfContract.includes('APARTMENT_STUDENT'))
       return [...acc, 'homecontentstudent_0']
     if (product.variant.typeOfContract.includes('APARTMENT')) return [...acc, 'homecontent_0']
+    if (product.variant.typeOfContract.includes('DOG')) return [...acc, 'dog_0']
+    if (product.variant.typeOfContract.includes('CAT')) return [...acc, 'cat_0']
 
     return acc
   }, [] as string[])
