892cb1db49d1f408cd38f8c0ff9e7b6df53a1aa3
Robin Andeer <robin.andeer@gmail.com>
2023-04-13 14:32:10 +0200

    Add ability to replace a cart item

---
+++ apps/store/src/components/CartPage/useEditProductOffer.ts
@@ -0,0 +1,70 @@
+import { useApolloClient } from '@apollo/client'
+import { datadogLogs } from '@datadog/browser-logs'
+import { useRouter } from 'next/router'
+import { useState } from 'react'
+import { useProductMetadata } from '@/components/LayoutWithMenu/ProductMetadataContext'
+import { OPEN_PRICE_CALCULATOR_QUERY_PARAM } from '@/components/ProductPage/PurchaseForm/useOpenPriceCalculatorQueryParam'
+import { fetchPriceTemplate } from '@/services/PriceCalculator/PriceCalculator.helpers'
+import { priceIntentServiceInitClientSide } from '@/services/priceIntent/PriceIntentService'
+
+type State = 'idle' | 'loading' | 'error'
+
+type Params = {
+  shopSessionId: string
+  offerId: string
+  data: any
+  productName: string
+}
+
+export const useEditProductOffer = () => {
+  const [state, setState] = useState<State>('idle')
+  const apolloClient = useApolloClient()
+  const products = useProductMetadata()
+  const router = useRouter()
+
+  const editProductOffer = async (params: Params) => {
+    setState('loading')
+    // TODO: Check if product offer is connected to price intent -- if not, create new one
+
+    const product = products?.find((item) => item.name === params.productName)
+    if (!product) throw new Error('Product not found')
+
+    const priceIntentService = priceIntentServiceInitClientSide(apolloClient)
+    const priceTemplate = fetchPriceTemplate(params.productName)
+    if (!priceTemplate) {
+      setState('error')
+      datadogLogs.logger.error('Price template not found', { productName: params.productName })
+      return
+    }
+
+    try {
+      const priceIntent = await priceIntentService.create({
+        shopSessionId: params.shopSessionId,
+        productName: params.productName,
+        priceTemplate,
+      })
+      await priceIntentService.update({
+        priceIntentId: priceIntent.id,
+        data: params.data,
+        customer: { shopSessionId: params.shopSessionId },
+      })
+
+      await router.push({
+        pathname: product.pageLink,
+        query: {
+          [OPEN_PRICE_CALCULATOR_QUERY_PARAM]: 1,
+          replace: params.offerId,
+        },
+      })
+      setState('idle')
+    } catch (error) {
+      setState('error')
+      datadogLogs.logger.error('Error while editing product offer', {
+        error,
+        offerId: params.offerId,
+      })
+    }
+  }
+
+  return [editProductOffer, state] as const
+}
