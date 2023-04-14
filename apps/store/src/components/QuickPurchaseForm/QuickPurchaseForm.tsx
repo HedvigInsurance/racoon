92a9d3fe8256ca458920224d796125680347c830
Guilherme Popolin <guilherme@hedvig.com>
2023-04-14 13:55:33 +0200

    feat(QuickPurchaseForm): added loading state

--- apps/store/src/components/QuickPurchaseForm/QuickPurchaseForm.tsx
+++ apps/store/src/components/QuickPurchaseForm/QuickPurchaseForm.tsx
@@ -11,9 +11,10 @@ export const PRODUCT_FIELDNAME = 'product'
 type Props = {
   productOptions: InputSelectProps['options']
   onSubmit?: FormEventHandler<HTMLFormElement>
+  loading?: boolean
 }
 
-export const QuickPurchaseForm = ({ productOptions, onSubmit }: Props) => {
+export const QuickPurchaseForm = ({ productOptions, onSubmit, loading }: Props) => {
   const { t } = useTranslation('purchase-form')
 
   return (
@@ -25,15 +26,19 @@ export const QuickPurchaseForm = ({ productOptions, onSubmit }: Props) => {
               label={t('FIELD_SSN_SE_LABEL')}
               name={SSN_FIELDNAME}
               required={true}
+              disabled={loading}
             />
             <InputSelect
               name={PRODUCT_FIELDNAME}
               options={productOptions}
               size="small"
               required={true}
+              disabled={loading}
             />
           </Space>
-          <Button type="submit">Se ditt pris</Button>
+          <Button type="submit" loading={loading}>
+            Se ditt pris
+          </Button>
         </Space>
       </form>
     </Wrapper>
