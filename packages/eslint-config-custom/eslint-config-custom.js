126b7c35e350a5040457869c4c4b9dd690ba5d19
Alexander Lebedev <alexander.lebedev@gmail.com>
2023-04-17 13:30:51 +0200

    Enable https://typescript-eslint.io/rules/no-unnecessary-condition/

--- packages/eslint-config-custom/eslint-config-custom.js
+++ packages/eslint-config-custom/eslint-config-custom.js
@@ -48,6 +48,7 @@ module.exports = {
     ],
     '@typescript-eslint/ban-ts-comment': ['error', { 'ts-expect-error': 'allow-with-description' }],
     '@typescript-eslint/no-unused-vars': 'error', // Also covers unused import
+    '@typescript-eslint/no-unnecessary-condition': 'error', // Also unneeded optional chaining
     '@typescript-eslint/no-empty-function': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
     '@typescript-eslint/no-empty-interface': 'off',
