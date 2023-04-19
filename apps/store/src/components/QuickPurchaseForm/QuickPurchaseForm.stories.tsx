e0360b7bb3124751a5ec96165db387808e19906b
Guilherme Popolin <guilherme@hedvig.com>
2023-04-19 17:03:13 +0200

    feat(QuickPurchaseForm): update select input and translations keys

--- apps/store/src/components/QuickPurchaseForm/QuickPurchaseForm.stories.tsx
+++ apps/store/src/components/QuickPurchaseForm/QuickPurchaseForm.stories.tsx
@@ -1,14 +1,33 @@
+import { INITIAL_VIEWPORTS } from '@storybook/addon-viewport'
 import { Meta, StoryFn } from '@storybook/react'
-import { QuickPurchaseForm } from './QuickPurchaseForm'
+import { QuickPurchaseForm, type ProductOption } from './QuickPurchaseForm'
 
 export default {
   title: 'QuickPurchaseForm',
   component: QuickPurchaseForm,
+  parameters: {
+    viewport: {
+      viewports: INITIAL_VIEWPORTS,
+      defaultViewport: 'iphonese2',
+    },
+  },
 } as Meta<typeof QuickPurchaseForm>
 
-const options = [
-  { name: 'Accident', value: 'SE_ACCIDENT' },
-  { name: 'Car', value: 'SE_CAR' },
+const options: Array<ProductOption> = [
+  {
+    name: 'Accident',
+    value: 'SE_ACCIDENT',
+    img: {
+      src: 'https://a.storyblok.com/f/165473/832x832/d6bf60c98b/hedvig-pillow-accident.png',
+    },
+  },
+  {
+    name: 'Car',
+    value: 'SE_CAR',
+    img: {
+      src: 'https://a.storyblok.com/f/165473/832x832/001eb5c8a1/hedvig-pillow-car.png',
+    },
+  },
 ]
 
 const Template: StoryFn<typeof QuickPurchaseForm> = () => {
--- apps/store/src/components/QuickPurchaseForm/QuickPurchaseForm.tsx
+++ apps/store/src/components/QuickPurchaseForm/QuickPurchaseForm.tsx
@@ -1,17 +1,27 @@
 import styled from '@emotion/styled'
+import * as SelectPrimitive from '@radix-ui/react-select'
 import { useTranslation } from 'next-i18next'
-import { FormEventHandler } from 'react'
-import { Button, Space, theme } from 'ui'
+import { FormEventHandler, Fragment } from 'react'
+import { Button, Space, CheckIcon, ChevronIcon, theme } from 'ui'
 import { useGetDiscountExplanation } from '@/components/CartInventory/CartInventory.helpers'
-import { InputSelect, InputSelectProps } from '@/components/InputSelect/InputSelect'
 import { PersonalNumberField } from '@/components/PersonalNumberField/PersonalNumberField'
+import { Pillow } from '@/components/Pillow/Pillow'
 import { RedeemedCampaign } from '@/services/apollo/generated'
 
 export const SSN_FIELDNAME = 'ssn'
 export const PRODUCT_FIELDNAME = 'product'
 
+export type ProductOption = {
+  name: string
+  value: string
+  img: {
+    src: string
+    alt?: string
+  }
+}
+
 type Props = {
-  productOptions: InputSelectProps['options']
+  productOptions: Array<ProductOption>
   onSubmit?: FormEventHandler<HTMLFormElement>
   loading?: boolean
   ssnDefaultValue?: string
@@ -46,16 +56,43 @@ export const QuickPurchaseForm = ({
               disabled={loading}
               defaultValue={ssnDefaultValue}
             />
-            <InputSelect
-              name={PRODUCT_FIELDNAME}
-              options={productOptions}
-              size="small"
-              required={true}
-              disabled={loading}
-            />
+
+            <SelectPrimitive.Root name={PRODUCT_FIELDNAME} disabled={loading} required={true}>
+              <SelectTrigger>
+                <SelectPrimitive.Value placeholder={t('FIELD_INSURANCE_SELECTOR_PLACEHOLDER')} />
+                <SelectPrimitive.Icon asChild>
+                  <StyledChevronIcon size="1rem" />
+                </SelectPrimitive.Icon>
+              </SelectTrigger>
+
+              <SelectPrimitive.Portal>
+                <SelectContent position="popper">
+                  <SelectPrimitive.Viewport>
+                    {productOptions.map((option) => (
+                      <Fragment key={option.value}>
+                        <Separator />
+                        <SelectItem value={option.value}>
+                          <SelectPrimitive.ItemText asChild>
+                            <ItemDisplay>
+                              <Pillow size="xxsmall" {...option.img} />
+                              <span>{option.name}</span>
+                            </ItemDisplay>
+                          </SelectPrimitive.ItemText>
+
+                          <SelectPrimitive.ItemIndicator asChild>
+                            <SelectedItemIcon size="1rem" />
+                          </SelectPrimitive.ItemIndicator>
+                        </SelectItem>
+                      </Fragment>
+                    ))}
+                  </SelectPrimitive.Viewport>
+                </SelectContent>
+              </SelectPrimitive.Portal>
+            </SelectPrimitive.Root>
           </Space>
+
           <Button type="submit" loading={loading}>
-            Se ditt pris
+            {t('BUTTON_LABEL_GET_PRICE')}
           </Button>
         </Space>
       </form>
@@ -66,7 +103,6 @@ export const QuickPurchaseForm = ({
 const Wrapper = styled(Space)({
   padding: theme.space.sm,
   borderRadius: theme.radius.xs,
-  border: `1px solid ${theme.colors.gray300}`,
 })
 
 const CampaignCodeBadge = styled.span({
@@ -74,4 +110,98 @@ const CampaignCodeBadge = styled.span({
   backgroundColor: theme.colors.gray200,
   textTransform: 'uppercase',
   padding: `${theme.space.xxs} ${theme.space.xs}`,
+})
+
+const SelectTrigger = styled(SelectPrimitive.Trigger)({
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'space-between',
+  gap: theme.space.xs,
+  width: '100%',
+  height: '3rem',
+  borderRadius: theme.radius.sm,
+  paddingInline: theme.space.md,
+  backgroundColor: theme.colors.opaque1,
+  fontSize: theme.fontSizes.xl,
+  cursor: 'pointer',
+
+  '&[data-state=open]': {
+    borderBottomLeftRadius: 'revert',
+    borderBottomRightRadius: 'revert',
+  },
+
+  '&[data-placeholder]': {
+    color: theme.colors.textSecondary,
+  },
+
+  '&:focus-visible': {
+    outline: `2px solid ${theme.colors.black}`,
+  },
+
+  '&:invalid, &:disabled': {
+    color: theme.colors.textSecondary,
+  },
+
+  '&:disabled': {
+    cursor: 'not-allowed',
+  },
+})
+
+const StyledChevronIcon = styled(ChevronIcon)({
+  transition: 'transform 200ms cubic-bezier(0.77, 0, 0.18, 1)',
+
+  [`${SelectTrigger}[data-state=open] &`]: {
+    transform: 'rotate(180deg)',
+  },
+})
+
+const SelectContent = styled(SelectPrimitive.Content)({
+  width: 'var(--radix-select-trigger-width)',
+  maxHeight: 'var(--radix-select-content-available-height)',
+  backgroundColor: theme.colors.opaque1,
+  borderBottomLeftRadius: theme.radius.sm,
+  borderBottomRightRadius: theme.radius.sm,
+  // Cut out borders
+  overflow: 'hidden',
+})
+
+const SelectItem = styled(SelectPrimitive.Item)({
+  position: 'relative',
+  minHeight: '3rem',
+  paddingInline: theme.space.md,
+  paddingBlock: theme.space.xs,
+  fontSize: theme.fontSizes.lg,
+  isolation: 'isolate',
+
+  '&[data-highlighted]': {
+    backgroundColor: theme.colors.gray300,
+  },
+
+  '@media (hover: hover)': {
+    ':hover': {
+      cursor: 'pointer',
+      backgroundColor: theme.colors.gray300,
+    },
+  },
+})
+
+const Separator = styled(SelectPrimitive.Separator)({
+  height: 1,
+  backgroundColor: theme.colors.opaque2,
+  marginInline: theme.space.md,
+})
+
+const ItemDisplay = styled.div({
+  display: 'flex',
+  alignItems: 'center',
+  gap: theme.space.xs,
+})
+
+const SelectedItemIcon = styled(CheckIcon)({
+  position: 'absolute',
+  right: theme.space.md,
+  // Centers icon vertically
+  top: '50%',
+  transform: 'translateY(-50%)',
+  zIndex: 1,
 })
