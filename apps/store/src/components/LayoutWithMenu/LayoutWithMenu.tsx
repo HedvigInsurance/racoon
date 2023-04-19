6997a5bfd48caec61adceb04fcccfc3420a0b315
Alexander Lebedev <alexander.lebedev@gmail.com>
2023-04-19 14:48:14 +0200

    Handle possibly missing story in LayoutWithMenu.tsx (404, cart, etc)

--- apps/store/src/components/LayoutWithMenu/LayoutWithMenu.tsx
+++ apps/store/src/components/LayoutWithMenu/LayoutWithMenu.tsx
@@ -2,7 +2,7 @@ import styled from '@emotion/styled'
 import { ReactElement } from 'react'
 import { FooterBlock } from '@/blocks/FooterBlock'
 import { HeaderBlock } from '@/blocks/HeaderBlock'
-import { StoryblokPageProps } from '@/services/storyblok/storyblok'
+import { PageStory, StoryblokPageProps } from '@/services/storyblok/storyblok'
 import { filterByBlockType, isProductStory } from '@/services/storyblok/Storyblok.helpers'
 import { useChangeLocale } from '@/utils/l10n/useChangeLocale'
 import { GlobalProductMetadata, GLOBAL_PRODUCT_METADATA_PROP_NAME } from './fetchProductMetadata'
@@ -15,9 +15,10 @@ const Wrapper = styled.div({
 
 type LayoutWithMenuProps = {
   children: ReactElement<
-    StoryblokPageProps & {
+    Omit<StoryblokPageProps, 'story'> & {
       className: string
       [GLOBAL_PRODUCT_METADATA_PROP_NAME]: GlobalProductMetadata
+      story: PageStory | undefined
     }
   >
   overlayMenu?: boolean
@@ -37,17 +38,17 @@ export const LayoutWithMenu = ({
   const headerBlock = filterByBlockType(globalStory.content.header, HeaderBlock.blockName)
   const footerBlock = filterByBlockType(globalStory.content.footer, FooterBlock.blockName)
 
-  const showFooter = !hideFooter && !story.content.hideFooter
+  const showFooter = !hideFooter && !story?.content.hideFooter
 
   return (
     <Wrapper className={className}>
-      {!story.content.hideMenu &&
+      {!story?.content.hideMenu &&
         headerBlock.map((nestedBlock) => (
           <HeaderBlock
             key={nestedBlock._uid}
             blok={nestedBlock}
-            overlay={story.content.overlayMenu ?? overlayMenu}
-            static={isProductStory(story)}
+            overlay={story?.content.overlayMenu ?? overlayMenu}
+            static={story && isProductStory(story)}
           />
         ))}
       {children}
