4011830ab9ef2e20a7a4cc0bca8feab0c3570621
Guilherme Popolin <guilherme@hedvig.com>
2023-04-20 09:50:53 +0200

    refact(QuickPurchaseForm): remove campaign code info

--- apps/store/src/components/QuickPurchaseForm/QuickPurchaseForm.tsx
+++ apps/store/src/components/QuickPurchaseForm/QuickPurchaseForm.tsx
@@ -3,10 +3,8 @@ import * as SelectPrimitive from '@radix-ui/react-select'
 import { useTranslation } from 'next-i18next'
 import { FormEventHandler, Fragment } from 'react'
 import { Button, Space, CheckIcon, ChevronIcon, theme } from 'ui'
-import { useGetDiscountExplanation } from '@/components/CartInventory/CartInventory.helpers'
 import { PersonalNumberField } from '@/components/PersonalNumberField/PersonalNumberField'
 import { Pillow } from '@/components/Pillow/Pillow'
-import { RedeemedCampaign } from '@/services/apollo/generated'
 
 export const SSN_FIELDNAME = 'ssn'
 export const PRODUCT_FIELDNAME = 'product'
@@ -25,7 +23,6 @@ type Props = {
   onSubmit?: FormEventHandler<HTMLFormElement>
   loading?: boolean
   ssnDefaultValue?: string
-  redeemedCampaign?: RedeemedCampaign
 }
 
 export const QuickPurchaseForm = ({
@@ -33,84 +30,62 @@ export const QuickPurchaseForm = ({
   onSubmit,
   loading,
   ssnDefaultValue = '',
-  redeemedCampaign,
 }: Props) => {
   const { t } = useTranslation('purchase-form')
-  const getDiscountExplanation = useGetDiscountExplanation()
 
   return (
-    <Wrapper y={0.75}>
-      {redeemedCampaign && (
-        <Space y={0.75}>
-          <CampaignCodeBadge>{redeemedCampaign.code}</CampaignCodeBadge>
-          <p>{getDiscountExplanation(redeemedCampaign.discount)}</p>
+    <form onSubmit={onSubmit}>
+      <Space y={0.75}>
+        <Space y={0.5}>
+          <PersonalNumberField
+            label={t('FIELD_SSN_SE_LABEL')}
+            name={SSN_FIELDNAME}
+            required={true}
+            disabled={loading}
+            defaultValue={ssnDefaultValue}
+          />
+
+          <SelectPrimitive.Root name={PRODUCT_FIELDNAME} disabled={loading} required={true}>
+            <SelectTrigger>
+              <SelectPrimitive.Value placeholder={t('FIELD_INSURANCE_SELECTOR_PLACEHOLDER')} />
+              <SelectPrimitive.Icon asChild>
+                <StyledChevronIcon size="1rem" />
+              </SelectPrimitive.Icon>
+            </SelectTrigger>
+
+            <SelectPrimitive.Portal>
+              <SelectContent position="popper">
+                <SelectPrimitive.Viewport>
+                  {productOptions.map((option) => (
+                    <Fragment key={option.value}>
+                      <Separator />
+                      <SelectItem value={option.value}>
+                        <SelectPrimitive.ItemText asChild>
+                          <ItemDisplay>
+                            <Pillow size="xxsmall" {...option.img} />
+                            <span>{option.name}</span>
+                          </ItemDisplay>
+                        </SelectPrimitive.ItemText>
+
+                        <SelectPrimitive.ItemIndicator asChild>
+                          <SelectedItemIcon size="1rem" />
+                        </SelectPrimitive.ItemIndicator>
+                      </SelectItem>
+                    </Fragment>
+                  ))}
+                </SelectPrimitive.Viewport>
+              </SelectContent>
+            </SelectPrimitive.Portal>
+          </SelectPrimitive.Root>
         </Space>
-      )}
-      <form onSubmit={onSubmit}>
-        <Space y={0.75}>
-          <Space y={0.5}>
-            <PersonalNumberField
-              label={t('FIELD_SSN_SE_LABEL')}
-              name={SSN_FIELDNAME}
-              required={true}
-              disabled={loading}
-              defaultValue={ssnDefaultValue}
-            />
 
-            <SelectPrimitive.Root name={PRODUCT_FIELDNAME} disabled={loading} required={true}>
-              <SelectTrigger>
-                <SelectPrimitive.Value placeholder={t('FIELD_INSURANCE_SELECTOR_PLACEHOLDER')} />
-                <SelectPrimitive.Icon asChild>
-                  <StyledChevronIcon size="1rem" />
-                </SelectPrimitive.Icon>
-              </SelectTrigger>
-
-              <SelectPrimitive.Portal>
-                <SelectContent position="popper">
-                  <SelectPrimitive.Viewport>
-                    {productOptions.map((option) => (
-                      <Fragment key={option.value}>
-                        <Separator />
-                        <SelectItem value={option.value}>
-                          <SelectPrimitive.ItemText asChild>
-                            <ItemDisplay>
-                              <Pillow size="xxsmall" {...option.img} />
-                              <span>{option.name}</span>
-                            </ItemDisplay>
-                          </SelectPrimitive.ItemText>
-
-                          <SelectPrimitive.ItemIndicator asChild>
-                            <SelectedItemIcon size="1rem" />
-                          </SelectPrimitive.ItemIndicator>
-                        </SelectItem>
-                      </Fragment>
-                    ))}
-                  </SelectPrimitive.Viewport>
-                </SelectContent>
-              </SelectPrimitive.Portal>
-            </SelectPrimitive.Root>
-          </Space>
-
-          <Button type="submit" loading={loading}>
-            {t('BUTTON_LABEL_GET_PRICE')}
-          </Button>
-        </Space>
-      </form>
-    </Wrapper>
+        <Button type="submit" loading={loading}>
+          {t('BUTTON_LABEL_GET_PRICE')}
+        </Button>
+      </Space>
+    </form>
   )
 }
-
-const Wrapper = styled(Space)({
-  padding: theme.space.sm,
-  borderRadius: theme.radius.xs,
-})
-
-const CampaignCodeBadge = styled.span({
-  borderRadius: theme.radius.xs,
-  backgroundColor: theme.colors.gray200,
-  textTransform: 'uppercase',
-  padding: `${theme.space.xxs} ${theme.space.xs}`,
-})
 
 const SelectTrigger = styled(SelectPrimitive.Trigger)({
   display: 'flex',
