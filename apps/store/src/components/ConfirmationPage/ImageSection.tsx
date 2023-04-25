2fbe859913e3da7c22d451e4d5ec709cb8fbf9d2
Robin Andeer <robin.andeer@gmail.com>
2023-04-25 12:04:53 +0200

    Update Confirmation page layout

---
+++ apps/store/src/components/ConfirmationPage/ImageSection.tsx
@@ -0,0 +1,31 @@
+import styled from '@emotion/styled'
+import Image from 'next/image'
+import { mq } from 'ui'
+
+type Props = {
+  image: { src: string; alt: string }
+}
+
+export const ImageSection = ({ image }: Props) => {
+  return (
+    <Wrapper>
+      <StyledImage {...image} fill={true} />
+    </Wrapper>
+  )
+}
+
+const Wrapper = styled.section({
+  position: 'relative',
+  top: '-20vw',
+  height: '100vw',
+
+  [mq.sm]: {
+    top: '-10vw',
+    height: '71vw',
+  },
+})
+
+const StyledImage = styled(Image)({
+  objectFit: 'cover',
+  objectPosition: 'center',
+})
