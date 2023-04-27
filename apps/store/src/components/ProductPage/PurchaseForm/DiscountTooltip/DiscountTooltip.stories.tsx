759547f50919ec410c7db95e95a6c47ef8d8df2a
Robin Andeer <robin.andeer@gmail.com>
2023-04-27 09:36:06 +0200

    Add visual discount tooltip component

---
+++ apps/store/src/components/ProductPage/PurchaseForm/DiscountTooltip/DiscountTooltip.stories.tsx
@@ -0,0 +1,22 @@
+import { Meta, StoryFn } from '@storybook/react'
+import { DiscountTooltip } from './DiscountTooltip'
+
+export default {
+  title: 'Product Page / Purchase Form / Discount Tooltip',
+  component: DiscountTooltip,
+} as Meta<typeof DiscountTooltip>
+
+export const Default: StoryFn<typeof DiscountTooltip> = (props) => {
+  return <DiscountTooltip {...props} />
+}
+Default.args = {
+  children: '50% rabatt i 3 månader',
+  subtitle: 'Därefter betalar du 398 kr/mån',
+}
+
+export const NoSubtitle: StoryFn<typeof DiscountTooltip> = (props) => {
+  return <DiscountTooltip {...props} />
+}
+NoSubtitle.args = {
+  children: '-10 kr/mån via Hedvig Forever',
+}
