a1936b418d5e993b085abe161faac8f95632280f
Gustav Eén <gustaveen@gmail.com>
2023-05-03 11:51:11 +0200

    Add balancer to text block and component

--- packages/ui/src/components/Text/Text.tsx
+++ packages/ui/src/components/Text/Text.tsx
@@ -1,6 +1,7 @@
 import isPropValid from '@emotion/is-prop-valid'
 import styled from '@emotion/styled'
 import { ReactNode } from 'react'
+import Balancer from 'react-wrap-balancer'
 import { Space, theme, UIColors } from 'ui'
 import { FontSizeProps, getFontSize } from '../../lib/fontSizes'
 
@@ -19,6 +20,7 @@ type TextColor = Pick<
 export type TextProps = {
   as?: 'p' | 'span' | 'div'
   align?: 'center' | 'left' | 'right'
+  balance?: boolean
   color?: keyof TextColor
   size?: FontSizeProps
   children?: ReactNode
@@ -29,7 +31,7 @@ const elementConfig = {
   shouldForwardProp: (prop: string) => isPropValid(prop) && prop !== 'color',
 }
 
-export const Text = styled(
+export const TextBase = styled(
   Space,
   elementConfig,
 )<TextProps>(({ align, color, size = 'md' }) => ({
@@ -37,3 +39,9 @@ export const Text = styled(
   ...getFontSize(size),
   ...(align && { textAlign: align }),
 }))
+
+export const Text = ({ as, balance, children, className, ...rest }: TextProps) => (
+  <TextBase as={as} className={className} {...rest}>
+    {!balance ? children : <Balancer>{children}</Balancer>}
+  </TextBase>
+)
