0fdae7bdd8e094a5930d6dd3461643108ec581bb
Alexander Lebedev <alexander.lebedev@gmail.com>
2023-05-04 09:54:22 +0200

    Remove PET_INSURANCE feature flag (#2280)

--- apps/store/src/blocks/ProductPillowsBlock/ProductPillowsBlock.tsx
+++ apps/store/src/blocks/ProductPillowsBlock/ProductPillowsBlock.tsx
@@ -1,10 +1,8 @@
 import styled from '@emotion/styled'
 import { storyblokEditable } from '@storyblok/react'
-import { useMemo } from 'react'
 import { mq, theme } from 'ui'
 import { SbBaseBlockProps, ExpectedBlockType } from '@/services/storyblok/storyblok'
 import { filterByBlockType } from '@/services/storyblok/Storyblok.helpers'
-import { isDisabledPetLink } from '@/utils/isDisabledPetLink'
 import { ProductPillowBlock, ProductPillowBlockProps } from './ProductPillowBlock'
 
 export type ProductPillowsBlockProps = SbBaseBlockProps<{
@@ -12,14 +10,7 @@ export type ProductPillowsBlockProps = SbBaseBlockProps<{
 }>
 
 export const ProductPillowsBlock = ({ blok }: ProductPillowsBlockProps) => {
-  const filteredProductPillowItems = filterByBlockType(blok.items, ProductPillowBlock.blockName)
-  const items = useMemo(
-    () =>
-      filteredProductPillowItems.filter(
-        (item) => !isDisabledPetLink(item.link.story?.full_slug ?? ''),
-      ),
-    [filteredProductPillowItems],
-  )
+  const items = filterByBlockType(blok.items, ProductPillowBlock.blockName)
   return (
     <ProductPillowsWrapper {...storyblokEditable(blok)}>
       <ScrollerContent>
