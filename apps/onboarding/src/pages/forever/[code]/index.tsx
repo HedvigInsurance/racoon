70af5588e061d840ddeb7d351373e995b0ef8894
Gustav Eén <gustaveen@gmail.com>
2023-05-23 09:51:33 +0200

    Remove onboarding forever pages (#2395)

--- apps/onboarding/src/pages/forever/[code]/index.tsx
+++
@@ -1,8 +0,0 @@
-import type { GetServerSideProps } from 'next'
-import { getStaticProps } from '..'
-
-export { default } from '..'
-
-export const getServerSideProps: GetServerSideProps = async (context) => {
-  return await getStaticProps(context)
-}
--- apps/onboarding/src/pages/forever/[code]/ready.tsx
+++
@@ -1,8 +0,0 @@
-import type { NextPage } from 'next'
-import { ForeverCodeReadyPage } from '@/components/ForeverPage/ForeverCodeReadyPage'
-
-const NextForeverCodeReadyPage: NextPage = () => <ForeverCodeReadyPage />
-
-export { getServerSideProps } from '.'
-
-export default NextForeverCodeReadyPage
