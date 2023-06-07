9b0b5b3519e70c3e1a1909ff0837c69a3de27ba0
Robin Andeer <robin.andeer@gmail.com>
2023-06-07 11:42:33 +0200

    SelectInsuranceGrid: add option to open price calculator

--- apps/store/src/blocks/SelectInsuranceGridBlock.tsx
+++ apps/store/src/blocks/SelectInsuranceGridBlock.tsx
@@ -3,16 +3,20 @@ import { storyblokEditable } from '@storyblok/react'
 import { Heading, Space, mq } from 'ui'
 import { GridLayout } from '@/components/GridLayout/GridLayout'
 import { useProductMetadata } from '@/components/LayoutWithMenu/ProductMetadataContext'
+import { OPEN_PRICE_CALCULATOR_QUERY_PARAM } from '@/components/ProductPage/PurchaseForm/useOpenPriceCalculatorQueryParam'
 import { ProductItem } from '@/components/SelectInsuranceGrid/ProductItem'
 import { SelectInsuranceGrid } from '@/components/SelectInsuranceGrid/SelectInsuranceGrid'
 import { SbBaseBlockProps } from '@/services/storyblok/storyblok'
+import { ORIGIN_URL } from '@/utils/PageLink'
 
 type Props = SbBaseBlockProps<{
   title?: string
+  openPriceCalculator?: boolean
 }>
 
 export const SelectInsuranceGridBlock = ({ blok, nested }: Props) => {
   const products = useProductMetadata()
+  const openPriceCalculator = blok.openPriceCalculator ?? true
 
   return (
     <Root data-nested={nested}>
@@ -29,7 +33,10 @@ export const SelectInsuranceGridBlock = ({ blok, nested }: Props) => {
               <ProductItem.Root key={item.id}>
                 <ProductItem.Pillow {...item.pillowImage} />
                 <ProductItem.Content>
-                  <ProductItem.TitleLink href={item.pageLink} title={item.displayNameFull}>
+                  <ProductItem.TitleLink
+                    href={getProductLink(item.pageLink, openPriceCalculator)}
+                    title={item.displayNameFull}
+                  >
                     {item.displayNameShort}
                   </ProductItem.TitleLink>
                   <ProductItem.Tagline>{item.tagline}</ProductItem.Tagline>
@@ -56,5 +63,17 @@ const Root = styled(GridLayout.Root)({
     },
   },
 })
+
+const getProductLink = (pageLink: string, openPriceCalculator: boolean) => {
+  return openPriceCalculator
+    ? addSearchParam(pageLink, OPEN_PRICE_CALCULATOR_QUERY_PARAM, '1')
+    : pageLink
+}
+
+const addSearchParam = (url: string, param: string, value: string) => {
+  const urlObj = new URL(url, ORIGIN_URL)
+  urlObj.searchParams.set(param, value)
+  return urlObj.toString()
+}
 
 SelectInsuranceGridBlock.blockName = 'selectInsuranceGrid'
