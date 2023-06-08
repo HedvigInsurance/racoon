574167fdd290b50e5e555e592426e0a19cfe83da
Alexander Lebedev <alexander.lebedev@gmail.com>
2023-06-08 14:48:53 +0200

    Add SAS_PARTNERSHIP feature (#2507)
    <!--
    PR title: GRW-123 / Feature / Awesome new thing
    -->
    
    ## Describe your changes
    
    Support SAS_PARTNERSHIP feature flag.  Set it to true in Vercel non-production environments
    
    <!--
    What changes are made?
    If there are many changes, a list might be a good format.
    If it makes sense, add screenshots and/or screen recordings here.
    -->
    
    ## Justify why they are needed
    
    ## Checklist before requesting a review
    
    - [ ] I have performed a self-review of my code

--- apps/store/src/utils/Features.ts
+++ apps/store/src/utils/Features.ts
@@ -13,6 +13,7 @@ const config = {
   SUCCESS_ANIMATION: process.env.NEXT_PUBLIC_FEATURE_SUCCESS_ANIMATION === 'true',
   MANYPETS_MIGRATION: process.env.NEXT_PUBLIC_FEATURE_MANYPETS_MIGRATION === 'true',
   STOREFRONT_CONNECT_PAYMENT: process.env.NEXT_PUBLIC_FEATURE_STOREFRONT_CONNECT_PAYMENT === 'true',
+  SAS_PARTNERSHIP: process.env.NEXT_PUBLIC_FEATURE_SAS_PARTNERSHIP === 'true',
 } as const
 
 export type FeatureFlag = keyof typeof config
