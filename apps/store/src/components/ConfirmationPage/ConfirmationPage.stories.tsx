c64e20c58a76c958144b8c7d47f039fd4c2c08d3
Alexander Lebedev <alexander.lebedev@gmail.com>
2023-06-09 14:09:07 +0200

    Fix storybook errors for CartEntryItem and ConfirmationPage

--- apps/store/src/components/ConfirmationPage/ConfirmationPage.stories.tsx
+++ apps/store/src/components/ConfirmationPage/ConfirmationPage.stories.tsx
@@ -1,5 +1,6 @@
 import { Meta } from '@storybook/react'
 import { CurrencyCode, ExternalInsuranceCancellationOption } from '@/services/apollo/generated'
+import { AppErrorProvider } from '@/services/appErrors/AppErrorContext'
 import { ConfirmationPage } from './ConfirmationPage'
 
 export default {
@@ -12,92 +13,94 @@ export default {
 
 export const Default = () => {
   return (
-    <ConfirmationPage
-      globalStory={{
-        ...storyblokStory,
-        content: {
-          header: [],
-          footer: [],
-        },
-      }}
-      shopSessionId="aiwdoiaiojoiwa"
-      memberPartnerData={null}
-      currency="SEK"
-      cart={{
-        id: '123',
-        cost: {
-          gross: { amount: 225, currencyCode: CurrencyCode.Sek },
-          net: { amount: 215, currencyCode: CurrencyCode.Sek },
-          discount: { amount: 10, currencyCode: CurrencyCode.Sek },
-        },
-        redeemedCampaigns: [],
-        campaignsEnabled: true,
-        entries: [
-          {
-            id: '123',
-            startDate: '2023-02-24',
-            priceIntentData: {},
-            variant: {
-              typeOfContract: 'SE_APARTMENT_BRF',
-              displayName: 'Hedvig BRF',
-              product: {
-                id: 'oidajwoijawd',
-                name: 'SE_APARTMENT_BRF',
-                displayNameFull: 'Home Insurance Homeowner',
-                pillowImage: {
-                  id: '6646216',
-                  alt: '',
-                  src: 'https://a.storyblok.com/f/165473/512x512/7996914970/se-apartment-rental.png',
+    <AppErrorProvider>
+      <ConfirmationPage
+        globalStory={{
+          ...storyblokStory,
+          content: {
+            header: [],
+            footer: [],
+          },
+        }}
+        shopSessionId="aiwdoiaiojoiwa"
+        memberPartnerData={null}
+        currency="SEK"
+        cart={{
+          id: '123',
+          cost: {
+            gross: { amount: 225, currencyCode: CurrencyCode.Sek },
+            net: { amount: 215, currencyCode: CurrencyCode.Sek },
+            discount: { amount: 10, currencyCode: CurrencyCode.Sek },
+          },
+          redeemedCampaigns: [],
+          campaignsEnabled: true,
+          entries: [
+            {
+              id: '123',
+              startDate: '2023-02-24',
+              priceIntentData: {},
+              variant: {
+                typeOfContract: 'SE_APARTMENT_BRF',
+                displayName: 'Hedvig BRF',
+                product: {
+                  id: 'oidajwoijawd',
+                  name: 'SE_APARTMENT_BRF',
+                  displayNameFull: 'Home Insurance Homeowner',
+                  pillowImage: {
+                    id: '6646216',
+                    alt: '',
+                    src: 'https://a.storyblok.com/f/165473/512x512/7996914970/se-apartment-rental.png',
+                  },
                 },
+                perils: [],
+                documents: [],
               },
-              perils: [],
-              documents: [],
+              price: {
+                amount: 125,
+                currencyCode: CurrencyCode.Sek,
+              },
+              cancellation: {
+                option: ExternalInsuranceCancellationOption.None,
+                requested: false,
+                bankSigneringApproveByDate: null,
+              },
             },
-            price: {
-              amount: 125,
-              currencyCode: CurrencyCode.Sek,
+          ],
+        }}
+        // TODO: get this from some fixture module
+        story={{
+          ...storyblokStory,
+          content: {
+            body: [],
+            title: 'Your purchase is complete!',
+            subtitle: 'A confirmation has been sent via email.',
+            footerTitle: 'Do you still need help?',
+            footerSubtitle:
+              'Download the Hedvig app and contact us in the chat and we will help you.',
+            footerImage: {
+              id: 6967517,
+              alt: '',
+              name: '',
+              focus: '',
+              title: '',
+              filename:
+                'https://a.storyblok.com/f/165473/2160x1549/7784154280/phone_heldnatural_en-1.png',
+              copyright: '',
+              fieldtype: 'asset',
+              is_external_url: false,
             },
-            cancellation: {
-              option: ExternalInsuranceCancellationOption.None,
-              requested: false,
-              bankSigneringApproveByDate: null,
-            },
-          },
-        ],
-      }}
-      // TODO: get this from some fixture module
-      story={{
-        ...storyblokStory,
-        content: {
-          body: [],
-          title: 'Your purchase is complete!',
-          subtitle: 'A confirmation has been sent via email.',
-          footerTitle: 'Do you still need help?',
-          footerSubtitle:
-            'Download the Hedvig app and contact us in the chat and we will help you.',
-          footerImage: {
-            id: 6967517,
-            alt: '',
-            name: '',
-            focus: '',
-            title: '',
-            filename:
-              'https://a.storyblok.com/f/165473/2160x1549/7784154280/phone_heldnatural_en-1.png',
-            copyright: '',
-            fieldtype: 'asset',
-            is_external_url: false,
-          },
-          checklistTitle: 'What happens next?',
-          checklistSubtitle: 'We will send you a confirmation via email.',
-          checklist: `Sign Hedvig
+            checklistTitle: 'What happens next?',
+            checklistSubtitle: 'We will send you a confirmation via email.',
+            checklist: `Sign Hedvig
           Connect payment method
           Download the app`,
-          faqTitle: 'Frequently asked questions',
-          faqSubtitle: 'Here are some answers to the most common questions.',
-          seoTitle: 'Your purchase is complete | Hedvig',
-        },
-      }}
-    />
+            faqTitle: 'Frequently asked questions',
+            faqSubtitle: 'Here are some answers to the most common questions.',
+            seoTitle: 'Your purchase is complete | Hedvig',
+          },
+        }}
+      />
+    </AppErrorProvider>
   )
 }
 
