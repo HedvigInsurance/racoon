23195433b3686820cc1e74aac328cf2959d5a0be
Robin Andeer <robin.andeer@gmail.com>
2023-06-14 09:54:55 +0200

    Add fetch insurance success component (#2538)
    <!--
    PR title: GRW-123 / Feature / Awesome new thing
    -->
    
    ## Describe your changes
    
    - Add Fetch Insurance Success state component
    
    ![Screenshot 2023-06-13 at 13.33.58.png](https://graphite-user-uploaded-assets-prod.s3.amazonaws.com/OgXegTwxM9IeuXHZyw5h/6047a0e7-1788-4871-a954-b8f0302ca4f8/Screenshot%202023-06-13%20at%2013.33.58.png)
    
    <!--
    What changes are made?
    If there are many changes, a list might be a good format.
    If it makes sense, add screenshots and/or screen recordings here.
    -->
    
    ## Justify why they are needed
    
    - Should be displayed at the end of Insurely flow
    
    ## Checklist before requesting a review
    
    - [ ] I have performed a self-review of my code

---
+++ apps/store/src/components/FetchInsuranceSuccess/FetchInsuranceSuccess.tsx
@@ -0,0 +1,32 @@
+import styled from '@emotion/styled'
+import { useTranslation } from 'next-i18next'
+import { type ReactNode } from 'react'
+import { Button, Heading, Space, theme } from 'ui'
+
+type Props = {
+  company: string
+  onClick: () => void
+  children: ReactNode
+}
+
+export const FetchInsuranceSuccess = ({ company, onClick, children }: Props) => {
+  const { t } = useTranslation('purchase-form')
+
+  return (
+    <Space y={1.5}>
+      <Heading as="h3" variant="standard.18">
+        {t('FETCH_INSURANCE_SUCCESS_TITLE', { company })}
+      </Heading>
+      <Space y={1}>
+        <GrayCard>{children}</GrayCard>
+        <Button onClick={onClick}>{t('FETCH_INSURANCE_SUCCESS_BUTTON')}</Button>
+      </Space>
+    </Space>
+  )
+}
+
+const GrayCard = styled.div({
+  backgroundColor: theme.colors.gray100,
+  borderRadius: theme.radius.xs,
+  padding: theme.space.md,
+})
