4a3985365caba0d345960ae93a02b9f5d49ce8a7
Robin Andeer <robin.andeer@gmail.com>
2023-06-14 09:51:17 +0200

    Add separate feature flag for Car Insurely

--- apps/store/src/utils/Features.ts
+++ apps/store/src/utils/Features.ts
@@ -13,6 +13,7 @@ const config = {
   SUCCESS_ANIMATION: process.env.NEXT_PUBLIC_FEATURE_SUCCESS_ANIMATION === 'true',
   MANYPETS_MIGRATION: process.env.NEXT_PUBLIC_FEATURE_MANYPETS_MIGRATION === 'true',
   SAS_PARTNERSHIP: process.env.NEXT_PUBLIC_FEATURE_SAS_PARTNERSHIP === 'true',
+  INSURELY_CAR: process.env.NEXT_PUBLIC_FEATURE_INSURELY_CAR === 'true',
 } as const
 
 export type FeatureFlag = keyof typeof config
