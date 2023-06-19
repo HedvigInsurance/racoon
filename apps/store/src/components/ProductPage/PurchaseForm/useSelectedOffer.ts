077ca9114f692dfab5f4a4a26d51cee296c066a8
Robin Andeer <robin.andeer@gmail.com>
2023-06-19 10:43:00 +0200

    Log when selecting a price matched offer

--- apps/store/src/components/ProductPage/PurchaseForm/useSelectedOffer.ts
+++ apps/store/src/components/ProductPage/PurchaseForm/useSelectedOffer.ts
@@ -1,8 +1,39 @@
-import { atom, useAtom } from 'jotai'
+import { datadogLogs } from '@datadog/browser-logs'
+import { SetStateAction, atom, useAtom } from 'jotai'
+import { useCallback } from 'react'
 import { ProductOfferFragment } from '@/services/apollo/generated'
 
 const selectedOfferAtom = atom<ProductOfferFragment | null>(null)
 
 export const useSelectedOffer = () => {
-  return useAtom(selectedOfferAtom)
+  const [selectedOffer, _setSelectedOffer] = useAtom(selectedOfferAtom)
+
+  const setSelectedOffer = useCallback(
+    (action: SetStateAction<ProductOfferFragment | null>): void => {
+      let offer: ProductOfferFragment | null = null
+
+      if (typeof action === 'function') {
+        _setSelectedOffer((prev) => {
+          offer = action(prev)
+          return offer
+        })
+      } else {
+        _setSelectedOffer(action)
+        offer = action
+      }
+
+      if (offer && offer.priceMatch) {
+        datadogLogs.logger.info('Selected price matched offer', {
+          offerId: offer.id,
+          product: offer.variant.product.name,
+          productVariant: offer.variant.displayName,
+          externalInsurer: offer.priceMatch.externalInsurer.displayName,
+          priceReduction: offer.priceMatch.priceReduction.amount,
+        })
+      }
+    },
+    [_setSelectedOffer],
+  )
+
+  return [selectedOffer, setSelectedOffer] as const
 }
