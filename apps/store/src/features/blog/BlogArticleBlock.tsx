f7afceec5a99d9de6b093d9dc7905f3804ee053e
Robin Andeer <robin.andeer@gmail.com>
2023-06-20 15:15:24 +0200

    Update blog article page header layout

--- apps/store/src/features/blog/BlogArticleBlock.tsx
+++ apps/store/src/features/blog/BlogArticleBlock.tsx
@@ -1,13 +1,13 @@
+import styled from '@emotion/styled'
 import { StoryblokComponent, storyblokEditable } from '@storyblok/react'
 import Link from 'next/link'
 import { render } from 'storyblok-rich-text-react-renderer'
-import { Heading, Space, Text } from 'ui'
+import { Heading, Space, Text, mq, theme } from 'ui'
 import { richTextRenderOptions } from '@/blocks/RichTextBlock/RichTextBlock'
 import { GridLayout } from '@/components/GridLayout/GridLayout'
 import { RichText } from '@/components/RichText/RichText'
 import { SpaceFlex } from '@/components/SpaceFlex/SpaceFlex'
 import { type SbBaseBlockProps } from '@/services/storyblok/storyblok'
-import { useFormatter } from '@/utils/useFormatter'
 import { ArticleTeaser } from './ArticleTeaser/ArticleTeaser'
 import { BLOG_ARTICLE_CONTENT_TYPE } from './blog.constants'
 import { convertToBlogArticleCategory } from './blog.helpers'
@@ -16,14 +16,14 @@ import { type BlogArticleContentType } from './blog.types'
 type Props = SbBaseBlockProps<BlogArticleContentType['content']>
 
 export const BlogArticleBlock = (props: Props) => {
-  const formatter = useFormatter()
   const categories = props.blok.categories.map(convertToBlogArticleCategory)
 
   return (
     <>
       <GridLayout.Root {...storyblokEditable(props.blok)}>
-        <GridLayout.Content width={{ lg: '2/3', xl: '1/2', xxl: '1/3' }} align="center">
-          <Space y={1}>
+        <GridLayout.Content width={{ md: '2/3', xl: '1/2', xxl: '1/3' }} align="center">
+          <TopPadding />
+          <Space y={0.5}>
             <SpaceFlex space={0.25}>
               {categories.map((item) => (
                 <Link key={item.id} href={item.href}>
@@ -31,12 +31,14 @@ export const BlogArticleBlock = (props: Props) => {
                 </Link>
               ))}
             </SpaceFlex>
-            <Heading as="h1" variant={{ _: 'serif.32', lg: 'serif.48' }}>
-              {props.blok.page_heading}
-            </Heading>
-            <Text size="sm" color="textSecondary">
-              {formatter.fromNow(new Date(props.blok.date))}
-            </Text>
+            <Space y={1.5}>
+              <Heading as="h1" variant={{ _: 'serif.32', lg: 'serif.48' }}>
+                {props.blok.page_heading}
+              </Heading>
+              <UppercaseText size="sm" color="textSecondary">
+                {formatDate(props.blok.date)}
+              </UppercaseText>
+            </Space>
           </Space>
           <RichText>{render(props.blok.content, richTextRenderOptions)}</RichText>
         </GridLayout.Content>
@@ -48,3 +50,20 @@ export const BlogArticleBlock = (props: Props) => {
   )
 }
 BlogArticleBlock.blockName = BLOG_ARTICLE_CONTENT_TYPE
+
+const TopPadding = styled.div({
+  height: theme.space.xxl,
+  [mq.md]: { height: theme.space.xxxl },
+})
+
+const UppercaseText = styled(Text)({
+  textTransform: 'uppercase',
+})
+
+const formatDate = (date: string) => {
+  return new Date(date).toLocaleDateString('sv-SE', {
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+  })
+}
