d9015332e0222b777ace273a78a9bb2368763e8d
Robin Andeer <robin.andeer@gmail.com>
2023-06-26 16:06:56 +0200

    Extract Product Hero component from Purchase Form

--- apps/store/src/components/Pillow/Pillow.tsx
+++ apps/store/src/components/Pillow/Pillow.tsx
@@ -8,7 +8,7 @@ type PillowProps = {
 }
 
 export const Pillow = ({ alt, src, ...props }: PillowProps) => {
-  if (!src) return <FallbackPillow size={props.size} />
+  if (!src) return <FallbackPillow {...props} size={props.size} />
   return (
     <StyledImage {...props} src={src} alt={alt ?? ''} width={208} height={208} decoding="sync" />
   )
@@ -16,9 +16,15 @@ export const Pillow = ({ alt, src, ...props }: PillowProps) => {
 
 const StyledImage = styled(Image)<PillowProps>(({ size = 'medium' }) => getSize(size))
 
-const FallbackPillow = ({ size }: Pick<PillowProps, 'size'>) => {
+const FallbackPillow = ({ size, ...props }: Pick<PillowProps, 'size'>) => {
   return (
-    <svg {...getSize(size)} viewBox="0 0 480 480" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <svg
+      {...props}
+      {...getSize(size)}
+      viewBox="0 0 480 480"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
       <path
         d="M3.79613e-05 240C2.56137e-05 428.992 101.382 480 240.298 480C379.214 480 480 432.012 480 240C480 47.9875 396.228 -5.43757e-05 240.298 -4.198e-05C84.3668 -6.01019e-05 5.03089e-05 51.0075 3.79613e-05 240Z"
         fill="#F5F5F5"
