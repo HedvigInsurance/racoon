bc89e0cc8ccf3b61d84b6d822362fcee02307457
Robin Andeer <robin.andeer@gmail.com>
2023-06-30 10:20:23 +0200

    Blog: implement show more button to load all articles (#2645)
    <!--
    PR title: GRW-123 / Feature / Awesome new thing
    -->
    
    ## Describe your changes
    
    - Implement a load more button for the blog listing pages
    
    - Display first 12 articles until you click the button
    
    <!--
    What changes are made?
    If there are many changes, a list might be a good format.
    If it makes sense, add screenshots and/or screen recordings here.
    -->
    
    ## Justify why they are needed
    
    - Don't overload the user with articles
    
    - Keep track of "show all state" in query param so we remember it when navigating back to the page
    
    ## Checklist before requesting a review
    
    - [ ] I have performed a self-review of my code

--- apps/store/src/features/blog/BlogArticleListBlock.tsx
+++ apps/store/src/features/blog/BlogArticleListBlock.tsx
@@ -1,6 +1,9 @@
 import styled from '@emotion/styled'
+import { useTranslation } from 'next-i18next'
+import { useRouter } from 'next/router'
 import { useMemo } from 'react'
-import { theme } from 'ui'
+import { Space, theme } from 'ui'
+import { ButtonNextLink } from '@/components/ButtonNextLink'
 import { GridLayout } from '@/components/GridLayout/GridLayout'
 import { type SbBaseBlockProps } from '@/services/storyblok/storyblok'
 import { useFormatter } from '@/utils/useFormatter'
@@ -8,14 +11,18 @@ import { ArticleTeaser } from './ArticleTeaser/ArticleTeaser'
 import { BLOG_ARTICLE_LIST_BLOCK } from './blog.constants'
 import { useBlogArticleTeasers } from './useBlog'
 
+const INITIAL_VISIBLE_COUNT = 12
+const QUERY_PARAM = 'showAll'
+
 type Props = SbBaseBlockProps<{
   categories?: Array<string>
 }>
 
 export const BlogArticleListBlock = (props: Props) => {
-  const teaserList = useBlogArticleTeasers()
+  const { t } = useTranslation()
   const formatter = useFormatter()
 
+  const teaserList = useBlogArticleTeasers()
   const filteredTeaserList = useMemo(() => {
     if (!props.blok.categories?.length) return teaserList
 
@@ -25,22 +32,43 @@ export const BlogArticleListBlock = (props: Props) => {
     })
   }, [teaserList, props.blok.categories])
 
+  const router = useRouter()
+  const showAll =
+    filteredTeaserList.length <= INITIAL_VISIBLE_COUNT || router.query[QUERY_PARAM] === '1'
+
+  const visibleTeaserList = showAll
+    ? filteredTeaserList
+    : filteredTeaserList.slice(0, INITIAL_VISIBLE_COUNT)
+
   return (
     <GridLayout.Root>
-      <GridLayout.Content width="1" align="center">
-        <List>
-          {filteredTeaserList.map((item) => (
-            <ArticleTeaser.Root
-              key={item.id}
-              href={item.href}
-              title={item.heading}
-              ingress={item.text}
-              date={formatter.dateFull(new Date(item.date))}
-            >
-              <ArticleTeaser.Image {...item.image} alt={item.image.alt} />
-            </ArticleTeaser.Root>
-          ))}
-        </List>
+      <GridLayout.Content width={{ xxl: '5/6' }} align="center">
+        <Space y={4}>
+          <List>
+            {visibleTeaserList.map((item, index) => (
+              <ArticleTeaser.Root
+                key={item.id}
+                href={item.href}
+                title={item.heading}
+                ingress={item.text}
+                date={formatter.dateFull(new Date(item.date))}
+              >
+                <ArticleTeaser.Image {...item.image} alt={item.image.alt} priority={index < 6} />
+              </ArticleTeaser.Root>
+            ))}
+          </List>
+
+          {!showAll && (
+            <ButtonWrapper>
+              <InlineButtonLink
+                href={{ query: { ...router.query, [QUERY_PARAM]: '1' } }}
+                shallow={true}
+              >
+                {t('BLOG_LOAD_MORE_BUTTON')}
+              </InlineButtonLink>
+            </ButtonWrapper>
+          )}
+        </Space>
       </GridLayout.Content>
     </GridLayout.Root>
   )
@@ -54,3 +82,10 @@ const List = styled.div({
   gridTemplateColumns: 'repeat(auto-fill, minmax(21rem, 1fr))',
   gridTemplateRows: 'max-content',
 })
+
+const ButtonWrapper = styled.div({
+  display: 'flex',
+  justifyContent: 'center',
+})
+
+const InlineButtonLink = styled(ButtonNextLink)({ width: 'auto' })
