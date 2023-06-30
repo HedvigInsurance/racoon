680c7cc62c38d4b51ea462c8b6e3a0fae94ef021
Robin Andeer <robin.andeer@gmail.com>
2023-06-30 10:19:39 +0200

    Blog: clean up alignment in article teaser layout (#2644)
    <!--
    PR title: GRW-123 / Feature / Awesome new thing
    -->
    
    ## Describe your changes
    
    ![Screenshot 2023-06-29 at 14.41.39.png](https://graphite-user-uploaded-assets-prod.s3.amazonaws.com/OgXegTwxM9IeuXHZyw5h/e225081c-feed-4306-8a83-d9d8f91f2bcb/Screenshot%202023-06-29%20at%2014.41.39.png)
    
    - Align blog title and ingress to start on the same line
    
    - Update aspect ratio for teaser images and fix content fit
    
    <!--
    What changes are made?
    If there are many changes, a list might be a good format.
    If it makes sense, add screenshots and/or screen recordings here.
    -->
    
    ## Justify why they are needed
    
    - More unified article teaser grid
    
    - Images where being stretched
    
    ## Checklist before requesting a review
    
    - [ ] I have performed a self-review of my code

--- apps/store/src/features/blog/ArticleTeaser/ArticleTeaser.stories.tsx
+++ apps/store/src/features/blog/ArticleTeaser/ArticleTeaser.stories.tsx
@@ -10,23 +10,21 @@ type Story = StoryObj
 
 export const Primary: Story = {
   render: () => (
-    <ArticleTeaser.Root>
+    <ArticleTeaser.Root
+      href="/se"
+      title="Hedvig och Mimmi Blomqvist lanserar hundskålar i limiterad upplaga"
+      ingress="I ett nytt samarbete har Hedvig tillsammans med designern Mimmi Blomqvist ställt sig frågan
+        vad som händer när en hundskål inte bara fyller en funktion, utan också blir ett
+        designobjekt. Resultatet är en limiterad upplaga handgjorda hundskålar i porslin med Mimmi
+        Blomqvists karaktäristiska formspråk."
+      date="2021.05.12"
+    >
       <ArticleTeaser.Image
         src="https://www.hedvig.com/f/62762/800x450/42924b7dde/mimmi-thumbnail.jpg"
         width={800}
         height={450}
         alt="Mimmi"
       />
-      <ArticleTeaser.Content
-        href="/se"
-        title="Hedvig och Mimmi Blomqvist lanserar hundskålar i limiterad upplaga"
-        date="2021.05.12"
-      >
-        I ett nytt samarbete har Hedvig tillsammans med designern Mimmi Blomqvist ställt sig frågan
-        vad som händer när en hundskål inte bara fyller en funktion, utan också blir ett
-        designobjekt. Resultatet är en limiterad upplaga handgjorda hundskålar i porslin med Mimmi
-        Blomqvists karaktäristiska formspråk.
-      </ArticleTeaser.Content>
     </ArticleTeaser.Root>
   ),
 }
