05467a82059dc3c637696dd94e6027bfd6105388
Robin Andeer <robin.andeer@gmail.com>
2023-07-07 17:08:06 +0200

    Add Slack endpoint to trigger a deploy of the website

--- bin/vercel-ignore-step.sh
+++ bin/vercel-ignore-step.sh
@@ -7,4 +7,10 @@ if [ "$VERCEL_GIT_COMMIT_REF" = "assets" ]; then
   exit 0
 fi
 
+# Support Vercel deploy hook
+if [ "$VERCEL_GIT_COMMIT_SHA" = "$VERCEL_GIT_PREVIOUS_SHA" ]; then
+  echo "Re-deploy of commit $VERCEL_GIT_COMMIT_SHA"
+  exit 1
+fi
+
 yarn dlx turbo-ignore
