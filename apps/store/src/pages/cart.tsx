a436406b884e6bd933b3b27916ecd4e72881345f
Robin Andeer <robin.andeer@gmail.com>
2023-07-11 11:04:09 +0200

    Add helper to get props for pages that use LayoutWithMenu component

--- apps/store/src/pages/cart.tsx
+++ apps/store/src/pages/cart.tsx
@@ -1,6 +1,5 @@
 import type { GetStaticProps, NextPageWithLayout } from 'next'
 import { useTranslation } from 'next-i18next'
-import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
 import Head from 'next/head'
 import { useMemo } from 'react'
 import {
@@ -10,16 +9,9 @@ import {
   getCartEntry,
 } from '@/components/CartInventory/CartInventory.helpers'
 import { CartPage } from '@/components/CartPage/CartPage'
-import {
-  fetchGlobalProductMetadata,
-  GLOBAL_PRODUCT_METADATA_PROP_NAME,
-} from '@/components/LayoutWithMenu/fetchProductMetadata'
+import { getLayoutWithMenuProps } from '@/components/LayoutWithMenu/getLayoutWithMenuProps'
 import { LayoutWithMenu } from '@/components/LayoutWithMenu/LayoutWithMenu'
-import { initializeApollo } from '@/services/apollo/client'
 import { useShopSession } from '@/services/shopSession/ShopSessionContext'
-import { getGlobalStory } from '@/services/storyblok/storyblok'
-import { GLOBAL_STORY_PROP_NAME } from '@/services/storyblok/Storyblok.constant'
-import { isRoutingLocale } from '@/utils/l10n/localeUtils'
 import { useGetDiscountExplanation } from '@/utils/useDiscountExplanation'
 
 const NextCartPage: NextPageWithLayout = (props) => {
@@ -69,23 +61,12 @@ const NextCartPage: NextPageWithLayout = (props) => {
 NextCartPage.getLayout = (children) => <LayoutWithMenu>{children}</LayoutWithMenu>
 
 export const getStaticProps: GetStaticProps = async (context) => {
-  const { locale } = context
-  if (!isRoutingLocale(locale)) return { notFound: true }
-  const apolloClient = initializeApollo({ locale })
-  const [globalStory, translations, productMetadata] = await Promise.all([
-    getGlobalStory({ locale }),
-    serverSideTranslations(locale),
-    fetchGlobalProductMetadata({ apolloClient }),
-  ])
+  const props = await getLayoutWithMenuProps(context)
+  if (props === null) return { notFound: true }
 
-  const revalidate = process.env.VERCEL_ENV === 'preview' ? 1 : false
   return {
-    props: {
-      ...translations,
-      [GLOBAL_STORY_PROP_NAME]: globalStory,
-      [GLOBAL_PRODUCT_METADATA_PROP_NAME]: productMetadata,
-    },
-    revalidate,
+    props,
+    revalidate: process.env.VERCEL_ENV === 'preview' ? 1 : false,
   }
 }
 
