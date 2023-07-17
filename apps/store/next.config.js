2934bca5e88f00548faf95169b2439f523f4637f
Alexander Lebedev <alexander.lebedev@gmail.com>
2023-07-17 11:12:41 +0200

    GEN-765 Redirect most-visited old landing pages with campaign code to new website

--- apps/store/next.config.js
+++ apps/store/next.config.js
@@ -95,8 +95,7 @@ module.exports = withBundleAnalyzer({
   },
   async redirects() {
     const locales = ['no', 'no-en', 'dk', 'dk-en']
-
-    return [
+    const shutDownMarketsInfo = [
       ...locales.map((locale) => ({
         source: `/${locale}/new-member`,
         destination: `/${locale}/info`,
@@ -110,6 +109,21 @@ module.exports = withBundleAnalyzer({
         locale: false,
       })),
     ]
+    const oldSiteCampaigns = [
+      {
+        source: '/(se/)?new-member(/hedvig)?',
+        has: [
+          {
+            type: 'query',
+            key: 'code',
+          },
+        ],
+        permanent: false,
+        destination: '/api/campaign/:code?code=&next=/se/forsakringar',
+        locale: false,
+      },
+    ]
+    return [...shutDownMarketsInfo, ...oldSiteCampaigns]
   },
 })
 
