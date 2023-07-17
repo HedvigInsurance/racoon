579c21c6c46f10eb312c0fccd788d8156793ebab
Guilherme Popolin <guilherme@hedvig.com>
2023-07-17 13:44:01 +0200

    feat: redirect home routes

--- apps/store/next.config.js
+++ apps/store/next.config.js
@@ -151,6 +151,42 @@ module.exports = withBundleAnalyzer({
               permanent: false,
               locale: false,
             },
+            {
+              source: '/se/new-member/home-insurance',
+              has: [
+                {
+                  type: 'query',
+                  key: 'code',
+                },
+              ],
+              destination: '/api/campaign/:code?code=&next=/se/forsakringar/hemforsakring',
+              permanent: false,
+              locale: false,
+            },
+            {
+              source: '/se/new-member/home-insurance',
+              destination: '/se/forsakringar/hemforsakring',
+              permanent: false,
+              locale: false,
+            },
+            {
+              source: '/se-en/new-member/home-insurance',
+              has: [
+                {
+                  type: 'query',
+                  key: 'code',
+                },
+              ],
+              destination: '/api/campaign/:code?code=&next=/se-en/insurances/home-insurance',
+              permanent: false,
+              locale: false,
+            },
+            {
+              source: '/se-en/new-member/home-insurance',
+              destination: '/se-en/insurances/home-insurance',
+              permanent: false,
+              locale: false,
+            },
           ]
         : []
     return [...shutDownMarketsInfo, ...oldSiteRedirects, ...getExperimentVariantRedirects()]
