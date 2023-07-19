b8f2dd2c203a85da40de54a6c1c9a2f8c4029430
Guilherme Popolin <guilherme@hedvig.com>
2023-07-19 13:56:07 +0200

    styles: update border radius levels

--- packages/ui/src/lib/theme/radius.ts
+++ packages/ui/src/lib/theme/radius.ts
@@ -4,7 +4,7 @@ const base = {
   2: 12,
   3: 16,
   4: 20,
-  5: 28,
+  5: 24,
 } as const
 
 export const radius = {
@@ -13,8 +13,8 @@ export const radius = {
   // Aliases
   xxs: base[0],
   xs: base[0],
-  sm: base[2],
-  md: base[3],
-  lg: base[4],
+  sm: base[1],
+  md: base[2],
+  lg: base[3],
   xl: base[5],
 }
