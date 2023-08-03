0bb7918081d4436d7884be023bb608cd463a3ea1
Robin Andeer <robin.andeer@gmail.com>
2023-08-03 11:58:34 +0200

    Partner Init: pre-fill information from search params in price intent

--- apps/store/src/pages/api/partner/init.ts
+++ apps/store/src/pages/api/partner/init.ts
@@ -1,118 +1,52 @@
-import { ApolloClient } from '@apollo/client'
 import { NextApiHandler } from 'next'
+import { getProductData } from '@/components/ProductPage/ProductPage.helpers'
+import { OPEN_PRICE_CALCULATOR_QUERY_PARAM } from '@/components/ProductPage/PurchaseForm/useOpenPriceCalculatorQueryParam'
 import { initializeApolloServerSide } from '@/services/apollo/client'
-import {
-  CountryCode,
-  PartnerWidgetInitDocument,
-  PartnerWidgetInitMutation,
-  PartnerWidgetInitMutationVariables,
-} from '@/services/apollo/generated'
-import { isIsoLocale, toRoutingLocale } from '@/utils/l10n/localeUtils'
-import { IsoLocale } from '@/utils/l10n/types'
+import { createPartnerShopSession } from '@/services/partner/createPartnerShopSession'
+import { parseSearchParams } from '@/services/partner/parseSearchParams'
+import { fetchPriceTemplate } from '@/services/PriceCalculator/PriceCalculator.helpers'
+import { priceIntentServiceInitServerSide } from '@/services/priceIntent/PriceIntentService'
+import { setupShopSessionServiceServerSide } from '@/services/shopSession/ShopSession.helpers'
 import { ORIGIN_URL, PageLink } from '@/utils/PageLink'
-
-const DEFAULT_LOCALE: IsoLocale = 'sv-SE'
-
-enum SearchParam {
-  Locale = 'locale',
-  PartnerId = 'partnerId',
-  ExternalMemberId = 'externalMemberId',
-  ProductType = 'productType',
-  ExternalRequestId = 'requestId',
-}
 
 const handler: NextApiHandler = async (req, res) => {
   if (req.url === undefined) throw new Error(`Missing req.url`)
 
-  const { searchParams } = new URL(req.url, ORIGIN_URL)
-  const searchLocale = searchParams.get(SearchParam.Locale)
-  const externalMemberId = searchParams.get(SearchParam.ExternalMemberId)
-  const externalRequestId = searchParams.get(SearchParam.ExternalRequestId)
-  const productType = searchParams.get(SearchParam.ProductType)
-  const partnerId = searchParams.get(SearchParam.PartnerId)
-
-  const locale = isIsoLocale(searchLocale) ? searchLocale : DEFAULT_LOCALE
-  const routingLocale = toRoutingLocale(locale)
-
-  // TODO: Redirect to some error page
-  const fallbackRedirect = `${ORIGIN_URL}${PageLink.home({ locale: routingLocale })}`
-
-  if (!partnerId) {
-    console.warn(`Missing partnerId`)
-    res.redirect(fallbackRedirect)
-    return
-  }
-  console.info(`Partner Init | Partner ID = ${partnerId}`)
-
-  const apolloClient = await initializeApolloServerSide({ locale: routingLocale, req, res })
-  let data: Awaited<ReturnType<typeof initPartnerWidget>>
   try {
-    data = await initPartnerWidget(apolloClient, {
-      partnerId,
-      externalMemberId,
-      externalRequestId,
+    const { searchParams } = new URL(req.url, ORIGIN_URL)
+    const { locale, partnerWidgetInitVariables, productName, priceIntentData, customerData } =
+      parseSearchParams(searchParams)
+    console.info(`Partner Init | Partner ID = ${partnerWidgetInitVariables.partnerId}`)
+
+    const apolloClient = await initializeApolloServerSide({ locale, req, res })
+    const { id: shopSessionId, partnerName } = await createPartnerShopSession(
+      apolloClient,
+      partnerWidgetInitVariables,
+    )
+    setupShopSessionServiceServerSide({ apolloClient, req, res }).saveId(shopSessionId)
+    console.info(`Partner Init | Shop Session = ${shopSessionId}, Partner = ${partnerName}`)
+
+    const service = priceIntentServiceInitServerSide({ req, res, apolloClient })
+    const priceTemplate = fetchPriceTemplate(productName)
+    if (!priceTemplate) throw new Error(`Missing price template for ${productName}`)
+    const priceIntent = await service.create({ productName, priceTemplate, shopSessionId })
+    await service.update({
+      priceIntentId: priceIntent.id,
+      data: priceIntentData,
+      customer: { shopSessionId, ...customerData },
     })
+    console.info(`Partner Init | Price Intent = ${priceIntent.id}`)
+
+    const { pageLink } = await getProductData({ apolloClient, productName })
+    const nextUrl = new URL(pageLink, ORIGIN_URL)
+    nextUrl.searchParams.set(OPEN_PRICE_CALCULATOR_QUERY_PARAM, '1')
+    res.redirect(nextUrl.toString())
   } catch (error) {
-    console.warn(`Failed to initialize partner widget session: ${error}`)
+    console.error(`Failed to initialize partner widget session: ${error}`)
+    // TODO: Redirect to some error page
+    const fallbackRedirect = `${ORIGIN_URL}${PageLink.home({ locale: 'se' })}`
     res.redirect(fallbackRedirect)
-    return
   }
-
-  const shopSessionId = data.id
-  const partnerName = data.partnerName ?? undefined
-  console.info(`Partner Init | Shop Session = ${shopSessionId}, Partner = ${partnerName}`)
-
-  // TODO: prefill price intent
-
-  const pageLink = getProductPageLink(productType)
-  res.redirect(
-    PageLink.session({
-      shopSessionId,
-      locale: routingLocale,
-      next: pageLink,
-    }),
-  )
 }
 
 export default handler
-
-const initPartnerWidget = async (
-  apolloClient: ApolloClient<unknown>,
-  variables: Omit<PartnerWidgetInitMutationVariables['input'], 'countryCode'>,
-): Promise<PartnerWidgetInitMutation['partnerWidgetInit']> => {
-  const response = await apolloClient.mutate<
-    PartnerWidgetInitMutation,
-    PartnerWidgetInitMutationVariables
-  >({
-    mutation: PartnerWidgetInitDocument,
-    variables: {
-      input: {
-        countryCode: CountryCode.Se,
-        ...variables,
-      },
-    },
-  })
-
-  if (!response.data) {
-    const errors = response.errors?.map((error) => error.message).join('\n')
-    throw new Error(errors)
-  }
-
-  return response.data.partnerWidgetInit
-}
-
-// Temporary
-const getProductPageLink = (productType: string | null) => {
-  switch (productType) {
-    case 'SWEDISH_APARTMENT':
-      return `/se/forsakringar/hemforsakring/hyresratt`
-
-    case 'SWEDISH_HOUSE':
-      return `/se/forsakringar/hemforsakring/villaforsakring`
-    case 'SWEDISH_ACCIDENT':
-      return `/se/forsakringar/olycksfallsforsakring`
-
-    default:
-      return PageLink.store({ locale: 'se' })
-  }
-}
