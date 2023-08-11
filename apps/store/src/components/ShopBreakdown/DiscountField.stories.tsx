5285f99d658794591db3a93c6c9e6cd4e34e0188
Robin Andeer <robin.andeer@gmail.com>
2023-08-11 17:27:25 +0200

    Add `DiscountField` component (#2941)
    <!--
    PR title: GRW-123 / Feature / Awesome new thing
    -->
    
    ## Describe your changes
    
    - Add new `DiscountField` component to be used in "shop breakdown"
    
    <!--
    What changes are made?
    If there are many changes, a list might be a good format.
    If it makes sense, add screenshots and/or screen recordings here.
    -->
    
    ## Justify why they are needed
    
    - Based on Figma UI kit
    
    - Make sure the component is not API dependent
    
    ## Checklist before requesting a review
    
    - [ ] I have performed a self-review of my code

---
+++ apps/store/src/components/ShopBreakdown/DiscountField.stories.tsx
@@ -0,0 +1,55 @@
+import { type Meta, type StoryObj } from '@storybook/react'
+import { DiscountField } from './DiscountField'
+
+const meta: Meta<typeof DiscountField> = {
+  title: 'Components / Shop Breakdown / Discount Field',
+  component: DiscountField,
+  argTypes: {
+    onAdd: { action: 'onAdd', table: { disable: true } },
+    onRemove: { action: 'onRemove', table: { disable: true } },
+  },
+}
+
+export default meta
+type Story = StoryObj<typeof DiscountField>
+
+export const Inactive: Story = {
+  args: {},
+}
+
+export const Active: Story = {
+  args: {
+    defaultActive: true,
+  },
+}
+
+export const ActiveLoading: Story = {
+  args: {
+    ...Active.args,
+    loadingAdd: true,
+  },
+}
+
+export const ActiveError: Story = {
+  args: {
+    ...Active.args,
+    errorMessage: 'Invalid code',
+  },
+}
+
+export const ActiveAdded: Story = {
+  args: {
+    ...Active.args,
+    campaign: {
+      code: 'SUMMER',
+      explanation: '3 months for free',
+    },
+  },
+}
+
+export const ActiveAddedLoading: Story = {
+  args: {
+    ...ActiveAdded.args,
+    loadingRemove: true,
+  },
+}
