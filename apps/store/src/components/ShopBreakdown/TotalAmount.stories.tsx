30a5966d54706159d43c0e0d09a09567a98224d7
Robin Andeer <robin.andeer@gmail.com>
2023-08-11 17:22:24 +0200

    Add `TotalAmount` component (#2940)
    <!--
    PR title: GRW-123 / Feature / Awesome new thing
    -->
    
    ## Describe your changes
    
    ![Screenshot 2023-08-11 at 10.33.17.png](https://graphite-user-uploaded-assets-prod.s3.amazonaws.com/OgXegTwxM9IeuXHZyw5h/7a7fcaef-d50a-49be-9573-4f529f1c0c1f/Screenshot%202023-08-11%20at%2010.33.17.png)
    
    - Add `TotalAmount` component based on Figma design (part of Shop Breakdown)
    
    <!--
    What changes are made?
    If there are many changes, a list might be a good format.
    If it makes sense, add screenshots and/or screen recordings here.
    -->
    
    ## Justify why they are needed
    
    - Will be used inside new Shop Breakdown component (to come)
    
    ## Checklist before requesting a review
    
    - [ ] I have performed a self-review of my code

---
+++ apps/store/src/components/ShopBreakdown/TotalAmount.stories.tsx
@@ -0,0 +1,28 @@
+import { type Meta, type StoryObj } from '@storybook/react'
+import { CurrencyCode } from '@/services/apollo/generated'
+import { TotalAmount } from './TotalAmount'
+
+const meta: Meta<typeof TotalAmount> = {
+  title: 'Components / Shop Breakdown / Total Amount',
+  component: TotalAmount,
+}
+
+export default meta
+type Story = StoryObj<typeof TotalAmount>
+
+export const Default: Story = {
+  args: {
+    currencyCode: CurrencyCode.Sek,
+    amount: 138,
+  },
+}
+
+export const WithDiscount: Story = {
+  args: {
+    ...Default.args,
+    discount: {
+      reducedAmount: 0,
+      explanation: 'i 6 månader, sedan 138 kr/mån',
+    },
+  },
+}
