d24600424cdc3e9bd231d34f0de63a290ba40fd1
Gustav Eén <gustaveen@gmail.com>
2023-08-16 14:08:23 +0200

    Fix broken apollo mock for PurchaseForm (#2932)
    <!--
    PR title: GRW-123 / Feature / Awesome new thing
    -->
    
    ## Describe your changes
    
    <!--
    What changes are made?
    If there are many changes, a list might be a good format.
    If it makes sense, add screenshots and/or screen recordings here.
    -->
    
    ## Justify why they are needed
    
    ## Checklist before requesting a review
    
    - [ ] I have performed a self-review of my code

--- apps/store/src/components/ProductPage/PurchaseForm/PurchaseForm.stories.tsx
+++ apps/store/src/components/ProductPage/PurchaseForm/PurchaseForm.stories.tsx
@@ -77,6 +77,7 @@ export const Default: Story = {
                     },
                     __typename: 'CartCost',
                   },
+                  redeemedCampaign: null,
                   entries: [],
                   __typename: 'Cart',
                 },
