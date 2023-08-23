05cf04003f449e955dadecd215561e5242786e1b
Robin Andeer <robin.andeer@gmail.com>
2023-08-23 16:50:27 +0200

    ShopBreakdown: add edit action button (container) (#3022)
    <!--
    PR title: GRW-123 / Feature / Awesome new thing
    -->
    
    ## Describe your changes
    
    - Add edit product offer action button component with related logic
    
    <!--
    What changes are made?
    If there are many changes, a list might be a good format.
    If it makes sense, add screenshots and/or screen recordings here.
    -->
    
    ## Justify why they are needed
    
    - Co-location
    
    ## Checklist before requesting a review
    
    - [ ] I have performed a self-review of my code

---
+++ apps/store/src/components/CartPage/EditActionButton.tsx
@@ -0,0 +1,57 @@
+import styled from '@emotion/styled'
+import { useTranslation } from 'next-i18next'
+import Balancer from 'react-wrap-balancer'
+import { Button, Text } from 'ui'
+import * as FullscreenDialog from '@/components/FullscreenDialog/FullscreenDialog'
+import { ActionButton } from '@/components/ProductItem/ProductItem'
+import { ProductOfferFragment } from '@/services/apollo/generated'
+import { useEditProductOffer } from './useEditProductOffer'
+
+type Props = {
+  shopSessionId: string
+  offer: ProductOfferFragment
+}
+
+export const EditActionButton = (props: Props) => {
+  const { t } = useTranslation('cart')
+  const [editProductOffer, state] = useEditProductOffer()
+
+  const handleConfirmEdit = () => {
+    editProductOffer({
+      shopSessionId: props.shopSessionId,
+      offerId: props.offer.id,
+      productName: props.offer.variant.product.name,
+      data: props.offer.priceIntentData,
+    })
+  }
+
+  return (
+    <FullscreenDialog.Root>
+      <FullscreenDialog.Trigger asChild={true}>
+        <ActionButton>{t('CART_ENTRY_EDIT_BUTTON')}</ActionButton>
+      </FullscreenDialog.Trigger>
+
+      <FullscreenDialog.Modal
+        center={true}
+        Footer={
+          <>
+            <Button onClick={handleConfirmEdit} loading={state === 'loading'}>
+              {t('EDIT_CONFIRMATION_MODAL_CONTINUE')}
+            </Button>
+            <FullscreenDialog.Close asChild={true}>
+              <Button type="button" variant="ghost">
+                {t('EDIT_CONFIRMATION_MODAL_CANCEL')}
+              </Button>
+            </FullscreenDialog.Close>
+          </>
+        }
+      >
+        <CappedText size={{ _: 'md', lg: 'xl' }} align="center">
+          <Balancer>{t('EDIT_CONFIRMATION_MODAL_PROMPT')}</Balancer>
+        </CappedText>
+      </FullscreenDialog.Modal>
+    </FullscreenDialog.Root>
+  )
+}
+
+const CappedText = styled(Text)({ maxWidth: '42rem' })
