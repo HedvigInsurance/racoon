44e1206abd24691c8cb4e049e2a5cb9682fca4b2
Robin Andeer <robin.andeer@gmail.com>
2023-08-23 16:39:25 +0200

    ShopBreakdown: add remove action button (container) (#3021)
    <!--
    PR title: GRW-123 / Feature / Awesome new thing
    -->
    
    ## Describe your changes
    
    - Add container component for the "remove offer from cart"-button
    
    <!--
    What changes are made?
    If there are many changes, a list might be a good format.
    If it makes sense, add screenshots and/or screen recordings here.
    -->
    
    ## Justify why they are needed
    
    - Co-locate logic
    
    ## Checklist before requesting a review
    
    - [ ] I have performed a self-review of my code

---
+++ apps/store/src/components/CartPage/RemoveActionButton.tsx
@@ -0,0 +1,73 @@
+import { datadogLogs } from '@datadog/browser-logs'
+import { useTranslation } from 'react-i18next'
+import { Button, Dialog, Text } from 'ui'
+import * as FullscreenDialog from '@/components/FullscreenDialog/FullscreenDialog'
+import { ActionButton } from '@/components/ProductItem/ProductItem'
+import {
+  ProductRecommendationsDocument,
+  ShopSessionDocument,
+  useCartEntryRemoveMutation,
+} from '@/services/apollo/generated'
+import { useAppErrorHandleContext } from '@/services/appErrors/AppErrorContext'
+import { useTracking } from '@/services/Tracking/useTracking'
+
+type Props = {
+  shopSessionId: string
+  offerId: string
+  title: string
+}
+
+export const RemoveActionButton = (props: Props) => {
+  const { t } = useTranslation('cart')
+
+  const { showError } = useAppErrorHandleContext()
+  const tracking = useTracking()
+  const [removeProductOffer, result] = useCartEntryRemoveMutation({
+    variables: { shopSessionId: props.shopSessionId, offerId: props.offerId },
+    refetchQueries: [ShopSessionDocument, ProductRecommendationsDocument],
+    awaitRefetchQueries: true,
+    onCompleted(data) {
+      const updatedShopSession = data.shopSessionCartEntriesRemove.shopSession
+      if (updatedShopSession) {
+        const offer = updatedShopSession.cart.entries.find((item) => item.id == props.offerId)
+        if (offer) {
+          tracking.reportDeleteFromCart(offer)
+        } else {
+          datadogLogs.logger.error('Failed to find offer being removed in session cart', {
+            shopSessionId: props.shopSessionId,
+            offerId: props.offerId,
+          })
+        }
+      }
+    },
+    onError: showError,
+  })
+
+  return (
+    <FullscreenDialog.Root>
+      <Dialog.Trigger asChild={true}>
+        <ActionButton>{t('REMOVE_ENTRY_BUTTON')}</ActionButton>
+      </Dialog.Trigger>
+
+      <FullscreenDialog.Modal
+        center={true}
+        Footer={
+          <>
+            <Button onClick={() => removeProductOffer()} loading={result.loading}>
+              {t('REMOVE_ENTRY_MODAL_CONFIRM_BUTTON')}
+            </Button>
+            <FullscreenDialog.Close asChild={true}>
+              <Button type="button" variant="ghost">
+                {t('REMOVE_ENTRY_MODAL_CANCEL_BUTTON')}
+              </Button>
+            </FullscreenDialog.Close>
+          </>
+        }
+      >
+        <Text size={{ _: 'md', lg: 'xl' }} align="center">
+          {t('REMOVE_ENTRY_MODAL_PROMPT', { name: props.title })}
+        </Text>
+      </FullscreenDialog.Modal>
+    </FullscreenDialog.Root>
+  )
+}
