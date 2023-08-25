ec027ed355c73af054ac02526535ee210316b087
Robin Andeer <robin.andeer@gmail.com>
2023-08-25 11:24:25 +0200

    Checkout page: add debug dialog (#3043)
    <!--
    PR title: GRW-123 / Feature / Awesome new thing
    -->
    
    ## Describe your changes
    
    - Add page debug dialog to the checkout page
    
    <!--
    What changes are made?
    If there are many changes, a list might be a good format.
    If it makes sense, add screenshots and/or screen recordings here.
    -->
    
    ## Justify why they are needed
    
    - better DX
    
    ## Checklist before requesting a review
    
    - [ ] I have performed a self-review of my code

---
+++ apps/store/src/components/CheckoutPage/PageDebugDialog.tsx
@@ -0,0 +1,12 @@
+'use client'
+
+import { DebugDialog } from '@/components/DebugDialog/DebugDialog'
+import { DebugShopSessionSection } from '@/components/DebugDialog/DebugShopSessionSection'
+
+export const PageDebugDialog = () => {
+  return (
+    <DebugDialog>
+      <DebugShopSessionSection />
+    </DebugDialog>
+  )
+}
