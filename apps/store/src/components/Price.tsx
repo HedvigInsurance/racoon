8c7811978755951decd77644f18de6bfba90a160
Robin Andeer <robin.andeer@gmail.com>
2023-08-28 11:19:24 +0200

    ShopBreakdown: add presentational `QuickAdd` component (#3052)
    <!--
    PR title: GRW-123 / Feature / Awesome new thing
    -->
    
    ## Describe your changes
    
    
    ![Screenshot 2023-08-25 at 11.28.49.png](https://graphite-user-uploaded-assets-prod.s3.amazonaws.com/OgXegTwxM9IeuXHZyw5h/f1daf795-a6c7-4ab7-9a81-0168176f7646/Screenshot%202023-08-25%20at%2011.28.49.png)
    
    
    - Add presentational component for `QuickAdd` (from Figma)
    
    <!--
    What changes are made?
    If there are many changes, a list might be a good format.
    If it makes sense, add screenshots and/or screen recordings here.
    -->
    
    ## Justify why they are needed
    
    - Will replace existing `CartEntryOfferItem`
    
    ## Checklist before requesting a review
    
    - [ ] I have performed a self-review of my code

--- apps/store/src/components/Price.tsx
+++ apps/store/src/components/Price.tsx
@@ -1,4 +1,5 @@
 import styled from '@emotion/styled'
+import { ComponentProps } from 'react'
 import { Text, theme } from 'ui'
 import { CurrencyCode } from '@/services/apollo/generated'
 import { useFormatter } from '@/utils/useFormatter'
@@ -7,22 +8,29 @@ type Props = {
   currencyCode: CurrencyCode
   amount: number
   reducedAmount?: number
+
+  color?: ComponentProps<typeof Text>['color']
+  secondaryColor?: ComponentProps<typeof Text>['color']
 }
 
-export const Price = (props: Props) => {
+export const Price = ({
+  color = 'textPrimary',
+  secondaryColor = 'textSecondary',
+  ...props
+}: Props) => {
   const formatter = useFormatter()
 
   return (
     <Wrapper>
       {props.reducedAmount !== undefined && (
-        <Text as="p" size="md" strikethrough={true} color="textSecondary">
+        <Text as="p" size="md" strikethrough={true} color={secondaryColor}>
           {formatter.monthlyPrice({
             amount: props.amount,
             currencyCode: props.currencyCode,
           })}
         </Text>
       )}
-      <Text as="p" size="md">
+      <Text as="p" size="md" color={color}>
         {formatter.monthlyPrice({
           currencyCode: props.currencyCode,
           amount: props.reducedAmount ?? props.amount,
---
+++ apps/store/src/components/ShopBreakdown/QuickAdd.stories.tsx
@@ -0,0 +1,43 @@
+import { type Meta, type StoryObj } from '@storybook/react'
+import { CurrencyCode } from '@/services/apollo/generated'
+import { QuickAdd } from './QuickAdd'
+
+const meta: Meta<typeof QuickAdd> = {
+  title: 'Components / Shop Breakdown / Quick Add',
+  component: QuickAdd,
+}
+
+export default meta
+type Story = StoryObj<typeof QuickAdd>
+
+export const Default: Story = {
+  args: {
+    title: 'Accident insurance',
+    subtitle: 'Covers 2 people',
+    pillow: {
+      src: 'https://a.storyblok.com/f/165473/832x832/a61cfbf4ae/hedvig-pillows-cat.png',
+    },
+    cost: {
+      currencyCode: CurrencyCode.Sek,
+      amount: 99,
+    },
+  },
+}
+
+export const WithDiscount: Story = {
+  args: {
+    ...Default.args,
+    cost: {
+      currencyCode: CurrencyCode.Sek,
+      amount: 99,
+      reducedAmount: 49,
+    },
+  },
+}
+
+export const Loading: Story = {
+  args: {
+    ...Default.args,
+    loading: true,
+  },
+}
