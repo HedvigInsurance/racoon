389d8e7ed1e0df7f95bc2522efc1aa440a7321ef
Robin Andeer <robin.andeer@gmail.com>
2023-08-29 13:14:23 +0200

    Create new CRM Retargeting Page

---
+++ apps/store/src/features/retargeting/RetargetingPage.tsx
@@ -0,0 +1,57 @@
+import { keyframes } from '@emotion/react'
+import styled from '@emotion/styled'
+import { type ComponentProps } from 'react'
+import { theme } from 'ui'
+import { Pillow } from '@/components/Pillow/Pillow'
+
+type Props = {
+  pillows: Array<
+    ComponentProps<typeof Pillow> & {
+      id: string
+    }
+  >
+}
+
+export const RetargetingPage = (props: Props) => {
+  return (
+    <Wrapper>
+      <Grid>
+        {props.pillows.map((item, index) => (
+          <PillowWrapper key={item.id} index={index}>
+            <Pillow {...item} />
+          </PillowWrapper>
+        ))}
+      </Grid>
+    </Wrapper>
+  )
+}
+
+const Wrapper = styled.div({
+  height: '100dvh',
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+})
+
+const Grid = styled.div({
+  display: 'grid',
+  gridAutoFlow: 'column',
+  gap: theme.space.xs,
+})
+
+const spin = keyframes({
+  '0%': { opacity: '0.25' },
+  '50%': {
+    opacity: '1',
+    transform: 'scale(1.05)',
+  },
+  '100%': { opacity: 0.25 },
+})
+
+const PillowWrapper = styled.div<{ index: number }>(({ index }) => {
+  const delay = index * 150
+  return {
+    animation: `${spin} 1000ms both infinite`,
+    animationDelay: `${delay}ms`,
+  }
+})
