3d837b2093d78b10bbc4d9fe887fc4e8a6eee3dd
Robin Andeer <robin.andeer@gmail.com>
2023-08-30 11:52:26 +0200

    Retargeting: refactor redirect user composition (#3088)
    <!--
    PR title: GRW-123 / Feature / Awesome new thing
    -->
    
    ## Describe your changes
    
    - Add `RedirectUser` component which can be conditionally rendered
    
    <!--
    What changes are made?
    If there are many changes, a list might be a good format.
    If it makes sense, add screenshots and/or screen recordings here.
    -->
    
    ## Justify why they are needed
    
    - I want to handle not having shop session ID in a single place
    
    - We could potentially do this also by throwing an error but that will leave some race conditions unaccounted for...
    
    - Let me know what you think of this approach!
    
    ## Checklist before requesting a review
    
    - [ ] I have performed a self-review of my code

--- apps/store/src/features/retargeting/useRedirectUser.ts
+++ apps/store/src/features/retargeting/useRedirectUser.ts
@@ -3,51 +3,31 @@ import { useRouter } from 'next/router'
 import { useShopSessionQuery } from '@/services/apollo/generated'
 import { useCurrentLocale } from '@/utils/l10n/useCurrentLocale'
 import { PageLink } from '@/utils/PageLink'
-import { QueryParam } from './retargeting.constants'
+import { type RedirectUserParams } from './retargeting.types'
 
-export const useRedirectUser = () => {
+export const useRedirectUser = (params: RedirectUserParams) => {
   const { routingLocale } = useCurrentLocale()
   const router = useRouter()
 
-  const queryParams = useQueryParams()
   useShopSessionQuery({
-    skip: !queryParams,
-    variables: queryParams ? { shopSessionId: queryParams.shopSessionId } : undefined,
+    variables: { shopSessionId: params.shopSessionId },
     onCompleted(data) {
       if (data.shopSession.cart.entries.length > 0) {
         router.push(
           PageLink.session({
             shopSessionId: data.shopSession.id,
             next: PageLink.cart({ locale: routingLocale }),
-            code: queryParams?.campaignCode,
+            code: params.campaignCode,
           }),
         )
       }
     },
     onError(error) {
       datadogLogs.logger.warn('Retargeting | Failed to fetch shop session', {
-        shopSessionId: queryParams?.shopSessionId,
+        shopSessionId: params.shopSessionId,
         error,
       })
       router.push(PageLink.store({ locale: routingLocale }))
     },
   })
-}
-
-type QueryParamData = {
-  shopSessionId: string
-  campaignCode?: string
-}
-
-const useQueryParams = (): QueryParamData | null => {
-  const router = useRouter()
-  const shopSessionId = router.query[QueryParam.ShopSession]
-
-  if (typeof shopSessionId !== 'string') return null
-
-  const campaignCode = router.query[QueryParam.CampaignCode]
-  return {
-    shopSessionId,
-    ...(typeof campaignCode === 'string' && { campaignCode }),
-  }
 }
