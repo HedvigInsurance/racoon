426c03dd10ae6ee2d4630c9de113670cf9473b12
Robin Andeer <robin.andeer@gmail.com>
2023-08-31 10:25:24 +0200

    Retargeting: add tests for helper functions

--- apps/store/src/pages/api/retargeting/[shopSessionId].ts
+++ apps/store/src/pages/api/retargeting/[shopSessionId].ts
@@ -1,17 +1,15 @@
 import { type NextApiRequest, type NextApiResponse } from 'next'
 import { fetchRetargetingData } from '@/features/retargeting/fetchRetargetingData'
 import { getUserRedirect } from '@/features/retargeting/getUserRedirect'
-import { QueryParam } from '@/features/retargeting/retargeting.constants'
-import { UserParams } from '@/features/retargeting/retargeting.types'
+import { parseQueryParams } from '@/features/retargeting/parseQueryParams'
 import { initializeApolloServerSide } from '@/services/apollo/client'
-import { getLocaleOrFallback, isRoutingLocale } from '@/utils/l10n/localeUtils'
 
 /**
  * Redirect user to the correct page from a retargeting link.
  */
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   console.info('Retargeting | Redirecting user')
-  const userParams = parseQueryParams(req)
+  const userParams = parseQueryParams(req.query)
   if (!userParams) {
     res.statusCode = 400
     res.end()
@@ -27,21 +25,3 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
 }
 
 export default handler
-
-const parseQueryParams = (req: NextApiRequest): UserParams | null => {
-  const shopSessionId = req.query[QueryParam.ShopSession]
-  if (typeof shopSessionId !== 'string') return null
-
-  const urlLocale = req.query[QueryParam.Locale]
-  let locale = getLocaleOrFallback().routingLocale
-  if (isRoutingLocale(urlLocale)) {
-    locale = urlLocale
-  }
-
-  const campaignCode = req.query[QueryParam.CampaignCode]
-  return {
-    shopSessionId,
-    locale,
-    ...(typeof campaignCode === 'string' && { campaignCode }),
-  }
-}
