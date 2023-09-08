e040213793a2271b28a7249dcb32b3e958c62aa2
Robin Andeer <robin.andeer@gmail.com>
2023-09-08 15:03:59 +0200

    GEN-1059 | Create `QuickAdd` component for incomplete quotes (#3172)
    <!--
    PR title: GRW-123 / Feature / Awesome new thing
    -->
    
    ## Describe your changes
    
    - Add container to represent an incomplete quick add card!
    
    <!--
    What changes are made?
    If there are many changes, a list might be a good format.
    If it makes sense, add screenshots and/or screen recordings here.
    -->
    
    ## Justify why they are needed
    
    - This is an exploration to understand what kind of data we need from the API to represent an incomplete quick add card. Therefore a lot of things are missing or contain placeholder data.
    
    - Not yet in use.
    
    ## Checklist before requesting a review
    
    - [ ] I have performed a self-review of my code

---
+++ apps/store/src/components/QuickAdd/QuickAddIncompleteContainer.tsx
@@ -0,0 +1,155 @@
+import { useTranslation } from 'next-i18next'
+import { useState } from 'react'
+import { Button, Space } from 'ui'
+import { ButtonNextLink } from '@/components/ButtonNextLink'
+import { InputStartDate } from '@/components/InputDate/InputStartDate'
+import { OPEN_PRICE_CALCULATOR_QUERY_PARAM } from '@/components/ProductPage/PurchaseForm/useOpenPriceCalculatorQueryParam'
+import { PRELOADED_PRICE_INTENT_QUERY_PARAM } from '@/components/ProductPage/PurchaseForm/usePreloadedPriceIntentId'
+import { useStartDateUpdateMutation } from '@/services/apollo/generated'
+import { PriceIntent } from '@/services/priceIntent/priceIntent.types'
+import { formatAPIDate } from '@/utils/date'
+import { ORIGIN_URL } from '@/utils/PageLink'
+import { useAddToCart } from '@/utils/useAddToCart'
+import { ProductDetail, QuickAdd } from './QuickAdd'
+import { useShowQuickAdd } from './useShowQuickAdd'
+
+const PILLOW_PLACEHOLDER =
+  'https://a.storyblok.com/f/165473/832x832/fa27811442/hedvig-pillow-home.png'
+
+type Props = {
+  shopSessionId: string
+  priceIntent: PriceIntent
+}
+
+export const QuickAddIncompleteContainer = (props: Props) => {
+  const { t } = useTranslation(['cart', 'common', 'purchase-form'])
+  const [show] = useShowQuickAdd()
+
+  const subtitle = useProductSubtitle()
+  const displayItems = useDisplayItems(props.priceIntent.data)
+
+  const editLink = new URL(props.priceIntent.product.pageLink, ORIGIN_URL)
+  editLink.searchParams.set(OPEN_PRICE_CALCULATOR_QUERY_PARAM, '1')
+  editLink.searchParams.set(PRELOADED_PRICE_INTENT_QUERY_PARAM, props.priceIntent.id)
+
+  type Offer = (typeof props.priceIntent.offers)[number]
+  const offer = props.priceIntent.offers[0] as Offer | undefined
+
+  const cost = offer
+    ? {
+        currencyCode: offer.cost.net.currencyCode,
+        amount: offer.cost.gross.amount,
+        reducedAmount: offer.cost.discount.amount > 0 ? offer.cost.net.amount : undefined,
+      }
+    : undefined
+
+  const [updateStartDate, startDateResult] = useStartDateUpdateMutation()
+  const [changedStartDate, setChangedStartDate] = useState<Date | undefined>(undefined)
+  const handleChangeStartDate = (newDate: Date) => {
+    const productOfferIds = props.priceIntent.offers.map((offer) => offer.id)
+
+    if (productOfferIds.length === 0) {
+      setChangedStartDate(newDate)
+      return
+    } else {
+      setChangedStartDate(undefined)
+    }
+
+    const startDate = formatAPIDate(newDate)
+    updateStartDate({ variables: { productOfferIds, startDate } })
+  }
+
+  const [addToCart, loadingAddToCart] = useAddToCart({
+    shopSessionId: props.shopSessionId,
+    onSuccess(productOfferId) {
+      console.log('Added to cart', productOfferId)
+    },
+  })
+  const handleClickAdd = async () => {
+    const productOfferId = offer?.id
+    if (!productOfferId) return
+
+    if (changedStartDate) {
+      const startDate = formatAPIDate(changedStartDate)
+      await updateStartDate({ variables: { productOfferIds: [productOfferId], startDate } })
+    }
+
+    await addToCart(productOfferId)
+  }
+
+  if (!show) return null
+
+  const Body = (
+    <Space y={1}>
+      <ul>
+        {displayItems.map((item) => (
+          <ProductDetail key={item.key} value={item.displayValue}>
+            {item.displayTitle}
+          </ProductDetail>
+        ))}
+      </ul>
+
+      <InputStartDate date={changedStartDate} onChange={handleChangeStartDate} />
+    </Space>
+  )
+
+  const loading = startDateResult.loading || loadingAddToCart
+
+  return (
+    <QuickAdd
+      // TODO: use "displayNameFull"
+      title={props.priceIntent.product.displayNameShort}
+      subtitle={subtitle}
+      // TODO: fetch from API
+      pillow={{ src: PILLOW_PLACEHOLDER }}
+      href={props.priceIntent.product.pageLink}
+      cost={cost}
+      Body={Body}
+    >
+      <Button size="medium" onClick={handleClickAdd} loading={loading}>
+        {t('QUICK_ADD_BUTTON')}
+      </Button>
+      <ButtonNextLink size="medium" variant="ghost" href={editLink}>
+        {t('common:READ_MORE')}
+      </ButtonNextLink>
+    </QuickAdd>
+  )
+}
+
+const STREET_ADDRESS_DATA_KEY = 'street'
+const useProductSubtitle = () => {
+  // TODO: Translate
+  return 'Se vad som ingår'
+}
+
+const ZIP_CODE_DATA_KEY = 'zipCode'
+const LIVING_SPACE_DATA_KEY = 'livingSpace'
+
+type DisplayItem = { key: string; displayValue: string; displayTitle: string }
+
+// TODO: translate or move to backend
+const useDisplayItems = (data: Record<string, unknown>): Array<DisplayItem> => {
+  const { t } = useTranslation('cart')
+
+  const streetAddress = data[STREET_ADDRESS_DATA_KEY] as string | undefined
+  const zipCode = data[ZIP_CODE_DATA_KEY] as string | undefined
+  const livingSpace = data[LIVING_SPACE_DATA_KEY] as number | undefined
+
+  return [
+    ...(streetAddress
+      ? [{ key: 'street', displayValue: streetAddress, displayTitle: 'Address' }]
+      : []),
+
+    ...(zipCode ? [{ key: 'zipCode', displayValue: zipCode, displayTitle: 'Postkod' }] : []),
+
+    ...(livingSpace
+      ? [
+          {
+            key: 'livingSpace',
+            displayValue: t('DATA_TABLE_LIVING_SPACE_VALUE', { area: livingSpace }),
+            displayTitle: 'Boyta',
+          },
+        ]
+      : []),
+  ]
+}
