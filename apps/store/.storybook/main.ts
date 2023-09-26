e06dd107f6944f56f942f5b2d37a6e8cc13093f7
Gustav Eén <gustaveen@gmail.com>
2023-09-26 10:41:26 +0200

    Colocate stories from UI package with store Storybook
    <!--
    PR title: GRW-123 / Feature / Awesome new thing
    -->
    
    ## Describe your changes
    Colocate UI package and store storybook stories locally and in Chromatic
    
    <img width="1728" alt="Screenshot 2023-09-25 at 16 39 15" src="https://github.com/HedvigInsurance/racoon/assets/6661511/8010a1d9-7dd2-4546-99bb-ec920bfe2bbf">
    
    <!--
    What changes are made?
    If there are many changes, a list might be a good format.
    If it makes sense, add screenshots and/or screen recordings here.
    -->
    
    ## Justify why they are needed
    Make it easier to view all stories 🙏
    
    ## Checklist before requesting a review
    
    - [ ] I have performed a self-review of my code

--- apps/store/.storybook/main.ts
+++ apps/store/.storybook/main.ts
@@ -2,7 +2,14 @@ import type { StorybookConfig } from '@storybook/nextjs'
 import babelConfig from './babelConfig'
 
 const config: StorybookConfig = {
-  stories: ['../src/**/*.stories.@(ts|tsx)'],
+  stories: [
+    {
+      directory: '../../../packages/ui/src',
+      files: '**/*.stories.@(ts|tsx|mdx)',
+      titlePrefix: 'UI Kit',
+    },
+    '../src/**/*.stories.@(ts|tsx|mdx)',
+  ],
   addons: [
     '@storybook/addon-links',
     '@storybook/addon-essentials',
