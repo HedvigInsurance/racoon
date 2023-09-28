0d0ba0b583f11c5ab74801b2b837024ce672654f
Robin Andeer <robin.andeer@gmail.com>
2023-09-28 15:19:17 +0200

    GEN-1218 | Add new `ToastBar` component

---
+++ apps/store/src/components/ToastBar/ToastBar.stories.tsx
@@ -0,0 +1,27 @@
+import { Meta, StoryObj } from '@storybook/react'
+import { Space } from 'ui'
+import { InfoToastBar, AttentionToastBar, ErrorToastBar } from './ToastBar'
+
+const meta: Meta<typeof InfoToastBar> = {
+  title: 'Components / Toast Bar',
+  component: InfoToastBar,
+}
+
+export default meta
+type Story = StoryObj<typeof InfoToastBar>
+
+export const Variants: Story = {
+  args: {
+    children: 'Info goes here',
+  },
+
+  render: (args) => (
+    <Space y={1}>
+      <InfoToastBar>{args.children}</InfoToastBar>
+
+      <AttentionToastBar>{args.children}</AttentionToastBar>
+
+      <ErrorToastBar>{args.children}</ErrorToastBar>
+    </Space>
+  ),
+}
