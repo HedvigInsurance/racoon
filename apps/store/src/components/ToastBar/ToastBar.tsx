ac63ff70c90041be43cfcca3927dabb37067be8d
Robin Andeer <robin.andeer@gmail.com>
2023-09-29 12:35:26 +0200

    GEN-1218 | Add new `ToastBar` component
    <!--
    PR title: GRW-123 / Feature / Awesome new thing
    -->
    
    ## Describe your changes
    
    
    ![Screenshot 2023-09-28 at 15.17.19.png](https://graphite-user-uploaded-assets-prod.s3.amazonaws.com/OgXegTwxM9IeuXHZyw5h/a19bf7b1-26af-4ea5-9dc8-de4cd3ee77ce/Screenshot%202023-09-28%20at%2015.17.19.png)
    
    
    - Add new `ToastBar` component from design system
    
    <!--
    What changes are made?
    If there are many changes, a list might be a good format.
    If it makes sense, add screenshots and/or screen recordings here.
    -->
    
    ## Justify why they are needed
    
    - It's very similar to the `InfoCard`, however, they are different in Figma so I feel like we should separate them in code as well.
    
    ## Checklist before requesting a review
    
    - [ ] I have performed a self-review of my code

---
+++ apps/store/src/components/ToastBar/ToastBar.tsx
@@ -0,0 +1,66 @@
+import { css } from '@emotion/react'
+import styled from '@emotion/styled'
+import { type PropsWithChildren } from 'react'
+import { InfoIcon, WarningTriangleIcon, Text, theme } from 'ui'
+
+type Props = PropsWithChildren<unknown>
+
+const Wrapper = styled.div({
+  paddingInline: theme.space.md,
+  paddingBlock: theme.space.sm,
+  borderRadius: theme.radius.sm,
+  borderWidth: 1,
+  borderStyle: 'solid',
+  borderColor: theme.colors.borderTranslucent1,
+  display: 'flex',
+  columnGap: theme.space.xs,
+  justifyContent: 'center',
+})
+
+const RigidIconStyled = css({
+  flexShrink: 0,
+  // Optical alignment with text
+  marginTop: 1,
+})
+
+export const InfoToastBar = (props: Props) => {
+  return (
+    <InfoWrapper>
+      <RigidInfoIcon color={theme.colors.signalBlueElement} />
+      <Text as="p" color="signalBlueText" size="xs">
+        {props.children}
+      </Text>
+    </InfoWrapper>
+  )
+}
+
+const InfoWrapper = styled(Wrapper)({ backgroundColor: theme.colors.signalBlueFill })
+const RigidInfoIcon = styled(InfoIcon)(RigidIconStyled)
+
+export const AttentionToastBar = (props: Props) => {
+  return (
+    <AttentionWrapper>
+      <RigidWarningTriangleIcon size="1rem" color={theme.colors.signalAmberElement} />
+      <Text as="p" color="signalAmberText" size="xs">
+        {props.children}
+      </Text>
+    </AttentionWrapper>
+  )
+}
+
+const AttentionWrapper = styled(Wrapper)({ backgroundColor: theme.colors.signalAmberFill })
+
+const RigidWarningTriangleIcon = styled(WarningTriangleIcon)(RigidIconStyled)
+
+export const ErrorToastBar = (props: Props) => {
+  return (
+    <ErrorWrapper>
+      <RigidWarningTriangleIcon size="1rem" color={theme.colors.signalRedElement} />
+      <Text as="p" color="signalRedText" size="xs">
+        {props.children}
+      </Text>
+    </ErrorWrapper>
+  )
+}
+
+const ErrorWrapper = styled(Wrapper)({ backgroundColor: theme.colors.signalRedFill })
