d03ef1349ee3cbf5cc11c67297e7de90f88585a9
Robin Andeer <robin.andeer@gmail.com>
2023-10-02 13:05:04 +0200

    Add `WithLink` component (without `Text`)

--- apps/store/src/components/TextWithLink.tsx
+++ apps/store/src/components/TextWithLink.tsx
@@ -4,28 +4,37 @@ import { type ComponentProps } from 'react'
 import { Text } from 'ui'
 import { nestedLinkStyles } from './RichText/RichText.styles'
 
-type Props = ComponentProps<typeof Text> &
-  Pick<ComponentProps<typeof Link>, 'href' | 'target'> & { children: string }
+type WithLinkProps = Pick<ComponentProps<typeof Link>, 'href' | 'target'> & { children: string }
+
+type Props = WithLinkProps & ComponentProps<typeof Text>
 
 export const TextWithLink = ({ children, href, target, ...otherProps }: Props) => {
-  const [beforeLink, rest] = children.split('[[', 2)
+  return (
+    <Text {...otherProps}>
+      <WithLink href={href} target={target}>
+        {children}
+      </WithLink>
+    </Text>
+  )
+}
+
+export const WithLink = (props: WithLinkProps) => {
+  const [beforeLink, rest] = props.children.split('[[', 2)
 
   // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
-  if (rest === undefined) {
-    return <Text {...otherProps}>{children}</Text>
-  }
+  if (rest === undefined) return <>props.children</>
 
   const [linkText, afterLink] = rest.split(']]', 2)
 
   return (
-    <StyledTextWithLink {...otherProps}>
+    <NestedLink>
       {beforeLink}
-      <Link href={href} target={target}>
+      <Link href={props.href} target={props.target}>
         {linkText}
       </Link>
       {afterLink}
-    </StyledTextWithLink>
+    </NestedLink>
   )
 }
 
-const StyledTextWithLink = styled(Text)(nestedLinkStyles)
+const NestedLink = styled.span(nestedLinkStyles)
