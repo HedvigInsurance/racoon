5a7d11a6c171904743142f947f39a1e36c8456f6
Alexander Lebedev <alexander.lebedev@gmail.com>
2023-10-04 13:03:16 +0200

    Ugly but functional login and logout (#3329)
    ![Screenshot 2023-10-04 at 12.19.00.png](https://graphite-user-uploaded-assets-prod.s3.amazonaws.com/GLY3tSM85RFBvAf6lFNA/f203b081-1aa2-4440-84ff-3ab6a12dfffd/Screenshot%202023-10-04%20at%2012.19.00.png)
    
    <!--
    PR title: GRW-123 / Feature / Awesome new thing
    -->
    
    ## Describe your changes
    
    Subj
    - Can login from login page
    - Can logout from member page
    - Support ?next on login page client-side
    
    <!--
    What changes are made?
    If there are many changes, a list might be a good format.
    If it makes sense, add screenshots and/or screen recordings here.
    -->
    
    ## Justify why they are needed
    
    ## Checklist before requesting a review
    
    - [ ] I have performed a self-review of my code

--- apps/store/src/features/memberArea/LoginPage.tsx
+++ apps/store/src/features/memberArea/LoginPage.tsx
@@ -1,18 +1,76 @@
+import styled from '@emotion/styled'
 import Head from 'next/head'
-import Link from 'next/link'
+import { useRouter } from 'next/router'
+import { FormEventHandler } from 'react'
+import { BankIdIcon, Button, Text, theme } from 'ui'
+import { PersonalNumberField } from '@/components/PersonalNumberField/PersonalNumberField'
 import { SpaceFlex } from '@/components/SpaceFlex/SpaceFlex'
+import { useBankIdContext } from '@/services/bankId/BankIdContext'
 
 export const LoginPage = () => {
+  const router = useRouter()
+  const handleLoginSuccess = async () => {
+    const { next: nextPathname = '/member', ...targetQuery } = router.query
+    const redirectTarget = { pathname: String(nextPathname), query: targetQuery }
+    console.log(`Logged in, redirecting to ${JSON.stringify(redirectTarget)}`)
+    await router.replace(redirectTarget)
+  }
+
   return (
     <>
       <Head>
         <title>Hedvig member login</title>
         <meta name="robots" content="noindex,follow" />
       </Head>
-      <SpaceFlex direction="vertical">
-        TODO: Login form here
-        <Link href={'/member'}>Login</Link>
-      </SpaceFlex>
+      <LoginForm onSuccess={handleLoginSuccess} />
     </>
   )
 }
+
+const SSN_FIELD_NAME = 'ssn'
+const LoginForm = ({ onSuccess }: { onSuccess: any }) => {
+  const { startLogin, currentOperation } = useBankIdContext()
+  const handleSubmit: FormEventHandler<HTMLFormElement> = (event) => {
+    event.preventDefault()
+    const ssn = new FormData(event.currentTarget).get(SSN_FIELD_NAME) as string
+    startLogin({
+      ssn,
+      onSuccess,
+    })
+  }
+  return (
+    <FormWrapper direction="vertical" align="center">
+      <Text>Please log in to continue</Text>
+      <Form onSubmit={handleSubmit}>
+        <FieldWrapper>
+          <PersonalNumberField
+            name={SSN_FIELD_NAME}
+            label="Personal number"
+            autoFocus={true}
+            defaultValue={''}
+            required={true}
+          />
+        </FieldWrapper>
+        <Button type="submit" loading={!!currentOperation}>
+          <SpaceFlex space={0.5} align="center">
+            <BankIdIcon />
+            Log in with BankID
+          </SpaceFlex>
+        </Button>
+      </Form>
+    </FormWrapper>
+  )
+}
+
+const FormWrapper = styled(SpaceFlex)({
+  marginTop: theme.space.xxxl,
+})
+
+const Form = styled.form({
+  minWidth: '20rem',
+})
+
+const FieldWrapper = styled.div({
+  marginTop: theme.space.lg,
+  marginBottom: theme.space.xxs,
+})
