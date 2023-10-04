eb0068ba21a0e3ce70bf54953743bd0fdcff98e0
Gustav Eén <gustaveen@gmail.com>
2023-10-04 16:31:01 +0200

    Add Insurances screen and InsuranceCard component

---
+++ apps/store/src/features/memberArea/Insurances.tsx
@@ -0,0 +1,33 @@
+import styled from '@emotion/styled'
+import { Heading, mq } from 'ui'
+import { MemberAreaMemberInfoQuery } from '@/services/apollo/generated'
+import { InsuranceCard } from './components/InsuranceCard'
+
+type MemberInfoProps = {
+  data: MemberAreaMemberInfoQuery
+}
+
+export const Insurances = ({ data }: MemberInfoProps) => {
+  return (
+    <>
+      <Heading as="h1">Your insurances</Heading>
+      <Grid>
+        {data.currentMember.activeContracts.map((contract) => (
+          // @ts-expect-error ignore
+          <InsuranceCard key={contract.id} contract={contract} />
+        ))}
+      </Grid>
+    </>
+  )
+}
+
+const Grid = styled.div({
+  display: 'grid',
+  gridGap: '1rem',
+  marginTop: '1rem',
+
+  [mq.lg]: {
+    display: 'grid',
+    gridTemplateColumns: '1fr 1fr',
+  },
+})
