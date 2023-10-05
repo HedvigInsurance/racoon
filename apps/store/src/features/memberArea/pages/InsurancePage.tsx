eaceec9c05eacb3c49a33367c05f2fca89a9e94e
Gustav Eén <gustaveen@gmail.com>
2023-10-05 21:04:45 +0200

    Restrucuture sections

--- apps/store/src/features/memberArea/pages/InsurancePage.tsx
+++ apps/store/src/features/memberArea/pages/InsurancePage.tsx
@@ -1,7 +1,7 @@
 import { NextPageWithLayout } from 'next'
-import { Insurances } from '@/features/memberArea/components/InsurancesSection'
 import { useMemberAreaMemberInfoQuery } from '@/services/apollo/generated'
 import { LayoutWithMenu } from '../components/LayoutWithMenu'
+import { Insurances } from '../InsuranceSection/InsurancesSection'
 
 export const InsurancePage: NextPageWithLayout = () => {
   const { loading } = useMemberAreaMemberInfoQuery()
