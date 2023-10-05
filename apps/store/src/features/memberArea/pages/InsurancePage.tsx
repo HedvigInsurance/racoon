6d765fb9b778eddc53844d48f87958a576bb369c
Gustav Eén <gustaveen@gmail.com>
2023-10-05 12:03:19 +0200

    Create payments page and refactor
    <!--
    PR title: GRW-123 / Feature / Awesome new thing
    -->
    
    ## Describe your changes
    Feel free to refactor how we distribute the data, didn't touch it now @alebedev 
    
    <!--
    What changes are made?
    If there are many changes, a list might be a good format.
    If it makes sense, add screenshots and/or screen recordings here.
    -->
    
    ## Justify why they are needed
    
    ## Checklist before requesting a review
    
    - [ ] I have performed a self-review of my code

--- apps/store/src/features/memberArea/pages/InsurancePage.tsx
+++ apps/store/src/features/memberArea/pages/InsurancePage.tsx
@@ -1,12 +1,7 @@
 import { NextPageWithLayout } from 'next'
-import { Heading } from 'ui'
-import { Insurances } from '@/features/memberArea/components/Insurances'
-import {
-  MemberAreaMemberInfoQuery,
-  useMemberAreaMemberInfoQuery,
-} from '@/services/apollo/generated'
+import { Insurances } from '@/features/memberArea/components/InsurancesSection'
+import { useMemberAreaMemberInfoQuery } from '@/services/apollo/generated'
 import { LayoutWithMenu } from '../components/LayoutWithMenu'
-import { PaymentsSection } from '../PaymentsSection'
 
 export const InsurancePage: NextPageWithLayout = () => {
   const { data, loading } = useMemberAreaMemberInfoQuery()
@@ -14,26 +9,7 @@ export const InsurancePage: NextPageWithLayout = () => {
   return (
     <>
       {loading && 'Loading...'}
-      {data && <MemberInfo data={data} />}
-    </>
-  )
-}
-
-type MemberInfoProps = {
-  data: MemberAreaMemberInfoQuery
-}
-const MemberInfo = ({ data }: MemberInfoProps) => {
-  const { currentMember } = data
-  const greeting = `Hello, ${currentMember.firstName} ${currentMember.lastName}`
-  return (
-    <>
-      <Heading as={'h2'} variant="standard.32">
-        {greeting}
-      </Heading>
-      <Insurances data={data} />
-      <br />
-      <br />
-      <PaymentsSection />
+      {data && <Insurances data={data} />}
     </>
   )
 }
