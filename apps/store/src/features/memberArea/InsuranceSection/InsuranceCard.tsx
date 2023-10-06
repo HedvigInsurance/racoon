62911b63a0283682838153fd941e6ebbf88981fa
Gustav Eén <gustaveen@gmail.com>
2023-10-06 09:59:26 +0200

    Add all pillow variants for insurances (#3353)
    <!--
    PR title: GRW-123 / Feature / Awesome new thing
    -->
    
    ## Describe your changes
    <img width="1422" alt="Screenshot 2023-10-05 at 23 30 35" src="https://github.com/HedvigInsurance/racoon/assets/6661511/a4a7f352-7eb9-42e9-8375-63bcd62b1d84">
    
    <!--
    What changes are made?
    If there are many changes, a list might be a good format.
    If it makes sense, add screenshots and/or screen recordings here.
    -->
    
    ## Justify why they are needed
    
    ## Checklist before requesting a review
    
    - [ ] I have performed a self-review of my code

--- apps/store/src/features/memberArea/InsuranceSection/InsuranceCard.tsx
+++ apps/store/src/features/memberArea/InsuranceSection/InsuranceCard.tsx
@@ -18,9 +18,9 @@ export const InsuranceCard = ({ contract }: InsuranceCardProps) => {
         <Text size="md" color="textNegative">
           {currentAgreement.productVariant.displayName}
         </Text>
-        <Text color="textTranslucentTertiary">{exposureDisplayName}</Text>
+        <Exposure>{exposureDisplayName}</Exposure>
       </Info>
-      <PillowBackground />
+      <PillowBackground imgSrc={getPillowSrc(currentAgreement.productVariant.typeOfContract)} />
     </Card>
   )
 }
@@ -45,7 +45,11 @@ const CardHeader = styled.div({
   justifyContent: 'flex-end',
 })
 
-const PillowBackground = styled.div({
+const Exposure = styled.div({
+  color: theme.colors.grayTranslucentDark500,
+})
+
+const PillowBackground = styled.div<{ imgSrc: string }>(({ imgSrc }) => ({
   position: 'absolute',
   top: '50%',
   left: '50%',
@@ -55,18 +59,46 @@ const PillowBackground = styled.div({
   backgroundPosition: 'center',
   backgroundSize: 'cover',
   filter: 'blur(20px)',
-  backgroundImage: `url('https://a.storyblok.com/f/165473/832x832/fa27811442/hedvig-pillow-home.png')`,
+  backgroundImage: `url('${imgSrc}')`,
   zIndex: -1,
   pointerEvents: 'none',
-  transition: 'filter 0.2s ease-in-out',
+  transition: 'all 0.2s ease-in-out',
 
   '@media (hover: hover)': {
     [`button:hover &`]: {
       filter: 'blur(50px)',
+      transform: 'scale(1.1) translate(-50%, -50%)',
     },
   },
-})
+}))
 
 const Info = styled.div({
   marginTop: 'auto',
 })
+
+const getPillowSrc = (typeOfContract: string) => {
+  switch (typeOfContract) {
+    case 'SE_HOUSE':
+      return 'https://a.storyblok.com/f/165473/832x832/a50cf6d846/hedvig-pillows-house-villa.png'
+    case 'SE_APARTMENT_RENT':
+      return 'https://a.storyblok.com/f/165473/832x832/fb3ddd4632/hedvig-pillows-rental.png'
+    case 'SE_APARTMENT_BRF':
+      return 'https://a.storyblok.com/f/165473/832x832/f98d88ac63/hedvig-pillows-homeowner.png'
+    case 'SE_ACCIDENT':
+      return 'https://a.storyblok.com/f/165473/832x832/1bb4813dd1/hedvig-pillows-accident.png'
+    case 'SE_CAR_FULL':
+    case 'SE_CAR_HALF':
+    case 'SE_CAR_TRAFFIC':
+      return 'https://a.storyblok.com/f/165473/832x832/1fe7a75de6/hedvig-pillows-car.png'
+    case 'SE_DOG_BASIC':
+    case 'SE_DOG_STANDARD':
+    case 'SE_DOG_PREMIUM':
+      return 'https://a.storyblok.com/f/165473/832x832/d19b86f8c4/hedvig-pillows-dog.png'
+    case 'SE_CAT_BASIC':
+    case 'SE_CAT_STANDARD':
+    case 'SE_CAT_PREMIUM':
+      return 'https://a.storyblok.com/f/165473/832x832/a61cfbf4ae/hedvig-pillows-cat.png'
+    default:
+      return 'https://a.storyblok.com/f/165473/832x832/cdaaa91242/hedvig-pillows-home.png'
+  }
+}
