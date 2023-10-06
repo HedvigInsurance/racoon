08aab1f847b39999ca76678c4a62eab6d012b42c
Alexander Lebedev <alexander.lebedev@gmail.com>
2023-10-06 11:02:41 +0200

    Payments section design

--- apps/store/src/features/memberArea/PaymentsSection/PaymentsSection.tsx
+++ apps/store/src/features/memberArea/PaymentsSection/PaymentsSection.tsx
@@ -1,39 +1,85 @@
 import { useApolloClient } from '@apollo/client'
 import { useCallback, useState } from 'react'
 import { Button, Heading, Text } from 'ui'
+import { ButtonNextLink } from '@/components/ButtonNextLink'
 import { ReadyState } from '@/components/PaymentConnectPage/ReadyState'
 import { SpaceFlex } from '@/components/SpaceFlex/SpaceFlex'
+import { useMemberAreaInfo } from '@/features/memberArea/useMemberAreaInfo'
 import { useMemberAreaMemberInfoQuery } from '@/services/apollo/generated'
 import { createTrustlyUrl } from '@/services/trustly/createTrustlyUrl'
 import { useCurrentLocale } from '@/utils/l10n/useCurrentLocale'
+import { useFormatter } from '@/utils/useFormatter'
 
 export const PaymentsSection = () => {
-  // Not using simplified hook, we need access to refetch
-  const { data, refetch } = useMemberAreaMemberInfoQuery()
-
-  if (data == null) {
-    console.warn('PaymentsSection expects data to be always present')
-    return null
-  }
-  const { currentMember } = data
-
   return (
     <SpaceFlex direction="vertical">
+      <GeneralInfo />
+      <InsuranceCost />
+      <PaymentConnection />
+    </SpaceFlex>
+  )
+}
+
+// Might be a CMS block in the future
+const GeneralInfo = () => {
+  return (
+    <>
+      <Text as="p">
+        At Hedvig, you pay at the end of the month for the current month. Your monthly payment is
+        handled via digital direct debit on the 27th of every month (or the closest following bank
+        day). We work with Trustly as our payment partner and you can connect your direct debit on
+        this page below
+      </Text>
+      {/* NOTE that URL is locale-specific */}
+      <ButtonNextLink href={'/se-en/help/faq'} locale={false} size="small" variant="secondary">
+        Payments FAQ
+      </ButtonNextLink>
+    </>
+  )
+}
+
+const InsuranceCost = () => {
+  const { insuranceCost } = useMemberAreaInfo()
+  const formatter = useFormatter()
+
+  return (
+    <>
+      <Heading as="h3" variant="standard.24">
+        Monthly cost
+      </Heading>
+      {insuranceCost.monthlyDiscount.amount > 0 && (
+        <Text color="textSecondary" strikethrough={true}>
+          {formatter.monthlyPrice(insuranceCost.monthlyGross)}
+        </Text>
+      )}
+      <Text>{formatter.monthlyPrice(insuranceCost.monthlyNet)}</Text>
+      {insuranceCost.freeUntil && (
+        <Text>Free until {formatter.dateFull(insuranceCost.freeUntil)}</Text>
+      )}
+    </>
+  )
+}
+
+const PaymentConnection = () => {
+  const currentMember = useMemberAreaInfo()
+
+  return (
+    <>
       <Heading as="h3" variant="standard.24">
         Payment connection
       </Heading>
       {currentMember.hasActivePaymentConnection ? (
         <>
-          <Text>✅&nbsp;Configured</Text>
-          <PaymentConfiguration startButtonText={'Change'} onSuccess={refetch} />
+          <Text>✅&nbsp;Connected</Text>
+          <PaymentConfiguration startButtonText="Change connection" />
         </>
       ) : (
         <>
-          <Text>Not configured yet</Text>
-          <PaymentConfiguration startButtonText="Configure" onSuccess={refetch} />
+          <Text>Not connected yet</Text>
+          <PaymentConfiguration startButtonText="Connect" />
         </>
       )}
-    </SpaceFlex>
+    </>
   )
 }
 
@@ -43,15 +89,11 @@ type PaymentConfigurationState =
   | { type: 'ERROR' }
   | { type: 'READY'; trustlyUrl: string }
 
-const PaymentConfiguration = ({
-  startButtonText,
-  onSuccess,
-}: {
-  startButtonText: string
-  onSuccess: () => void
-}) => {
+const PaymentConfiguration = ({ startButtonText }: { startButtonText: string }) => {
   const { routingLocale } = useCurrentLocale()
   const apolloClient = useApolloClient()
+  // Not using simplified hook, we need access to refetch
+  const { refetch } = useMemberAreaMemberInfoQuery()
   const [state, setState] = useState<PaymentConfigurationState>({ type: 'IDLE' })
 
   const handleStart = useCallback(async () => {
@@ -74,7 +116,10 @@ const PaymentConfiguration = ({
       <div style={{ minWidth: '70vw' }}>
         <ReadyState
           trustlyUrl={state.trustlyUrl}
-          onSuccess={onSuccess}
+          onSuccess={async () => {
+            await refetch()
+            setState({ type: 'IDLE' })
+          }}
           onFail={handleTrustlyError}
         />
       </div>
