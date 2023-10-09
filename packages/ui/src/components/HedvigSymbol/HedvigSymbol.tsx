00fb10fc5c5df414a644afd6307e05319641286b
Alexander Lebedev <alexander.lebedev@gmail.com>
2023-10-09 10:54:27 +0200

    Keep locale during login flow

--- packages/ui/src/components/HedvigSymbol/HedvigSymbol.tsx
+++ packages/ui/src/components/HedvigSymbol/HedvigSymbol.tsx
@@ -17,8 +17,8 @@ export const HedvigSymbol = ({ color = 'currentColor', size = '1.5rem' }: Hedvig
       fill="#FAFAFA"
     />
     <path
-      fill-rule="evenodd"
-      clip-rule="evenodd"
+      fillRule="evenodd"
+      clipRule="evenodd"
       d="M0 20C0 8.94515 8.94516 0 20 0C31.0549 0 40 8.94515 40 20C40 31.0549 31.0549 40 20 40C8.94516 40 0 31.0549 0 20ZM2.70043 20C2.70043 29.5359 10.4641 37.2996 20 37.2996C29.5359 37.2996 37.2996 29.5359 37.2996 20C37.2996 10.4641 29.5359 2.70042 20 2.70042C10.4641 2.70042 2.70043 10.4641 2.70043 20Z"
       fill="#FAFAFA"
     />
