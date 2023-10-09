c59dc7338a0e27f2dc619e5055bbd49f3dd48655
Alexander Lebedev <alexander.lebedev@gmail.com>
2023-10-09 16:47:37 +0200

    Fix jest run in UI, don't use next-specific transform

--- packages/ui/jest.transform.js
+++
@@ -1,9 +0,0 @@
-// OK to use require in node context
-// eslint-disable-next-line
-const babel = require('babel-jest')
-
-module.exports = babel.default.createTransformer({
-  presets: ['next/babel'],
-  // this is to be able to use [MyComponent] as a CSS selector
-  plugins: ['@emotion'],
-})
