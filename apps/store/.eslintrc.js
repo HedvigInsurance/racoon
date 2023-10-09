899fa3913e9e6e850d05bad1be21a238d3315573
Alexander Lebedev <alexander.lebedev@gmail.com>
2023-10-09 15:10:18 +0200

    Fix typecheck and lint in store

--- apps/store/.eslintrc.js
+++ apps/store/.eslintrc.js
@@ -2,35 +2,36 @@ const baseConfig = require('eslint-config-custom')
 
 /** @type {import('eslint').Linter.Config} */
 module.exports = {
-    root: true,
-    ...baseConfig,
-    ignorePatterns: ['src/services/graphql/*', '*.js'],
-    rules: {
-        ...baseConfig.rules,
-        // This allows us to use to autofocus attribute on input elements (or all elements, really)
-        // It's used for the accordion effects in PriceCalculator. Without autofocus user would have to tab
-        // through the entire page to get to the newly shown form items, which is worse than messing
-        // with auto-focus. Probably.
-        // https://github.com/jsx-eslint/eslint-plugin-jsx-a11y/blob/main/docs/rules/no-autofocus.md
-        'jsx-a11y/no-autofocus': 'off',
+  root: true,
+  ...baseConfig,
+  ignorePatterns: ['src/services/graphql/*', '*.js'],
+  rules: {
+    ...baseConfig.rules,
+    // This allows us to use to autofocus attribute on input elements (or all elements, really)
+    // It's used for the accordion effects in PriceCalculator. Without autofocus user would have to tab
+    // through the entire page to get to the newly shown form items, which is worse than messing
+    // with auto-focus. Probably.
+    // https://github.com/jsx-eslint/eslint-plugin-jsx-a11y/blob/main/docs/rules/no-autofocus.md
+    'jsx-a11y/no-autofocus': 'off',
+  },
+  overrides: [
+    ...baseConfig.overrides,
+    {
+      files: ['**/*.{ts,tsx}'],
+      parserOptions: {
+        project: ['./tsconfig.json'],
+        tsconfigRootDir: __dirname,
+      },
+      rules: {
+        // Causes eslint timeout on our code (src/blocks/HeaderBlock.tsx)
+        '@typescript-eslint/no-misused-promises': 'off',
+        // TODO: Consider fixing errors and enabling these rules
+
+        '@typescript-eslint/no-floating-promises': 'off',
+        '@typescript-eslint/no-unsafe-argument': 'off',
+        '@typescript-eslint/no-unsafe-assignment': 'off',
+        '@typescript-eslint/no-unsafe-member-access': 'off',
+      },
     },
-    overrides: [
-        ...baseConfig.overrides,
-        {
-            files: ['**/*.{ts,tsx}'],
-            parserOptions: {
-                project: ['./tsconfig.json', '../../packages/*/tsconfig.json'],
-                tsconfigRootDir: __dirname,
-            },
-            rules: {
-                // Causes eslint timeout on our code (src/blocks/HeaderBlock.tsx)
-                '@typescript-eslint/no-misused-promises': 'off',
-                // TODO: Consider fixing errors and enabling these rules
-                '@typescript-eslint/no-floating-promises': 'off',
-                '@typescript-eslint/no-unsafe-argument': 'off',
-                '@typescript-eslint/no-unsafe-assignment': 'off',
-                '@typescript-eslint/no-unsafe-member-access': 'off',
-            },
-        },
-    ],
+  ],
 }
