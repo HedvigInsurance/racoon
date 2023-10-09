3106a3b65a96b8231c9815beecb716cea8ed34f7
Alexander Lebedev <alexander.lebedev@gmail.com>
2023-10-09 16:46:03 +0200

    Fix eslintrc validation is eslint

--- packages/ui/.eslintrc.js
+++ packages/ui/.eslintrc.js
@@ -1,17 +1,18 @@
+/* eslint-disable */
 const baseConfig = require('eslint-config-custom')
 
 /** @type {import('eslint').Linter.Config} */
 module.exports = {
   root: true,
   ...baseConfig,
+  parserOptions: {
+    project: ['./tsconfig.json'],
+    tsconfigRootDir: __dirname,
+  },
   overrides: [
     ...baseConfig.overrides,
     {
       files: ['**/*.{ts,tsx}'],
-      parserOptions: {
-        project: ['./tsconfig.json'],
-        tsconfigRootDir: __dirname,
-      },
     },
   ],
 }
