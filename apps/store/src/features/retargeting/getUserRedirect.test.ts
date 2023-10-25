84d09f053be5835df86f40cd2ef1cc6acd7ee8c5
Alexander Lebedev <alexander.lebedev@gmail.com>
2023-10-25 16:13:04 +0200

    Update test definition style in getUserRedirect.test to match the rest of the project (#3484)
    <!--
    PR title: GRW-123 / Feature / Awesome new thing
    -->
    
    ## Describe your changes
    
    Style changes for getUserRedirect.test
    - Remove top-level describe, file name is enough
    - it -> test
    
    <!--
    What changes are made?
    If there are many changes, a list might be a good format.
    If it makes sense, add screenshots and/or screen recordings here.
    -->
    
    ## Justify why they are needed
    
    Code style consistency
    
    ## Checklist before requesting a review
    
    - [ ] I have performed a self-review of my code

--- apps/store/src/features/retargeting/getUserRedirect.test.ts
+++ apps/store/src/features/retargeting/getUserRedirect.test.ts
@@ -1,137 +1,135 @@
-import { describe, it, expect } from '@jest/globals'
+import { test, expect } from '@jest/globals'
 import { ShopSessionRetargetingQuery } from '@/services/apollo/generated'
 import { RedirectType, getUserRedirect } from './getUserRedirect'
 import { UserParams } from './retargeting.types'
 
-describe('getUserRedirect', () => {
-  const userParams: UserParams = { shopSessionId: '123', locale: 'se', queryParams: [] }
+const userParams: UserParams = { shopSessionId: '123', locale: 'se', queryParams: [] }
 
-  it('should return fallback redirect if no data is provided', () => {
-    // Arrange
-    const data = null
+test('should return fallback redirect if no data is provided', () => {
+  // Arrange
+  const data = null
 
-    // Act
-    const result = getUserRedirect(userParams, data)
+  // Act
+  const result = getUserRedirect(userParams, data)
 
-    // Assert
-    expect(result.type).toEqual(RedirectType.Fallback)
-  })
+  // Assert
+  expect(result.type).toEqual(RedirectType.Fallback)
+})
 
-  it('should return cart redirect if cart is not empty', () => {
-    // Arrange
-    const data: ShopSessionRetargetingQuery = {
-      shopSession: {
-        id: '123',
-        cart: {
-          entries: [{ id: '123:offer1' }],
+test('should return cart redirect if cart is not empty', () => {
+  // Arrange
+  const data: ShopSessionRetargetingQuery = {
+    shopSession: {
+      id: '123',
+      cart: {
+        entries: [{ id: '123:offer1' }],
+      },
+      priceIntents: [
+        {
+          id: '123',
+          product: { name: 'test' },
+          data: { street: 'testgatan 1' },
+          defaultOffer: FAKE_OFFER,
         },
-        priceIntents: [
-          {
-            id: '123',
-            product: { name: 'test' },
-            data: { street: 'testgatan 1' },
-            defaultOffer: FAKE_OFFER,
-          },
-        ],
+      ],
+    },
+  }
+
+  // Act
+  const result = getUserRedirect(userParams, data)
+
+  // Assert
+  expect(result.type).toEqual(RedirectType.Cart)
+})
+
+test('should return product redirect if single product is present', () => {
+  // Arrange
+  const priceIntentId = '12345'
+  const data: ShopSessionRetargetingQuery = {
+    shopSession: {
+      id: '123',
+      cart: {
+        entries: [],
       },
-    }
-
-    // Act
-    const result = getUserRedirect(userParams, data)
-
-    // Assert
-    expect(result.type).toEqual(RedirectType.Cart)
-  })
-
-  it('should return product redirect if single product is present', () => {
-    // Arrange
-    const priceIntentId = '12345'
-    const data: ShopSessionRetargetingQuery = {
-      shopSession: {
-        id: '123',
-        cart: {
-          entries: [],
+      priceIntents: [
+        {
+          id: priceIntentId,
+          product: { name: 'test' },
+          data: { street: 'testgatan 1' },
+          defaultOffer: FAKE_OFFER,
         },
-        priceIntents: [
-          {
-            id: priceIntentId,
-            product: { name: 'test' },
-            data: { street: 'testgatan 1' },
-            defaultOffer: FAKE_OFFER,
-          },
-        ],
-      },
-    }
+      ],
+    },
+  }
 
-    // Act
-    const result = getUserRedirect(userParams, data)
+  // Act
+  const result = getUserRedirect(userParams, data)
 
-    // Assert
-    expect(result.type).toEqual(RedirectType.Product)
-    expect(result.url.toString()).toContain(priceIntentId)
-  })
+  // Assert
+  expect(result.type).toEqual(RedirectType.Product)
+  expect(result.url.toString()).toContain(priceIntentId)
+})
 
-  it('should return product redirect for multiple price intents with the same exposure', () => {
-    // Arrange
-    const priceIntentId = '12345'
-    const data: ShopSessionRetargetingQuery = {
-      shopSession: {
-        id: '123',
-        cart: { entries: [] },
-        priceIntents: [
-          {
-            id: '123',
-            product: { name: 'SE_CAR' },
-            data: { registrationNumber: 'FRE123' },
-            defaultOffer: FAKE_OFFER,
-          },
-          {
-            id: priceIntentId,
-            product: { name: 'SE_CAR' },
-            data: { registrationNumber: 'FRE123' },
-            defaultOffer: FAKE_OFFER,
-          },
-        ],
-      },
-    }
+test('should return product redirect for multiple price intents with the same exposure', () => {
+  // Arrange
+  const priceIntentId = '12345'
+  const data: ShopSessionRetargetingQuery = {
+    shopSession: {
+      id: '123',
+      cart: { entries: [] },
+      priceIntents: [
+        {
+          id: '123',
+          product: { name: 'SE_CAR' },
+          data: { registrationNumber: 'FRE123' },
+          defaultOffer: FAKE_OFFER,
+        },
+        {
+          id: priceIntentId,
+          product: { name: 'SE_CAR' },
+          data: { registrationNumber: 'FRE123' },
+          defaultOffer: FAKE_OFFER,
+        },
+      ],
+    },
+  }
 
-    // Act
-    const result = getUserRedirect(userParams, data)
+  // Act
+  const result = getUserRedirect(userParams, data)
 
-    // Assert
-    expect(result.type).toEqual(RedirectType.Product)
-    expect(result.url.toString()).toContain(priceIntentId)
-  })
+  // Assert
+  expect(result.type).toEqual(RedirectType.Product)
+  expect(result.url.toString()).toContain(priceIntentId)
+})
 
-  it('should return modified cart if multiple price intents with different exposures are present', () => {
-    // Arrange
-    const data: ShopSessionRetargetingQuery = {
-      shopSession: {
-        id: '123',
-        cart: { entries: [] },
-        priceIntents: [
-          {
-            id: '123',
-            product: { name: 'SE_CAR' },
-            data: { registrationNumber: 'FRE123' },
-            defaultOffer: FAKE_OFFER,
-          },
-          {
-            id: '456',
-            product: { name: 'SE_CAR' },
-            data: { registrationNumber: 'FRE456' },
-            defaultOffer: FAKE_OFFER,
-          },
-        ],
-      },
-    }
+test('should return modified cart if multiple price intents with different exposures are present', () => {
+  // Arrange
+  const data: ShopSessionRetargetingQuery = {
+    shopSession: {
+      id: '123',
+      cart: { entries: [] },
+      priceIntents: [
+        {
+          id: '123',
+          product: { name: 'SE_CAR' },
+          data: { registrationNumber: 'FRE123' },
+          defaultOffer: FAKE_OFFER,
+        },
+        {
+          id: '456',
+          product: { name: 'SE_CAR' },
+          data: { registrationNumber: 'FRE456' },
+          defaultOffer: FAKE_OFFER,
+        },
+      ],
+    },
+  }
 
-    // Act
-    const result = getUserRedirect(userParams, data)
+  // Act
+  const result = getUserRedirect(userParams, data)
 
-    // Assert
-    expect(result.type).toEqual(RedirectType.ModifiedCart)
-  })
+  // Assert
+  expect(result.type).toEqual(RedirectType.ModifiedCart)
 })
 
 const FAKE_OFFER = {
