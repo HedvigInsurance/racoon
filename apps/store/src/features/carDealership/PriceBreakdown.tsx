8c08eb8cd0e8aa3ac3478e57f5989beecd261a3f
Gustav Eén <gustaveen@gmail.com>
2023-11-08 11:43:49 +0100

    Add new PriceBreakdown component for car dealership

---
+++ apps/store/src/features/carDealership/PriceBreakdown.tsx
@@ -0,0 +1,68 @@
+import styled from '@emotion/styled'
+import { Text, theme } from 'ui'
+import { CurrencyCode } from '@/services/apollo/generated'
+import { useFormatter } from '@/utils/useFormatter'
+
+type Props = {
+  amount: number
+  defaultAmount?: number
+  currencyCode: CurrencyCode
+  title: string
+  subTitle: string
+  priceExplanation: string
+}
+
+export const PriceBreakdown = ({
+  amount,
+  defaultAmount,
+  currencyCode,
+  title,
+  subTitle,
+  priceExplanation,
+}: Props) => {
+  const formatter = useFormatter()
+
+  return (
+    <div>
+      <Row>
+        <Text>{title}</Text>
+        <Wrapper>
+          {defaultAmount !== undefined && (
+            <Text as="p" size="md" strikethrough={true} color="textSecondary">
+              {formatter.monthlyPrice({
+                amount: defaultAmount,
+                currencyCode: currencyCode,
+              })}
+            </Text>
+          )}
+          <Text as="p" size="md" color="textPrimary">
+            {formatter.monthlyPrice({
+              currencyCode: currencyCode,
+              amount: amount,
+            })}
+          </Text>
+        </Wrapper>
+      </Row>
+
+      <Row>
+        <Text size="xs" color="textSecondary">
+          {subTitle}
+        </Text>
+        <Text size="xs" color="textSecondary">
+          {priceExplanation}
+        </Text>
+      </Row>
+    </div>
+  )
+}
+
+const Row = styled.div({
+  display: 'flex',
+  justifyContent: 'space-between',
+  alignItems: 'center',
+})
+
+const Wrapper = styled.div({
+  display: 'flex',
+  columnGap: theme.space.xs,
+})
