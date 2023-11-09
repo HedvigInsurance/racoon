faa58a0e38b12340af1e43bb2795c4e44e056079
Alexander Lebedev <alexander.lebedev@gmail.com>
2023-11-09 16:10:18 +0100

    Fix jest setup by using browser msw mocks instead of server ones

--- apps/store/src/mocks/helpers.ts
+++
@@ -1,6 +0,0 @@
-const GRAPHQL_ENDPOINT = process.env.NEXT_PUBLIC_GRAPHQL_ENDPOINT
-if (GRAPHQL_ENDPOINT === undefined) throw new Error('NEXT_PUBLIC_GRAPHQL_ENDPOINT is not defined')
-
-export const graphqlConstants = () => ({
-  GRAPHQL_ENDPOINT,
-})
