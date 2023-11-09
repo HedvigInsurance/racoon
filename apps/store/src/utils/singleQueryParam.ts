2b31904ac44354b753375f875ea202dcbbd33d51
Alexander Lebedev <alexander.lebedev@gmail.com>
2023-11-09 12:09:58 +0100

    CAR-29: Support specifying default open tab via ?section=overview|coverage|documents in member insurance details

---
+++ apps/store/src/utils/singleQueryParam.ts
@@ -0,0 +1,9 @@
+import { NextParsedUrlQuery } from 'next/dist/server/request-meta'
+
+export const singleQueryParam = (query: NextParsedUrlQuery, key: string): string | undefined => {
+  const val = query[key]
+  if (Array.isArray(val)) {
+    return val[0]
+  }
+  return val
+}
