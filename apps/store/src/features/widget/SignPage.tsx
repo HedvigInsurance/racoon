f0e50104507a78d2e5096dca0365287268541c1a
Guilherme S. Popolin <guilherme@hedvig.com>
2023-11-14 15:51:51 +0100

    GEN-1397 - feat(widget): added checkout page (#3554)
    ## Describe your changes
    
    - Added Widget Checkout page
    
    ## Justify why they are needed
    
    Foundation for Widget Checkout page. Other aspects of it like localisation and rendering CMS content will be addressed on subsequent PRs.

---
+++ apps/store/src/features/widget/SignPage.tsx
@@ -0,0 +1,249 @@
+import { useApolloClient } from '@apollo/client'
+import styled from '@emotion/styled'
+import { useTranslation } from 'next-i18next'
+import { useState, type PropsWithChildren } from 'react'
+import { Heading, Text, Button, Space, BankIdIcon, CheckIcon, theme } from 'ui'
+import { FormElement } from '@/components/CheckoutPage/CheckoutPage.constants'
+import { useHandleSubmitCheckout } from '@/components/CheckoutPage/useHandleSubmitCheckout'
+import * as FullscreenDialog from '@/components/FullscreenDialog/FullscreenDialog'
+import { GridLayout } from '@/components/GridLayout/GridLayout'
+import { PersonalNumberField } from '@/components/PersonalNumberField/PersonalNumberField'
+import { ProductItemContainer } from '@/components/ProductItem/ProductItemContainer'
+import { useProductRecommendations } from '@/components/ProductRecommendationList/useProductRecommendations'
+import { QuickAddOfferContainer } from '@/components/QuickAdd/QuickAddOfferContainer'
+import { DiscountFieldContainer } from '@/components/ShopBreakdown/DiscountFieldContainer'
+import { Divider, ShopBreakdown } from '@/components/ShopBreakdown/ShopBreakdown'
+import { TotalAmountContainer } from '@/components/ShopBreakdown/TotalAmountContainer'
+import { TextField } from '@/components/TextField/TextField'
+import { TextWithLink } from '@/components/TextWithLink'
+import {
+  CurrentMemberQuery,
+  CurrentMemberQueryVariables,
+  CurrentMemberDocument,
+  ShopSessionAuthenticationStatus,
+} from '@/services/apollo/generated'
+import { type ShopSession } from '@/services/shopSession/ShopSession.types'
+import { useShopSession } from '@/services/shopSession/ShopSessionContext'
+import { useTracking } from '@/services/Tracking/useTracking'
+import { useCurrentLocale } from '@/utils/l10n/useCurrentLocale'
+import { PageLink } from '@/utils/PageLink'
+import { Header } from './Header'
+
+type Props = {
+  shopSession: ShopSession
+  customerAuthenticationStatus: ShopSessionAuthenticationStatus
+  ssn: string
+  shouldCollectEmail: boolean
+  shouldCollectName: boolean
+  suggestedEmail?: string
+}
+
+export const SignPage = (props: Props) => {
+  const { t } = useTranslation('checkout')
+  const { routingLocale } = useCurrentLocale()
+
+  const { offerRecommendation } = useProductRecommendations()
+
+  const { reset: resetShopSession } = useShopSession()
+  const apolloClient = useApolloClient()
+  const [showSignError, setShowSignError] = useState(false)
+  const tracking = useTracking()
+  const [handleSubmitSign, { loading, userError }] = useHandleSubmitCheckout({
+    shopSessionId: props.shopSession.id,
+    ssn: props.ssn,
+    customerAuthenticationStatus: props.customerAuthenticationStatus,
+    async onSuccess() {
+      const { data } = await apolloClient.query<CurrentMemberQuery, CurrentMemberQueryVariables>({
+        query: CurrentMemberDocument,
+      })
+      const memberId = data.currentMember.id
+
+      tracking.reportPurchase(
+        props.shopSession.cart,
+        memberId,
+        props.customerAuthenticationStatus === ShopSessionAuthenticationStatus.None,
+      )
+
+      resetShopSession()
+
+      // TODO: Redirect to payment page
+    },
+    onError() {
+      setShowSignError(true)
+    },
+  })
+
+  const userErrorMessage = userError?.message
+
+  return (
+    <>
+      <Space y={4}>
+        <Header step="SIGN" />
+
+        <GridLayout.Root>
+          <GridLayout.Content width="1/3" align="center">
+            <Space y={3.5}>
+              <Headings>
+                <Heading as="h1" variant="standard.24" align="center">
+                  {/* TODO: translate this */}
+                  Ditt erbjudande
+                </Heading>
+                <Text as="p" balance={true} color="textSecondary" align="center" size="xl">
+                  {/* TODO: translate this */}
+                  Välj startdatum och signera nedan
+                </Text>
+              </Headings>
+
+              <Space y={{ base: 1, lg: 1.5 }}>
+                <ShopBreakdown>
+                  {props.shopSession.cart.entries.map((item) => (
+                    <ProductItemContainer key={item.id} offer={item}>
+                      {/* <EditActionButton shopSessionId={shopSession.id} offer={item} /> */}
+                    </ProductItemContainer>
+                  ))}
+                </ShopBreakdown>
+
+                <DiscountFieldContainer shopSession={props.shopSession} />
+
+                <Divider />
+
+                <TotalAmountContainer cart={props.shopSession.cart} />
+
+                {offerRecommendation && (
+                  <QuickAddOfferContainer
+                    shopSessionId={props.shopSession.id}
+                    {...offerRecommendation}
+                  />
+                )}
+
+                <form onSubmit={handleSubmitSign}>
+                  <Space y={0.25}>
+                    <PersonalNumberField
+                      label={t('FIELD_PERSONAL_NUMBER_SE_LABEL')}
+                      value={props.ssn}
+                      readOnly
+                      disabled
+                    />
+
+                    {props.shouldCollectName && (
+                      <>
+                        <TextField
+                          type="text"
+                          label={t('FORM_FIRST_NAME_LABEL')}
+                          name={FormElement.FirstName}
+                          required
+                        />
+                        <TextField
+                          type="text"
+                          label={t('FORM_LAST_NAME_LABEL')}
+                          name={FormElement.LastName}
+                          required
+                        />
+                      </>
+                    )}
+
+                    {props.shouldCollectEmail && (
+                      <TextField
+                        type="email"
+                        label={t('FORM_EMAIL_LABEL')}
+                        name={FormElement.Email}
+                        defaultValue={props.suggestedEmail}
+                        required
+                      />
+                    )}
+
+                    <Space y={1}>
+                      <SignButton loading={loading} showBankIdIcon={true}>
+                        {t('SIGN_BUTTON', { count: props.shopSession.cart.entries.length })}
+                      </SignButton>
+
+                      <Space y={1.6}>
+                        <UspWrapper>
+                          <CheckIcon size="1rem" />
+                          <Text size="xs">
+                            {/* TODO: translate this */}
+                            Ingen bindningstid
+                          </Text>
+                        </UspWrapper>
+
+                        {userErrorMessage ? (
+                          <Text as="p" size="xs" color="textSecondary" align="center">
+                            {userErrorMessage}
+                          </Text>
+                        ) : (
+                          <TextWithLink
+                            as="p"
+                            color="textSecondary"
+                            size="xs"
+                            align="center"
+                            balance={true}
+                            href={PageLink.privacyPolicy({ locale: routingLocale })}
+                            target="_blank"
+                          >
+                            {t('SIGN_DISCLAIMER')}
+                          </TextWithLink>
+                        )}
+                      </Space>
+                    </Space>
+                  </Space>
+                </form>
+              </Space>
+            </Space>
+          </GridLayout.Content>
+        </GridLayout.Root>
+      </Space>
+
+      <FullscreenDialog.Root open={showSignError} onOpenChange={setShowSignError}>
+        <FullscreenDialog.Modal
+          center={true}
+          Footer={
+            <FullscreenDialog.Close asChild>
+              <Button type="button" variant="primary">
+                {t('ERROR_GENERAL_DIALOG_ACTION_TRY_AGAIN')}
+              </Button>
+            </FullscreenDialog.Close>
+          }
+        >
+          <ErrorPrompt size={{ _: 'md', lg: 'lg' }} align="center">
+            {t('ERROR_GENERAL_DIALOG_PROMPT')}
+          </ErrorPrompt>
+        </FullscreenDialog.Modal>
+      </FullscreenDialog.Root>
+    </>
+  )
+}
+
+const Headings = styled.div({ maxWidth: '35ch', marginInline: 'auto' })
+
+type SignButtonProps = PropsWithChildren<{ loading: boolean; showBankIdIcon: boolean }>
+const SignButton = ({ children, loading, showBankIdIcon }: SignButtonProps) => {
+  return (
+    <Button type="submit" loading={loading}>
+      <StyledSignButtonContent>
+        {showBankIdIcon && <BankIdIcon color="white" />}
+        {children}
+      </StyledSignButtonContent>
+    </Button>
+  )
+}
+
+const StyledSignButtonContent = styled.div({
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+  gap: theme.space.sm,
+  width: '100%',
+})
+
+const ErrorPrompt = styled(Text)({
+  maxWidth: '42rem',
+  marginLeft: 'auto',
+  marginRight: 'auto',
+})
+
+const UspWrapper = styled.div({
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+  gap: theme.space.xs,
+})
