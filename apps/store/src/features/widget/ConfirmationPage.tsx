c4471a9c755ee80be02a8b28cf73f8fc1db955f7
Robin Andeer <robin.andeer@gmail.com>
2023-11-15 11:23:51 +0100

    GEN-1395 | Create basic Widget Confirmation page

---
+++ apps/store/src/features/widget/ConfirmationPage.tsx
@@ -0,0 +1,43 @@
+import { Button, Heading, Space } from 'ui'
+import { GridLayout } from '@/components/GridLayout/GridLayout'
+import { ProductItemContainer } from '@/components/ProductItem/ProductItemContainer'
+import { TotalAmountContainer } from '@/components/ShopBreakdown/TotalAmountContainer'
+import { ShopSession } from '@/services/shopSession/ShopSession.types'
+import { Header } from './Header'
+import { publishWidgetEvent } from './publishWidgetEvent'
+
+type Props = {
+  shopSession: ShopSession
+  backToAppButton?: string
+}
+
+export const ConfirmationPage = (props: Props) => {
+  const handleClickBackToApp = () => {
+    publishWidgetEvent({ status: 'close' })
+  }
+
+  return (
+    <Space y={4}>
+      <Header step="CONFIRMATION" />
+      <GridLayout.Root>
+        <GridLayout.Content width="1/3" align="center">
+          <Space y={4}>
+            <Heading as="h1" variant="standard.24" align="center">
+              {/* TODO: lokalise */}
+              Ditt köp är klart!
+            </Heading>
+            <Space y={1}>
+              {props.shopSession.cart.entries.map((item) => (
+                <ProductItemContainer key={item.id} offer={item} />
+              ))}
+              <TotalAmountContainer cart={props.shopSession.cart} />
+              {props.backToAppButton && (
+                <Button onClick={handleClickBackToApp}>{props.backToAppButton}</Button>
+              )}
+            </Space>
+          </Space>
+        </GridLayout.Content>
+      </GridLayout.Root>
+    </Space>
+  )
+}
---
+++ apps/store/src/pages/widget/[flow]/[shopSessionId]/confirmation.tsx
@@ -0,0 +1,59 @@
+import { type GetServerSideProps } from 'next'
+import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
+import { type ComponentProps } from 'react'
+import { ConfirmationPage } from '@/features/widget/ConfirmationPage'
+import { addApolloState, initializeApolloServerSide } from '@/services/apollo/client'
+import { useShopSessionQuery } from '@/services/apollo/generated'
+import { setupShopSessionServiceServerSide } from '@/services/shopSession/ShopSession.helpers'
+import { type WidgetFlowStory, getStoryById } from '@/services/storyblok/storyblok'
+import { isRoutingLocale } from '@/utils/l10n/localeUtils'
+
+type Props = Omit<ComponentProps<typeof ConfirmationPage>, 'shopSession'> & {
+  shopSessionId: string
+}
+
+type Params = {
+  flow: string
+  shopSessionId: string
+}
+
+export const getServerSideProps: GetServerSideProps<Props, Params> = async (context) => {
+  if (!context.params) throw new Error('Missing params')
+  if (!isRoutingLocale(context.locale)) throw new Error(`Invalid locale: ${context.locale}`)
+
+  const apolloClient = await initializeApolloServerSide({
+    req: context.req,
+    res: context.res,
+    locale: context.locale,
+  })
+  const shopSessionService = setupShopSessionServiceServerSide({ apolloClient })
+  const [translations, story] = await Promise.all([
+    serverSideTranslations(context.locale),
+    getStoryById<WidgetFlowStory>({
+      id: context.params.flow,
+      version: context.draftMode ? 'draft' : undefined,
+    }),
+    shopSessionService.fetchById(context.params.shopSessionId),
+  ])
+
+  return addApolloState(apolloClient, {
+    props: {
+      ...translations,
+      ...context.params,
+      backToAppButton: story.content.backToAppButtonLabel,
+    },
+  })
+}
+
+const Page = (props: Props) => {
+  const shopSessionResult = useShopSessionQuery({
+    variables: { shopSessionId: props.shopSessionId },
+  })
+  const shopSession = shopSessionResult.data?.shopSession
+
+  if (!shopSession) return null
+
+  return <ConfirmationPage {...props} shopSession={shopSession} />
+}
+
+export default Page
