2b53e396535c132dcf60ec413ff5c7800fa2f607
Alexander Lebedev <alexander.lebedev@gmail.com>
2023-11-17 14:05:40 +0100

    Change default quality to 73 to flush images that got stuck on 74 quality (#3588)
    <!--
    PR title: GRW-123 / Feature / Awesome new thing
    -->
    
    ## Describe your changes
    
    <!--
    What changes are made?
    If there are many changes, a list might be a good format.
    If it makes sense, add screenshots and/or screen recordings here.
    -->
    
    ## Justify why they are needed
    
    ## Checklist before requesting a review
    
    - [ ] I have performed a self-review of my code

--- packages/ui/src/lib/theme/theme.ts
+++ packages/ui/src/lib/theme/theme.ts
@@ -22,6 +22,6 @@ export const getColor = (color: UIColorKeys) => {
   return theme.colors[color]
 }
 
-// Overriding default of 75 to flush old image cache.  Do not change it back to 75
+// Overriding default of 75 to flush old image cache.  Do not change it back to 75 or 74
 // unless you're ready to manually change every single storyblok image or solve it some other way
-export const DEFAULT_IMAGE_QUALITY = 74
+export const DEFAULT_IMAGE_QUALITY = 73
