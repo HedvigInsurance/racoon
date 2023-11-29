b01394afdae08c9af35c343ad1c22e884dc456bd
Robin Andeer <robin.andeer@gmail.com>
2023-11-29 12:11:28 +0100

    Add tracking providers to widget pages (#3659)
    <!--
    PR title: GRW-123 / Feature / Awesome new thing
    -->
    
    ## Describe your changes
    
    - Add tracking providers for select, calculate price, and sign widget pages
    
    <!--
    What changes are made?
    If there are many changes, a list might be a good format.
    If it makes sense, add screenshots and/or screen recordings here.
    -->
    
    ## Justify why they are needed
    
    - Makes sure that we are using the correct information for tracking
    
    ## Checklist before requesting a review
    
    - [ ] I have performed a self-review of my code

--- apps/store/src/pages/widget/[flow]/[shopSessionId]/[priceIntentId]/calculate-price.tsx
+++ apps/store/src/pages/widget/[flow]/[shopSessionId]/[priceIntentId]/calculate-price.tsx
@@ -89,7 +89,7 @@ const Page = (props: Props) => {
       <Head>
         <title>{`Hedvig | ${t('CALCULATE_PRICE_PAGE_TITLE')}`}</title>
       </Head>
-      <TrackingProvider shopSession={shopSession}>
+      <TrackingProvider shopSession={shopSession} priceIntent={priceIntent}>
         <CalculatePricePage {...props} shopSession={shopSession} priceIntent={priceIntent} />
       </TrackingProvider>
     </>
