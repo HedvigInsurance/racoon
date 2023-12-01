fe2378449ff7ce83ca773a9efe011ec9884946c5
Robin Andeer <robin.andeer@gmail.com>
2023-12-01 15:40:47 +0100

    Refactor basic auth for car trial validation form

--- apps/store/src/pages/car-buyer/validation/[dealerId].tsx
+++ apps/store/src/pages/car-buyer/validation/[dealerId].tsx
@@ -1,36 +1,50 @@
-import { type GetServerSideProps } from 'next'
+import { type GetServerSidePropsContext, type GetServerSideProps } from 'next'
 import Head from 'next/head'
 import { type ComponentProps } from 'react'
+import { Heading } from 'ui'
 import { CarBuyerValidationPage } from '@/features/carDealership/CarBuyerValidationPage'
 
-type Props = ComponentProps<typeof CarBuyerValidationPage>
+type Props = ComponentProps<typeof CarBuyerValidationPage> & { authenticated: boolean }
 type Params = { dealerId: string }
 
 // @ts-expect-error not an async function
 export const getServerSideProps: GetServerSideProps<Props, Params> = (context) => {
   if (!context.params) throw new Error('Missing params')
 
-  const expectedUsername = process.env.CAR_TRIAL_VALIDATION_USERNAME
-  const expectedPassword = process.env.CAR_TRIAL_VALIDATION_PASSWORD
-  const isProtected = expectedUsername && expectedPassword
-  if (isProtected) {
-    const { authorization } = context.req.headers
-    const [type, token] = authorization ? authorization.split(' ') : []
-    const [username, password] = Buffer.from(token, 'base64').toString().split(':')
-
-    if (type !== 'Basic' || username !== expectedUsername || password !== expectedPassword) {
-      context.res.setHeader('WWW-Authenticate', 'Basic realm="Hedvig"')
-      context.res.statusCode = 401
-      return { props: {} }
-    }
+  if (needsAuthentication(context)) {
+    context.res.setHeader('WWW-Authenticate', 'Basic realm="Hedvig"')
+    context.res.statusCode = 401
+    return { props: { authenticated: false } }
   }
 
   return {
     props: {
+      authenticated: true,
       dealerId: context.params.dealerId,
       hideChat: true,
     },
   }
+}
+
+const needsAuthentication = (context: GetServerSidePropsContext): boolean => {
+  const expectedUsername = process.env.CAR_TRIAL_VALIDATION_USERNAME
+  const expectedPassword = process.env.CAR_TRIAL_VALIDATION_PASSWORD
+  const isProtected = !!expectedUsername && !!expectedPassword
+
+  if (!isProtected) return false
+
+  const { authorization } = context.req.headers
+  if (!authorization) return true
+
+  const [type, token] = authorization ? authorization.split(' ') : []
+  if (!type || !token) return true
+
+  const [username, password] = Buffer.from(token, 'base64').toString().split(':')
+  if (type !== 'Basic' || username !== expectedUsername || password !== expectedPassword) {
+    return true
+  }
+
+  return false
 }
 
 const Page = (props: Props) => {
@@ -40,7 +54,13 @@ const Page = (props: Props) => {
         <title>Hedvig | Validera bil-kund</title>
         <meta name="robots" content="noindex" />
       </Head>
-      <CarBuyerValidationPage dealerId={props.dealerId} />
+      {props.authenticated ? (
+        <CarBuyerValidationPage dealerId={props.dealerId} />
+      ) : (
+        <Heading as="h1" align="center" variant="serif.32">
+          Ladda om sidan för att logga in
+        </Heading>
+      )}
     </>
   )
 }
