465404cd6e41575dc6f46a91d7b5c761838fe257
Gustav Eén <gustaveen@gmail.com>
2023-12-04 16:01:27 +0100

    Add support for light Hedvig logo (#3686)
    <!--
    PR title: GRW-123 / Feature / Awesome new thing
    -->
    
    ## Describe your changes
    Add support for light Hedvig logo 
    <img width="287" alt="Screenshot 2023-12-04 at 11 32 37" src="https://github.com/HedvigInsurance/racoon/assets/6661511/a5592169-7773-4a80-915f-5b71736e2c60">
    
    <!--
    What changes are made?
    If there are many changes, a list might be a good format.
    If it makes sense, add screenshots and/or screen recordings here.
    -->
    
    ## Justify why they are needed
    It was hard coded to always be black
    
    ## Checklist before requesting a review
    
    - [ ] I have performed a self-review of my code

--- packages/ui/src/components/HedvigLogo/HedvigLogo.stories.tsx
+++ packages/ui/src/components/HedvigLogo/HedvigLogo.stories.tsx
@@ -1,17 +1,25 @@
 import { Meta, StoryFn } from '@storybook/react'
+import { theme } from '../..'
 import { HedvigLogo } from './HedvigLogo'
 
 export default {
   title: 'Hedvig Logo',
   component: HedvigLogo,
-  argTypes: {
-    backgroundColor: { control: 'color' },
-  },
 } as Meta<typeof HedvigLogo>
 
 const Template: StoryFn<typeof HedvigLogo> = (args) => <HedvigLogo {...args} />
 
-export const Primary = Template.bind({})
-Primary.args = {
+export const Dark = Template.bind({})
+Dark.args = {
   width: 94,
+}
+
+export const Light = Template.bind({})
+Light.args = {
+  width: 94,
+  color: theme.colors.light,
+}
+
+Light.parameters = {
+  backgrounds: { default: 'Dark' },
 }
