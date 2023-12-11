d91dbfe2b5b180b7a7b74c877368013c08ef2a37
Guilherme Popolin <guilherme@hedvig.com>
2023-12-11 11:17:22 -0300

    feat: added Rating component

---
+++ apps/store/src/components/Rating/Rating.tsx
@@ -0,0 +1,52 @@
+import styled from '@emotion/styled'
+import { useTranslation } from 'next-i18next'
+import { Text, InfoIcon, theme } from 'ui'
+import { SpaceFlex } from '@/components/SpaceFlex/SpaceFlex'
+import { Tooltip } from '@/components/Tooltip/Tooltip'
+
+type Props = {
+  score: number
+  maxScore: number
+  reviewsCount: number
+  explanation?: string
+}
+
+export const Rating = (props: Props) => {
+  const { t } = useTranslation('common')
+
+  return (
+    <Wrapper>
+      <Text
+        as="span"
+        color="textPrimary"
+        size={{
+          _: 10,
+          md: 11,
+        }}
+      >
+        {t('RATING_SCORE_LABEL', { score: props.score, maxScore: props.maxScore })}
+      </Text>
+
+      <SpaceFlex direction="horizontal" space={0.25} align="center">
+        <Text as="span" color="textSecondary">
+          {t('RATING_REVIEWS_COUNT_LABEL', { reviewsCount: props.reviewsCount })}
+        </Text>
+
+        {props.explanation && (
+          <Tooltip message={props.explanation}>
+            <button style={{ marginBottom: -2 }}>
+              <InfoIcon color={theme.colors.textSecondary} />
+            </button>
+          </Tooltip>
+        )}
+      </SpaceFlex>
+    </Wrapper>
+  )
+}
+
+const Wrapper = styled.div({
+  display: 'inline-flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+  width: 'min(28.5rem, 100%)',
+})
