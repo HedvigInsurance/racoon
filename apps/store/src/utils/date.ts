28143ef4bb7ed2e3b3500aa34f0558284c286911
Robin Andeer <robin.andeer@gmail.com>
2023-12-11 13:59:38 +0100

    Parse API date without converting between time zones

--- apps/store/src/utils/date.ts
+++ apps/store/src/utils/date.ts
@@ -1,3 +1,5 @@
+import { format } from 'date-fns'
+
 export const convertToDate = (value: unknown): Date | null => {
   if (typeof value === 'string') {
     const date = new Date(value)
@@ -9,7 +11,7 @@ export const convertToDate = (value: unknown): Date | null => {
 }
 
 export const formatAPIDate = (date: Date): string => {
-  return date.toISOString().split('T')[0]
+  return format(date, 'yyyy-MM-dd')
 }
 
 export const formatInputDateValue = formatAPIDate
