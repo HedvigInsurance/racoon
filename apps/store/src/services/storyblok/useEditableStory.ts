0df71534ae549bd6c5b5567bd595eb3923087df6
Alexander Lebedev <alexander.lebedev@gmail.com>
2023-12-21 16:20:19 +0100

    Fix useEditableStory in server-side build

--- apps/store/src/services/storyblok/useEditableStory.ts
+++ apps/store/src/services/storyblok/useEditableStory.ts
@@ -1,12 +1,13 @@
 import { registerStoryblokBridge } from '@storyblok/js'
 import { TUseStoryblokState } from '@storyblok/react'
 import { useEffect, useRef, useState } from 'react'
+import { isBrowser } from '@/utils/env'
 
 // Non-buggy version of useStoryblokState which was broken recently
 // https://github.com/storyblok/storyblok-react/issues/112#issuecomment-1864426869
 export const useEditableStory: TUseStoryblokState = (initialStory = null) => {
   const [story, setStory] = useState(initialStory)
-  const isBridgeEnabled = typeof (window as any)?.storyblokRegisterEvent !== 'undefined'
+  const isBridgeEnabled = isBrowser() && typeof window.storyblokRegisterEvent !== 'undefined'
   const storyId = (initialStory as any).internalId || story?.id
   const mountedRef = useRef(false)
 
