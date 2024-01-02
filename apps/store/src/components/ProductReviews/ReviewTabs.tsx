5e00922f90de97b2be5937ee184ee48c4f5f60fb
Guilherme S. Popolin <guilherme@hedvig.com>
2024-01-02 12:10:08 -0300

    GEN-1645 - refact(ProductReviews): extract ReviewTabs component
    ## Describe your changes
    
    * Extract tabs into its own component: `ReviewTabs`. As we show it twice, for `ProductReviews` component and reviews dialog, it makes sense to reuse it.
    
    <img width="507" alt="Screenshot 2024-01-02 at 10 11 42" src="https://github.com/HedvigInsurance/racoon/assets/19200662/a0b64534-2a58-4b25-9211-ad168d59ebc9">
    
    <img width="585" alt="Screenshot 2024-01-02 at 10 11 50" src="https://github.com/HedvigInsurance/racoon/assets/19200662/820ffcc9-f7cf-4b2d-aa3c-673dce5dfb7d">
    
    Can be tested [here](https://hedvig-dot-com-git-gen-1645-refactreview-tabs-hedvig.vercel.app/se/forsakringar/olycksfallsforsakring/av)
    
    ## Justify why they are needed
    
    Avoid code duplication.

---
+++ apps/store/src/components/ProductReviews/ReviewTabs.tsx
@@ -0,0 +1,55 @@
+import { useTranslation } from 'next-i18next'
+import { Button } from 'ui'
+import { useProductPageContext } from '@/components/ProductPage/ProductPageContext'
+import { SpaceFlex } from '@/components/SpaceFlex/SpaceFlex'
+import { useTrustpilotData } from '@/services/trustpilot/trustpilot'
+
+export const TABS = {
+  PRODUCT: 'product',
+  TRUSTPILOT: 'trustpilot',
+} as const
+
+export type Tab = (typeof TABS)[keyof typeof TABS]
+
+type Props = {
+  selectedTab: Tab
+  onTabChange: (tab: Tab) => void
+}
+
+export const ReviewTabs = (props: Props) => {
+  const { t } = useTranslation('common')
+  const { averageRating } = useProductPageContext()
+  const trustpilotData = useTrustpilotData()
+
+  const handleTabChange: React.MouseEventHandler<HTMLButtonElement> = (event) => {
+    const tab = event.currentTarget.value as Tab
+    props.onTabChange(tab)
+  }
+
+  if (!averageRating || !trustpilotData) {
+    return null
+  }
+
+  return (
+    <SpaceFlex space={0.5}>
+      <Button
+        value={TABS.PRODUCT}
+        size="medium"
+        variant={props.selectedTab === TABS.PRODUCT ? 'primary-alt' : 'secondary'}
+        fullWidth={true}
+        onClick={handleTabChange}
+      >
+        {t('PRODUCT_REVIEWS_TAB_LABEL', { score: averageRating.score })}
+      </Button>
+      <Button
+        value={TABS.TRUSTPILOT}
+        size="medium"
+        variant={props.selectedTab === TABS.TRUSTPILOT ? 'primary-alt' : 'secondary'}
+        fullWidth={true}
+        onClick={handleTabChange}
+      >
+        {t('TRUSTPILOT_REVIEWS_TAB_LABEL', { score: trustpilotData.score })}
+      </Button>
+    </SpaceFlex>
+  )
+}
