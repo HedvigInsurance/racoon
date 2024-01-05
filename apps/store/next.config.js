efb0c0426bd953e45c8fd666bb5e364135a1e317
Robin Andeer <robin.andeer@gmail.com>
2024-01-05 09:50:56 +0100

    Enable server actions in app router (#3803)
    <!--
    PR title: GRW-123 / Feature / Awesome new thing
    -->
    
    ## Describe your changes
    
    - Enable server actions in app router
    
    - Add dummy locale (se) to app router
    
    - Implement server actions in shop session debugger
    
    <!--
    What changes are made?
    If there are many changes, a list might be a good format.
    If it makes sense, add screenshots and/or screen recordings here.
    -->
    
    ## Justify why they are needed
    
    - Experiment with app router and what features we can adopt
    
    - Needed a locale in the routes to avoid automatic redirects
    
    ## Checklist before requesting a review
    
    - [ ] I have performed a self-review of my code

--- apps/store/next.config.js
+++ apps/store/next.config.js
@@ -11,6 +11,7 @@ module.exports = withBundleAnalyzer({
   experimental: {
     instrumentationHook: true,
     strictNextHead: true,
+    serverActions: true,
   },
   reactStrictMode: true,
   swcMinify: true,
