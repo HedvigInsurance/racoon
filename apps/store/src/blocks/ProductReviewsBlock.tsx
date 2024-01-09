b95856244f87d6a7d65aa59fee4a1803ba8dd635
Guilherme S. Popolin <guilherme@hedvig.com>
2024-01-09 17:15:13 +0100

    refact(ProductReviews): use lokalise as tooltip content source (#3823)

--- apps/store/src/blocks/ProductReviewsBlock.tsx
+++ apps/store/src/blocks/ProductReviewsBlock.tsx
@@ -1,12 +1,7 @@
 import { ProductReviews } from '@/components/ProductReviews/ProductReviews'
-import { SbBaseBlockProps } from '@/services/storyblok/storyblok'
 
-type Props = SbBaseBlockProps<{
-  tooltipText?: string
-}>
-
-export const ProductReviewsBlock = (props: Props) => {
-  return <ProductReviews tooltipText={props.blok.tooltipText} />
+export const ProductReviewsBlock = () => {
+  return <ProductReviews />
 }
 
 ProductReviewsBlock.blockName = 'productReviews'
