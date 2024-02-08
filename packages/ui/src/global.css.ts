106b4327930c7f59d2e3dac7d1a5fd47257769b6
Alexander Lebedev <alexander.lebedev@gmail.com>
2024-02-08 16:36:45 +0100

    Move global styles to vanilla-extract (#3904)
    <!--
    PR title: GRW-123 / Feature / Awesome new thing
    -->
    
    ## Describe your changes
    
    Move most of ui/theme/globalStyles to vanilla-extract global.css.ts
    
    <!--
    What changes are made?
    If there are many changes, a list might be a good format.
    If it makes sense, add screenshots and/or screen recordings here.
    -->
    
    ## Justify why they are needed
    
    - Showcase of `globalStyle` API
    - Compile-time styles are faster
    
    ## Checklist before requesting a review
    
    - [ ] I have performed a self-review of my code

---
+++ packages/ui/src/global.css.ts
@@ -0,0 +1,69 @@
+/***
+ The following CSS reset is heavily inspired by:
+ https://github.com/elad2412/the-new-css-reset
+ ***/
+
+// Remove all the styles of the "User-Agent-Stylesheet", except for the 'display' property
+import { globalStyle } from '@vanilla-extract/css'
+import { animationAllowed } from './theme'
+
+globalStyle('*:where(:not(iframe, canvas, img, svg, video):not(svg *))', {
+  all: 'unset',
+  display: 'revert',
+})
+
+// Preferred box-sizing model
+globalStyle('*, *::before, *::after', {
+  boxSizing: 'border-box',
+})
+
+globalStyle('html', {
+  scrollBehavior: 'smooth',
+  WebkitTapHighlightColor: 'transparent',
+  '@media': {
+    [animationAllowed]: {
+      scrollBehavior: 'auto',
+    },
+  },
+})
+
+globalStyle('body', {
+  WebkitFontSmoothing: 'antialiased',
+  MozOsxFontSmoothing: 'grayscale',
+})
+
+// Responsive images
+globalStyle('img', {
+  maxWidth: '100%',
+  height: 'auto',
+})
+
+// Sane display value for media elements
+globalStyle('img, iframe, picture, video, canvas, svg', {
+  display: 'block',
+})
+
+// Remove default list styles
+globalStyle('ol, ul', {
+  listStyle: 'none',
+  margin: 0,
+  padding: 0,
+})
+
+// Reapply the pointer cursor for anchor tags
+globalStyle('a, button', {
+  cursor: 'revert',
+})
+
+// Fix the feature of 'hidden' attribute.
+//   "display:revert;" revert to element instead of attribute
+globalStyle(':where([hidden])', {
+  display: 'none',
+})
+
+// Isolate app stacking context, so it doesn't mess up with portals
+globalStyle('#__next', {
+  isolation: 'isolate',
+})
+
+export default {}
