54b347c722d4103cd4e4ffc9ce48025835566ae4
Gustav Eén <gustaveen@gmail.com>
2024-02-28 17:03:51 +0100

    Add collapsible part of consent body text

--- apps/store/src/features/carDealership/MyMoneyConsent/MyMoneyConsent.tsx
+++ apps/store/src/features/carDealership/MyMoneyConsent/MyMoneyConsent.tsx
@@ -1,14 +1,22 @@
 import * as Checkbox from '@radix-ui/react-checkbox'
 import { useAtom } from 'jotai'
 import { useTranslation } from 'next-i18next'
-import React from 'react'
+import React, { useState } from 'react'
 import { CheckIcon, Text } from 'ui'
-import { checkboxIndicator, checkboxRoot, consentBody, consentWrapper } from './MyMoneyConsent.css'
+import * as Collapsible from '@/components/Collapsible'
+import {
+  checkboxIndicator,
+  checkboxRoot,
+  consentBody,
+  consentWrapper,
+  trigger,
+} from './MyMoneyConsent.css'
 import { concentAcceptedAtom } from './MyMoneyConsentAtom'
 
 export const MyMoneyConsent = () => {
-  const { t } = useTranslation('carDealership')
+  const { t } = useTranslation(['carDealership', 'common'])
   const [concentAccepted, setConsentAccepted] = useAtom(concentAcceptedAtom)
+  const [expanded, setExpanded] = useState(false)
 
   const handleCheckedChange = (checked: boolean) => {
     setConsentAccepted(checked)
@@ -28,11 +36,24 @@ export const MyMoneyConsent = () => {
       </Checkbox.Root>
 
       <Text size="md">
-        <label htmlFor="myMoneyConsent">{t('MY_MONEY_CONSENT_HEADING')}</label>
+        <label htmlFor="myMoneyConsent">{t('carDealership:MY_MONEY_CONSENT_HEADING')}</label>
       </Text>
-      <Text color="textSecondary" size="xxs" className={consentBody}>
-        {t('MY_MONEY_CONSENT_BODY')}
-      </Text>
+
+      <Collapsible.Root open={expanded} onOpenChange={setExpanded} className={consentBody}>
+        <Text color="textSecondary" size="xxs">
+          <Text as="span" color="textSecondary" size="xxs">
+            {t('carDealership:MY_MONEY_CONSENT_BODY')}
+          </Text>
+          <Collapsible.Trigger asChild={true}>
+            <span className={trigger}>{t('common:READ_MORE')}</span>
+          </Collapsible.Trigger>
+        </Text>
+        <Collapsible.Content style={{ cursor: 'initial' }}>
+          <Text as="p" color="textSecondary" size="xxs">
+            {t('carDealership:MY_MONEY_CONSENT_BODY_COLLAPSED')}
+          </Text>
+        </Collapsible.Content>
+      </Collapsible.Root>
     </div>
   )
 }
