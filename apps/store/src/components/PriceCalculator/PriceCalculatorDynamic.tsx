719e7d3b0954a7db46490c0cc4ca7b026c9feaba
Alexander Lebedev <alexander.lebedev@gmail.com>
2024-02-29 15:05:26 +0100

    Add loading skeleton for price calculator

--- apps/store/src/components/PriceCalculator/PriceCalculatorDynamic.tsx
+++ apps/store/src/components/PriceCalculator/PriceCalculatorDynamic.tsx
@@ -1,5 +1,18 @@
 import dynamic from 'next/dynamic'
+import { Skeleton } from '@/components/Skeleton'
+import { SpaceFlex } from '../SpaceFlex/SpaceFlex'
+import { sectionSkeleton } from './PriceCalculatorDynamic.css'
 
-export const PriceCalculatorDynamic = dynamic(() =>
-  import('./PriceCalculator').then((mod) => mod.PriceCalculator),
-)
+export const PriceCalculatorDynamic = dynamic({
+  loader: async () => {
+    const { PriceCalculator } = await import('./PriceCalculator')
+    return PriceCalculator
+  },
+  loading: () => (
+    <SpaceFlex direction="vertical" space={0.5}>
+      <Skeleton className={sectionSkeleton} />
+      <Skeleton className={sectionSkeleton} />
+      <Skeleton className={sectionSkeleton} />
+    </SpaceFlex>
+  ),
+})
