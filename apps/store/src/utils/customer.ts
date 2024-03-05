0fd56dee1860b121598594491a0d7380e3bedc53
Youakeem <youakeem@gmail.com>
2024-03-05 09:10:40 +0100

    RND-525 / Fix / Require missing email for returning customers (#4013)
    <!--
    PR title: RND-525 / Fix / Require missing email for returning customers
    -->
    ## Describe your changes
    Make sure we collect the missing emails during the purchase journey for returning customers.
    
    <!--
    What changes are made?
    Previously we only collected emails of new customers, we now collect it for returning customers with missing emails as well.
    -->
    
    ## Justify why they are needed
    Some old customers are missing an email, causing a backend service to crash when they return to make another purchase.
    
    ## Checklist before requesting a review
    - [x] I have performed a self-review of my code

--- apps/store/src/utils/customer.ts
+++ apps/store/src/utils/customer.ts
@@ -4,7 +4,8 @@ const isNewMember = (customer: ShopSessionCustomer) =>
   customer.authenticationStatus === ShopSessionAuthenticationStatus.None
 
 export const getShouldCollectEmail = (customer?: ShopSessionCustomer | null) => {
-  return !customer || customer.authenticationStatus === ShopSessionAuthenticationStatus.None
+  // Collect emails of new customers as well as returning customers with no email
+  return !customer?.email
 }
 
 export const getShouldCollectName = (customer: ShopSessionCustomer) => {
