fb47f69de630af23208652daac304b93687a34e5
Gustav Eén <gustaveen@gmail.com>
2024-03-11 17:02:01 +0100

    Set correct font size

--- apps/store/src/components/QuickAdd/QuickAddOfferContainer.tsx
+++ apps/store/src/components/QuickAdd/QuickAddOfferContainer.tsx
@@ -61,7 +61,7 @@ export const QuickAddOfferContainer = (props: Props) => {
           Body={
             // Assume Accident insurance
             <>
-              <Text as="p" color="textTranslucentSecondary" size={{ _: 'sm', md: 'lg' }}>
+              <Text as="p" color="textTranslucentSecondary">
                 {t('ACCIDENT_OFFER_DESCRIPTION_BUNDLE')}
                 <QuickAddInfoDialog
                   Header={
