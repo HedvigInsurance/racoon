246e3b4a9726a4181cf6c0bea0f5d6b30f606d9b
Rafael Youakeem <youakeem@gmail.com>
2024-03-13 15:05:21 +0100

    Increase debugger layout bottom padding

--- apps/store/src/app/debugger/debugger.css.ts
+++ apps/store/src/app/debugger/debugger.css.ts
@@ -2,11 +2,11 @@ import { style } from '@vanilla-extract/css'
 import { minWidth } from 'ui'
 
 export const contentWrapper = style({
-  paddingTop: '3.5rem',
+  paddingBlock: '3.5rem',
 
   '@media': {
     [minWidth.md]: {
-      paddingTop: '5rem',
+      paddingBlock: '5rem',
     },
   },
 })
