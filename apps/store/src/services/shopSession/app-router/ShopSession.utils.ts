49e238f90165af994c044328e4c0779729a69dcc
Rafael Youakeem <youakeem@gmail.com>
2024-03-14 12:18:29 +0100

    Create app router shop session service setup util

--- apps/store/src/services/shopSession/app-router/ShopSession.utils.ts
+++ apps/store/src/services/shopSession/app-router/ShopSession.utils.ts
@@ -1,5 +1,12 @@
+import { ApolloClient } from '@apollo/client'
 import { cookies } from 'next/headers'
+import { AppRouterCookiePersister } from '@/services/persister/app-router/AppRouterCookiePersister'
 import { COOKIE_KEY_SHOP_SESSION } from '../ShopSession.constants'
+import { ShopSessionService } from '../ShopSessionService'
+
+export const setupShopSession = (apolloClient: ApolloClient<unknown>) => {
+  return new ShopSessionService(new AppRouterCookiePersister(COOKIE_KEY_SHOP_SESSION), apolloClient)
+}
 
 export const getShopSessionId = () => {
   const cookieStore = cookies()
