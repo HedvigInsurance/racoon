cb3eddaaab606fb9c0ace0eae74885ec14a2b2ee
Rafael Youakeem <youakeem@gmail.com>
2024-03-18 16:52:57 +0100

    Validate debug session SSN server side

--- apps/store/src/app/debugger/components/CreateSessionForm/CreateSessionForm.tsx
+++ apps/store/src/app/debugger/components/CreateSessionForm/CreateSessionForm.tsx
@@ -1,33 +1,38 @@
 'use client'
 
+import { createCustomerSession } from 'app/debugger/actions'
 import { useEffect, useState } from 'react'
+import { useFormState } from 'react-dom'
 import { Space } from 'ui'
 import { SubmitButton } from '@/appComponents/SubmitButton'
 import { TextField } from '@/components/TextField/TextField'
-import { createCustomerSession } from 'app/debugger/actions'
 import { wrapper } from './CreateSessionForm.css'
 
 const HEDVIG_DEBUGGER_SSN = 'hedvig:debugger-ssn'
 
 export const CreateSessionForm = () => {
-  const [defaultSsn, setDefaultSsn] = useState<string | undefined>(undefined)
+  const [state, formAction] = useFormState(createCustomerSession, null)
+  const [defaultSSN, setDefaultSSN] = useState<string>()
 
   useEffect(() => {
     const ssn = window.localStorage.getItem(HEDVIG_DEBUGGER_SSN)
-    if (ssn) setDefaultSsn(ssn)
+    if (ssn) setDefaultSSN(ssn)
   }, [])
+
+  const SSNError = state?.errors?.fields?.ssn
 
   return (
     <div className={wrapper}>
-      <form action={createCustomerSession}>
+      <form action={formAction}>
         <Space y={0.25}>
           <TextField
             label="YYYYMMDDXXXX"
-            key={defaultSsn}
-            defaultValue={defaultSsn}
+            key={defaultSSN}
+            defaultValue={defaultSSN}
             autoFocus
-            required
             name="ssn"
+            warning={!!SSNError}
+            message={SSNError}
           />
           <SubmitButton>Create session</SubmitButton>
         </Space>
