a0fcf7ffa86579bcc306f4e8c71803b1d9ceee8d
Gustav Eén <gustaveen@gmail.com>
2024-03-18 16:21:43 +0100

    Return 404 in production

--- apps/store/src/pages/debugger/car-trial.tsx
+++ apps/store/src/pages/debugger/car-trial.tsx
@@ -1,1 +1,15 @@
+import { GetServerSideProps } from 'next'
+
 export { DebuggerCarTrialPage as default } from '@/features/carDealership/DebuggerCarTrial/DebuggerCarTrialPage'
+
+// @ts-expect-error not an async function
+export const getServerSideProps: GetServerSideProps = () => {
+  if (process.env.VERCEL_ENV === 'production')
+    return {
+      notFound: true,
+    }
+
+  return {
+    props: {},
+  }
+}
