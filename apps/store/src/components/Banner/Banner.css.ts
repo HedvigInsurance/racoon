2bcac07dd305e1f7466023bab411c49df366cefd
Alexander Lebedev <alexander.lebedev@gmail.com>
2024-04-09 13:12:03 +0200

    Convert Banner styles to vanilla-extract

---
+++ apps/store/src/components/Banner/Banner.css.ts
@@ -0,0 +1,69 @@
+import { style, styleVariants } from '@vanilla-extract/css'
+import { colors, minWidth, theme } from 'ui/src/theme'
+
+const GridArea = {
+  Content: 'content',
+  CloseBtn: 'close-btn',
+}
+
+export const bannerRoot = styleVariants({
+  base: {
+    minHeight: '3rem',
+    display: 'grid',
+    // Using grid here to perfect center align banner's content, excluding close button
+    gridTemplateAreas: `
+    ". ${GridArea.Content} ${GridArea.CloseBtn}"
+  `,
+    gridTemplateColumns: '1fr auto 1fr',
+    alignItems: 'center',
+    paddingBlock: theme.space.sm,
+    paddingInline: theme.space.md,
+    fontSize: theme.fontSizes.xs,
+
+    borderBottom: `0.5px solid ${theme.colors.borderTranslucent1}`,
+
+    '@media': {
+      [minWidth.lg]: {
+        paddingInline: theme.space.xl,
+      },
+    },
+  },
+  info: {
+    color: theme.colors.blue800,
+    backgroundColor: theme.colors.blueFill1,
+  },
+  campaign: {
+    color: theme.colors.textGreen,
+    backgroundColor: theme.colors.signalGreenFill,
+  },
+  warning: {
+    color: theme.colors.textAmber,
+    backgroundColor: theme.colors.signalAmberFill,
+  },
+  error: {
+    color: theme.colors.textRed,
+    backgroundColor: theme.colors.signalRedFill,
+  },
+})
+
+export const bannerIcon = style({
+  alignSelf: 'start',
+  // Optical alignment
+  marginTop: 1,
+})
+
+export const bannerContent = style({
+  gridArea: GridArea.Content,
+  display: 'grid',
+  gridTemplateColumns: 'auto 1fr',
+  alignItems: 'center',
+  gap: theme.space.xs,
+})
+
+export const bannerCloseButton = style({
+  gridArea: GridArea.CloseBtn,
+  justifySelf: 'end',
+  color: colors.textPrimary,
+  paddingLeft: theme.space.xs,
+  cursor: 'pointer',
+})
--- apps/store/src/components/Banner/Banner.tsx
+++ apps/store/src/components/Banner/Banner.tsx
@@ -1,13 +1,14 @@
-import styled from '@emotion/styled'
-import type { ComponentPropsWithoutRef} from 'react';
+import { clsx } from 'clsx'
+import type { ComponentPropsWithoutRef } from 'react'
 import { type ReactNode } from 'react'
-import { InfoIcon, CampaignIcon, WarningTriangleIcon, CrossIconSmall, mq, theme } from 'ui'
+import { InfoIcon, CampaignIcon, WarningTriangleIcon, CrossIconSmall, theme } from 'ui'
+import {
+  bannerCloseButton,
+  bannerContent,
+  bannerIcon,
+  bannerRoot,
+} from '@/components/Banner/Banner.css'
 import type { BannerVariant } from './Banner.types'
-
-const Area = {
-  Content: 'content',
-  CloseBtn: 'close-btn',
-}
 
 type Props = {
   children: ReactNode
@@ -17,15 +18,15 @@ type Props = {
 
 export const Banner = ({ children, handleClose, variant = 'info' }: Props) => {
   return (
-    <Wrapper variant={variant}>
-      <Content>
-        <StyledIcon variant={variant} />
+    <div className={clsx(bannerRoot.base, bannerRoot[variant])}>
+      <div className={bannerContent}>
+        <Icon variant={variant} className={bannerIcon} />
         {children}
-      </Content>
-      <CloseButton onClick={handleClose}>
+      </div>
+      <button className={bannerCloseButton} onClick={handleClose}>
         <CrossIconSmall size="1rem" />
-      </CloseButton>
-    </Wrapper>
+      </button>
+    </div>
   )
 }
 
@@ -53,74 +54,4 @@ const Icon = ({ variant, ...others }: IconProps) => {
   }
 
   return icon
-}
-
-const StyledIcon = styled(Icon)({
-  alignSelf: 'start',
-  // Optical alignment
-  marginTop: 1,
-})
-
-const Wrapper = styled.div<{ variant: BannerVariant }>(({ variant }) => ({
-  minHeight: '3rem',
-  display: 'grid',
-  // Using grid here to perfect center align banner's content, excluding close button
-  gridTemplateAreas: `
-    ". ${Area.Content} ${Area.CloseBtn}"
-  `,
-  gridTemplateColumns: '1fr auto 1fr',
-  alignItems: 'center',
-  paddingBlock: theme.space.sm,
-  paddingInline: theme.space.md,
-  fontSize: theme.fontSizes.xs,
-
-  borderBottom: `0.5px solid ${theme.colors.borderTranslucent1}`,
-  ...getVariantStyles(variant),
-
-  [mq.lg]: {
-    paddingInline: theme.space.xl,
-  },
-}))
-
-const Content = styled.div({
-  gridArea: Area.Content,
-  display: 'grid',
-  gridTemplateColumns: 'auto 1fr',
-  alignItems: 'center',
-  gap: theme.space.xs,
-})
-
-const CloseButton = styled.button({
-  gridArea: Area.CloseBtn,
-  justifySelf: 'end',
-  color: theme.colors.textPrimary,
-  paddingLeft: theme.space.xs,
-  cursor: 'pointer',
-})
-
-const getVariantStyles = (variant: BannerVariant) => {
-  switch (variant) {
-    case 'info':
-      return {
-        color: theme.colors.blue800,
-        backgroundColor: theme.colors.blueFill1,
-      }
-    case 'campaign':
-      return {
-        color: theme.colors.textGreen,
-        backgroundColor: theme.colors.signalGreenFill,
-      }
-    case 'warning':
-      return {
-        color: theme.colors.textAmber,
-        backgroundColor: theme.colors.signalAmberFill,
-      }
-    case 'error':
-      return {
-        color: theme.colors.textRed,
-        backgroundColor: theme.colors.signalRedFill,
-      }
-    default:
-      return {}
-  }
 }
