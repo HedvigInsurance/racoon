58328b0ba778fa6fa24395b603bbb561bafe26a4
Gustav Eén <gustaveen@gmail.com>
2024-04-18 10:54:43 +0200

    Add widget loaded event

--- apps/store/src/features/widget/usePublishWidgetInitEvent.ts
+++ apps/store/src/features/widget/usePublishWidgetInitEvent.ts
@@ -1,5 +1,5 @@
 import { useRouter } from 'next/router'
-import { useEffect } from 'react'
+import { useEffect, useRef } from 'react'
 import { publishWidgetEvent } from './publishWidgetEvent'
 import { EXTERNAL_REQUEST_ID_QUERY_PARAM } from './widget.constants'
 
@@ -7,8 +7,16 @@ export const usePublishWidgetInitEvent = () => {
   const router = useRouter()
 
   const requestId = router.query[EXTERNAL_REQUEST_ID_QUERY_PARAM]
+  const widgetLoadedSent = useRef(false)
+
   useEffect(() => {
     if (!router.isReady) return
+
+    if (router.pathname.includes('widget') && !widgetLoadedSent.current) {
+      publishWidgetEvent.widgetLoaded()
+      widgetLoadedSent.current = true
+    }
+
     if (typeof requestId !== 'string') return
 
     publishWidgetEvent.event({ requestId })
