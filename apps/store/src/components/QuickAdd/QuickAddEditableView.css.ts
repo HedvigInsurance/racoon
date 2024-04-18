829650ecdf03c253cf62de4f7d566fa4f23f0a5c
Guilherme S. Popolin <guilherme@hedvig.com>
2024-04-18 11:54:15 +0200

    styles(QuickAddEditableView): update background color

--- apps/store/src/components/QuickAdd/QuickAddEditableView.css.ts
+++ apps/store/src/components/QuickAdd/QuickAddEditableView.css.ts
@@ -5,7 +5,8 @@ export const card = style({
   padding: theme.space.md,
   border: `1px solid ${theme.colors.borderTranslucent1}`,
   borderRadius: theme.radius.md,
-  backgroundColor: theme.colors.blueFill1,
+  // TODO: replace this to theme.colors.signalbluefill when we update Uikit colors
+  backgroundColor: 'hsl(201, 84%, 90%)',
 
   '@media': {
     [minWidth.lg]: {
