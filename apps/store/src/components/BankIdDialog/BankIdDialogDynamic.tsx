e3be8c0ebb56ce285a697d6b0ce0b77d5a82ef79
Guilherme S. Popolin <guilherme@hedvig.com>
2024-04-23 15:46:58 +0200

    chore(bankid): BankdIdV6Dialog -> BankIdDialog

--- apps/store/src/components/BankIdDialog/BankIdDialogDynamic.tsx
+++ apps/store/src/components/BankIdDialog/BankIdDialogDynamic.tsx
@@ -1,13 +1,13 @@
 import dynamic from 'next/dynamic'
 
-// By the time it gets used, BankIdV6Dialog will probably be loaded/cached already so
+// By the time it gets used, BankIdDialog will probably be loaded/cached already so
 // no reason to add a 'loading' component here. At the end of the day it's also hard to
 // add a loading component as this dialog is not opened directly by the user
 // but as a consequence of having a BankId operation in progress.
-export const BankIdV6DialogDynamic = dynamic(
+export const BankIdDialogDynamic = dynamic(
   async () => {
-    const { BankIdV6Dialog } = await import('./BankIdV6Dialog')
-    return BankIdV6Dialog
+    const { BankIdDialog } = await import('./BankIdDialog')
+    return BankIdDialog
   },
   {
     ssr: false,
