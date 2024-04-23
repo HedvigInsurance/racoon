844b67c0a3831c1e140dcc583423b02976e03047
Guilherme S. Popolin <guilherme@hedvig.com>
2024-04-23 13:54:33 +0200

    GEN-1893 - styles(QuickAddEditableView): avoid badge overflowing
    ## Describe your changes
    
    * Reduces inline padding and gap to avoid badge overflowing.
    
    https://github.com/HedvigInsurance/racoon/assets/19200662/ef80a2ac-7f93-4ea0-9052-89439ada25fa
    
    https://github.com/HedvigInsurance/racoon/assets/19200662/ee7086e0-e1c4-4684-a1e5-b30bf7270a41

--- apps/store/src/components/QuickAdd/QuickAddEditableView.css.ts
+++ apps/store/src/components/QuickAdd/QuickAddEditableView.css.ts
@@ -2,22 +2,32 @@ import { style } from '@vanilla-extract/css'
 import { theme, minWidth } from 'ui/src/theme'
 
 export const card = style({
-  padding: theme.space.md,
+  padding: '0.875rem',
   border: `1px solid ${theme.colors.borderTranslucent1}`,
   borderRadius: theme.radius.md,
   // TODO: replace this to theme.colors.signalbluefill when we update Uikit colors
   backgroundColor: 'hsl(201, 84%, 90%)',
 
   '@media': {
+    [minWidth.md]: {
+      padding: theme.space.md,
+    },
     [minWidth.lg]: {
       padding: theme.space.lg,
     },
   },
 })
 
-export const Wrapper = style({
-  width: '3rem',
-  aspectRatio: '1 / 1',
+export const cardHeader = style({
+  display: 'flex',
+  alignItems: 'center',
+  gap: theme.space.xs,
+
+  '@media': {
+    [minWidth.md]: {
+      gap: theme.space.md,
+    },
+  },
 })
 
 export const link = style({
