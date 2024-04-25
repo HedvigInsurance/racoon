4fa79d0d9e0620da395bac0a77a9d93d32ea4e83
Gustav Eén <gustaveen@gmail.com>
2024-04-25 11:45:20 +0200

    Cleanup TickerBlock

--- apps/store/src/blocks/TickerBlock.tsx
+++ apps/store/src/blocks/TickerBlock.tsx
@@ -2,7 +2,6 @@
 
 import { StoryblokComponent } from '@storyblok/react'
 import type { FontSizes } from 'ui'
-import { theme } from 'ui'
 import { Ticker, TickerItem } from '@/components/Ticker/Ticker'
 import type { ExpectedBlockType } from '@/services/storyblok/storyblok'
 import { type SbBaseBlockProps } from '@/services/storyblok/storyblok'
@@ -10,8 +9,6 @@ import type { HeadingBlockProps } from './HeadingBlock'
 import type { TextBlockProps } from './TextBlock'
 
 type Props = SbBaseBlockProps<{
-  topMargin: boolean
-  uspText?: string
   showCheckIcon?: boolean
   tickerItems?: ExpectedBlockType<HeadingBlockProps | TextBlockProps>
   tickerHeight: FontSizes
@@ -20,9 +17,6 @@ type Props = SbBaseBlockProps<{
 
 export const TickerBlock = ({ blok }: Props) => {
   const tickerItems = blok.tickerItems ?? []
-  /* TODO: Deprecated, remove when Storyblok content has migrated to tickerItems */
-  const uspList = blok.uspText?.split('\n') ?? []
-  const showTickerItems = tickerItems.length > 1
 
   const sizes = {
     _: blok.tickerHeight,
@@ -30,24 +24,14 @@ export const TickerBlock = ({ blok }: Props) => {
   }
 
   return (
-    <>
-      {/* TODO: Top margin will be deprecated, remove once migrated */}
-      {blok.topMargin && <div style={{ height: theme.space.md }} />}
-      <Ticker size={sizes}>
-        {showTickerItems
-          ? tickerItems.map((nestedBlock) => (
-              <TickerItem key={nestedBlock._uid} showCheckIcon={blok.showCheckIcon}>
-                <StoryblokComponent blok={nestedBlock} nested={true}>
-                  {nestedBlock}
-                </StoryblokComponent>
-              </TickerItem>
-            ))
-          : uspList.map((item, index) => (
-              <TickerItem key={index} showCheckIcon={true}>
-                {item}
-              </TickerItem>
-            ))}
-      </Ticker>
-    </>
+    <Ticker size={sizes}>
+      {tickerItems.map((nestedBlock) => (
+        <TickerItem key={nestedBlock._uid} showCheckIcon={blok.showCheckIcon}>
+          <StoryblokComponent blok={nestedBlock} nested={true}>
+            {nestedBlock}
+          </StoryblokComponent>
+        </TickerItem>
+      ))}
+    </Ticker>
   )
 }
