ee1bf486ad6c760a1b1d2166fd89fe82399adef2
Rafael Youakeem <youakeem@gmail.com>
2024-05-03 17:08:05 +0200

    Barebones

--- apps/store/src/app/[locale]/cms/[[...slug]]/page.tsx
+++ apps/store/src/app/[locale]/cms/[[...slug]]/page.tsx
@@ -1,22 +1,19 @@
-import type { Metadata } from 'next'
-import { removeTrailingSlash } from 'next/dist/shared/lib/router/utils/remove-trailing-slash'
 import { headers } from 'next/headers'
 import { notFound } from 'next/navigation'
 import { cache } from 'react'
-import { PageBlock } from '@/blocks/PageBlock'
 import type { PageStory } from '@/services/storyblok/storyblok'
-import { MOST_VISITED_PATHS } from '@/services/storyblok/Storyblok.constant'
-import { getImgSrc, isProductStory } from '@/services/storyblok/Storyblok.helpers'
-import { getCmsPageLinks, getStoryBySlug } from '@/services/storyblok/storyblok.serverOnly'
-import { isRoutingLocale, toIsoLocale } from '@/utils/l10n/localeUtils'
-import type { IsoLocale, RoutingLocale } from '@/utils/l10n/types'
-import { ProductCmsPage } from './ProductCmsPage'
-import { StoryBreadcrumbs } from './StoryBreadcrumbs'
+import { getStoryBySlug } from '@/services/storyblok/storyblok.serverOnly'
+import type { RoutingLocale } from '@/utils/l10n/types'
 
 export type CmsPageRoutingParams = { locale: RoutingLocale; slug?: Array<string> }
 type Props = {
   params: CmsPageRoutingParams
+  searchParams: object
 }
+
+// DEBUG: Set SWR for 60 seconds
+// export const dynamic = 'force-static'
+// export const revalidate = 0
 
 export default async function CmsPage(props: Props) {
   const story = await fetchStory(props.params.locale, props.params.slug?.join('/'))
@@ -25,94 +22,90 @@ export default async function CmsPage(props: Props) {
   const headersList = headers()
   const referer = headersList.get('referer')
 
-  console.log({ referer })
+  console.log({ story, referer })
   // Patching incorrect data from Storyblok for /se-en/
-  let { hideBreadcrumbs } = story.content
-  if ((props.params.slug?.length ?? 0) < 1) {
-    hideBreadcrumbs = true
-  }
-  if (isProductStory(story)) {
-    return (
-      <>
-        <ProductCmsPage locale={props.params.locale} story={story} />
-        <StoryBreadcrumbs params={props.params} currentPageTitle={story.name} />
-      </>
-    )
-  }
-  return (
-    <>
-      <PageBlock blok={story.content} />
-      {!hideBreadcrumbs && <StoryBreadcrumbs params={props.params} currentPageTitle={story.name} />}
-    </>
-  )
+  // let { hideBreadcrumbs } = story.content
+  // if ((props.params.slug?.length ?? 0) < 1) {
+  //   hideBreadcrumbs = true
+  // }
+  // if (isProductStory(story)) {
+  //   return (
+  //     <>
+  //       <ProductCmsPage locale={props.params.locale} story={story} />
+  //       <StoryBreadcrumbs params={props.params} currentPageTitle={story.name} />
+  //     </>
+  //   )
+  // }
+  // return (
+  //   <>
+  //     <PageBlock blok={story.content} />
+  //     {!hideBreadcrumbs && <StoryBreadcrumbs params={props.params} currentPageTitle={story.name} />}
+  //   </>
+  // )
 }
 
-export async function generateMetadata({ params }: Props): Promise<Metadata> {
-  const story = await fetchStory(params.locale, params.slug?.join('/'))
-  const pageUrl = [params.locale, ...(params.slug ?? [])].join('/')
+// export async function generateMetadata({ params }: Props): Promise<Metadata> {
+//   const story = await fetchStory(params.locale, params.slug?.join('/'))
+//   const pageUrl = [params.locale, ...(params.slug ?? [])].join('/')
 
-  const alternates = {
-    canonical: story.content.canonicalUrl || pageUrl,
-    languages: {
-      [toIsoLocale(params.locale)]: pageUrl,
-    } as Record<IsoLocale, string>,
-  }
-  for (const alt of story.alternates) {
-    const routingLocale = alt.full_slug.split('/')[0]
-    if (!isRoutingLocale(routingLocale)) continue
-    alternates.languages[toIsoLocale(routingLocale)] = alt.full_slug
-  }
-  const description = story.content.seoMetaDescription
-  const title = story.content.seoTitle
-  const result: Metadata = {
-    alternates,
-    description,
-    openGraph: {
-      type: 'website',
-      url: pageUrl,
-      description,
-      images: story.content.seoMetaOgImage
-        ? {
-            url: getImgSrc(story.content.seoMetaOgImage.filename),
-            alt: story.content.seoMetaOgImage.alt,
-          }
-        : undefined,
-    },
-    // Prevent indexing until we're ready to replace pages/[[...slug]]
-    robots: 'noindex, nofollow',
-    // robots: story.content.robots,
-    title,
-  }
-  return result
-}
+//   const alternates = {
+//     canonical: story.content.canonicalUrl || pageUrl,
+//     languages: {
+//       [toIsoLocale(params.locale)]: pageUrl,
+//     } as Record<IsoLocale, string>,
+//   }
+//   for (const alt of story.alternates) {
+//     const routingLocale = alt.full_slug.split('/')[0]
+//     if (!isRoutingLocale(routingLocale)) continue
+//     alternates.languages[toIsoLocale(routingLocale)] = alt.full_slug
+//   }
+//   const description = story.content.seoMetaDescription
+//   const title = story.content.seoTitle
+//   const result: Metadata = {
+//     alternates,
+//     description,
+//     openGraph: {
+//       type: 'website',
+//       url: pageUrl,
+//       description,
+//       images: story.content.seoMetaOgImage
+//         ? {
+//             url: getImgSrc(story.content.seoMetaOgImage.filename),
+//             alt: story.content.seoMetaOgImage.alt,
+//           }
+//         : undefined,
+//     },
+//     // Prevent indexing until we're ready to replace pages/[[...slug]]
+//     robots: 'noindex, nofollow',
+//     // robots: story.content.robots,
+//     title,
+//   }
+//   return result
+// }
 
-export async function generateStaticParams({
-  params,
-}: {
-  params: Pick<CmsPageRoutingParams, 'locale'>
-}): Promise<Array<{ slug: Array<string> }>> {
-  // TODO: Remove when main CMS route is removed from pages router
-  if (process.env.APP_ROUTER_GENERATE_CMS_PAGES !== 'true') {
-    return []
-  }
+// export async function generateStaticParams({
+//   params,
+// }: {
+//   params: Pick<CmsPageRoutingParams, 'locale'>
+// }): Promise<Array<{ slug: Array<string> }>> {
+//   // TODO: Remove when main CMS route is removed from pages router
+//   if (process.env.APP_ROUTER_GENERATE_CMS_PAGES !== 'true') {
+//     return []
+//   }
 
-  const pageLinks = await getCmsPageLinks(`${params.locale}/`)
-  const mostVisitedLinks = pageLinks.filter((item) =>
-    MOST_VISITED_PATHS.has(`/${removeTrailingSlash(item.link.slug)}`),
-  )
-  const result = mostVisitedLinks.map((link) => ({
-    slug: link.slugParts,
-  }))
-  return result
-}
+//   const pageLinks = await getCmsPageLinks(`${params.locale}/`)
+//   const mostVisitedLinks = pageLinks.filter((item) =>
+//     MOST_VISITED_PATHS.has(`/${removeTrailingSlash(item.link.slug)}`),
+//   )
+//   const result = mostVisitedLinks.map((link) => ({
+//     slug: link.slugParts,
+//   }))
+//   return result
+// }
 
 // Only some routes are statically built, other are resolved dynamically and then cached
 // See https://nextjs.org/docs/app/api-reference/file-conventions/route-segment-config#dynamicparams
-export const dynamicParams = true
-
-// DEBUG: Set SWR for 60 seconds
-// export const dynamic = 'force-static'
-export const revalidate = 40
+// export const dynamicParams = true
 
 // Cache speeds up development mode by deduplicating requests between metadata and main renderer
 const fetchStory = cache(async (locale: RoutingLocale, slug = '') => {
