3cd1ddf829c2e9f83f953a262b655fa04527d6eb
Alexander Lebedev <alexander.lebedev@gmail.com>
2024-05-13 16:57:45 +0200

    Convert TopMenuMobile styles to vanilla-extract

---
+++ apps/store/src/components/Header/TopMenuMobile/TopMenuMobile.css.ts
@@ -0,0 +1,33 @@
+import { style } from '@vanilla-extract/css'
+import { theme } from 'ui/src/theme'
+import { MENU_BAR_HEIGHT_MOBILE } from '@/components/Header/Header.constants'
+
+export const contentWrapper = style({
+  position: 'fixed',
+  top: 0,
+  width: '100%',
+  color: theme.colors.textPrimary,
+})
+
+export const buttonWrapper = style({
+  display: 'grid',
+  gridTemplateColumns: '1fr 1fr',
+  columnGap: theme.space.xs,
+  paddingTop: theme.space.lg,
+})
+
+export const topMenuHeader = style({
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'space-between',
+  height: MENU_BAR_HEIGHT_MOBILE,
+  paddingInline: theme.space.md,
+  gap: theme.space.xs,
+})
+
+export const dialogOverlay = style({
+  position: 'fixed',
+  inset: 0,
+  top: 0,
+  backgroundColor: theme.colors.light,
+})
