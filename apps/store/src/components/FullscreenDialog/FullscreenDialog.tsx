d2272c2b6a57b520f08d9adafb1646efaaf8b008
Alexander Lebedev <alexander.lebedev@gmail.com>
2024-05-14 11:21:17 +0200

    Conver FullscreenDialog styles to vanilla-extract (#4258)
    <!--
    PR title: GRW-123 / Feature / Awesome new thing
    -->
    
    ## Describe your changes
    
    Subj.
    
    <!--
    What changes are made?
    If there are many changes, a list might be a good format.
    If it makes sense, add screenshots and/or screen recordings here.
    -->
    
    ## Justify why they are needed
    
    Made this as performance experiment. No measurable difference where I measured, but still nice to have
    
    ## Checklist before requesting a review
    
    - [ ] I have performed a self-review of my code

--- apps/store/src/components/FullscreenDialog/FullscreenDialog.tsx
+++ apps/store/src/components/FullscreenDialog/FullscreenDialog.tsx
@@ -1,7 +1,13 @@
-import styled from '@emotion/styled'
 import { motion, type Transition } from 'framer-motion'
 import { type ReactNode } from 'react'
-import { CrossIcon, Dialog, mq, theme } from 'ui'
+import { CrossIcon, Dialog, theme } from 'ui'
+import {
+  dialogCloseIcon,
+  dialogContent,
+  dialogFooterWrapper,
+  dialogHeader,
+  dialogMain,
+} from './FullscreenDialog.css'
 
 type Props = {
   children: ReactNode
@@ -12,110 +18,32 @@ type Props = {
 
 export const Modal = ({ children, Header, Footer, center = false }: Props) => {
   return (
-    <Content frostedOverlay={true}>
-      {Header === undefined ? (
-        <ModalHeader>
-          <CloseButton>
+    <Dialog.Content className={dialogContent} frostedOverlay={true}>
+      {Header ?? (
+        <header className={dialogHeader}>
+          <Dialog.Close className={dialogCloseIcon}>
             <CrossIcon />
-          </CloseButton>
-        </ModalHeader>
-      ) : (
-        Header
+          </Dialog.Close>
+        </header>
       )}
-      {center ? (
-        <CenteredMain>
-          <AnimateContentWrapper>{children}</AnimateContentWrapper>
-        </CenteredMain>
-      ) : (
-        <Main>
-          <ClearHeader />
-          <AnimateContentWrapper>{children}</AnimateContentWrapper>
-          <ClearFooter />
-        </Main>
-      )}
+      <main className={dialogMain[center ? 'centered' : 'regular']}>
+        <AnimateContentWrapper>{children}</AnimateContentWrapper>
+      </main>
       {Footer && (
-        <FooterWrapper
+        <motion.footer
           initial={{ opacity: 0 }}
           animate={{ opacity: 1 }}
           transition={ANIMATE_TRANSITION}
+          className={dialogFooterWrapper}
         >
           {Footer}
-        </FooterWrapper>
+        </motion.footer>
       )}
-    </Content>
+    </Dialog.Content>
   )
 }
 
-const Content = styled(Dialog.Content)({
-  height: '100%',
-  overflowY: 'auto',
-  isolation: 'isolate',
-})
-
-const HEADER_HEIGHT = '3.5rem'
-const ModalHeader = styled.header({
-  display: 'flex',
-  alignItems: 'center',
-  justifyContent: 'flex-end',
-  paddingInline: theme.space.md,
-  height: HEADER_HEIGHT,
-  position: 'fixed',
-  top: 0,
-  left: 0,
-  right: 0,
-  zIndex: 1,
-
-  [mq.lg]: {
-    paddingInline: theme.space.xl,
-  },
-})
-
-const CloseButton = styled(Dialog.Close)({ cursor: 'pointer' })
-
-const Main = styled.main({
-  display: 'flex',
-  flexDirection: 'column',
-})
-
-const CenteredMain = styled(Main)({
-  height: '100%',
-  display: 'flex',
-  alignItems: 'center',
-  justifyContent: 'center',
-  paddingInline: theme.space.md,
-})
-
-const ClearHeader = styled.div({
-  height: HEADER_HEIGHT,
-})
-
-const BUTTON_HEIGHT = '3.25rem'
-const ClearFooter = styled.div({
-  height: `calc(${BUTTON_HEIGHT} + ${theme.space.md} * 2)`,
-  [mq.lg]: {
-    height: `calc(${BUTTON_HEIGHT} + ${theme.space.xxl} * 2)`,
-  },
-})
-
-const FooterWrapper = styled(motion.footer)({
-  position: 'fixed',
-  bottom: 0,
-  left: 0,
-  right: 0,
-
-  display: 'grid',
-  gridTemplateColumns: 'minmax(0, 24rem)',
-  justifyContent: 'center',
-  gap: theme.space.xxs,
-  paddingInline: theme.space.md,
-  paddingBottom: theme.space.md,
-
-  [mq.lg]: {
-    paddingBottom: theme.space.xxl,
-  },
-})
-
-export const AnimateContentWrapper = ({ children }: { children: ReactNode }) => {
+export function AnimateContentWrapper({ children }: { children: ReactNode }) {
   return (
     <motion.div
       initial={{
