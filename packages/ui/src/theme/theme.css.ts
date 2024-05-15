578f81fd0a2b2b639a75a32f6236e946e8957205
Rafael Youakeem <youakeem@gmail.com>
2024-05-15 14:35:30 +0200

    Create theme contract

--- packages/ui/src/theme/theme.css.ts
+++ packages/ui/src/theme/theme.css.ts
@@ -1,4 +1,7 @@
-import { createTheme } from '@vanilla-extract/css'
+import { createTheme, createThemeContract } from '@vanilla-extract/css'
 import { theme } from './theme'
+import { nullifyObject } from './utils'
 
-export const [mainTheme, tokens] = createTheme(theme)
+export const tokens = createThemeContract(nullifyObject(theme))
+
+export const mainTheme = createTheme(tokens, theme)
