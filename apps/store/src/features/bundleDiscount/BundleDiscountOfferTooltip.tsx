219c9cecb60f34ef4ffd11dc95344d334839bd75
Alexander Lebedev <alexander.lebedev@gmail.com>
2024-05-20 10:10:41 +0200

    Fix bundle size calculation when editing cart item

--- apps/store/src/features/bundleDiscount/BundleDiscountOfferTooltip.tsx
+++ apps/store/src/features/bundleDiscount/BundleDiscountOfferTooltip.tsx
@@ -1,5 +1,6 @@
 import { useTranslation } from 'next-i18next'
 import { DiscountTooltip } from '@/components/ProductPage/PurchaseForm/DiscountTooltip/DiscountTooltip'
+import { useCartEntryToReplace } from '@/components/ProductPage/useCartEntryToReplace'
 import {
   BUNDLE_DISCOUNT_ELIGIBLE_PRODUCT_IDS,
   BUNDLE_DISCOUNT_PERCENTAGE,
@@ -9,13 +10,17 @@ import { useShopSession } from '@/services/shopSession/ShopSessionContext'
 export function BundleDiscountOfferTooltip() {
   const { t } = useTranslation('purchase-form')
   const { shopSession } = useShopSession()
+  const isEditingCartItem = useCartEntryToReplace() != null
   if (shopSession == null) {
     return null
   }
-  const numberOfEligibleCartItems = shopSession.cart.entries.filter((item) =>
+  let numberOfEligibleCartItems = shopSession.cart.entries.filter((item) =>
     BUNDLE_DISCOUNT_ELIGIBLE_PRODUCT_IDS.has(item.product.id),
   ).length
+  if (isEditingCartItem) {
+    numberOfEligibleCartItems -= 1
+  }
   const key =
-    numberOfEligibleCartItems === 0 ? 'BUNDLE_DISCOUNT_FIRST_OFFER' : 'BUNDLE_DISCOUNT_SECOND_OFFER'
+    numberOfEligibleCartItems < 1 ? 'BUNDLE_DISCOUNT_FIRST_OFFER' : 'BUNDLE_DISCOUNT_SECOND_OFFER'
   return <DiscountTooltip subtitle={t(key, { percentage: BUNDLE_DISCOUNT_PERCENTAGE })} />
 }
