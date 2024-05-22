086eec1ed0869643db28cde1b0cecd5c5a2494a1
Gustav Eén <gustaveen@gmail.com>
2024-05-22 11:19:16 +0200

    Fix broken active state on Menu

---
+++ apps/store/src/features/memberArea/components/Menu/Menu.css.ts
@@ -0,0 +1,99 @@
+import { style } from '@vanilla-extract/css'
+import { minWidth, tokens } from 'ui'
+
+export const navigation = style({
+  paddingInline: tokens.space.md,
+  paddingBottom: tokens.space.sm,
+  overflowX: 'auto',
+
+  '@media': {
+    [minWidth.lg]: {
+      paddingInline: tokens.space.xl,
+    },
+  },
+})
+
+export const memberInfo = style({
+  display: 'none',
+  '@media': {
+    [minWidth.lg]: {
+      display: 'block',
+      width: '100%',
+      marginBottom: tokens.space.lg,
+      lineHeight: 1.33,
+    },
+  },
+})
+
+export const navigationList = style({
+  display: 'flex',
+  alignItems: 'baseline',
+  gap: tokens.space.xs,
+  width: 'fit-content',
+
+  '@media': {
+    [minWidth.lg]: {
+      flexDirection: 'column',
+      gap: tokens.space.md,
+      width: '100%',
+    },
+  },
+})
+
+export const navigationItem = style({
+  display: 'flex',
+  '@media': {
+    [minWidth.lg]: {
+      width: '100%',
+    },
+  },
+})
+
+export const navigationLink = style({
+  display: 'flex',
+  alignItems: 'center',
+  height: '2.5rem',
+  padding: tokens.space.sm,
+  borderRadius: tokens.radius.sm,
+  whiteSpace: 'nowrap',
+  fontSize: tokens.fontSizes.md,
+
+  '@media': {
+    [minWidth.lg]: {
+      width: '100%',
+      height: '3rem',
+    },
+    '(hover: hover)': {
+      ':hover': {
+        backgroundColor: tokens.colors.grayTranslucent100,
+      },
+    },
+  },
+
+  selectors: {
+    '&[data-active="true"]': {
+      backgroundColor: tokens.colors.grayTranslucent100,
+    },
+  },
+})
+
+export const navigationButton = style({
+  color: tokens.colors.signalRedElement,
+  cursor: 'pointer',
+})
+
+export const navItemSkeleton = style({
+  width: '165px',
+  height: '2.5rem',
+
+  selectors: {
+    '&:nth-child(even)': { width: '98px' },
+  },
+
+  '@media': {
+    [minWidth.lg]: {
+      width: '100%',
+      height: '3rem',
+    },
+  },
+})
---
+++ apps/store/src/features/memberArea/components/Menu/Menu.tsx
@@ -0,0 +1,113 @@
+import clsx from 'clsx'
+import Link from 'next/link'
+import { useRouter } from 'next/router'
+import { useTranslation } from 'next-i18next'
+import { Text } from 'ui'
+import { Skeleton } from '@/components/Skeleton'
+import { resetAuthTokens } from '@/services/authApi/persist'
+import { useRoutingLocale } from '@/utils/l10n/useRoutingLocale'
+import { PageLink } from '@/utils/PageLink'
+import { useFormatter } from '@/utils/useFormatter'
+import { useMemberAreaInfo } from '../../useMemberAreaInfo'
+import {
+  memberInfo,
+  navigation,
+  navigationButton,
+  navigationItem,
+  navigationLink,
+  navigationList,
+  navItemSkeleton,
+} from './Menu.css'
+
+export const Menu = () => {
+  const router = useRouter()
+  const { firstName, lastName, ssn } = useMemberAreaInfo()
+  const locale = useRoutingLocale()
+  const { t } = useTranslation('memberArea')
+  const formatter = useFormatter()
+
+  const currentRoute = router.pathname
+  const memberName = `${firstName} ${lastName}`
+
+  // Remove locale from the pathname to make the comparison work
+  const normalisePath = (path: string) => path.replace(/^\/?[^/]+\//, '/')
+
+  const internalLinks = {
+    claim: PageLink.memberAreaClaim({ locale }).pathname,
+    insurances: PageLink.memberAreaInsurances({ locale }).pathname,
+    payments: PageLink.memberAreaPayments({ locale }).pathname,
+  }
+
+  return (
+    <nav className={navigation}>
+      <div className={memberInfo}>
+        <Text>{memberName}</Text>
+        {ssn && <Text color="textSecondary">{formatter.ssn(ssn)}</Text>}
+      </div>
+      <ul className={navigationList}>
+        <li className={navigationItem}>
+          <Link
+            className={navigationLink}
+            href={internalLinks.insurances}
+            data-active={normalisePath(currentRoute) === normalisePath(internalLinks.insurances)}
+          >
+            {t('MENU_ITEM_LABEL_INSURANCE')}
+          </Link>
+        </li>
+        <li className={navigationItem}>
+          <Link
+            className={navigationLink}
+            href={internalLinks.payments}
+            data-active={currentRoute.includes(normalisePath(internalLinks.payments))}
+          >
+            {t('MENU_ITEM_LABEL_PAYMENT')}
+          </Link>
+        </li>
+        <li className={navigationItem}>
+          <Link
+            className={navigationLink}
+            href={internalLinks.claim}
+            data-active={currentRoute.includes(normalisePath(internalLinks.claim))}
+          >
+            {t('MENU_ITEM_LABEL_CLAIM')}
+          </Link>
+        </li>
+        <li className={navigationItem}>
+          <Link className={navigationLink} href={PageLink.faq({ locale })}>
+            FAQ
+          </Link>
+        </li>
+        <LogoutButton />
+      </ul>
+    </nav>
+  )
+}
+
+const LogoutButton = () => {
+  // Perhaps we should have /api/logout route in the future. Using native href would be nice
+  const handleLogout = () => {
+    resetAuthTokens()
+    window.location.reload()
+  }
+
+  return (
+    <button className={clsx(navigationLink, navigationButton)} onClick={handleLogout}>
+      Logout
+    </button>
+  )
+}
+
+export const MenuLoadingState = () => {
+  return (
+    <div className={navigation}>
+      <ul className={navigationList}>
+        <Skeleton className={navItemSkeleton} />
+        <Skeleton className={navItemSkeleton} />
+        <Skeleton className={navItemSkeleton} />
+        <Skeleton className={navItemSkeleton} />
+        <Skeleton className={navItemSkeleton} />
+        <Skeleton className={navItemSkeleton} />
+      </ul>
+    </div>
+  )
+}
