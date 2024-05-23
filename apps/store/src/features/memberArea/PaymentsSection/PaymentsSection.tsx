e8abf02ad342ee404ca18957960c04b4cfe2b409
Gustav Eén <gustaveen@gmail.com>
2024-05-23 10:53:02 +0200

    Payment sections improvment

--- apps/store/src/features/memberArea/PaymentsSection/PaymentsSection.tsx
+++ apps/store/src/features/memberArea/PaymentsSection/PaymentsSection.tsx
@@ -1,63 +1,61 @@
-import styled from '@emotion/styled'
 import { useTranslation } from 'next-i18next'
-import { Heading, Text } from 'ui'
-import { ButtonNextLink } from '@/components/ButtonNextLink'
-import { InfoCard } from '@/components/InfoCard/InfoCard'
-import { SpaceFlex } from '@/components/SpaceFlex/SpaceFlex'
+import { Space, Text } from 'ui'
+import { AttentionCard } from '@/components/InfoCard/InfoCard'
 import { useMemberAreaInfo } from '@/features/memberArea/useMemberAreaInfo'
 import { MemberPaymentConnectionStatus } from '@/services/graphql/generated'
-import { CONTENT_WIDTH } from '../InsuranceSection/InsuranceSection.constants'
-import { InsuranceCost } from './InsuranceCost'
+import { InsuranceCost } from './InsuranceCost/InsuranceCost'
 import { PaymentConnection } from './PaymentConnection'
+import { row, wrapper } from './PaymentsSection.css'
 
 export const PaymentsSection = () => {
   const currentMember = useMemberAreaInfo()
   const { t } = useTranslation('memberArea')
-
   const needsPaymentSetup =
     currentMember.paymentInformation.status === MemberPaymentConnectionStatus.NeedsSetup
 
   return (
-    <Wrapper direction="vertical">
-      <InsuranceCost />
+    <div className={wrapper}>
+      <Space y={0.5}>
+        <InsuranceCost />
 
-      <Heading as="h3" variant="standard.24">
-        {t('PAYMENTS_CONNECTION_TITLE')}
-      </Heading>
-      {needsPaymentSetup ? (
-        <>
-          <Text>{t('PAYMENTS_PAYMENT_NOT_CONNECTED')}</Text>
-          <PaymentConnection startButtonText="Connect" />
-        </>
-      ) : (
-        <>
-          <Text>✅&nbsp;{t('PAYMENTS_PAYMENT_CONNECTED')}</Text>
-          <PaymentConnection startButtonText="Change connection" />
-        </>
-      )}
+        {needsPaymentSetup ? (
+          <Space y={0.5}>
+            <AttentionCard>
+              <Text as="p" color="signalAmberText" size="xs">
+                {t('PAYMENTS_NOTIFICATION_MISSING_PAYMENT')}
+              </Text>
+            </AttentionCard>
+            <PaymentConnection startButtonText={t('PAYMENTS_DIRECT_DEBIT_BUTTON')} />
+          </Space>
+        ) : (
+          <div>
+            <div className={row}>
+              <Text size={{ _: 'sm', lg: 'md' }}>{t('PAYMENTS_PAYMENT_METHOD')}</Text>
+              <Text color="textSecondary" size={{ _: 'sm', lg: 'md' }}>
+                {t('PAYMENTS_AUTOGIRO_LABEL')}
+              </Text>
+            </div>
 
-      {/* NOTE that URL is locale-specific */}
-      <ButtonNextLink href={'/se-en/help/faq'} size="small" variant="secondary">
-        Payments FAQ
-      </ButtonNextLink>
-      <GeneralInfo />
-    </Wrapper>
+            <div className={row}>
+              <Text size={{ _: 'sm', lg: 'md' }}>{t('PAYMENTS_ACCOUNT_LABEL')}</Text>
+              <Text color="textSecondary" size={{ _: 'sm', lg: 'md' }}>
+                {currentMember.paymentInformation.connection?.descriptor}
+              </Text>
+            </div>
+
+            <div className={row}>
+              <Text size={{ _: 'sm', lg: 'md' }}>{t('PAYMENTS_BANK_LABEL')}</Text>
+              <Text color="textSecondary" size={{ _: 'sm', lg: 'md' }}>
+                {currentMember.paymentInformation.connection?.displayName}
+              </Text>
+            </div>
+            <PaymentConnection startButtonText={t('PAYMENTS_DIRECT_DEBIT_REPLACE_BUTTON')} />
+          </div>
+        )}
+        <Text align="center" color="textSecondary" size="xs">
+          {t('PAYMENTS_CONNECT_PAYMENT_DESCRIPTION')}
+        </Text>
+      </Space>
+    </div>
   )
 }
-
-// Might be a CMS block in the future
-const GeneralInfo = () => {
-  return (
-    <InfoCard>
-      At Hedvig, you pay at the end of the month for the current month. Your monthly payment is
-      handled via digital direct debit on the 27th of every month (or the closest following bank
-      day). We work with Trustly as our payment partner and you can connect your direct debit on
-      this page below
-    </InfoCard>
-  )
-}
-
-const Wrapper = styled(SpaceFlex)({
-  width: '100%',
-  maxWidth: CONTENT_WIDTH,
-})
