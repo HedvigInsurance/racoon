fed08464ac1310b351675691d178313643520d7a
Rafael Youakeem <youakeem@gmail.com>
2024-05-28 09:26:15 +0200

    lint

--- apps/store/src/app/debugger/components/CreateSessionForm/CreateSessionForm.tsx
+++ apps/store/src/app/debugger/components/CreateSessionForm/CreateSessionForm.tsx
@@ -1,11 +1,11 @@
 'use client'
 
-import { createCustomerSession } from 'app/debugger/actions'
 import { useEffect, useState } from 'react'
 import { useFormState } from 'react-dom'
 import { Space } from 'ui'
 import { SubmitButton } from '@/appComponents/SubmitButton'
 import { PersonalNumberField } from '@/components/PersonalNumberField/PersonalNumberField'
+import { createCustomerSession } from 'app/debugger/actions'
 import { wrapper } from './CreateSessionForm.css'
 
 const HEDVIG_DEBUGGER_SSN = 'hedvig:debugger-ssn'
