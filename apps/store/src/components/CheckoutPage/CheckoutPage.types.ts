871e2b2d17ad5d3aa4655b97d84fac5d9f525d99
Alexander Lebedev <alexander.lebedev@gmail.com>
2024-05-31 07:46:51 +0200

    Fix Edit offer interaction on checkout page (#4324)
    <!--
    PR title: GRW-123 / Feature / Awesome new thing
    -->
    
    ## Describe your changes
    
    Fix crash when trying to use "Edit" button from checkout page. We were missing product metadata in page context, added it
    
    <!--
    What changes are made?
    If there are many changes, a list might be a good format.
    If it makes sense, add screenshots and/or screen recordings here.
    -->
    
    ## Justify why they are needed
    
    Found in app router testing, but happens in current `main` too
    
    ## Checklist before requesting a review
    
    - [ ] I have performed a self-review of my code

--- apps/store/src/components/CheckoutPage/CheckoutPage.types.ts
+++ apps/store/src/components/CheckoutPage/CheckoutPage.types.ts
@@ -1,4 +1,8 @@
 import type { CheckoutStep } from '@/components/CheckoutHeader/Breadcrumbs'
+import type {
+  GLOBAL_PRODUCT_METADATA_PROP_NAME,
+  GlobalProductMetadata,
+} from '@/components/LayoutWithMenu/fetchProductMetadata'
 import type { ShopSession } from '@/services/shopSession/ShopSession.types'
 
 export type CheckoutPageProps = {
@@ -9,4 +13,5 @@ export type CheckoutPageProps = {
   shouldCollectName: boolean
   checkoutSteps: Array<CheckoutStep>
   shopSession: ShopSession
+  [GLOBAL_PRODUCT_METADATA_PROP_NAME]: GlobalProductMetadata
 }
