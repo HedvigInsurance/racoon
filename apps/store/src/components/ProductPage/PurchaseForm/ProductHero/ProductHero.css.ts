8d9d7a7232342c7a86ef9901797683420876fd0c
Guilherme S. Popolin <guilherme@hedvig.com>
2024-06-11 09:29:39 +0200

    fix(ProductHero): text vertical spacing

--- apps/store/src/components/ProductPage/PurchaseForm/ProductHero/ProductHero.css.ts
+++ apps/store/src/components/ProductPage/PurchaseForm/ProductHero/ProductHero.css.ts
@@ -1,18 +1,5 @@
 import { style } from '@vanilla-extract/css'
-import { sprinkles } from 'ui/src/theme/sprinkles.css'
-import { theme } from 'ui'
-
-export const wrapper = style([
-  sprinkles({ display: 'flex', flexDirection: 'column', alignItems: 'center' }),
-  {
-    rowGap: theme.space.md,
-  },
-])
 
 export const pillow = style({
   marginInline: 'auto',
-})
-
-export const textWrapper = style({
-  rowGap: theme.space.sm,
 })
