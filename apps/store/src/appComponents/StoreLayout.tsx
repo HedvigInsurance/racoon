27251e8758b1d69c86560a568c089a097741d7b3
Alexander Lebedev <alexander.lebedev@gmail.com>
2024-06-12 14:42:44 +0200

    Update eslint config to prefer inline type imports

--- apps/store/src/appComponents/StoreLayout.tsx
+++ apps/store/src/appComponents/StoreLayout.tsx
@@ -1,5 +1,4 @@
-import type { ReactNode } from 'react'
-import { Suspense } from 'react'
+import { type ReactNode, Suspense } from 'react'
 import { AppTrackingTriggers } from '@/app/[locale]/AppTrackingTriggers'
 import { StoryblokLayout } from '@/app/[locale]/StoryblokLayout'
 import { ProductMetadataProvider } from '@/appComponents/providers/ProductMetadataProvider'
@@ -13,11 +12,11 @@ import { CompanyReviewsMetadataProvider } from '@/features/memberReviews/Company
 import { fetchCompanyReviewsMetadata } from '@/features/memberReviews/memberReviews'
 import { getApolloClient } from '@/services/apollo/app-router/rscClient'
 import { AppErrorProvider } from '@/services/appErrors/AppErrorContext'
-import type { GlobalStory } from '@/services/storyblok/storyblok'
+import { type GlobalStory } from '@/services/storyblok/storyblok'
 import { getStoryBySlug } from '@/services/storyblok/storyblok.rsc'
 import { TrackingProvider } from '@/services/Tracking/TrackingContext'
 import { Features } from '@/utils/Features'
-import type { RoutingLocale } from '@/utils/l10n/types'
+import { type RoutingLocale } from '@/utils/l10n/types'
 
 type StoreLayoutProps = {
   locale: RoutingLocale
