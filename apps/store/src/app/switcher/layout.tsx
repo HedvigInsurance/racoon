0dc41602929d625b48b71cc8b7a631bc3dbe244d
Youakeem <youakeem@gmail.com>
2024-06-14 17:06:21 +0200

    fix: Switcher missing translations (#4375)

--- apps/store/src/app/switcher/layout.tsx
+++ apps/store/src/app/switcher/layout.tsx
@@ -1,16 +1,28 @@
 import { type ReactNode } from 'react'
+import { TranslationsProvider } from '@/appComponents/providers/TranslationsProvider'
 import { RootLayout } from '@/appComponents/RootLayout/RootLayout'
 import { Header } from '@/components/Header/Header'
+import { FALLBACK_LOCALE } from '@/utils/l10n/locales'
+import { toRoutingLocale } from '@/utils/l10n/localeUtils'
+import { initTranslations } from '../i18n'
 
 type Props = {
   children: ReactNode
 }
 
-const Layout = ({ children }: Props) => {
+const Layout = async ({ children }: Props) => {
+  const locale = toRoutingLocale(FALLBACK_LOCALE)
+
+  const { resources } = await initTranslations(locale, {
+    ns: ['contractSwitchConfirmationForm', 'common'],
+  })
+
   return (
     <RootLayout>
-      <Header />
-      <main>{children}</main>
+      <TranslationsProvider locale={locale} resources={resources}>
+        <Header />
+        <main>{children}</main>
+      </TranslationsProvider>
     </RootLayout>
   )
 }
