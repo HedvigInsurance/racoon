d1806b111ae899187c871ca9bd3a8c27d27143e8
Gustav Eén <gustaveen@gmail.com>
2024-06-24 11:39:01 +0200

    Refactor TextField to vanilla css

--- apps/store/src/components/TextField/TextField.tsx
+++ apps/store/src/components/TextField/TextField.tsx
@@ -1,11 +1,18 @@
 'use client'
 
-import { keyframes } from '@emotion/react'
-import styled from '@emotion/styled'
 import type { ChangeEventHandler, InputHTMLAttributes, MouseEventHandler } from 'react'
 import { useId, useRef, useState } from 'react'
-import { CrossIconSmall, LockIcon, Space, Text, WarningTriangleIcon, theme } from 'ui'
+import { sprinkles } from 'ui/src/theme/sprinkles.css'
+import { CrossIconSmall, LockIcon, Text, WarningTriangleIcon, theme, yStack } from 'ui'
 import { useHighlightAnimation } from '@/utils/useHighlightAnimation'
+import {
+  deleteButton,
+  input,
+  inputLabel,
+  inputWrapper,
+  messageWithIcon,
+  wrapper,
+} from './TextField.css'
 
 type BaseInputProps = Omit<
   InputHTMLAttributes<HTMLInputElement>,
@@ -60,14 +67,10 @@ export const TextField = (props: Props) => {
 
   const inputValue = inputProps.value || value
 
-  const [Wrapper, InputWrapper, Input] =
-    variant === 'large'
-      ? ([LargeWrapper, LargeInputWrapper, LargeInput] as const)
-      : ([SmallWrapper, SmallInputWrapper, SmallInput] as const)
-
   return (
-    <Space y={0.25}>
-      <Wrapper
+    <div className={yStack({ gap: 'xxs' })}>
+      <div
+        className={wrapper[variant]}
         {...animationProps}
         data-active={!!inputValue || !!inputProps.placeholder}
         data-warning={warning}
@@ -75,11 +78,16 @@ export const TextField = (props: Props) => {
         data-hidden={inputProps.hidden ?? undefined}
         onClick={handleClickWrapper}
       >
-        <Label htmlFor={identifier} data-disabled={inputProps.disabled} data-variant={variant}>
+        <label
+          className={inputLabel[variant]}
+          htmlFor={identifier}
+          data-disabled={inputProps.disabled}
+        >
           {label}
-        </Label>
-        <InputWrapper>
-          <Input
+        </label>
+        <div className={inputWrapper[variant]}>
+          <input
+            className={input[variant]}
             {...inputProps}
             id={identifier}
             ref={inputRef}
@@ -98,161 +106,29 @@ export const TextField = (props: Props) => {
             (inputProps.disabled || inputProps.readOnly ? (
               <LockIcon size="1rem" color={theme.colors.textSecondary} />
             ) : (
-              <DeleteButton
+              <button
+                className={deleteButton}
                 type="button"
                 onClick={handleClickDelete}
                 aria-hidden={true}
                 tabIndex={-1}
               >
                 <CrossIconSmall />
-              </DeleteButton>
+              </button>
             ))}
-        </InputWrapper>
-      </Wrapper>
+        </div>
+      </div>
       {message &&
         (warning ? (
-          <MessageWithIcon size="sm">
+          <Text className={messageWithIcon} size="sm">
             <WarningTriangleIcon size="1em" color={theme.colors.amber600} />
             {message}
-          </MessageWithIcon>
+          </Text>
         ) : (
-          <MessageText size="sm">{message}</MessageText>
+          <Text className={sprinkles({ paddingLeft: 'md' })} size="sm">
+            {message}
+          </Text>
         ))}
-    </Space>
+    </div>
   )
 }
-
-const warningAnimation = keyframes({
-  '0%': {
-    backgroundColor: theme.colors.amber100,
-    color: theme.colors.signalAmberText,
-  },
-  '100%': {
-    backgroundColor: theme.colors.gray100,
-    color: theme.colors.textPrimary,
-  },
-})
-
-const warningColorAnimation = keyframes({
-  '0%': {
-    color: theme.colors.signalAmberText,
-  },
-  '100%': {
-    color: theme.colors.textSecondary,
-  },
-})
-
-const BaseWrapper = styled.div({
-  position: 'relative',
-  display: 'flex',
-  flexDirection: 'column',
-  justifyContent: 'center',
-  borderRadius: theme.radius.sm,
-  backgroundColor: theme.colors.translucent1,
-  width: '100%',
-  cursor: 'text',
-
-  '&[data-hidden=true]': {
-    display: 'none',
-  },
-
-  '&[data-warning=true]': {
-    animation: `${warningAnimation} 1.5s cubic-bezier(0.2, -2, 0.8, 2) 1`,
-  },
-})
-
-const LargeWrapper = styled(BaseWrapper)({ height: '4.5rem' })
-const SmallWrapper = styled(BaseWrapper)({ height: '3.5rem' })
-
-const Label = styled.label({
-  position: 'absolute',
-  left: 0,
-  right: 0,
-  overflow: 'hidden',
-  whiteSpace: 'nowrap',
-  textOverflow: 'ellipsis',
-  color: theme.colors.textSecondary,
-
-  pointerEvents: 'none',
-  transformOrigin: 'top left',
-  transition: 'transform 200ms cubic-bezier(0, 0, 0.2, 1) 0ms',
-  transform: 'translate(0, 0) scale(1)',
-  paddingInline: theme.space.md,
-
-  fontSize: theme.fontSizes.xl,
-  '&[data-variant=small]': { fontSize: theme.fontSizes.lg },
-
-  [`${LargeWrapper}:focus-within > &, ${LargeWrapper}[data-active=true] > &`]: {
-    overflow: 'visible',
-    transform: `translate(calc(${theme.space.md} * 0.4), -0.6rem) scale(0.6)`,
-  },
-
-  [`${SmallWrapper}:focus-within > &, ${SmallWrapper}[data-active=true] > &`]: {
-    transform: `translate(calc(${theme.space.md} * 0.2), -0.6rem) scale(0.8)`,
-  },
-
-  [`${LargeWrapper}[data-warning=true] > &, ${SmallWrapper}[data-warning=true] > &`]: {
-    animation: `${warningColorAnimation} 1.5s cubic-bezier(0.2, -2, 0.8, 2) 1`,
-  },
-
-  '&&[data-disabled=true]': {
-    color: theme.colors.textTertiary,
-  },
-})
-
-const LargeInputWrapper = styled.div({
-  position: 'absolute',
-  bottom: '0.625rem',
-  width: '100%',
-
-  display: 'flex',
-  alignItems: 'center',
-  gap: theme.space.xs,
-  paddingRight: theme.space.md,
-})
-
-const SmallInputWrapper = styled(LargeInputWrapper)({ bottom: '0.3125rem' })
-
-const LargeInput = styled.input({
-  width: '100%',
-  fontSize: theme.fontSizes.xl,
-  paddingLeft: theme.space.md,
-
-  ':disabled, :read-only': {
-    color: theme.colors.textSecondary,
-
-    // Webkit overrides
-    WebkitTextFillColor: theme.colors.textSecondary,
-    opacity: 1,
-  },
-  ':disabled': {
-    cursor: 'not-allowed',
-  },
-})
-
-const SmallInput = styled(LargeInput)({ fontSize: theme.fontSizes.lg })
-
-const MessageText = styled(Text)({
-  paddingLeft: theme.space.md,
-})
-
-const MessageWithIcon = styled(Text)({
-  paddingLeft: theme.space.xxs,
-  display: 'flex',
-  alignItems: 'center',
-  gap: theme.space.xs,
-})
-
-const DeleteButton = styled.button({
-  cursor: 'pointer',
-  opacity: 0,
-
-  ':focus-visible': {
-    boxShadow: theme.shadow.focus,
-  },
-
-  [`${LargeWrapper}:focus-within &, ${SmallWrapper}:focus-within &, ${LargeWrapper}[data-readonly] &, ${SmallWrapper}[data-readonly] &`]:
-    {
-      opacity: 1,
-    },
-})
