02ad751d4513384e94ce42874bc310d422db3eff
Alexander Lebedev <alexander.lebedev@gmail.com>
2024-07-03 13:40:25 +0200

    Small render optimizations to purchase form

--- apps/store/src/components/ProductPage/PurchaseForm/PriceCalculatorDialog.tsx
+++ apps/store/src/components/ProductPage/PurchaseForm/PriceCalculatorDialog.tsx
@@ -1,17 +1,20 @@
 import { type ReactNode } from 'react'
-import { CrossIcon, Dialog } from 'ui'
+import { CrossIcon, Dialog, Heading, yStack } from 'ui'
 import { AnimateContentWrapper } from '@/components/FullscreenDialog/FullscreenDialog'
+import { Pillow } from '@/components/Pillow/Pillow'
+import { useProductData } from '@/components/ProductData/ProductDataProvider'
 import { sendDialogEvent } from '@/utils/dialogEvent'
 import { dialogContent, dialogTitle, iconButton } from './PriceCalculatorDialog.css'
 
 type Props = {
-  header?: ReactNode
   children: ReactNode
   isOpen: boolean
   toggleDialog: (open: boolean) => void
 }
 
-export const PriceCalculatorDialog = ({ children, header, isOpen, toggleDialog }: Props) => {
+export const PriceCalculatorDialog = ({ children, isOpen, toggleDialog }: Props) => {
+  const productData = useProductData()
+
   const handleOpenChange = (open: boolean) => {
     sendDialogEvent(open ? 'open' : 'close')
     toggleDialog(open)
@@ -27,7 +30,14 @@ export const PriceCalculatorDialog = ({ children, header, isOpen, toggleDialog }
           </button>
         </Dialog.Close>
         <AnimateContentWrapper>
-          <Dialog.Title className={dialogTitle}>{header}</Dialog.Title>
+          <Dialog.Title className={dialogTitle}>
+            <div className={yStack({ gap: 'xs', alignItems: 'center' })}>
+              <Pillow size="large" {...productData.pillowImage} />
+              <Heading as="h2" variant="standard.18">
+                {productData.displayNameShort}
+              </Heading>
+            </div>
+          </Dialog.Title>
           {children}
         </AnimateContentWrapper>
       </Dialog.Content>
