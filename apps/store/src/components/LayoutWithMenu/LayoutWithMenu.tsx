6a10f1edd4caec07242e41dc15a25845174d6cf7
Gustav Eén <gustaveen@gmail.com>
2024-07-04 15:16:40 +0200

    Add new HeaderBlock to layout for pages router

--- apps/store/src/components/LayoutWithMenu/LayoutWithMenu.tsx
+++ apps/store/src/components/LayoutWithMenu/LayoutWithMenu.tsx
@@ -2,9 +2,11 @@ import { clsx } from 'clsx'
 import { type ReactElement } from 'react'
 import { FooterBlock } from '@/blocks/FooterBlock/FooterBlock'
 import { HeaderBlock } from '@/blocks/HeaderBlock/HeaderBlock'
+import { HeaderBlock as HeaderBlockNew } from '@/blocks/HeaderBlockNew/HeaderBlock'
 import { wrapper } from '@/components/LayoutWithMenu/LayoutWithMenu.css'
 import type { GlobalStory } from '@/services/storyblok/storyblok'
 import { filterByBlockType } from '@/services/storyblok/Storyblok.helpers'
+import { Features } from '@/utils/Features'
 import type { GlobalProductMetadata } from './fetchProductMetadata'
 import { GLOBAL_PRODUCT_METADATA_PROP_NAME } from './fetchProductMetadata'
 import { useHydrateProductMetadata } from './productMetadataHooks'
@@ -34,9 +36,13 @@ export const LayoutWithMenu = (props: LayoutWithMenuProps) => {
     <>
       <div className={clsx(wrapper, className)}>
         {!props.hideMenu &&
-          headerBlock.map((nestedBlock) => (
-            <HeaderBlock key={nestedBlock._uid} blok={nestedBlock} />
-          ))}
+          headerBlock.map((nestedBlock) =>
+            Features.enabled('NEW_HEADER') ? (
+              <HeaderBlockNew key={nestedBlock._uid} blok={nestedBlock} />
+            ) : (
+              <HeaderBlock key={nestedBlock._uid} blok={nestedBlock} />
+            ),
+          )}
         {props.children}
         {!props.hideFooter &&
           footerBlock.map((nestedBlock) => (
