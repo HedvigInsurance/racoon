ad5ed437946ec2ee87f367e2bdd7a20c494cb49b
Alexander Lebedev <alexander.lebedev@gmail.com>
2024-07-09 14:20:47 +0200

    Fill form step for new price calculator (#4461)
    <!--
    PR title: GRW-123 / Feature / Awesome new thing
    -->
    
    ## Describe your changes
    
    Happy path for new price calculator: filling the form
    - Temporary layout of the page with sticky left panel
    - Create new SE_PET_DOG purchase form with 2 sections - SSN and everything else
    - Use `PriceLoader` to indicate wait period while price intent is being confirmed
    - Show JSON dump of offers if any
    - Start with offers step if price intent was already confirmed
    
    TODO: Video
    
    <!--
    What changes are made?
    If there are many changes, a list might be a good format.
    If it makes sense, add screenshots and/or screen recordings here.
    -->
    
    ## Justify why they are needed
    
    ## Checklist before requesting a review
    
    - [ ] I have performed a self-review of my code

---
+++ apps/store/src/components/PriceCalculatorPage/PurchaseFormNew.tsx
@@ -0,0 +1,158 @@
+'use client'
+
+import { datadogLogs } from '@datadog/browser-logs'
+import { datadogRum } from '@datadog/browser-rum'
+import { useStore } from 'jotai'
+import { useTranslation } from 'next-i18next'
+import { startTransition, useCallback, useEffect, useState } from 'react'
+import { PriceCalculator } from '@/components/PriceCalculator/PriceCalculator'
+import { PriceLoader } from '@/components/PriceLoader'
+import {
+  priceIntentAtom,
+  useIsPriceIntentStateReady,
+  usePriceIntent,
+  usePriceIntentId,
+  useSyncPriceIntentState,
+} from '@/components/ProductPage/PurchaseForm/priceIntentAtoms'
+import { useSelectedOffer } from '@/components/ProductPage/PurchaseForm/useSelectedOffer'
+import { Skeleton } from '@/components/Skeleton/Skeleton'
+import { BankSigneringEvent } from '@/services/bankSignering'
+import {
+  ExternalInsuranceCancellationOption,
+  usePriceIntentConfirmMutation,
+} from '@/services/graphql/generated'
+
+type PurchaseFormStep = 'loading' | 'fillForm' | 'viewOffers'
+
+// TODO:
+// - support preloaded priceIntentId
+// - show preview of completed sections with "edit" links
+// - handle errors
+export function PurchaseFormNew() {
+  useSyncPriceIntentState(undefined)
+  const store = useStore()
+  const isReady = useIsPriceIntentStateReady()
+  const [step, setStep] = useState<PurchaseFormStep>('loading')
+  useEffect(() => {
+    setStep(() => {
+      if (!isReady) {
+        return 'loading'
+      }
+      const priceIntent = store.get(priceIntentAtom)
+      if (priceIntent?.offers.length) {
+        return 'viewOffers'
+      }
+      return 'fillForm'
+    })
+  }, [isReady, store])
+
+  const handleConfirmSuccess = () => {
+    setStep('viewOffers')
+  }
+  const handleEdit = () => {
+    startTransition(() => setStep('fillForm'))
+  }
+  if (step === 'loading') {
+    return <Skeleton style={{ height: '75vh' }} />
+  } else if (step === 'fillForm') {
+    return <PriceCalculatorForm onConfirmSuccess={handleConfirmSuccess} />
+  } else {
+    return <PriceIntentOffers onEdit={handleEdit} />
+  }
+}
+
+type PriceCalculatorFormProps = {
+  onConfirmSuccess: () => void
+}
+
+function PriceCalculatorForm({ onConfirmSuccess }: PriceCalculatorFormProps) {
+  const [confirmPriceIntent, isLoadingPrice] = useHandleConfirmPriceIntent({
+    onSuccess() {
+      onConfirmSuccess()
+    },
+    onError(message) {
+      console.log('Failed to confirm price intent, message:', message)
+      // TODO: show error
+    },
+  })
+  if (isLoadingPrice) {
+    return <PriceLoader />
+  }
+  return <PriceCalculator onConfirm={confirmPriceIntent}></PriceCalculator>
+}
+
+type ConfirmPriceIntentOptions = {
+  onSuccess: () => void
+  onError: (message: string) => void
+}
+const useHandleConfirmPriceIntent = ({ onSuccess, onError }: ConfirmPriceIntentOptions) => {
+  const priceIntentId = usePriceIntentId()
+  const { t } = useTranslation('purchase-form')
+  const [isLoadingPrice, setIsLoadingPrice] = useState(false)
+  const [startMutation] = usePriceIntentConfirmMutation({
+    variables: { priceIntentId },
+
+    onError(error) {
+      datadogLogs.logger.warn('Failed to confirm price intent', {
+        error,
+        priceIntentId,
+      })
+      if (error.networkError || error.graphQLErrors.length > 0) {
+        // Unknown error
+        onError(t('GENERAL_ERROR_DIALOG_PROMPT'))
+      } else {
+        // User error
+        onError(error.message)
+      }
+      setIsLoadingPrice(false)
+    },
+    onCompleted(data) {
+      const updatedPriceIntent = data.priceIntentConfirm.priceIntent
+      if (updatedPriceIntent) {
+        const hasBankSigneringOffer = updatedPriceIntent.offers.some(
+          (item) => item.cancellation.option === ExternalInsuranceCancellationOption.Banksignering,
+        )
+        if (hasBankSigneringOffer) {
+          datadogRum.addAction(BankSigneringEvent.Offered)
+        }
+        onSuccess()
+      } else {
+        throw new Error(
+          `UNEXPECTED: price intent not updated without user error (${priceIntentId})`,
+        )
+      }
+    },
+  })
+
+  const confirmPriceIntent = useCallback(
+    (...args: Parameters<typeof startMutation>) => {
+      setIsLoadingPrice(true)
+      startMutation(...args)
+    },
+    [startMutation],
+  )
+
+  return [confirmPriceIntent, isLoadingPrice] as const
+}
+
+type PriceIntentOffersProps = {
+  onEdit: () => void
+}
+function PriceIntentOffers({ onEdit }: PriceIntentOffersProps) {
+  const priceIntent = usePriceIntent()
+  const [selectedOffer] = useSelectedOffer()
+  console.log('selectedOffer', selectedOffer)
+  return (
+    <div>
+      {/*TODO: i18n, style*/}
+      <button onClick={onEdit}>Edit form</button>
+      <hr />
+      <h2>Offers:</h2>
+      <pre>
+        <code style={{ fontFamily: 'monospace' }}>
+          {JSON.stringify(priceIntent.offers, null, 2)}
+        </code>
+      </pre>
+    </div>
+  )
+}
---
+++ apps/store/src/services/PriceCalculator/data/SE_PET_DOG_NEW.ts
@@ -0,0 +1,104 @@
+import {
+  emailField,
+  LAYOUT,
+  livingSpaceField,
+  postalCodeField,
+  ssnSeSection,
+  streetAddressField,
+} from '@/services/PriceCalculator/formFragments'
+import { setI18nNamespace, tKey } from '@/utils/i18n'
+import type { Template } from '../PriceCalculator.types'
+
+setI18nNamespace('purchase-form')
+
+export const SE_PET_DOG_NEW: Template = {
+  name: 'SE_PET_DOG_NEW',
+  sections: [
+    ssnSeSection,
+    {
+      id: 'your-dog',
+      title: { key: tKey('SECTION_TITLE_YOUR_DOG') },
+      submitLabel: { key: tKey('SUBMIT_LABEL_PROCEED') },
+      items: [
+        {
+          field: {
+            type: 'text',
+            name: 'name',
+            label: { key: tKey('FIELD_NAME_PET_LABEL') },
+            required: true,
+          },
+          layout: LAYOUT.FULL_WIDTH,
+        },
+        {
+          field: {
+            type: 'radio',
+            stacking: 'horizontal',
+            name: 'gender',
+            label: { key: tKey('FIELD_GENDER_LABEL') },
+            options: [
+              { label: { key: tKey('FIELD_GENDER_OPTION_MALE_DOG') }, value: 'MALE' },
+              { label: { key: tKey('FIELD_GENDER_OPTION_FEMALE_DOG') }, value: 'FEMALE' },
+            ],
+            required: true,
+          },
+          layout: LAYOUT.FULL_WIDTH,
+        },
+        {
+          field: {
+            type: 'pet-dog-breeds',
+            name: 'breeds',
+            label: { key: tKey('FIELD_BREED_LABEL') },
+            required: true,
+          },
+          layout: LAYOUT.FULL_WIDTH,
+        },
+        {
+          field: {
+            type: 'date',
+            name: 'birthDate',
+            label: { key: tKey('FIELD_BIRTH_DATE_PET_LABEL') },
+            required: true,
+            // Arbitrary date range to be able to show year dropdown
+            // UW filter breeches are handled by the backend
+            min: '1990-01-01',
+            max: 'TODAY',
+          },
+          layout: LAYOUT.FULL_WIDTH,
+        },
+        {
+          field: {
+            type: 'radio',
+            name: 'isNeutered',
+            label: { key: tKey('FIELD_IS_NEUTERED_DOG_LABEL') },
+            options: [
+              { label: { key: tKey('LABEL_YES') }, value: 'true' },
+              { label: { key: tKey('LABEL_NO') }, value: 'false' },
+            ],
+            required: true,
+          },
+          layout: LAYOUT.FULL_WIDTH,
+        },
+        {
+          field: {
+            type: 'radio',
+            name: 'isPreviousDogOwner',
+            label: { key: tKey('FIELD_PREVIOUS_DOG_OWNER_LABEL') },
+            options: [
+              { label: { key: tKey('LABEL_YES') }, value: 'true' },
+              { label: { key: tKey('LABEL_NO') }, value: 'false' },
+            ],
+            required: true,
+          },
+          layout: LAYOUT.FULL_WIDTH,
+        },
+        { field: streetAddressField, layout: LAYOUT.FULL_WIDTH },
+        { field: postalCodeField, layout: LAYOUT.HALF_WIDTH },
+        { field: livingSpaceField, layout: LAYOUT.HALF_WIDTH },
+        { field: emailField, layout: LAYOUT.FULL_WIDTH },
+      ],
+      preview: {
+        fieldName: 'name',
+      },
+    },
+  ],
+}
