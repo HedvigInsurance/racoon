70b6cb38cbe286860a93bd2d1da299ecfdf084e6
Alexander Lebedev <alexander.lebedev@gmail.com>
2024-08-01 16:38:57 +0200

    Apply new deisgn to TierSelectorV2 (#4551)
    <!--
    PR title: GRW-123 / Feature / Awesome new thing
    -->
    
    ## Describe your changes
    
    Apply intended design to deductible selector in Price Calculator page
    - Add radio indicator
    - Use smaller gaps and front size
    
    ![Screenshot 2024-07-31 at 14.41.39.png](https://graphite-user-uploaded-assets-prod.s3.amazonaws.com/GLY3tSM85RFBvAf6lFNA/038ef224-d091-4447-baa2-9523635b195f.png)
    
    <!--
    What changes are made?
    If there are many changes, a list might be a good format.
    If it makes sense, add screenshots and/or screen recordings here.
    -->
    
    ## Justify why they are needed
    
    ## Checklist before requesting a review
    
    - [ ] I have performed a self-review of my code

--- apps/store/src/features/priceCalculator/CardRadioGroup.tsx
+++ apps/store/src/features/priceCalculator/CardRadioGroup.tsx
@@ -1,8 +1,13 @@
 import * as RadioGroup from '@radix-ui/react-radio-group'
-import { type RadioGroupItemProps, type RadioGroupProps } from '@radix-ui/react-radio-group'
+import {
+  type RadioGroupIndicatorProps,
+  type RadioGroupItemProps,
+  type RadioGroupProps,
+} from '@radix-ui/react-radio-group'
 import { clsx } from 'clsx'
 import { yStack } from 'ui'
-import { item } from './CardRadioGroup.css'
+import { RadioIndicatorIcon } from '@/features/priceCalculator/RadioIndicatorIcon'
+import { indicator, indicatorBorderColor, indicatorInnerColor, item } from './CardRadioGroup.css'
 
 export function Root({ children, className, ...forwardedProps }: RadioGroupProps) {
   return (
@@ -17,5 +22,21 @@ export function Item({ children, className, value, ...forwardedProps }: RadioGro
     <RadioGroup.Item value={value} {...forwardedProps} asChild>
       <div className={clsx(item, className)}>{children}</div>
     </RadioGroup.Item>
+  )
+}
+
+export function Indicator({
+  className,
+  forceMount = true,
+  ...forwardedProps
+}: RadioGroupIndicatorProps) {
+  return (
+    <RadioGroup.Indicator
+      className={clsx(className, indicator)}
+      forceMount={forceMount}
+      {...forwardedProps}
+    >
+      <RadioIndicatorIcon borderColor={indicatorBorderColor} innerColor={indicatorInnerColor} />
+    </RadioGroup.Indicator>
   )
 }
