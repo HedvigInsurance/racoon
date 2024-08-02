9aba0d05d60d8d204c820ade8f923163b875317e
Alexander Lebedev <alexander.lebedev@gmail.com>
2024-08-02 15:22:33 +0200

    Extract OfferSummary component inside OfferPresenterV2 and start implementing new design for it (#4552)
    <!--
    PR title: GRW-123 / Feature / Awesome new thing
    -->
    
    ## Describe your changes
    
    First version of offer summary in Price Calculator Page
    - Split tier and deductible selector from offer summary
    - Planned next steps
    
    Limitations
    - "Show details" is a placeholder, there's no details inside
    - Price and discount still use old design
    
    <!--
    What changes are made?
    If there are many changes, a list might be a good format.
    If it makes sense, add screenshots and/or screen recordings here.
    -->
    
    ![Screenshot 2024-08-01 at 16.35.10.png](https://graphite-user-uploaded-assets-prod.s3.amazonaws.com/GLY3tSM85RFBvAf6lFNA/d9078a96-3d97-4ab9-a16e-719ed154b945.png)
    
    ## Justify why they are needed
    
    ## Checklist before requesting a review
    
    - [ ] I have performed a self-review of my code

--- apps/store/src/features/priceCalculator/OfferPresenterV2.tsx
+++ apps/store/src/features/priceCalculator/OfferPresenterV2.tsx
@@ -4,20 +4,22 @@ import { useInView } from 'framer-motion'
 import { useRouter } from 'next/navigation'
 import { useTranslation } from 'next-i18next'
 import { memo, type MouseEventHandler, type ReactNode, useEffect, useMemo, useRef } from 'react'
-import { Button, Text, yStack } from 'ui'
+import { Button, Text, tokens, xStack, yStack } from 'ui'
 import { CancellationForm } from '@/components/Cancellation/CancellationForm'
+import { Pillow } from '@/components/Pillow/Pillow'
+import { useProductData } from '@/components/ProductData/ProductDataProvider'
 import { DiscountTooltip } from '@/components/ProductPage/PurchaseForm/DiscountTooltip/DiscountTooltip'
 import { useDiscountTooltipProps } from '@/components/ProductPage/PurchaseForm/DiscountTooltip/useDiscountTooltipProps'
 import { usePriceIntent } from '@/components/ProductPage/PurchaseForm/priceIntentAtoms'
 import { useSelectedOffer } from '@/components/ProductPage/PurchaseForm/useSelectedOffer'
 import { useTiersAndDeductibles } from '@/components/ProductPage/PurchaseForm/useTiersAndDeductibles'
 import { useCartEntryToReplace } from '@/components/ProductPage/useCartEntryToReplace'
+import { pillow } from '@/components/ProductPillow/ProductPillow.css'
 import { BUNDLE_DISCOUNT_ELIGIBLE_PRODUCT_IDS } from '@/features/bundleDiscount/bundleDiscount'
 import { BundleDiscountOfferTooltip } from '@/features/bundleDiscount/BundleDiscountOfferTooltip'
 import { DeductibleSelectorV2 } from '@/features/priceCalculator/DeductibleSelectorV2'
 import { ProductTierSelectorV2 } from '@/features/priceCalculator/ProductTierSelectorV2'
 import { BankSigneringEvent } from '@/services/bankSignering'
-import type { ProductOfferFragment } from '@/services/graphql/generated'
 import { ExternalInsuranceCancellationOption } from '@/services/graphql/generated'
 import { useShopSession } from '@/services/shopSession/ShopSessionContext'
 import { useTracking } from '@/services/Tracking/useTracking'
@@ -36,9 +38,6 @@ export const OfferPresenterV2 = memo(() => {
   if (selectedOffer == null) {
     throw new Error('selectedOffer must be defined')
   }
-  const { t } = useTranslation('purchase-form')
-  const formatter = useFormatter()
-  const locale = useRoutingLocale()
   const { tiers, deductibles } = useTiersAndDeductibles({
     offers: priceIntent.offers,
     selectedOffer,
@@ -64,49 +63,6 @@ export const OfferPresenterV2 = memo(() => {
     }
   }, [selectedOffer, tracking, isInView])
 
-  const entryToReplace = useCartEntryToReplace()
-  // Awkward solution to varying post-success behavior between addToCart and goToCheckout
-  const onSuccessRef = useRef<(addedProductOffer: ProductOfferFragment) => void>(() => {})
-  const [addToCart, loadingAddToCart] = useAddToCart({
-    shopSessionId: shopSession.id,
-    entryToReplace: entryToReplace?.id,
-    onSuccess(productOfferId) {
-      const addedProductOffer = priceIntent.offers.find((offer) => offer.id === productOfferId)
-
-      if (addedProductOffer === undefined) {
-        throw new Error(`Unknown offer added to cart: ${productOfferId}`)
-      }
-
-      tracking.reportAddToCart(addedProductOffer, 'store')
-
-      const isBankSignering =
-        addedProductOffer.cancellation.option === ExternalInsuranceCancellationOption.Banksignering
-      if (isBankSignering) {
-        datadogRum.addAction(BankSigneringEvent.Available)
-        if (addedProductOffer.cancellation.requested) {
-          datadogRum.addAction(BankSigneringEvent.Requested)
-        }
-      }
-      onSuccessRef.current(addedProductOffer)
-    },
-  })
-
-  const discountTooltipProps = useDiscountTooltipProps(
-    selectedOffer,
-    shopSession.cart.redeemedCampaign ?? undefined,
-  )
-  let discountTooltip: ReactNode = null
-  if (discountTooltipProps != null) {
-    discountTooltip = <DiscountTooltip {...discountTooltipProps} />
-  } else if (
-    shopSession.experiments?.bundleDiscount &&
-    BUNDLE_DISCOUNT_ELIGIBLE_PRODUCT_IDS.has(priceIntent.product.id)
-  ) {
-    discountTooltip = <BundleDiscountOfferTooltip offer={selectedOffer} />
-  }
-
-  const displayPrice = formatter.monthlyPrice(selectedOffer.cost.net)
-
   const selectedTier = useMemo(() => {
     const tier = tiers.find(
       (item) => item.variant.typeOfContract === selectedOffer.variant.typeOfContract,
@@ -123,27 +79,8 @@ export const OfferPresenterV2 = memo(() => {
     return tier
   }, [tiers, selectedOffer])
 
-  const productOfferIds = useMemo(
-    () => priceIntent.offers.map(({ id }) => id),
-    [priceIntent.offers],
-  )
-
-  const router = useRouter()
-  const handleAddToCart: MouseEventHandler = async (event) => {
-    event.preventDefault()
-    await addToCart(selectedOffer.id)
-    router.push(PageLink.cart({ locale }).pathname)
-  }
-
   return (
     <div className={yStack({})}>
-      <div className={yStack({ alignItems: 'center' })}>
-        {discountTooltip}
-        <Text as="p" align="center" size="xl">
-          {displayPrice}
-        </Text>
-      </div>
-
       {tiers.length > 1 && (
         <div className={yStack({ alignItems: 'stretch' })}>
           <ProductTierSelectorV2
@@ -164,6 +101,106 @@ export const OfferPresenterV2 = memo(() => {
         </div>
       )}
 
+      <OfferSummary />
+    </div>
+  )
+})
+OfferPresenterV2.displayName = 'OfferPresenterV2'
+
+function OfferSummary() {
+  const { shopSession } = useShopSession()
+  if (shopSession == null) {
+    throw new Error('shopSession must be defined')
+  }
+  const [selectedOffer] = useSelectedOffer()
+  if (selectedOffer == null) {
+    throw new Error('selectedOffer must be defined')
+  }
+  const { t } = useTranslation('purchase-form')
+  const formatter = useFormatter()
+  const priceIntent = usePriceIntent()
+  const discountTooltipProps = useDiscountTooltipProps(
+    selectedOffer,
+    shopSession.cart.redeemedCampaign ?? undefined,
+  )
+  let discountTooltip: ReactNode = null
+  if (discountTooltipProps != null) {
+    discountTooltip = <DiscountTooltip {...discountTooltipProps} />
+  } else if (
+    shopSession.experiments?.bundleDiscount &&
+    BUNDLE_DISCOUNT_ELIGIBLE_PRODUCT_IDS.has(priceIntent.product.id)
+  ) {
+    discountTooltip = <BundleDiscountOfferTooltip offer={selectedOffer} />
+  }
+
+  const displayPrice = formatter.monthlyPrice(selectedOffer.cost.net)
+
+  const entryToReplace = useCartEntryToReplace()
+  const tracking = useTracking()
+  const [addToCart, loadingAddToCart] = useAddToCart({
+    shopSessionId: shopSession.id,
+    entryToReplace: entryToReplace?.id,
+    onSuccess(productOfferId) {
+      const addedProductOffer = priceIntent.offers.find((offer) => offer.id === productOfferId)
+
+      if (addedProductOffer === undefined) {
+        throw new Error(`Unknown offer added to cart: ${productOfferId}`)
+      }
+
+      tracking.reportAddToCart(addedProductOffer, 'store')
+
+      const isBankSignering =
+        addedProductOffer.cancellation.option === ExternalInsuranceCancellationOption.Banksignering
+      if (isBankSignering) {
+        datadogRum.addAction(BankSigneringEvent.Available)
+        if (addedProductOffer.cancellation.requested) {
+          datadogRum.addAction(BankSigneringEvent.Requested)
+        }
+      }
+    },
+  })
+
+  const router = useRouter()
+  const locale = useRoutingLocale()
+  const handleAddToCart: MouseEventHandler = async (event) => {
+    event.preventDefault()
+    await addToCart(selectedOffer.id)
+    router.push(PageLink.cart({ locale }).pathname)
+  }
+
+  const productData = useProductData()
+  const productOfferIds = useMemo(
+    () => priceIntent.offers.map(({ id }) => id),
+    [priceIntent.offers],
+  )
+
+  return (
+    // TODO: Use Card from ui when it's available in new design
+    <div
+      style={{ border: 'solid 1px lightgray', borderRadius: tokens.radius.xl }}
+      className={yStack({ gap: 'md', padding: 'lg' })}
+    >
+      <div className={xStack({ alignItems: 'center' })}>
+        <Pillow src={productData.pillowImage.src} size="small" className={pillow} />
+        <div className={yStack({ gap: 'none' })}>
+          <Text as="span" size="lg">
+            {productData.displayNameFull}
+          </Text>
+          <Text as="span" size="lg" color="textSecondary">
+            {selectedOffer.exposure.displayNameShort}
+          </Text>
+        </div>
+      </div>
+
+      <Button variant="secondary-alt" onClick={() => window.alert('TODO: Show details')}>
+        {t('SHOW_DETAILS_BUTTON_LABEL', { ns: 'cart' })}
+      </Button>
+
+      {discountTooltip}
+      <Text as="p" align="center" size="xl">
+        {displayPrice}
+      </Text>
+
       <CancellationForm productOfferIds={productOfferIds} offer={selectedOffer} />
 
       <Button
@@ -176,5 +213,4 @@ export const OfferPresenterV2 = memo(() => {
       </Button>
     </div>
   )
-})
-OfferPresenterV2.displayName = 'OfferPresenterV2'
+}
