b37996af760ed602c851bd1460e1c98bba963510
Alexander Lebedev <alexander.lebedev@gmail.com>
2024-08-06 09:20:45 +0200

    Fix collapsible animation in campaign code field (#4564)
    <!--
    PR title: GRW-123 / Feature / Awesome new thing
    -->
    
    ## Describe your changes
    
    Fix animation jump during campaign code field collapse/expand
    
    Before (note jump at the very end of animation)
    
    <div class='graphite__hidden'>
              <div>🎥 Video uploaded on Graphite:</div>
                <a href="https://app.graphite.dev/media/video/GLY3tSM85RFBvAf6lFNA/1b338f5a-e8a6-43ec-94bd-77c8dfa71666.mov">
                  <img src="https://app.graphite.dev/api/v1/graphite/video/thumbnail/GLY3tSM85RFBvAf6lFNA/1b338f5a-e8a6-43ec-94bd-77c8dfa71666.mov">
                </a>
              </div>
    <video src="https://graphite-user-uploaded-assets-prod.s3.amazonaws.com/GLY3tSM85RFBvAf6lFNA/1b338f5a-e8a6-43ec-94bd-77c8dfa71666.mov">Screen Recording 2024-08-05 at 15.28.05.mov</video>
    
    
    After
    
    <div class='graphite__hidden'>
              <div>🎥 Video uploaded on Graphite:</div>
                <a href="https://app.graphite.dev/media/video/GLY3tSM85RFBvAf6lFNA/2de5f0f7-d600-4dc7-883e-470cad0b16c2.mov">
                  <img src="https://app.graphite.dev/api/v1/graphite/video/thumbnail/GLY3tSM85RFBvAf6lFNA/2de5f0f7-d600-4dc7-883e-470cad0b16c2.mov">
                </a>
              </div>
    <video src="https://graphite-user-uploaded-assets-prod.s3.amazonaws.com/GLY3tSM85RFBvAf6lFNA/2de5f0f7-d600-4dc7-883e-470cad0b16c2.mov">Screen Recording 2024-08-05 at 15.27.15.mov</video>
    
    <!--
    What changes are made?
    If there are many changes, a list might be a good format.
    If it makes sense, add screenshots and/or screen recordings here.
    -->
    
    ## Justify why they are needed
    
    ## Checklist before requesting a review
    
    - [x] I have performed a self-review of my code

--- apps/store/src/components/Collapsible/Collapsible.css.ts
+++ apps/store/src/components/Collapsible/Collapsible.css.ts
@@ -29,10 +29,10 @@ export const trigger = style({
 
 export const content = style({
   selectors: {
-    '[data-state=open] &': {
+    '&[data-state=open]': {
       animation: `${slideDown} 400ms cubic-bezier(0.65,0.05,0.36,1)`,
     },
-    '[data-state=closed] &': {
+    '&[data-state=closed]': {
       animation: `${slideUp} 400ms cubic-bezier(0.65,0.05,0.36,1)`,
     },
   },
