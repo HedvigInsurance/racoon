6b9ef90a34fa3c33f9daef3cf23fcbf2e34d8a13
Guilherme S. Popolin <guilherme@hedvig.com>
2024-08-14 14:02:54 +0200

    styles(StoryblokLayout): make sure footer is always at the bottom of the page

--- apps/store/src/app/[locale]/StoryblokLayout.css.ts
+++ apps/store/src/app/[locale]/StoryblokLayout.css.ts
@@ -3,6 +3,10 @@ import { bodyBgColor, bodyTextColor, footerBgColor } from 'ui/src/theme/vars.css
 import { tokens } from 'ui'
 
 export const wrapper = style({
+  display: 'grid',
+  // 'Sticky' footer at the bottom of the page.
+  // Based on 'GlobalStory' type a 'header' and a 'footer' are always expected
+  gridTemplateRows: 'auto 1fr auto',
   minHeight: '100vh',
   isolation: 'isolate',
   selectors: {
