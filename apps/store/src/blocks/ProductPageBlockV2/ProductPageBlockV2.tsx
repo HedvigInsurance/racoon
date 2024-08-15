9ae1368d981d06d1858cfe1f618610291e089da6
Gustav Eén <gustaveen@gmail.com>
2024-08-15 16:27:28 +0200

    Strucutre top section of Product Page V2

--- apps/store/src/blocks/ProductPageBlockV2/ProductPageBlockV2.tsx
+++ apps/store/src/blocks/ProductPageBlockV2/ProductPageBlockV2.tsx
@@ -2,13 +2,17 @@
 import type { SbBlokData } from '@storyblok/react'
 import { storyblokEditable, StoryblokComponent } from '@storyblok/react'
 import { notFound } from 'next/navigation'
+import * as GridLayout from '@/components/GridLayout/GridLayout'
 import { type PurchaseFormProps } from '@/components/ProductPage/PurchaseForm/PurchaseForm'
 import type { SbBaseBlockProps } from '@/services/storyblok/storyblok'
 import { Features } from '@/utils/Features'
+import { wrapper } from './ProductPageBlockV2.css'
 
 export type ProductPageBlockProps = SbBaseBlockProps<
   {
     body: Array<SbBlokData>
+    topSectionFirstColumn: Array<SbBlokData>
+    topSectionSecondColumn: Array<SbBlokData>
   } & Pick<PurchaseFormProps, 'showAverageRating'>
 >
 
@@ -18,7 +22,20 @@ export const ProductPageBlockV2 = ({ blok }: ProductPageBlockProps) => {
   }
 
   return (
-    <main {...storyblokEditable(blok)}>
+    <main className={wrapper} {...storyblokEditable(blok)}>
+      <GridLayout.Root>
+        <GridLayout.Content width={{ lg: '1/2' }} align="left">
+          {blok.topSectionFirstColumn.map((nestedBlock) => (
+            <StoryblokComponent blok={nestedBlock} key={nestedBlock._uid} />
+          ))}
+        </GridLayout.Content>
+        <GridLayout.Content width={{ lg: '1/2' }} align="right">
+          {blok.topSectionSecondColumn.map((nestedBlock) => (
+            <StoryblokComponent blok={nestedBlock} key={nestedBlock._uid} />
+          ))}
+        </GridLayout.Content>
+      </GridLayout.Root>
+
       {blok.body.map((nestedBlock) => (
         <StoryblokComponent blok={nestedBlock} key={nestedBlock._uid} />
       ))}
