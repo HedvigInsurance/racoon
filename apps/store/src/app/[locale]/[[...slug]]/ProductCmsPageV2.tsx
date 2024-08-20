9fa7cc435d53e27797bdb1dfc32959caabc1cabe
Gustav Eén <gustaveen@gmail.com>
2024-08-20 09:53:57 +0200

    Add debug dialog to ProductPageV2

--- apps/store/src/app/[locale]/[[...slug]]/ProductCmsPageV2.tsx
+++ apps/store/src/app/[locale]/[[...slug]]/ProductCmsPageV2.tsx
@@ -1,5 +1,6 @@
 import { StoryblokStory } from '@storyblok/react/rsc'
 import { storyblokBridgeOptions } from '@/appComponents/storyblokBridgeOptions'
+import { DefaultDebugDialog } from '@/components/DebugDialog/DefaultDebugDialog'
 import type { ProductPageStory } from '@/services/storyblok/storyblok'
 import type { RoutingLocale } from '@/utils/l10n/types'
 
@@ -12,6 +13,7 @@ export const ProductCmsPageV2 = ({ story }: ProductCmsPageProps) => {
   return (
     <>
       <StoryblokStory story={story} bridgeOptions={storyblokBridgeOptions} />
+      <DefaultDebugDialog />
     </>
   )
 }
