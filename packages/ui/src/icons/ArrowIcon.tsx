1684661d7ae8c502551c22d50c2d6e54a7ec454d
Alexander Lebedev <alexander.lebedev@gmail.com>
2024-08-21 14:10:45 +0200

    Mark IconRoot as client component as assignInlineVars does not work on <svg> in RSC

--- packages/ui/src/icons/ArrowIcon.tsx
+++ packages/ui/src/icons/ArrowIcon.tsx
@@ -1,4 +1,3 @@
-'use client'
 import type { IconRootProps } from './Root'
 import { IconRoot } from './Root'
 
--- packages/ui/src/icons/Root.tsx
+++ packages/ui/src/icons/Root.tsx
@@ -1,3 +1,7 @@
+'use client'
+// NOTE: 'use client' prevents the following
+// "Warning: Only plain objects can be passed to Client Components from Server Components. Classes or other objects with methods are not supported"
+// We're passing down style object with assignInlineVars
 import { assignInlineVars } from '@vanilla-extract/dynamic'
 import { clsx } from 'clsx'
 import { forwardRef, type ComponentProps } from 'react'
