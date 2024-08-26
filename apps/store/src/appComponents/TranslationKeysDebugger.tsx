229b01179482152074ba6e328a7f1118ca8e14fd
Guilherme S. Popolin <guilherme@hedvig.com>
2024-08-26 09:28:41 +0200

    chore(translation keys debugger): enable it for app router (#4638)

---
+++ apps/store/src/appComponents/TranslationKeysDebugger.tsx
@@ -0,0 +1,9 @@
+'use client'
+
+import { useDebugTranslationKeys } from '@/utils/l10n/useDebugTranslationKeys'
+
+export function TranslationKeysDebugger() {
+  useDebugTranslationKeys()
+
+  return null
+}
