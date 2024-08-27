e144bdd28b035c3afed34904e6487d35641911bb
Guilherme S. Popolin <guilherme@hedvig.com>
2024-08-27 09:58:41 +0200

    fix(checkoutv2): don't index the checkout page

--- apps/store/src/app/[locale]/new/checkout/page.tsx
+++ apps/store/src/app/[locale]/new/checkout/page.tsx
@@ -1,3 +1,4 @@
+import type { Metadata } from 'next'
 import type { RoutingLocale } from '@/utils/l10n/types'
 import { CheckoutPage } from './CheckoutPage'
 
@@ -7,6 +8,13 @@ type Props = { params: Params }
 
 export default function Page({ params }: Props) {
   return <CheckoutPage locale={params.locale} />
+}
+
+export const metadata: Metadata = {
+  robots: {
+    index: false,
+    follow: true,
+  },
 }
 
 export const dynamic = 'force-dynamic'
