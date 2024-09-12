d5f7488f1903678f1d77cf832656e7c38da0c8d5
Rafael Youakeem <youakeem@gmail.com>
2024-09-12 18:52:20 +0200

    Enable custom Pillow SVG fill

--- packages/ui/src/components/BasePillow/BasePillow.tsx
+++ packages/ui/src/components/BasePillow/BasePillow.tsx
@@ -1,7 +1,6 @@
-
 import { Slot } from '@radix-ui/react-slot'
 import clsx from 'clsx'
-import { type PropsWithChildren } from 'react'
+import { type ComponentProps, type PropsWithChildren } from 'react'
 import { type Level } from '../../theme'
 import { pillowVariants } from './BasePillow.css'
 export { pillowSize } from './BasePillow.css'
@@ -16,37 +15,43 @@ type PillowSize =
   | 'xlarge'
   | 'xxlarge'
 
-type BaseProps = {
-  size?: PillowSize | Partial<Record<Level | '_', PillowSize>>
+type BasePillowProps = {
   src?: string
   alt?: string | null
   priority?: boolean
   className?: string
 }
 
-type Props = BaseProps & {
-  shouldFallback?: boolean
+type SharedProps = {
+  size?: PillowSize | Partial<Record<Level | '_', PillowSize>>
 }
 
-export const BasePillow = ({
-  size = 'medium',
-  className,
-  children,
-  shouldFallback,
-  ...props
-}: PropsWithChildren<Props>) => {
-  if (shouldFallback) {
+export type PillowProps = SharedProps & BasePillowProps
+
+type BasePillowComponentProps = { shouldFallback: false } & PropsWithChildren<BasePillowProps>
+type SVGComponentProps = { shouldFallback: true } & ComponentProps<'svg'>
+
+type Props = SharedProps & (BasePillowComponentProps | SVGComponentProps)
+
+export const BasePillow = ({ size = 'medium', className, children, ...props }: Props) => {
+  // Need to check with `prop.shouldFallback` for the type guard to work
+  if (props.shouldFallback === true) {
+    // Omit `shouldFallback` from props as it's not a native attribute
+    // `fill` is only available when  `shouldFallback` is true
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    const { shouldFallback: _, fill = '#F5F5F5', ...svgProps } = props
+
     return (
       <svg
-        {...props}
         className={clsx(pillowVariants(size), className)}
         viewBox="0 0 480 480"
         fill="none"
         xmlns="http://www.w3.org/2000/svg"
+        {...svgProps}
       >
         <path
           d="M3.79613e-05 240C2.56137e-05 428.992 101.382 480 240.298 480C379.214 480 480 432.012 480 240C480 47.9875 396.228 -5.43757e-05 240.298 -4.198e-05C84.3668 -6.01019e-05 5.03089e-05 51.0075 3.79613e-05 240Z"
-          fill="#F5F5F5"
+          fill={fill}
         />
       </svg>
     )
