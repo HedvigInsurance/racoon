1e59452aa153341997e4613bcdaf4cef0bcfd7c9
Rafael Youakeem <youakeem@gmail.com>
2024-09-12 18:54:14 +0200

    Add initial ProductCard version

---
+++ packages/ui/src/components/Card/Card.css.ts
@@ -0,0 +1,33 @@
+import { style } from '@vanilla-extract/css'
+import { recipe } from '@vanilla-extract/recipes'
+import { xStack, yStack } from '../../patterns'
+import { sprinkles, tokens } from '../../theme'
+
+export const cardRoot = recipe({
+  base: [
+    yStack({ gap: 'md' }),
+    {
+      borderRadius: tokens.radius.xl,
+    },
+  ],
+  variants: {
+    variant: {
+      primary: {
+        background: tokens.colors.backgroundStandard,
+        border: '1px solid',
+        borderColor: tokens.colors.borderPrimary,
+        boxShadow: tokens.shadow.card,
+      },
+    },
+    size: {
+      md: sprinkles({ padding: 'lg' }),
+    },
+  },
+
+  defaultVariants: {
+    variant: 'primary',
+    size: 'md',
+  },
+})
+
+export const cardHeader = style([xStack({ gap: 'sm' })])
---
+++ packages/ui/src/components/Card/Card.tsx
@@ -0,0 +1,61 @@
+import { Slot } from '@radix-ui/react-slot'
+import { type RecipeVariants } from '@vanilla-extract/recipes'
+import clsx from 'clsx'
+import { type ComponentProps, type PropsWithChildren } from 'react'
+import { Heading, Text } from 'ui'
+import { cardHeader, cardRoot } from './Card.css'
+
+type RootStyleProps = RecipeVariants<typeof cardRoot>
+type RootProps = ComponentProps<'div'> & RootStyleProps
+
+function CardRoot({ variant, size, className, children }: PropsWithChildren<RootProps>) {
+  return <article className={clsx(cardRoot({ variant, size }), className)}>{children}</article>
+}
+
+// type HeaderStyleProps = RecipeVariants<typeof card>
+type HeaderProps = ComponentProps<'header'>
+
+function CardHeader({ className, children }: PropsWithChildren<HeaderProps>) {
+  return <header className={clsx(cardHeader, className)}>{children}</header>
+}
+
+type MediaProps = ComponentProps<typeof Slot>
+
+function CardMedia({ children }: PropsWithChildren<MediaProps>) {
+  return <Slot>{children}</Slot>
+}
+
+type HeadingProps = ComponentProps<'div'>
+
+function CardHeading({ className, children }: PropsWithChildren<HeadingProps>) {
+  return <div className={className}>{children}</div>
+}
+
+type TitleProps = ComponentProps<typeof Heading>
+
+function CardTitle({ className, children }: PropsWithChildren<TitleProps>) {
+  return (
+    <Heading variant="standard.18" className={className}>
+      {children}
+    </Heading>
+  )
+}
+
+type SubtitleProps = ComponentProps<typeof Text>
+
+function CartSubtitle({ className, children }: PropsWithChildren<SubtitleProps>) {
+  return (
+    <Text color="textSecondary" className={className}>
+      {children}
+    </Text>
+  )
+}
+
+export const Card = {
+  Root: CardRoot,
+  Header: CardHeader,
+  Media: CardMedia,
+  Heading: CardHeading,
+  Title: CardTitle,
+  Subtitle: CartSubtitle,
+}
