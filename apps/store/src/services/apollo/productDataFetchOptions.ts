f983811214f8262da0a55ac50e03b50241b6308c
Alexander Lebedev <alexander.lebedev@gmail.com>
2024-09-13 09:30:59 +0200

    Increase product metadata TTL in production from 10m to 12h

--- apps/store/src/services/apollo/productDataFetchOptions.ts
+++ apps/store/src/services/apollo/productDataFetchOptions.ts
@@ -2,7 +2,9 @@
 // GOTCHA: storefront has its own caching for product metadata configured in storyblok, so you may see delays there
 export const productDataFetchOptions = {
   next: {
-    revalidate: process.env.VERCEL_ENV === 'production' ? 10 * 60 : 60,
+    // We want to see terms-hub changes soon in preview environments
+    // but production requires higher performance, hence long cache TTL
+    revalidate: process.env.VERCEL_ENV === 'production' ? 12 * 60 * 60 : 60,
     // Not used yet, but makes debugging with NEXT_PRIVATE_DEBUG_CACHE=1 easier
     tags: ['productData'],
   },
