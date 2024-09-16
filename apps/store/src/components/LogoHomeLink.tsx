135454f37092ae89ba1543e1973513d312d4b71f
Alexander Lebedev <alexander.lebedev@gmail.com>
2024-09-16 11:40:57 +0200

    Convert LogoHomeLink to vanilla-extract and remove prefetch

--- apps/store/src/components/LogoHomeLink.tsx
+++ apps/store/src/components/LogoHomeLink.tsx
@@ -1,22 +1,26 @@
-import styled from '@emotion/styled'
 import Link from 'next/link'
 import { useTranslation } from 'next-i18next'
 import { HedvigLogo } from 'ui'
 import { useRoutingLocale } from '@/utils/l10n/useRoutingLocale'
 import { PageLink } from '@/utils/PageLink'
+import { link } from './LogoHomeLink.css'
 
 export const LogoHomeLink = () => {
   const { t } = useTranslation('common')
   const locale = useRoutingLocale()
 
+  // Optimization: we don't want prefetch on navigation element, it's always visible
+  // and not used often enough to justify eager preload.
+  // Not prefetching also makes it easier to measure page-specific performance
+  // (no extra resources from preloaded pages)
   return (
-    <LogoLink href={PageLink.home({ locale }).pathname} aria-label={t('HOME_PAGE_LINK_LABEL')}>
+    <Link
+      className={link}
+      href={PageLink.home({ locale }).pathname}
+      aria-label={t('HOME_PAGE_LINK_LABEL')}
+      prefetch={false}
+    >
       <HedvigLogo />
-    </LogoLink>
+    </Link>
   )
 }
-
-export const LogoLink = styled(Link)({
-  display: 'inline-block',
-  '&:active': { opacity: 0.75 },
-})
