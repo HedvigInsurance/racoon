8859dd377644ff37d508b1834655358ee804dec2
Youakeem <youakeem@gmail.com>
2024-09-19 09:51:34 +0200

    tech: Initial Card UI component (#4705)
    <!--
    PR title: tech / Add Initial version of the Card UI component
    -->
    
    ## Describe your changes
    
    ![Screenshot 2024-09-16 at 11.56.48.png](https://graphite-user-uploaded-assets-prod.s3.amazonaws.com/RV4Oucd6mLf2QBBMAjRa/73ed1052-5ee5-4428-89ac-7d77d73384e6.png)
    
    
    <!--
    What changes are made?
    If there are many changes, a list might be a good format.
    If it makes sense, add screenshots and/or screen recordings here.
    -->
    
    ## Justify why they are needed
    Building block for ProductCards and other type of cards
    
    https://www.figma.com/design/5kmmDdh6StpXzbEfr7WevV/Hedvig-UI-Kit?node-id=16269-14623&t=EyGtApoX5yoK0pGI-1
    
    ## Checklist before requesting a review
    
    - [x] I have performed a self-review of my code

---
+++ packages/ui/src/components/Card/Card.stories.tsx
@@ -0,0 +1,50 @@
+import type { Meta, StoryObj } from '@storybook/react'
+import { type ComponentProps } from 'react'
+import { tokens } from '../../theme'
+import { BasePillow } from '../BasePillow/BasePillow'
+import { Card } from './Card'
+
+type Controls = ComponentProps<typeof Card.Root>
+
+const meta: Meta<Controls> = {
+  title: 'Card',
+  component: Card.Root,
+  argTypes: {
+    variant: {
+      options: ['primary', 'secondary'],
+      control: { type: 'select' },
+    },
+  },
+  parameters: {
+    design: {
+      allowFullscreen: true,
+      type: 'figma',
+      url: 'https://www.figma.com/file/5kmmDdh6StpXzbEfr7WevV/Hedvig-UI-Kit?type=design&node-id=16269-14623&mode=design&t=AWGrq69c6GtZiBoY-1',
+    },
+  },
+}
+export default meta
+
+type Story = StoryObj<Controls>
+
+export const Default: Story = {
+  render: (args: Controls) => (
+    <div style={{ maxWidth: '400px' }}>
+      <Card.Root variant={args.variant}>
+        <Card.Header>
+          <Card.Media>
+            <BasePillow fill={tokens.colors.amber300} />
+          </Card.Media>
+          <Card.Heading>
+            <Card.Title>Homeowner Insurance</Card.Title>
+            <Card.Subtitle>Bellmansgatan 19A</Card.Subtitle>
+          </Card.Heading>
+        </Card.Header>
+        A short message about something that needs attention, an error, info or...
+      </Card.Root>
+    </div>
+  ),
+  args: {
+    variant: 'primary',
+  },
+}
---
+++ packages/ui/src/components/Card/Card.tsx
@@ -0,0 +1,71 @@
+import { Slot } from '@radix-ui/react-slot'
+import { type RecipeVariants } from '@vanilla-extract/recipes'
+import clsx from 'clsx'
+import { type ComponentProps, type PropsWithChildren } from 'react'
+import { Heading, Text } from 'ui'
+import { cardHeader, cardRoot } from './Card.css'
+
+type RootStyleProps = RecipeVariants<typeof cardRoot>
+type RootProps = ComponentProps<'article'> & RootStyleProps
+
+function CardRoot({ variant, size, className, children, ...props }: RootProps) {
+  return (
+    <article className={clsx(cardRoot({ variant, size }), className)} {...props}>
+      {children}
+    </article>
+  )
+}
+
+type HeaderProps = ComponentProps<'header'>
+
+function CardHeader({ className, children, ...props }: HeaderProps) {
+  return (
+    <header className={clsx(cardHeader, className)} {...props}>
+      {children}
+    </header>
+  )
+}
+
+function CardMedia({ children }: PropsWithChildren) {
+  // Slot ensures only one element is passed as children
+  return <Slot>{children}</Slot>
+}
+
+type HeadingProps = ComponentProps<'div'>
+
+function CardHeading({ className, children, ...props }: HeadingProps) {
+  return (
+    <div className={className} {...props}>
+      {children}
+    </div>
+  )
+}
+
+type TitleProps = ComponentProps<typeof Heading>
+
+function CardTitle({ className, children, ...props }: TitleProps) {
+  return (
+    <Heading variant="standard.18" className={className} {...props}>
+      {children}
+    </Heading>
+  )
+}
+
+type SubtitleProps = ComponentProps<typeof Text>
+
+function CardSubtitle({ className, children, ...props }: SubtitleProps) {
+  return (
+    <Text color="textSecondary" className={className} {...props}>
+      {children}
+    </Text>
+  )
+}
+
+export const Card = {
+  Root: CardRoot,
+  Header: CardHeader,
+  Media: CardMedia,
+  Heading: CardHeading,
+  Title: CardTitle,
+  Subtitle: CardSubtitle,
+}
