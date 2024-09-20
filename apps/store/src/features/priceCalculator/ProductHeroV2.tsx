4501782e589e9ce7dac1a76646176d3f6a89f580
Gustav Eén <gustaveen@gmail.com>
2024-09-20 12:09:21 +0200

    Add price for current tier in Product Hero

--- apps/store/src/features/priceCalculator/ProductHeroV2.tsx
+++ apps/store/src/features/priceCalculator/ProductHeroV2.tsx
@@ -1,19 +1,28 @@
 'use client'
 import { clsx } from 'clsx'
-import { Heading, yStack } from 'ui'
+import { Heading, sprinkles, Text, yStack } from 'ui'
 import { Pillow } from '@/components/Pillow/Pillow'
 import { useProductData } from '@/components/ProductData/ProductDataProvider'
-import { pillow } from './ProductHeroV2.css'
+import { useSelectedOffer } from '@/components/ProductPage/PurchaseForm/useSelectedOffer'
+import { useFormatter } from '@/utils/useFormatter'
+import { pillow, priceLabel } from './ProductHeroV2.css'
 
 export function ProductHeroV2({ className }: { className?: string }) {
   const productData = useProductData()
+  const [selectedOffer] = useSelectedOffer()
+  const formatter = useFormatter()
   return (
     <div className={clsx(yStack({ alignItems: 'center' }), className)}>
       <Pillow className={pillow} size="medium" {...productData.pillowImage} priority={true} />
 
-      <Heading as="h1" variant="standard.18" align="center">
-        {productData.displayNameFull}
-      </Heading>
+      <div className={sprinkles({ textAlign: 'center' })}>
+        <Heading as="h1" variant="standard.18" align="center">
+          {productData.displayNameFull}
+        </Heading>
+        <Text color="textSecondary" size="md" className={priceLabel}>
+          {selectedOffer && formatter.monthlyPrice(selectedOffer.cost.net)}
+        </Text>
+      </div>
     </div>
   )
 }
