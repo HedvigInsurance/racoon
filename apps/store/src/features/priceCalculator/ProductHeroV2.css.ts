12b75e9b36dad0c507ffd109e99e1e4b02ebae96
Gustav Eén <gustaveen@gmail.com>
2024-09-23 16:45:37 +0200

    Display sub type in ProductHero for mobile and desktop

--- apps/store/src/features/priceCalculator/ProductHeroV2.css.ts
+++ apps/store/src/features/priceCalculator/ProductHeroV2.css.ts
@@ -1,5 +1,10 @@
 import { style } from '@vanilla-extract/css'
+import { badgeFontColor } from 'ui/src/components/Badge/Badge.css'
 import { pillowSize, responsiveStyles, tokens } from 'ui'
+
+export const pillowWrapper = style({
+  position: 'relative',
+})
 
 export const pillow = style({
   vars: {
@@ -13,4 +18,27 @@ export const pillow = style({
 export const priceLabel = style({
   // Make sure there's no layout shift when price appears
   minHeight: tokens.space.lg,
+})
+
+export const subTypeLabel = style({
+  ...responsiveStyles({
+    lg: { display: 'none' },
+  }),
+})
+
+export const subTypeBadge = style({
+  vars: {
+    [badgeFontColor]: tokens.colors.textNegative,
+  },
+  display: 'none',
+  ...responsiveStyles({
+    lg: {
+      display: 'block',
+      position: 'absolute',
+      top: tokens.space.lg,
+      right: `calc(-1 * ${tokens.space.sm})`,
+      backgroundColor: 'transparent',
+      backdropFilter: 'blur(50px)',
+    },
+  }),
 })
--- apps/store/src/features/priceCalculator/ProductHeroV2.tsx
+++ apps/store/src/features/priceCalculator/ProductHeroV2.tsx
@@ -1,23 +1,32 @@
 'use client'
 import { clsx } from 'clsx'
-import { Heading, sprinkles, Text, yStack } from 'ui'
+import { Badge, Heading, sprinkles, Text, yStack } from 'ui'
 import { Pillow } from '@/components/Pillow/Pillow'
 import { useProductData } from '@/components/ProductData/ProductDataProvider'
 import { useSelectedOffer } from '@/components/ProductPage/PurchaseForm/useSelectedOffer'
 import { useFormatter } from '@/utils/useFormatter'
-import { pillow, priceLabel } from './ProductHeroV2.css'
+import { pillow, pillowWrapper, priceLabel, subTypeBadge, subTypeLabel } from './ProductHeroV2.css'
 
 export function ProductHeroV2({ className }: { className?: string }) {
   const productData = useProductData()
   const [selectedOffer] = useSelectedOffer()
+  const subType = selectedOffer?.variant.displayNameSubtype
   const formatter = useFormatter()
   return (
     <div className={clsx(yStack({ alignItems: 'center' }), className)}>
-      <Pillow className={pillow} size="medium" {...productData.pillowImage} priority={true} />
+      <div className={pillowWrapper}>
+        <Pillow className={pillow} size="medium" {...productData.pillowImage} priority={true} />
+        {subType && (
+          <Badge className={subTypeBadge} size="big">
+            {subType}
+          </Badge>
+        )}
+      </div>
 
       <div className={sprinkles({ textAlign: 'center' })}>
         <Heading as="h1" variant="standard.18" align="center">
           {productData.displayNameFull}
+          {subType && <span className={subTypeLabel}>{` ${subType}`}</span>}
         </Heading>
         <Text color="textSecondary" size="md" className={priceLabel}>
           {selectedOffer && formatter.monthlyPrice(selectedOffer.cost.net)}
