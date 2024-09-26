703fc933377dde48553628122397bea6c023394b
Rafael Youakeem <youakeem@gmail.com>
2024-09-26 12:41:20 +0200

    Create CartTotal feature

---
+++ apps/store/src/features/CartTotal/CartTotal.tsx
@@ -0,0 +1,23 @@
+import { TotalPrice } from '@/components/ProductCard/TotalPrice'
+import type { ShopSession } from '@/services/shopSession/ShopSession.types'
+import { useCartTotal } from './hooks/useCartTotal'
+
+type Props = {
+  cart: ShopSession['cart']
+}
+
+export function CartTotal({ cart }: Props) {
+  const { amount, reducedAmount, explanation } = useCartTotal({
+    discount: cart.redeemedCampaign?.discount,
+    cost: cart.cost,
+  })
+
+  return (
+    <TotalPrice
+      currencyCode={amount.currencyCode}
+      amount={amount.amount}
+      reducedAmount={reducedAmount}
+      note={explanation}
+    />
+  )
+}
