66e7401fc8b0d4b9e71c653c8d0d3168bb2dad02
Rafael Youakeem <youakeem@gmail.com>
2024-09-26 20:00:52 +0200

    Separate cross sell section headings from offer cards

--- apps/store/src/app/[locale]/checkout/components/CrossSell/CrossSell.tsx
+++ apps/store/src/app/[locale]/checkout/components/CrossSell/CrossSell.tsx
@@ -2,27 +2,29 @@
 
 import Link from 'next/link'
 import { useTranslation } from 'next-i18next'
-import { Badge, Button, Card, grid, Heading, sprinkles, yStack } from 'ui'
+import { Badge, Button, Card, grid, sprinkles, yStack } from 'ui'
 import { Pillow } from '@/components/Pillow/Pillow'
 import { Price } from '@/components/Price'
 import { DetailsList } from '@/components/ProductCard/DetailsList/DetailsList'
-import { useBonusOffer } from '@/components/QuickAdd/useBonusOffer'
 import { useShopSession } from '@/services/shopSession/ShopSessionContext'
 import { getOfferPrice } from '@/utils/getOfferPrice'
 import { AccidentCrossSellForm } from './components/AccidentCrossSellForm'
+import { type OfferRecommendation } from './hooks/useRecommendations'
 
-export function CrossSell() {
+type Props = {
+  recommendation: OfferRecommendation
+}
+
+export function CrossSell({ recommendation }: Props) {
   const { t } = useTranslation(['cart', 'common'])
-
-  const offerRecommendation = useBonusOffer()
 
   const { shopSession } = useShopSession()
 
-  if (!offerRecommendation || !shopSession) {
+  if (!shopSession) {
     return null
   }
 
-  const { product, offer } = offerRecommendation
+  const { product, offer } = recommendation
 
   // We only support cross-selling of Accident insurance at the moment
   if (product.name !== 'SE_ACCIDENT') {
@@ -31,57 +33,47 @@ export function CrossSell() {
   }
 
   return (
-    <section className={yStack({ gap: 'lg' })}>
-      <header>
-        <Heading as="h2" variant="standard.32">
-          {t('QUICK_ADD_BUNDLE_HEADER')}
-        </Heading>
-        <Heading as="h2" variant="standard.32" color="textSecondary">
-          Save up to 20% for 12 months
-        </Heading>
-      </header>
-      <Card.Root>
-        <Card.Aside>
-          <Badge color="pinkFill1">{t('QUICK_ADD_BADGE_LABEL')}</Badge>
-        </Card.Aside>
-        <Card.Header>
-          <Card.Media>
-            <Pillow size="small" {...product.pillowImage} />
-          </Card.Media>
-          <Card.Heading>
-            <Card.Title>{product.displayNameFull}</Card.Title>
-            <Card.Subtitle>{t('USP_NO_BINDING_TIME')}</Card.Subtitle>
-          </Card.Heading>
-        </Card.Header>
+    <Card.Root>
+      <Card.Aside>
+        <Badge color="pinkFill1">{t('QUICK_ADD_BADGE_LABEL')}</Badge>
+      </Card.Aside>
+      <Card.Header>
+        <Card.Media>
+          <Pillow size="small" {...product.pillowImage} />
+        </Card.Media>
+        <Card.Heading>
+          <Card.Title>{product.displayNameFull}</Card.Title>
+          <Card.Subtitle>{t('USP_NO_BINDING_TIME')}</Card.Subtitle>
+        </Card.Heading>
+      </Card.Header>
 
-        <AccidentCrossSellForm offer={offer}>
-          {({ isCoInsuredUpdated, isPending }) => (
-            <footer className={yStack({ gap: 'md' })}>
-              <DetailsList.Root size="md">
-                <DetailsList.Item className={sprinkles({ color: 'textPrimary' })}>
-                  <DetailsList.Label>{t('CHECKOUT_PRICE_TOTAL')}</DetailsList.Label>
-                  <DetailsList.Value>
-                    <Price
-                      className={sprinkles({ justifyContent: 'flex-end' })}
-                      {...getOfferPrice(offer.cost)}
-                    />
-                  </DetailsList.Value>
-                </DetailsList.Item>
-              </DetailsList.Root>
+      <AccidentCrossSellForm offer={offer}>
+        {({ isCoInsuredUpdated, isPending }) => (
+          <footer className={yStack({ gap: 'md' })}>
+            <DetailsList.Root size="md">
+              <DetailsList.Item className={sprinkles({ color: 'textPrimary' })}>
+                <DetailsList.Label>{t('CHECKOUT_PRICE_TOTAL')}</DetailsList.Label>
+                <DetailsList.Value>
+                  <Price
+                    className={sprinkles({ justifyContent: 'flex-end' })}
+                    {...getOfferPrice(offer.cost)}
+                  />
+                </DetailsList.Value>
+              </DetailsList.Item>
+            </DetailsList.Root>
 
-              <div {...grid({ columns: '2', gap: 'xs' })}>
-                <Button as={Link} href="#" variant="secondary" size="medium">
-                  {t('common:READ_MORE')}
-                </Button>
+            <div {...grid({ columns: '2', gap: 'xs' })}>
+              <Button as={Link} href={product.pageLink} variant="secondary" size="medium">
+                {t('common:READ_MORE')}
+              </Button>
 
-                <Button type="submit" size="medium" disabled={isPending} loading={isPending}>
-                  {isCoInsuredUpdated ? t('QUICK_ADD_UPDATE') : t('QUICK_ADD_BUTTON')}
-                </Button>
-              </div>
-            </footer>
-          )}
-        </AccidentCrossSellForm>
-      </Card.Root>
-    </section>
+              <Button type="submit" size="medium" disabled={isPending} loading={isPending}>
+                {isCoInsuredUpdated ? t('QUICK_ADD_UPDATE') : t('QUICK_ADD_BUTTON')}
+              </Button>
+            </div>
+          </footer>
+        )}
+      </AccidentCrossSellForm>
+    </Card.Root>
   )
 }
