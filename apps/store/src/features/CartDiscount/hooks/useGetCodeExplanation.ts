db273ad58903997a5fc59517bdd9475d5cc9c841
Rafael Youakeem <youakeem@gmail.com>
2024-09-26 16:06:06 +0200

    Create Cart Discount feature

---
+++ apps/store/src/features/CartDiscount/hooks/useGetCodeExplanation.ts
@@ -0,0 +1,39 @@
+import { useTranslation } from 'next-i18next'
+import type { CampaignDiscount } from '@/services/graphql/generated'
+import { CampaignDiscountType } from '@/services/graphql/generated'
+import { useFormatter } from '@/utils/useFormatter'
+
+export const useGetCampaignCodeExplanation = () => {
+  const { t } = useTranslation('cart')
+  const formatter = useFormatter()
+
+  return (discount?: CampaignDiscount) => {
+    if (!discount) {
+      return
+    }
+
+    switch (discount.type) {
+      case CampaignDiscountType.MonthlyCost:
+        return `-${formatter.monthlyPrice(discount.amount)}`
+
+      case CampaignDiscountType.FreeMonths:
+        return t('DISCOUNT_STATE_FREE_MONTHS', { count: discount.months })
+
+      case CampaignDiscountType.MonthlyPercentage:
+        return t('DISCOUNT_STATE_MONTHLY_PERCENTAGE', {
+          percentage: discount.percentage,
+          count: discount.months,
+        })
+
+      case CampaignDiscountType.IndefinitePercentage:
+        return t('DISCOUNT_STATE_INDEFINITE_PERCENTAGE', {
+          percentage: discount.percentage,
+        })
+
+      // Not a case we care about, this discount type is evil and should no longer be used
+      // For now it's used with some partner integrations
+      case CampaignDiscountType.VisibleNoDiscount:
+        return ''
+    }
+  }
+}
