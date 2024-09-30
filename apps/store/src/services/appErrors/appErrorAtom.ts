445bac57d7ce62d9e3578cb15ccbdedab7ee08a3
Alexander Lebedev <alexander.lebedev@gmail.com>
2024-09-30 11:00:07 +0200

    Place appErrorAtom explicitly into layout jotai store. Fixes error dialogs

--- apps/store/src/services/appErrors/appErrorAtom.ts
+++ apps/store/src/services/appErrors/appErrorAtom.ts
@@ -1,8 +1,9 @@
 import { atom, useSetAtom } from 'jotai'
+import { layoutJotaiStore } from '@/app/LayoutJotaiProvider'
 
 export const appErrorAtom = atom<Error | null>(null)
 
 export const useShowAppError = () => {
-  const showAppError = useSetAtom(appErrorAtom)
+  const showAppError = useSetAtom(appErrorAtom, { store: layoutJotaiStore })
   return showAppError
 }
