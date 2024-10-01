6c10c14b0e5b2ba94b99f1d9dfb4a6db6c32729b
Rafael Youakeem <youakeem@gmail.com>
2024-10-01 14:14:22 +0200

    Increase checkout bottom space

--- apps/store/src/app/[locale]/checkout/CheckoutPage.css.ts
+++ apps/store/src/app/[locale]/checkout/CheckoutPage.css.ts
@@ -1,5 +1,5 @@
 import { style } from '@vanilla-extract/css'
-import { yStack } from 'ui'
+import { tokens, yStack } from 'ui'
 
 export const container = style([
   yStack({
@@ -8,10 +8,11 @@ export const container = style([
       sm: 'xxxl',
     },
     paddingInline: 'md',
-    paddingBlock: 'xxl',
+    paddingTop: 'xxl',
   }),
   {
     maxWidth: '500px',
     margin: 'auto',
+    paddingBottom: `calc(${tokens.space[10]} * 1.5)`,
   },
 ])
