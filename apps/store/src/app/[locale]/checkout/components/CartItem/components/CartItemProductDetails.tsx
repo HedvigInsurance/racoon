cd83d78f77aa2e5d7525855c58dadee08bd6d350
Youakeem <youakeem@gmail.com>
2024-10-08 09:58:28 +0200

    chore: [GEN-2609] Extract product details and documents components (#4776)
    <!--
    PR title: GRW-123 / Chore / Awesome new thing
    -->
    
    ## Describe your changes
    Subj
    
    <!--
    What changes are made?
    If there are many changes, a list might be a good format.
    If it makes sense, add screenshots and/or screen recordings here.
    -->
    
    ## Justify why they are needed
    Simplify the CartItem component
    
    ## Checklist before requesting a review
    
    - [x] I have performed a self-review of my code

---
+++ apps/store/src/app/[locale]/checkout/components/CartItem/components/CartItemProductDetails.tsx
@@ -0,0 +1,29 @@
+import { useTranslation } from 'next-i18next'
+import { Text, sprinkles } from 'ui'
+import { DetailsList } from '@/components/ProductCard/DetailsList/DetailsList'
+import { type useOfferDetails } from '../hooks/useOfferDetails'
+
+type Props = {
+  details: ReturnType<typeof useOfferDetails>
+}
+
+export function CartItemProductDetails({ details }: Props) {
+  const { t } = useTranslation('cart')
+
+  return (
+    <div>
+      <Text size={{ _: 'body', sm: 'md' }} className={sprinkles({ mb: 'xxs' })}>
+        {t('VIEW_ENTRY_DETAILS_BUTTON')}
+      </Text>
+
+      <DetailsList.Root size={{ _: 'body', sm: 'md' }} gap={{ _: 'none', sm: 'xxs' }}>
+        {details.map(({ title, value }) => (
+          <DetailsList.Item key={title}>
+            <DetailsList.Label>{title}</DetailsList.Label>
+            <DetailsList.Value>{value}</DetailsList.Value>
+          </DetailsList.Item>
+        ))}
+      </DetailsList.Root>
+    </div>
+  )
+}
