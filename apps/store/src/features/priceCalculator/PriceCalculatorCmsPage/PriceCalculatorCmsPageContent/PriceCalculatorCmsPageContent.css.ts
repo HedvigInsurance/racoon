7348cbbed51d2805fd6c00debf49edf81fa61179
Gustav Eén <gustaveen@gmail.com>
2024-10-17 14:51:27 +0200

    Refactor ProductHeroV2 layout

--- apps/store/src/features/priceCalculator/PriceCalculatorCmsPage/PriceCalculatorCmsPageContent/PriceCalculatorCmsPageContent.css.ts
+++ apps/store/src/features/priceCalculator/PriceCalculatorCmsPage/PriceCalculatorCmsPageContent/PriceCalculatorCmsPageContent.css.ts
@@ -1,5 +1,5 @@
 import { style } from '@vanilla-extract/css'
-import { responsiveStyles, tokens, yStack } from 'ui'
+import { responsiveStyles, sprinkles, tokens } from 'ui'
 import { HEADER_HEIGHT_DESKTOP } from '@/components/Header/Header.css'
 import { HEADER_HEIGHT_MOBILE } from '@/components/Header/HeaderMenuMobile/HeaderMenuMobile.css'
 
@@ -27,16 +27,14 @@ export const pageGrid = style({
 })
 
 export const productHeroSection = style([
-  yStack({ justifyContent: 'center' }),
+  sprinkles({ display: 'flex', flexDirection: 'column', justifyContent: 'center' }),
   {
-    height: '13rem',
-    alignSelf: 'start',
-
     ...responsiveStyles({
       lg: {
         position: 'sticky',
         top: 0,
         gridColumn: '1 / span 7',
+        alignSelf: 'start',
         minHeight: `calc(100vh - ${HEADER_HEIGHT_DESKTOP})`,
       },
     }),
@@ -49,19 +47,6 @@ export const priceCalculatorSection = style({
   ...responsiveStyles({
     lg: {
       gridColumn: '9 / span 4',
-    },
-  }),
-})
-
-export const productHero = style({
-  display: 'flex',
-  gap: tokens.space.xs,
-
-  ...responsiveStyles({
-    lg: {
-      gap: tokens.space.md,
-      // Visually center Product Hero
-      marginTop: `calc(-1 * ${HEADER_HEIGHT_DESKTOP})`,
     },
   }),
 })
