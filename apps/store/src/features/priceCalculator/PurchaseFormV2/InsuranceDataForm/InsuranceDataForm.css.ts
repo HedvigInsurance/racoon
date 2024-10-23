a2d427cfcbb2b75bde85a354de7ca060133a3040
Gustav Eén <gustaveen@gmail.com>
2024-10-23 14:49:00 +0200

    Center form sections visually

--- apps/store/src/features/priceCalculator/PurchaseFormV2/InsuranceDataForm/InsuranceDataForm.css.ts
+++ apps/store/src/features/priceCalculator/PurchaseFormV2/InsuranceDataForm/InsuranceDataForm.css.ts
@@ -2,6 +2,8 @@ import { style } from '@vanilla-extract/css'
 import { responsiveStyles } from 'ui'
 import { HEADER_HEIGHT_DESKTOP } from '@/components/Header/Header.css'
 
+const SECTION_NAVIGATION_OFFSET = '1.75rem'
+
 export const gdprLink = style({
   marginTop: '-1rem', // Offset from default gap of 2rem
 })
@@ -13,6 +15,17 @@ export const ssnSectionWrapper = style({
       justifyContent: 'center',
       // Visually center SSN form section
       marginTop: `calc(-1 * (${HEADER_HEIGHT_DESKTOP} / 2))`,
+    },
+  }),
+})
+
+export const formWrapper = style({
+  ...responsiveStyles({
+    lg: {
+      // Visually center form section with respect to header and section navigation
+      position: 'relative',
+      top: `calc(-1 * (${HEADER_HEIGHT_DESKTOP} / 2) - ${SECTION_NAVIGATION_OFFSET})`,
+      marginBlock: 'auto',
     },
   }),
 })
